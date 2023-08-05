406ebc4a10b87a4bd4f5b79e62c9151ddc8c6618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:09 +0000

    Add package defichain/0.0.3/defichain-0.0.3.tar.gz

---
+++ packages/defichain/defichain-0.0.3.tar.gz/defichain-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import path
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Defichain Python Library'
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="defichain",
+    version=VERSION,
+    author="Intr0c",
+    author_email="introc@volz.link",
+    url="https://github.com/eric-volz/DefichainPython",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["requests"],
+    keywords=['python', 'defichain', 'node', 'ocean'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
