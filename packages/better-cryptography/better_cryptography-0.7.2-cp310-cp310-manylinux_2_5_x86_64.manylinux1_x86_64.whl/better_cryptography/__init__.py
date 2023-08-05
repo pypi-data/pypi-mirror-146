1cdd4746246b72b4258e021178ce1cbec5febc3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:51 +0000

    Add package better-cryptography/0.7.2/better_cryptography-0.7.2-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/better-cryptography/better_cryptography-0.7.2-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.whl/better_cryptography/__init__.py
@@ -0,0 +1,2 @@
+from .ciphers import hash_, random_choice, compare_hashes, token_generate, generate_password, sec_delete, delete, XOR, is_prime_number, get_prime_number, get_GCD, englishToKey, keyToEnglish, Ciphers, Cipher_Constructor, init_diagonostic, one_time_pad_encrypt, one_time_pad_decrypt
+from .log import Log
