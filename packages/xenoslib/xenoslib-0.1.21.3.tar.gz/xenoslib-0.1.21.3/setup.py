2f0cf95e85e031c92fe93b91ee5b9f6b20b551ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:45 +0000

    Add package xenoslib/0.1.21.3/xenoslib-0.1.21.3.tar.gz

---
+++ packages/xenoslib/xenoslib-0.1.21.3.tar.gz/xenoslib-0.1.21.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'xenoslib'))
+import about  # noqa
+# from about import __version__  # noqa
+
+setup(
+    name=about.__title__,
+    version=about.__version__,
+    packages=find_packages(),
+    description=about.__description__,
+    long_description_content_type='text/markdown',
+    author=about.__author__,
+    author_email=about.__author_email__,
+    license=about.__license__,
+    url=about.__url__,
+    install_requires=[
+        'PyYAML>=5.4',
+    ],
+    python_requires='>=3.6',
+    extras_require={
+        ':sys_platform == "win32"': ['pywin32>=225'],
+        ':python_version >= "3.10"': ['requests>=2.19'],
+        ':python_version <= "3.9"': ['requests>=2.0.0'],
+        'colorful:sys_platform == "win32"': ['colorama>=0.4.4'],
+        ':"linux" in sys_platform': [],
+    },
+    tests_require=['pytest>=2.8.0'],
+)
