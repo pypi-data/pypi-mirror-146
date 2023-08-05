9a6724d6c8cdafac22a2d484380b7b2d6017ccbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:20 +0000

    Add package deepwin-alpha/0.0.3/deepwin_alpha-0.0.3.tar.gz

---
+++ packages/deepwin-alpha/deepwin_alpha-0.0.3.tar.gz/deepwin_alpha-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deepwin_alpha",
+    version="0.0.3",
+    author="leon",
+    author_email="leonye0226@gmail.com",
+    description="a small backtest",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/Ye980226/deepwin_alpha",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
