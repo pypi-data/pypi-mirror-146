a8295bbeeb649474ef1f2cb732a00272b09608c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:48 +0000

    Add package wrapg/0.1.2/wrapg-0.1.2-py3-none-any.whl

---
+++ packages/wrapg/wrapg-0.1.2-py3-none-any.whl/wrapg/__init__.py
@@ -0,0 +1,11 @@
+# 'from wrapg import wrapg' will import
+# unneeded objects like pd, os, etc
+# Only import objects to use in code via __init__.py
+from wrapg.wrapg import (
+    query,
+    copy_from_csv,
+    create_table,
+    update,
+    upsert,
+    insert_ignore,
+)
