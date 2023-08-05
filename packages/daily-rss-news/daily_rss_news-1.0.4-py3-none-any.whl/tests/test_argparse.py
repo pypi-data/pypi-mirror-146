bbfe8e0fcd9b5e83459b4c73aa26df73b93eec17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:13 +0000

    Add package daily-rss-news/1.0.4/daily_rss_news-1.0.4-py3-none-any.whl

---
+++ packages/daily-rss-news/daily_rss_news-1.0.4-py3-none-any.whl/tests/test_argparse.py
@@ -0,0 +1,27 @@
+import argparse
+import logging
+
+import pytest
+
+from rss_reader.arg_parser import ArgParser
+
+
+@pytest.fixture
+def set_cli_args_options(monkeypatch):
+    def mock_parse_args(*args, **kwargs):
+        """ Mocks the behaviour cli argument options """
+
+        return argparse.Namespace(source='some source', limit=1, json=True, loglevel=logging.DEBUG)
+
+    monkeypatch.setattr(argparse.ArgumentParser, 'parse_args', mock_parse_args)
+    args = ArgParser()
+    return args.cli_args
+
+
+def test_arguments(set_cli_args_options):
+    """ Test results of mocked arguments  """
+
+    assert set_cli_args_options.source == 'some source'
+    assert set_cli_args_options.limit == 1
+    assert set_cli_args_options.json is True
+    assert set_cli_args_options.loglevel == logging.DEBUG
