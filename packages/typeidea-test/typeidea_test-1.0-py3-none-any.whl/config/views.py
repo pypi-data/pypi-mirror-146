5a9a72e68e53d03618b6caab9c10106d25063229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:38 +0000

    Add package typeidea-test/1.0/typeidea_test-1.0-py3-none-any.whl

---
+++ packages/typeidea-test/typeidea_test-1.0-py3-none-any.whl/config/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+from blog.views import CommonViewMixin
+from config.models import Link
+
+class LinkListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'link_list'
