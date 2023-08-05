464217d9a5cc48d61c6d4652e00ea02dabd4bd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:49 +0000

    Add package khldaemon/0.0.5/khldaemon-0.0.5-py3-none-any.whl

---
+++ packages/khldaemon/khldaemon-0.0.5-py3-none-any.whl/khldaemon/plugin/plugin_manager.py
@@ -0,0 +1,41 @@
+import os
+from typing import List
+
+from khl import Message, MessageTypes
+
+from .plugin_interface import PluginInterface
+from .type.plugin import Plugin
+
+
+class PluginManager:
+
+    plugins: List[Plugin]
+
+    def __init__(self, config, logger) -> None:
+        self.plugins = []
+        self.config = config
+        self.logger = logger
+        self.interface = PluginInterface(self)
+        self.interface.bot.client.register(MessageTypes.TEXT, self.on_message)
+
+    def search_all_plugin(self):
+        self.plugins.clear()
+        self.plugins.append(Plugin('khldaemon.plugin.builtin.khl_plugin'))
+        for DIR in self.config.plugin_directories:
+            file_list = os.listdir(DIR)
+            for file in file_list:
+                if file.endswith('.py'):
+                    self.plugins.append(Plugin(f'{DIR}.{file.replace(".py", "")}'))
+
+    def load_plugins(self):
+        self.search_all_plugin()
+        for plugin in self.plugins:
+            plugin.on_load(self.interface)
+
+    def unload_plugins(self):
+        for plugin in self.plugins:
+            plugin.on_unload(self.interface)
+
+    async def on_message(self, msg: Message):
+        for plugin in self.plugins:
+            await plugin.on_message(msg)
