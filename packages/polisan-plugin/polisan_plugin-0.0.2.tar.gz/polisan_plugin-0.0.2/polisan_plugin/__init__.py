f5386f03ba7dea12eda277ef1cc3ebc856813f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:20 +0000

    Add package polisan-plugin/0.0.2/polisan_plugin-0.0.2.tar.gz

---
+++ packages/polisan-plugin/polisan_plugin-0.0.2.tar.gz/polisan_plugin-0.0.2/polisan_plugin/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: LandaM:
+:license: MIT
+
+:copyright: (C) 2021 Adlemas
+"""
+
+from .main import *
+
+__author__ = "LandaM"
+__version__ = "0.0.2"
+__email__ = "qwer.009771@gmail.com"
