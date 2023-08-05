1e10071787d0dd7540d1bc773ee169a8efb418f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package asterisk-ami/0.2.dev1649433897/asterisk-ami-0.2.dev1649433897.tar.gz

---
+++ packages/asterisk-ami/asterisk-ami-0.2.dev1649433897.tar.gz/asterisk-ami-0.2.dev1649433897/setup.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+__version__ = '0.2.dev1649433897'
+__revision__ = '01a72621e04720ebc2b8c5df6b5ff93132513134'
+
+def read(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
+        return f.read()
+
+setup(
+    name='asterisk-ami',
+    version=__version__,
+    description='Python AMI Client',
+    long_description=read('README.rst'),
+    url='https://github.com/ettoreleandrotognoli/python-ami/',
+    download_url='https://github.com/ettoreleandrotognoli/python-ami/tree/%s/' % __revision__,
+    license='BSD',
+    author=u'Éttore Leandro Tognoli',
+    author_email='ettore.leandro.tognoli@gmail.com',
+    data_files=['requirements.txt'],
+    packages=find_packages(
+        './src/main/python/',
+    ),
+    package_dir={'': 'src/main/python'},
+    include_package_data=True,
+    keywords=['asterisk', 'ami'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+    ],
+    install_requires=[
+
+    ],
+    tests_require=[
+        "rx==1.6",
+        "coverage",
+        "twine",
+        "autopep8",
+    ],
+)
