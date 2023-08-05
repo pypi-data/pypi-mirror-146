934deae06cb61377b0e0ae23c35fbd0344986d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:26 +0000

    Add package spektral/1.1.0/spektral-1.1.0-py3-none-any.whl

---
+++ packages/spektral/spektral-1.1.0-py3-none-any.whl/spektral/layers/pooling/__init__.py
@@ -0,0 +1,14 @@
+from .diff_pool import DiffPool
+from .global_pool import (
+    GlobalAttentionPool,
+    GlobalAttnSumPool,
+    GlobalAvgPool,
+    GlobalMaxPool,
+    GlobalSumPool,
+    SortPool,
+)
+from .la_pool import LaPool
+from .mincut_pool import MinCutPool
+from .sag_pool import SAGPool
+from .src import SRCPool
+from .topk_pool import TopKPool
