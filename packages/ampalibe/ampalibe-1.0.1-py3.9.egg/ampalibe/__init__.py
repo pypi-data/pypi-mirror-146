d2cbac379282a239fac3aeb2be33da4512c54a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:52 +0000

    Add package ampalibe/1.0.1/ampalibe-1.0.1-py3.9.egg

---
+++ packages/ampalibe/ampalibe-1.0.1-py3.9.egg/ampalibe/__init__.py
@@ -0,0 +1,8 @@
+from .requete import Model
+from .messenger import Messenger
+from .core import webserver, Extra as init
+from .utils import action, command, Payload
+
+__version__ = '1.0.1'
+__author__ = 'iTeam-$'
+__contributors__ = ['gaetan193', 'rivo2302', 'rootkit7628', 'SergioDev22']
