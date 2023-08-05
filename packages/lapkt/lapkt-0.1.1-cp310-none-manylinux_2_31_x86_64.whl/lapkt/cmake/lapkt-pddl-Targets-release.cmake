b88b77c6f3d56f3fa02b8eef3aa6e09e74264d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:25 +0000

    Add package lapkt/0.1.1/lapkt-0.1.1-cp310-none-manylinux_2_31_x86_64.whl

---
+++ packages/lapkt/lapkt-0.1.1-cp310-none-manylinux_2_31_x86_64.whl/lapkt/cmake/lapkt-pddl-Targets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pddl" for configuration "Release"
+set_property(TARGET pddl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pddl PROPERTIES
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "Python::Python;core;wrapper"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lapkt/core/lib/pddl.cpython-310-x86_64-linux-gnu.so"
+  IMPORTED_SONAME_RELEASE "pddl.cpython-310-x86_64-linux-gnu.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS pddl )
+list(APPEND _IMPORT_CHECK_FILES_FOR_pddl "${_IMPORT_PREFIX}/lapkt/core/lib/pddl.cpython-310-x86_64-linux-gnu.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
