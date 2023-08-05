b6a8b52caf47fff1dc48de1c0ee7928f9b3d4e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package DE-GeraArquivos/0.0.3/DE_GeraArquivos-0.0.3.tar.gz

---
+++ packages/DE-GeraArquivos/DE_GeraArquivos-0.0.3.tar.gz/DE_GeraArquivos-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DE_GeraArquivos',
+    version='0.0.3',
+    packages=[''],
+    url='https://github.com/almirjgomes/DE_GeraAquivos.git',
+    license='MIT',
+    author='Almir J Gomes',
+    author_email='almir.jg@hotmail.com',
+    description='Gera arquivos baseado em dataframe'
+)
