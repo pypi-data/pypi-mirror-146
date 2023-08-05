1224aa70cd560f6197d9f945d066fd3868ba26a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package Byond-API/0.1.0/Byond_API-0.1.0.tar.gz

---
+++ packages/Byond-API/Byond_API-0.1.0.tar.gz/Byond_API-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Byond_API",
+    version='0.1.0',
+    url="https://github.com/HaCsO/Byond-API",
+    description="Simple way to take data from BYOND",
+    packages=['Byond_API'],
+    author_email="hacso@mail.ru",
+    zip_file=False,
+)
