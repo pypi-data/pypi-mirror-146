19f497bf101884650d3d3baa04d78c38366c10bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:49 +0000

    Add package spelling-bee-game/1/spelling-bee-game-1.tar.gz

---
+++ packages/spelling-bee-game/spelling-bee-game-1.tar.gz/spelling-bee-game-1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '1'
+DESCRIPTION = 'A simple spelling bee game'
+LONG_DESCRIPTION = 'A game that takes a word and gets images related to it. Prompts for input and records your scores. '
+
+# Setting up
+setup(
+    name="spelling-bee-game",
+    version=VERSION,
+    author="ChurningLava",
+    author_email="snoopydankl@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['opencv-python'],
+    keywords=['python', 'spelling-bee','game']
+)
