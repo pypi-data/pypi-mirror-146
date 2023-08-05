18b2d4719271560df6318cfb6435c88932f1cbe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:33 +0000

    Add package package-image-processing/0.0.2/package_image_processing-0.0.2.tar.gz

---
+++ packages/package-image-processing/package_image_processing-0.0.2.tar.gz/package_image_processing-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_image_processing",
+    version="0.0.2",
+    author="Irene Santos",
+    author_email="ireneptc87@gmail.com",
+    description="Criação de pacotes de processamento de imagens",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IreneSantos872/pacoteprocessamentoimagens",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
