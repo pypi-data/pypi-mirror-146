df49e0b20a54f0604bc3281954556b6399d1f5b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:54 +0000

    Add package start-django-project/1.0.9/start_django_project-1.0.9.tar.gz

---
+++ packages/start-django-project/start_django_project-1.0.9.tar.gz/start_django_project-1.0.9/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['start_django_project',
+ 'start_django_project.django-template',
+ 'start_django_project.django-template.app',
+ 'start_django_project.django-template.app.migrations',
+ 'start_django_project.django-template.project']
+
+package_data = \
+{'': ['*'],
+ 'start_django_project.django-template': ['.vscode/*',
+                                          'static/images/*',
+                                          'static/style/*'],
+ 'start_django_project.django-template.app': ['templates/*', 'templates/app/*']}
+
+entry_points = \
+{'console_scripts': ['start-django-project = start_django_project.cli:cli']}
+
+setup_kwargs = {
+    'name': 'start-django-project',
+    'version': '1.0.9',
+    'description': 'Init a new django project with a simple bootstrap layout',
+    'long_description': '# make-django-app\nTo download:\n\n`pip install make-django-project`\n\nTo use:\n\n`start-django-project ./path_of_your_project`\n\nOr to init inside a folder:\n\n`start-django-project ./`',
+    'author': 'TechHeart',
+    'author_email': 'contact@TechHeart.co.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
