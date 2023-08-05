f21c4354c75242f789321d54da117c88abd31f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:49 +0000

    Add package bestFit/0.1/bestFit-0.1.tar.gz

---
+++ packages/bestFit/bestFit-0.1.tar.gz/bestFit-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+  name = "bestFit",
+  version='0.1',
+  licence ="MIT",
+  author = "SebassNoob",
+  author_email="sebastian.ong@hotmail.com",
+  packages=find_packages('src'),
+  install_requires=[
+    'numpy',
+    'matplotlib'
+  ],
+  
+  
+)
