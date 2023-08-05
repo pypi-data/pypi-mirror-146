6e9287527190b27f61c4ce01b10d8ccdf61feb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:54 +0000

    Add package polisan-plugin/0.0.5/polisan_plugin-0.0.5.tar.gz

---
+++ packages/polisan-plugin/polisan_plugin-0.0.5.tar.gz/polisan_plugin-0.0.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from io import open
+from setuptools import setup
+ 
+version="0.0.5"
+
+with open("./README.md", encoding="utf-8") as f:
+      long_description = f.read()
+
+setup(name='polisan_plugin',
+      version=version,
+      license='MIT',
+      author='LandaM',
+      author_email='qwer.009771@gmail.com',
+      description='Use this lib to build your plugins to polisan servers',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=['polisan_plugin'],
+      zip_safe=False)
