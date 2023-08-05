c2a58c35d8f48b4622d3a1b7a390e0bd738b3e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:47 +0000

    Add package ccxtools/0.0.10/ccxtools-0.0.10-py3-none-any.whl

---
+++ packages/ccxtools/ccxtools-0.0.10-py3-none-any.whl/ccxtools/bybit.py
@@ -0,0 +1,53 @@
+from decimal import Decimal
+import ccxt
+from ccxtools.exchange import Exchange
+
+
+class Bybit(Exchange):
+
+    def __init__(self, who, config):
+        self.ccxt_inst = ccxt.bybit({
+            'apiKey': config(f'BYBIT_API_KEY{who}'),
+            'secret': config(f'BYBIT_SECRET_KEY{who}')
+        })
+
+    def get_balance(self, market, ticker):
+        return self.ccxt_inst.fetch_balance()[ticker]['total']
+
+    def get_max_trading_qtys(self):
+        markets = self.ccxt_inst.fetch_markets()
+
+        result = {}
+        for market in markets:
+            if not market['linear']:
+                continue
+
+            ticker = market['base']
+            result[ticker] = Decimal(market['info']['lot_size_filter']['max_trading_qty'])
+
+        return result
+
+    def get_risk_limit(self, market, ticker):
+        if market == 'USDT':
+            return self.ccxt_inst.public_linear_get_risk_limit({
+                'symbol': f'{ticker}USDT'
+            })
+
+    def set_risk_limit(self, market, ticker, side, risk_id):
+        if market == 'USDT':
+            try:
+                return self.ccxt_inst.private_linear_post_position_set_risk({
+                    'symbol': f'{ticker}USDT',
+                    'side': side,
+                    'risk_id': risk_id
+                })
+            except ccxt.errors.ExchangeError as exchange_error:
+                if 'risk id not modified' in str(exchange_error):
+                    return {
+                        'ret_msg': 'OK',
+                        'result': {
+                            'risk_id': risk_id
+                        }
+                    }
+
+                raise Exception(exchange_error)
