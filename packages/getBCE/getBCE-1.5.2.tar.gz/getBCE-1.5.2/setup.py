02361e4ed56efad1f172aec9835bef53ffd885cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:56 +0000

    Add package getBCE/1.5.2/getBCE-1.5.2.tar.gz

---
+++ packages/getBCE/getBCE-1.5.2.tar.gz/getBCE-1.5.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='getBCE',
+    packages=['getBCE'],
+    package_data={"getBCE": ["VERSION"]},
+    version="1.5.2",
+    description='Download data from BCE (Banco Central del ecuador) webpage',
+    author='S. Daniel Jaramillo',
+    author_email='losteven2018@outlook.com',
+    url='https://github.com//Daniejar27/BCE',
+    download_url='https://github.com//Daniejar27/BCE',
+    keywords=['Economics', 'Banco Central', 'BCE', 'economía'],
+    python_requires=">= 3.8",
+    classifiers=[],
+    install_requires=['requests', 'pandas', 'bs4', 'urllib3']
+)
