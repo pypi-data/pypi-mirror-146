aa4c768c85a577671c23f2b9fc7f4f67e6de2d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:48 +0000

    Add package logsight-cli-py/0.0.47/logsight-cli-py-0.0.47.tar.gz

---
+++ packages/logsight-cli-py/logsight-cli-py-0.0.47.tar.gz/logsight-cli-py-0.0.47/tests/test_config.py
@@ -0,0 +1,25 @@
+from click.testing import CliRunner
+
+from logsight_cli.logsight_cli import cli
+
+
+AUTH = ['--email', 'jorge.cardoso.pt@gmail.com', '--password', 'xymneq-jasqA9-faxtyz']
+
+
+def test_config():
+    runner = CliRunner()
+    result = runner.invoke(cli, AUTH + ['config'], obj={})
+    assert result.exit_code == 0
+    assert 'EMAIL' in result.output
+
+
+def test_application():
+    runner = CliRunner()
+    result = runner.invoke(cli, AUTH + ['application', 'ls'], obj={})
+    assert result.exit_code == 0
+    assert 'APPLICATION' in result.output
+
+
+if __name__ == '__main__':
+    test_config()
+    test_application()
