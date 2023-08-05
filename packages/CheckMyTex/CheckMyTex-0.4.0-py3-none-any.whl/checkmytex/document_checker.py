0361975cd2112d07dd76dee101945c89846063be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:31 +0000

    Add package CheckMyTex/0.4.0/CheckMyTex-0.4.0-py3-none-any.whl

---
+++ packages/CheckMyTex/CheckMyTex-0.4.0-py3-none-any.whl/checkmytex/document_checker.py
@@ -0,0 +1,65 @@
+import typing
+
+from .checker import Checker, Languagetool, SiUnitx, ChkTex, CheckSpell, \
+    Proselint
+from checkmytex.latex_document import LatexDocument
+from checkmytex.problem import Problem
+from checkmytex.whitelist import Whitelist
+
+
+class DocumentChecker:
+    """
+    Simple class to return the files and its problems of a latex document.
+    """
+
+    def __init__(self, checker=None):
+        self.checker = []
+        if checker:
+            for c in checker:
+                self.add_checker(c)
+        else:
+            self.add_checker(ChkTex())
+            self.add_checker(CheckSpell())
+            self.add_checker(Languagetool())
+            self.add_checker(SiUnitx())
+            self.add_checker(Proselint())
+
+    def add_checker(self, checker: Checker) -> None:
+        """
+        Add a checker if it is available.
+        :param checker: The checker to be added.
+        :return: None
+        """
+        if checker.is_available():
+            self.checker.append(checker)
+        else:
+            print(str(checker), "is not available.")
+            guide = checker.installation_guide()
+            if guide:
+                print(guide)
+
+    def find_problems(self,
+                      latex_document: LatexDocument,
+                      whitelist: Whitelist = None) \
+            -> typing.Iterable[Problem]:
+        """
+        Finds problems of the document. Returns an iterator of file names and sorted problems.
+        :param latex_document: The latex document to check.
+        :param whitelist: The whitelist of problems to ignore.
+        :return: Iterator over file name and sorted problems within this file.
+        """
+        whitelist = whitelist if whitelist else Whitelist()
+        for c in self.checker:
+            for p in c.check(latex_document):
+                if p not in whitelist:
+                    yield p
+
+    def sort_problems_by_file(self, latex_document, problems) \
+            -> typing.Iterable[typing.Tuple[str, typing.List[Problem]]]:
+        problems_of_files = {f: [] for f in latex_document.files()}
+        for p in problems:
+            problems_of_files[p.origin.file].append(p)
+        for f in latex_document.files():
+            problems = [p for p in problems_of_files[f]]
+            problems.sort(key=lambda p: (p.origin.begin, p.origin.end))
+            yield f, problems
---
+++ packages/CheckMyTex/CheckMyTex-0.4.0-py3-none-any.whl/checkmytex/languagetool.py
@@ -0,0 +1,51 @@
+import json
+import shutil
+import typing
+
+from .abstract_checker import Checker
+from .latex_document import LatexDocument
+from .problem import Problem
+
+
+class Languagetool(Checker):
+    def check(self, document: LatexDocument) -> typing.Iterable[Problem]:
+        result, err, ex = self._run(
+            f"{shutil.which('languagetool')} --json -l en-US "
+            f"--disable "
+            f"MORFOLOGIK_RULE_EN_US,"
+            f"WHITESPACE_RULE,"
+            f"COMMA_PARENTHESIS_WHITESPACE",
+            input=document.get_text())
+        data = json.loads(result)
+        for problem in data["matches"]:
+            try:
+                look_up_url = problem["rule"]["urls"][0]["value"]
+            except KeyError:
+                look_up_url = None
+            origin = document.get_origin_of_text(problem["offset"],
+                                                 problem["offset"] +
+                                                 problem["length"])
+            yield Problem(origin=origin,
+                          context=problem["context"]["text"],
+                          message=problem["message"],
+                          long_id=problem["message"]
+                                  + problem["context"]["text"],
+                          rule=problem["rule"]["id"],
+                          tool="languagetool",
+                          look_up_url=look_up_url)
+
+    def is_available(self) -> bool:
+        return bool(shutil.which("languagetool"))
+
+    def needs_detex(self):
+        return True
+
+    def installation_guide(self) -> str:
+        return "You can probably install install languagetool directly" \
+               " with your package manager.\n" \
+               " e.g. brew install languagetool\n" \
+               "      apt-get install languagetool\n" \
+               "      pacman -S languagetool\n" \
+               "...\n" \
+               "Otherwise, you can install it by hand:" \
+               " https://github.com/languagetool-org/languagetool"
