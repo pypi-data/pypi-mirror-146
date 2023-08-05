a68aacdddea4a55eaf9f6b9f59201590a9c5e81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:47 +0000

    Add package swift-module-copiseded/0.3.9/swift_module_copiseded-0.3.9-py3-none-any.whl

---
+++ packages/swift-module-copiseded/swift_module_copiseded-0.3.9-py3-none-any.whl/swift_apple/python3学习示例.py
@@ -0,0 +1,6 @@
+import os
+def HELLOWORLD():
+    display = 'print(\'hello,world\')\nperson=input(\'what\\\'s your name?\')\nprint(\'hello,\',person)'
+    displayString = '结果：\n>>>print(\'hello,world\')\nhello,world\n>>>person=input(\'what\\\'s your name?\')\nwhat\'s your name?copiseded(你可以输入你自己的名字)\nprint(\'hello,\',person)\nhello,lin'
+    print(display)
+    print(displayString)
