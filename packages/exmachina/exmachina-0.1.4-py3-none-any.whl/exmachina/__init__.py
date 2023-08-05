91585903ed0dd306d258a2b464d3d22ce8b709dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:18 +0000

    Add package exmachina/0.1.4/exmachina-0.1.4-py3-none-any.whl

---
+++ packages/exmachina/exmachina-0.1.4-py3-none-any.whl/exmachina/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.4"
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.getLogger(__name__).propagate = False
+
+from .core.depends_contoroller import get_depends  # noqa
+from .core.machina import Event, Machina  # noqa
+from .core.params_function import Depends  # noqa
---
+++ packages/exmachina/exmachina-0.1.4-py3-none-any.whl/exmachina/core/helper.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import logging
+
+try:
+    from typing import Literal  # type: ignore
+except ImportError:
+    from typing_extensions import Literal
+
+try:
+    from rich.logging import RichHandler
+except ModuleNotFoundError:
+    raise ImportError("pip install exmachina[rich]")
+
+
+def set_verbose(verbose: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] | None):
+    """exmachinaのログを表示する
+
+    Args:
+        verbose (Literal[): 10, 20, 30, 40, 50に相当
+    """
+    if verbose is not None:
+        handler = RichHandler(rich_tracebacks=True, enable_link_path=False, level=verbose)
+        logger = logging.getLogger("exmachina")
+        logger.setLevel(verbose)
+        if "RichHandler" not in [h.__class__.__name__ for h in logger.handlers]:
+            logger.addHandler(handler)
