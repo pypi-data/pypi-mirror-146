0d45c89470c2c5c7dcb3d90b3415536007dcd6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:18 +0000

    Add package xrd-simulator/0.3.13/xrd_simulator-0.3.13.tar.gz

---
+++ packages/xrd-simulator/xrd_simulator-0.3.13.tar.gz/xrd_simulator-0.3.13/xrd_simulator/laue.py
@@ -0,0 +1,119 @@
+#TODO: Review docs.
+"""Collection of functions for solving the Laue equations for package specific parametrization.
+"""
+
+import numpy as np
+
+
+def get_G(U, B, G_hkl):
+    """Compute the diffraction vector G=UBG_HKL
+
+    Args:
+        U (:obj:`numpy array`) Orientation matrix of ``shape=(3,3)`` (unitary).
+        B (:obj:`numpy array`): Reciprocal to grain coordinate mapping matrix of ``shape=(3,3)``.
+        G_hkl (:obj:`numpy array`): Miller indices, i.e the h,k,l integers (``shape=(3,n)``).
+
+
+    Returns:
+        G (:obj:`numpy array`): Sample coordinate system diffraction vector. (``shape=(3,n)``)
+
+    """
+    return np.dot(np.dot(U, B), G_hkl)
+
+
+def get_bragg_angle(G, wavelength):
+    """Compute a Bragg angle given a diffraction (scattering) vector.
+
+    Args:
+        G (:obj:`numpy array`): Sample coordinate system diffraction vector. (``shape=(3,n)``)
+        wavelength (:obj:`float`): Photon wavelength in units of angstrom.
+
+    Returns:
+        Bragg angles (:obj:`float`): in units of radians. (``shape=(n,)``)
+
+    """
+    return np.arcsin(np.linalg.norm(G, axis=0) * wavelength / (4 * np.pi))
+
+
+def get_sin_theta_and_norm_G(G, wavelength):
+    """Compute a Bragg angle given a diffraction (scattering) vector.
+
+    Args:
+        G (:obj:`numpy array`): Sample coordinate system diffraction vector.
+        wavelength (:obj:`float`): Photon wavelength in units of angstrom.
+
+    Returns:
+        sin(Bragg angle) (:obj:`float`): in units of radians and ||G||.
+
+    """
+    normG = np.linalg.norm(G, axis=0)
+    return normG * wavelength / (4 * np.pi), normG
+
+
+def get_tangens_half_angle_equation(k1, theta, G, rhat):
+    """Find coefficient to the equation
+
+    .. math::
+        c_0 \\cos(s \\alpha) + c_1 \\sin(s \\alpha) + c_2 = 0. \\quad\\quad (1)
+
+    """
+    c_0 = np.dot(k1, G)
+    c_1 = np.dot(np.cross(rhat, k1), G)
+    c_2 = np.linalg.norm(k1) * np.linalg.norm(G) * np.sin(theta)
+    return c_0, c_1, c_2
+
+
+def find_solutions_to_tangens_half_angle_equation(c_0, c_1, c_2, alpha):
+    """Find all solutions, :obj:`s`, to the equation (maximum 2 solutions exists)
+
+    .. math::
+        c_0 \\cos(s \\alpha) + c_1 \\sin(s \\alpha) + c_2 = 0. \\quad\\quad (1)
+
+    by rewriting as
+
+    .. math::
+        (c_2 - c_0) t^2 + 2 c_1 t + (c_0 + c_2) = 0. \\quad\\quad (2)
+
+    where
+
+    .. math::
+        t = \\tan(s \\alpha / 2). \\quad\\quad (3)
+
+    and .. math::\\alpha is the angle between k1 and k2
+
+    Args:
+        c_0,c_1,c_2 (:obj:`float`): Coefficients c_0,c_1 and c_2 of equation (1).
+
+    Returns:
+        (:obj:`tuple` of :obj:`float` or :obj:`None`): solutions if existing otherwise returns None.
+
+    """
+
+    if c_0 == c_2:
+        if c_1 == 0:
+            t1 = t2 = None
+        else:
+            t1 = -c_0 / c_1
+            t2 = None
+    else:
+        rootval = (c_1 / (c_2 - c_0))**2 - (c_0 + c_2) / (c_2 - c_0)
+        leadingterm = (-c_1 / (c_2 - c_0))
+        if rootval < 0:
+            t1, t2 = None, None
+        else:
+            t1 = leadingterm + np.sqrt(rootval)
+            t2 = leadingterm - np.sqrt(rootval)
+
+    s1, s2 = None, None
+
+    if t1 is not None:
+        s1 = 2 * np.arctan(t1) / alpha
+        if s1 > 1 or s1 < 0:
+            s1 = None
+
+    if t2 is not None:
+        s2 = 2 * np.arctan(t2) / alpha
+        if s2 > 1 or s2 < 0:
+            s2 = None
+
+    return s1, s2
---
+++ packages/xrd-simulator/xrd_simulator-0.3.13.tar.gz/xrd_simulator-0.3.13/xrd_simulator/scattering_unit.py
@@ -0,0 +1,105 @@
+#TODO: Review docs.
+import numpy as np
+
+
+class ScatteringUnit(object):
+
+    """Defines a scattering single crystal as a convex polyhedra.
+
+    Args:
+        convex_hull (:obj:`scipy.spatial.ConvexHull`): Object describing the convex hull of the self.
+        kprime (:obj:`string`): Scattering vector, i.e the wavevector pointing in the direction of diffraction.
+        s (:obj:`float`): Parametric value in range [0,1] where 0 corresponds to a beam with wavevector k1
+            while s=1 to a beam with wavevector k2. The s value of the scattering_unit implies what detector
+            position is applicable during diffraction.
+        phase (:obj:`Phase`): The Phase object representing the material of the self.
+        hkl_indx (:obj:`int`): Index of Miller index in the `phase.miller_indices` list.
+
+    Attributes:
+        convex_hull (:obj:`scipy.spatial.ConvexHull`): Object describing the convex hull of the self.
+        kprime (:obj:`string`): Scattering vector, i.e the wavevector pointing in the direction of diffraction.
+        s (:obj:`float`): Parametric value in range [0,1] where 0 corresponds to a beam with wavevector k1
+            while s=1 to a beam with wavevector k2. The s value of the scattering_unit implies what detector
+            position is applicable during diffraction.
+
+    """
+
+    def __init__(
+            self,
+            convex_hull,
+            scattered_wave_vector,
+            incident_wave_vector,
+            wavelength,
+            incident_polarization_vector,
+            rotation_axis,
+            time,
+            phase,
+            hkl_indx):
+        self.convex_hull = convex_hull
+        self.scattered_wave_vector = scattered_wave_vector
+        self.incident_wave_vector = incident_wave_vector
+        self.wavelength = wavelength
+        self.incident_polarization_vector = incident_polarization_vector
+        self.rotation_axis = rotation_axis
+        self.time = time
+        self.phase = phase
+        self.hkl_indx = hkl_indx
+
+    @property
+    def hkl(self):
+        """hkl (:obj:`numpy array`): Miller indices [h,k,l] ``shape=(3,)``."""
+        return self.phase.miller_indices[self.hkl_indx]
+
+    @property
+    def real_structure_factor(self):
+        """hkl (:obj:`numpy array`): Real part of unit cell structure factor"""
+        if self.phase.structure_factors is not None:
+            return self.phase.structure_factors[self.hkl_indx, 0]
+        else:
+            return self.phase.structure_factors
+
+    @property
+    def imaginary_structure_factor(self):
+        """hkl (:obj:`numpy array`): Imaginary part of unit cell structure factor"""
+        if self.phase.structure_factors is not None:
+            return self.phase.structure_factors[self.hkl_indx, 1]
+        else:
+            return self.phase.structure_factors
+
+    @property
+    def lorentz_factor(self):
+        """Compute the Lorentz intensity factor for a scattering_unit.
+        """
+        k = self.incident_wave_vector
+        kp = self.scattered_wave_vector
+        theta = np.arccos(k.dot(kp) / (np.linalg.norm(k)**2)) / 2.
+        korthogonal = kp - (k * kp.dot(k) / (np.linalg.norm(k)**2))
+        eta = np.arccos(
+            self.rotation_axis.dot(korthogonal) /
+            np.linalg.norm(korthogonal))
+        tol = 0.5
+        if np.abs(np.degrees(eta)) < tol or np.abs(np.degrees(eta)) > 180-tol or np.degrees(theta) < tol:
+            return np.inf
+        else:
+            return 1. / (np.sin(2 * theta) * np.abs(np.sin(eta)))
+
+    @property
+    def polarization_factor(self):
+        """Compute the Polarization intensity factor for a scattering_unit.
+        """
+        khatp = self.scattered_wave_vector / \
+            np.linalg.norm(self.scattered_wave_vector)
+        return 1 - np.dot(self.incident_polarization_vector, khatp)**2
+
+    @property
+    def centroid(self):
+        """centroid (:obj:`numpy array`): centroid of the scattering region. ``shape=(3,)``
+        """
+        return np.mean(
+            self.convex_hull.points[self.convex_hull.vertices], axis=0)
+
+    @property
+    def volume(self):
+        """volume (:obj:`float`): volume of the scattering region volume
+        """
+        return self.convex_hull.volume
