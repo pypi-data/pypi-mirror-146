56a9ae316dba6f29a67818490979428a0ba37758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:04 +0000

    Add package Mixture-Models/0.0.3/Mixture_Models-0.0.3.tar.gz

---
+++ packages/Mixture-Models/Mixture_Models-0.0.3.tar.gz/Mixture_Models-0.0.3/Mixture_Models/__init__.py
@@ -0,0 +1,9 @@
+from .mixture_models import *
+from .gmm import *
+from .gmm_conc import *
+from .mfa import *
+from .pgmm import *
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
