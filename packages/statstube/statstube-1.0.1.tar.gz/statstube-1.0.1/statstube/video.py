ead328e4640bf7e6fdb1956a782e88484a26a3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:34 +0000

    Add package statstube/1.0.1/statstube-1.0.1.tar.gz

---
+++ packages/statstube/statstube-1.0.1.tar.gz/statstube-1.0.1/statstube/video.py
@@ -0,0 +1,11 @@
+def get_video_by_id(id: str) -> str:
+    """
+    Calculate the great circle distance between two points on the
+    earth (specified in decimal degrees), returns the distance in
+    meters.
+    All arguments must be of equal length.
+    :param id: video id
+    :return: will return an object
+    """
+
+    return int(id) * 2
