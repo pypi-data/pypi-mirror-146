b64172ce269ecffc71fcdcafc013801c80bed03d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:35 +0000

    Add package texto-no-espelho/0.0.2/texto_no_espelho-0.0.2.tar.gz

---
+++ packages/texto-no-espelho/texto_no_espelho-0.0.2.tar.gz/texto_no_espelho-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="texto_no_espelho",
+    version="0.0.2",
+    author="Israel Ruiz",
+    author_email="israelruiz2005@gmail.com",
+    description="Texto no espelho",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/israelruiz2005/texto_no_espelho_package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
---
+++ packages/texto-no-espelho/texto_no_espelho-0.0.2.tar.gz/texto_no_espelho-0.0.2/texto_no_espelho/espelho.py
@@ -0,0 +1,3 @@
+#espelho do texto
+def espelho_texto(umtexto):
+    return umtexto + "<-|->" + umtexto[::-1]  
