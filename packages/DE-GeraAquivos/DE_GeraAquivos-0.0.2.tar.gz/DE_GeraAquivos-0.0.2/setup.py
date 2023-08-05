a7a8c912f33c059601c8dff860ab687afc3dcc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package DE-GeraAquivos/0.0.2/DE_GeraAquivos-0.0.2.tar.gz

---
+++ packages/DE-GeraAquivos/DE_GeraAquivos-0.0.2.tar.gz/DE_GeraAquivos-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DE_GeraAquivos',
+    version='0.0.2',
+    packages=[''],
+    url='https://github.com/almirjgomes/DE_GeraAquivos.git',
+    license='MIT',
+    author='Almir J Gomes',
+    author_email='almir.jg@hotmail.com',
+    description='Gera arquivos baseado em dataframe'
+)
