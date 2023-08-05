8304034161c9e2cd282441db730d2dc028bb40f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:39 +0000

    Add package PhyPraKit/1.2.2/PhyPraKit-1.2.2.tar.gz

---
+++ packages/PhyPraKit/PhyPraKit-1.2.2.tar.gz/PhyPraKit-1.2.2/PhyPraKit/_version_info.py
@@ -0,0 +1,24 @@
+'''
+.. module:: _version_info
+   :platform: python 2.7, >=3.5
+   :synopsis: Version 1.1.2 of PhyPraKit, rel. Jan 2021
+
+.. moduleauthor:: Guenter Quast <g.quast@kit.edu>
+'''
+
+major = 1
+minor = 2
+revision = 2
+
+def _get_version_tuple():
+  '''
+  version as a tuple
+  '''
+  return (major, minor, revision)
+
+def _get_version_string():
+  '''
+  version as a string
+  '''
+  return "%d.%d.%d" % _get_version_tuple()
+
