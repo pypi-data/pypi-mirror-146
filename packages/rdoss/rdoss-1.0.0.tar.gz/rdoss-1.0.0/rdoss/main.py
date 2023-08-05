cdb77d0cbae0b917eee78aed552f39477d966247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:29 +0000

    Add package rdoss/1.0.0/rdoss-1.0.0.tar.gz

---
+++ packages/rdoss/rdoss-1.0.0.tar.gz/rdoss-1.0.0/rdoss/main.py
@@ -0,0 +1,22 @@
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+import sys
+import logging
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+class Rdfs(CosS3Client):
+    def __init__(self,secret_id,secret_key,region = 'ap-shanghai'):
+        self.secret_id =secret_id
+        self.secret_key = secret_key
+        self.region =region
+        token = None
+        scheme = 'https'
+        self.config = CosConfig(Region=self.region, SecretId=self.secret_id, SecretKey=self.secret_key, Token=token, Scheme=scheme)
+        CosS3Client.__init__(self=self,conf=self.config)
+if __name__ =='__main__':
+    pass
+
+
+
+
+
+
