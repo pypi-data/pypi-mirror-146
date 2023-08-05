4e4908c72a4db1c042431f879aef93c55beb3169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:50 +0000

    Add package meerschaum/0.5.15/meerschaum-0.5.15-py3-none-any.whl

---
+++ packages/meerschaum/meerschaum-0.5.15-py3-none-any.whl/meerschaum/actions/arguments/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+This package includes argument parsing utilities.
+"""
+
+from meerschaum.actions.arguments._parse_arguments import parse_arguments, parse_line
+from meerschaum.actions.arguments._parser import parser, add_plugin_argument
---
+++ packages/meerschaum/meerschaum-0.5.15-py3-none-any.whl/meerschaum/config/_version.py
@@ -0,0 +1,5 @@
+"""
+Specify the Meerschaum release version.
+"""
+
+__version__ = "0.5.15"
