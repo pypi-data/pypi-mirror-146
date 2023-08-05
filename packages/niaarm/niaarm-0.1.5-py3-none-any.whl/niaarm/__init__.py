68d63fb1a8cadce94bc8b7478efc8b0fc850b012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:17 +0000

    Add package niaarm/0.1.5/niaarm-0.1.5-py3-none-any.whl

---
+++ packages/niaarm/niaarm-0.1.5-py3-none-any.whl/niaarm/__init__.py
@@ -0,0 +1,11 @@
+from niaarm.niaarm import NiaARM
+from niaarm.dataset import Dataset
+from niaarm.rule import Rule
+from niaarm.feature import Feature
+from niaarm.mine import get_rules
+from niaarm.rule_list import RuleList
+
+
+__all__ = ['NiaARM', 'Dataset', 'Feature', 'Rule', 'RuleList', 'get_rules']
+
+__version__ = "0.1.4"
