df697cc8760f2252ece5331091fa18f4aba58c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package meshctrl/0.1.12/meshctrl-0.1.12.tar.gz

---
+++ packages/meshctrl/meshctrl-0.1.12.tar.gz/meshctrl-0.1.12/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="meshctrl",
+    version="0.1.12",
+    description="Python port of MeshCentral's Meshctrl.js program",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amidaware/meshctrl-py",
+    author="Josh Krawczyk",
+    author_email="josh@torchlake.com",
+    license="MIT",
+    install_requires=["websockets==10.2", "pycryptodome==3.14.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
