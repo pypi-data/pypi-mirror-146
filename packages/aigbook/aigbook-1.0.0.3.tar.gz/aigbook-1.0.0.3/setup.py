87642f1a53044b7a7215e7a1a58ad493be3d1c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:08 +0000

    Add package aigbook/1.0.0.3/aigbook-1.0.0.3.tar.gz

---
+++ packages/aigbook/aigbook-1.0.0.3.tar.gz/aigbook-1.0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aigbook',
+    version='1.0.0.3',
+    license="MIT Licence",
+    description="",
+
+    author='Yaronzz',
+    author_email="yaronhuang@foxmail.com",
+
+    packages=find_packages(),
+    platforms="any",
+    include_package_data=True,
+    install_requires=["requests", "aigpy", "lxml"],
+)
