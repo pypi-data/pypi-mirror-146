47c67336ebda3712507c6f013918a53e30b2e46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:31 +0000

    Add package ftpc/0.0.4/ftpc-0.0.4.tar.gz

---
+++ packages/ftpc/ftpc-0.0.4.tar.gz/ftpc-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+# This call to setup() does all the work
+
+setup(
+    name="ftpc",
+    version='0.0.4',
+    author="Baxromov",
+    description="Develop ftpc",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(where="ftpc"),
+    python_requires=">=3.6",
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    long_description="You should install -> sudo apt-get install default-jre libreoffice-java-common to Ubuntu",
+)
