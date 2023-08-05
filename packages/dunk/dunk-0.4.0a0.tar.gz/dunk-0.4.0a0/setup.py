15546a0bfd5636ca1b6a0cec753dc812f33c1957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:45 +0000

    Add package dunk/0.4.0a0/dunk-0.4.0a0.tar.gz

---
+++ packages/dunk/dunk-0.4.0a0.tar.gz/dunk-0.4.0a0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dunk']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['rich>=12.1.0,<13.0.0', 'unidiff>=0.7.3,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['dunk = dunk.dunk:main']}
+
+setup_kwargs = {
+    'name': 'dunk',
+    'version': '0.4.0a0',
+    'description': 'Beautiful side-by-side diffs',
+    'long_description': None,
+    'author': 'Darren Burns',
+    'author_email': 'darrenb900@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
