9c5db4b121b6f9ab38dfe2d5b01df5c6b77ce1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:12 +0000

    Add package mzflaskfive/0.1.2/mzflaskfive-0.1.2.tar.gz

---
+++ packages/mzflaskfive/mzflaskfive-0.1.2.tar.gz/mzflaskfive-0.1.2/mzflaskfive/__init__.py
@@ -0,0 +1,12 @@
+from mzflaskfive.MzView import MzView
+
+from flask import *
+
+app = Flask('mzflaskfive')
+
+app.add_url_rule('/', view_func=MzView.as_view('index'))
+print(app.url_map)  # 输出所有路由
+
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', 5000)
