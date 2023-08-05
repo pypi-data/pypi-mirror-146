30f9a594e345ba0a32e00fb11d47d464eb96736a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:45 +0000

    Add package averag-mult-three/0.0.1/averag_mult_three-0.0.1-py3-none-any.whl

---
+++ packages/averag-mult-three/averag_mult_three-0.0.1-py3-none-any.whl/averag_mult_three/average_three_numb/average_calculation.py
@@ -0,0 +1,6 @@
+def calc_average_three():
+    numb1 = float(input("Insira o primeiro número: "))
+    numb2 = float(input("Insira o segundo número: "))
+    numb3 = float(input("Insira o terceiro número: "))
+    average = (numb1 + numb2 + numb3)/3
+    print("A média dos três números é:", average)
---
+++ packages/averag-mult-three/averag_mult_three-0.0.1-py3-none-any.whl/averag_mult_three/multiplication_three_numb/multiplication_calculation.py
@@ -0,0 +1,6 @@
+def calc_multipl_three():
+    numb1 = float(input("Insira o primeiro número: "))
+    numb2 = float(input("Insira o segundo número: "))
+    numb3 = float(input("Insira o terceiro número: "))
+    multipl = (numb1 * numb2 * numb3)
+    print("A multiplicação dos três números é:", multipl)
