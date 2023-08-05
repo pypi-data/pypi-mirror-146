a4b94ab0b8cc2272bd2a74492a0b91b47dc79c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package py-xdb/0.1.28/py-xdb-0.1.28.tar.gz

---
+++ packages/py-xdb/py-xdb-0.1.28.tar.gz/py-xdb-0.1.28/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup,find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="py-xdb",
+    version="0.1.28",
+    description="generic database client for CLI lovers",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/walkerever/py-xdb",
+    author="Yonghang Wang",
+    author_email="wyhang@gmail.com",
+    license="MIT License",
+    classifiers=["License :: OSI Approved :: Apache Software License"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[ "xtable","sqlalchemy","pygments","prompt_toolkit","pandas"],
+    keywords=[ "database","client","sql","CLI" ],
+    entry_points={ "console_scripts": 
+        [ 
+            "xdb=xdb:xdb_main", 
+        ] 
+    },
+)
