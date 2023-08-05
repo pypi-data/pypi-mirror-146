7b52e60758e4eb861740ad771606bcf012a70e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:04 +0000

    Add package vcspull/1.12.0b8/vcspull-1.12.0b8.tar.gz

---
+++ packages/vcspull/vcspull-1.12.0b8.tar.gz/vcspull-1.12.0b8/tests/test_config.py
@@ -0,0 +1,50 @@
+import pytest
+
+from vcspull import config
+
+
+@pytest.fixture
+def load_yaml(tmpdir):
+    def fn(content, dir="randomdir", filename="randomfilename.yaml"):
+        _dir = tmpdir.join(dir)
+        _dir.mkdir()
+        _dir.join(filename).write(content)
+
+        configs = config.find_config_files(path=str(_dir))
+        repos = config.load_configs(configs, str(_dir))
+        return _dir, configs, repos
+
+    return fn
+
+
+def test_simple_format(load_yaml):
+    dir, _, repos = load_yaml(
+        """
+vcspull:
+  libvcs: git+https://github.com/vcs-python/libvcs
+   """
+    )
+
+    assert len(repos) == 1
+    repo = repos[0]
+
+    assert str(dir.join("vcspull")) == repo["parent_dir"]
+    assert str(dir.join("vcspull", "libvcs")) == repo["repo_dir"]
+
+
+def test_relative_dir(load_yaml):
+    dir, _, repos = load_yaml(
+        """
+./relativedir:
+  docutils: svn+http://svn.code.sf.net/p/docutils/code/trunk
+   """
+    )
+
+    configs = config.find_config_files(path=str(dir))
+    repos = config.load_configs(configs, str(dir))
+
+    assert len(repos) == 1
+    repo = repos[0]
+
+    assert str(dir.join("relativedir")) == repo["parent_dir"]
+    assert str(dir.join("relativedir", "docutils")) == repo["repo_dir"]
---
+++ packages/vcspull/vcspull-1.12.0b8.tar.gz/vcspull-1.12.0b8/vcspull/util.py
@@ -0,0 +1,36 @@
+"""Utility functions for vcspull.
+
+vcspull.util
+~~~~~~~~~~~~
+
+"""
+import os
+from collections.abc import Mapping
+
+CONFIG_DIR = os.path.expanduser("~/.vcspull/")  # remove dupes of this
+
+
+def update_dict(d, u):
+    """Return updated dict.
+
+    Parameters
+    ----------
+    d : dict
+    u : dict
+
+    Returns
+    -------
+    dict :
+        Updated dictionary
+
+    Notes
+    -----
+    Thanks: http://stackoverflow.com/a/3233356
+    """
+    for k, v in u.items():
+        if isinstance(v, Mapping):
+            r = update_dict(d.get(k, {}), v)
+            d[k] = r
+        else:
+            d[k] = u[k]
+    return d
