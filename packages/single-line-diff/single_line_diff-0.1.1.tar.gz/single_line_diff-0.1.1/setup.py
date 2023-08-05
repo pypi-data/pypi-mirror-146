65bc8def03f28299da6c450ecf53b496b3f24133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:32 +0000

    Add package single-line-diff/0.1.1/single_line_diff-0.1.1.tar.gz

---
+++ packages/single-line-diff/single_line_diff-0.1.1.tar.gz/single_line_diff-0.1.1/setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup
+
+setup(
+	name='single_line_diff',
+	version='0.1.1',
+	description='A package that can diff single lines',
+	url='https://gitlab.com/azuredown/single_line_diff',
+	author='Andrew Zuo',
+	author_email='andrew@andrewzuo.com',
+	python_requires='>=3',
+)
+
+'''
+license='BSD 2-clause',
+packages=['pyexample'],
+install_requires=[
+	'mpi4py>=2.0',
+	'numpy',
+],
+
+classifiers=[
+	'Development Status :: 1 - Planning',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: BSD License',
+	'Operating System :: POSIX :: Linux',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 2.7',
+	'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+],
+'''
