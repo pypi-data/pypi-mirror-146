3aa1f2b0c7cb4a01ac0aabf2b1ab34c7c77be06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:51 +0000

    Add package mzflaskfive/0.0.3/mzflaskfive-0.0.3.tar.gz

---
+++ packages/mzflaskfive/mzflaskfive-0.0.3.tar.gz/mzflaskfive-0.0.3/mzflaskfive/MzView.py
@@ -0,0 +1,13 @@
+from flask.views import MethodView
+from flask import *
+from MzChess import MzChess
+
+# 网页视图类
+class MzView(MethodView):
+	def get(self):
+		return render_template('index.html')
+	def post(self):
+		chessboardstr = request.form['chessboardstr']
+		mzChess = MzChess(chessboardstr)         # 实例化棋盘对象
+		choice = mzChess.choice()                 # 获取ai决策 return:[x, y]
+		return choice
