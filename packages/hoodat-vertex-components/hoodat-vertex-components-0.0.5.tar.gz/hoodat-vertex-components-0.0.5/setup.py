acc8d1c6167d1da534c369f5efa6b19aef08027e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:56 +0000

    Add package hoodat-vertex-components/0.0.5/hoodat-vertex-components-0.0.5.tar.gz

---
+++ packages/hoodat-vertex-components/hoodat-vertex-components-0.0.5.tar.gz/hoodat-vertex-components-0.0.5/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="hoodat-vertex-components",
+    packages=["hoodat-vertex-components"],
+    version="0.0.5",
+    license="MIT",
+    description="Vertex components for hoodat",
+    author="Eugene Brown",
+    author_email="efbbrown@gmail.com",
+    url="https://github.com/efbbrown/hoodat-vertex-components",
+    download_url="https://github.com/efbbrown/hoodat-vertex-components/archive/refs/tags/v.0.0.5.tar.gz",
+    include_package_data=True,
+)
