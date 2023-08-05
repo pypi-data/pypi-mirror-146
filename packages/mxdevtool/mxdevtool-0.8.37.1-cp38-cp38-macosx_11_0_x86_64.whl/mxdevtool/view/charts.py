ce39643b6a27962ff10814b43af7873530c998d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:57 +0000

    Add package mxdevtool/0.8.37.1/mxdevtool-0.8.37.1-cp38-cp38-macosx_11_0_x86_64.whl

---
+++ packages/mxdevtool/mxdevtool-0.8.37.1-cp38-cp38-macosx_11_0_x86_64.whl/mxdevtool/view/charts.py
@@ -0,0 +1,15 @@
+import mxdevtool as mx
+# import matplotlib.pyplot as plt
+
+# def graph(name, xdata, ydata_d, **kwargs):
+#     # plot the data
+#     fig = plt.figure()
+#     ax = fig.add_subplot(1, 1, 1)
+#     for k, y in ydata_d.items():
+#         ax.plot(xdata, y, 'o-', color='tab:blue')
+
+#     ax.set_title(name)
+#     if kwargs.get('show') is True:
+#         plt.show()
+
+#     return ax
