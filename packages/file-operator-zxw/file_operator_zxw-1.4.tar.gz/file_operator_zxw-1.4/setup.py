d4522c7827f83d9b02557c39e227b5f0ff27bf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:20 +0000

    Add package file-operator-zxw/1.4/file_operator_zxw-1.4.tar.gz

---
+++ packages/file-operator-zxw/file_operator_zxw-1.4.tar.gz/file_operator_zxw-1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name="file_operator_zxw",
+    version="1.4",
+    description="文件常用操作",
+    author="XueWei Zhang",
+    author_email="307080785@qq.com",
+    py_modules=['file_operator_zxw.扫描文件','file_operator_zxw.创建文件夹','file_operator_zxw.识别移动硬盘',
+                'file_operator_zxw.get项目根目录','file_operator_zxw.获取磁盘剩余空间','file_operator_zxw.创建excel流',
+                'file_operator_zxw.获取文件占用空间',]
+)
