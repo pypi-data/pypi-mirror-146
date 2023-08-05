70690356b8cddbd26d09c21fb212c98d73cbdfa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:17 +0000

    Add package pysigma-pipeline-crowdstrike/0.1.5/pysigma-pipeline-crowdstrike-0.1.5.tar.gz

---
+++ packages/pysigma-pipeline-crowdstrike/pysigma-pipeline-crowdstrike-0.1.5.tar.gz/pysigma-pipeline-crowdstrike-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sigma', 'sigma.pipelines.crowdstrike']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pysigma>=0.5.0,<0.6.0']
+
+setup_kwargs = {
+    'name': 'pysigma-pipeline-crowdstrike',
+    'version': '0.1.5',
+    'description': 'pySigma CrowdStrike processing pipelines',
+    'long_description': None,
+    'author': 'Thomas Patzke',
+    'author_email': 'thomas@patzke.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
