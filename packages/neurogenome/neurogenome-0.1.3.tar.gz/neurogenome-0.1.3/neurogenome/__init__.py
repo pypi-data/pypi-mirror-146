0c484ce257deed899269e0e0d78c90a46447e1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:15 +0000

    Add package neurogenome/0.1.3/neurogenome-0.1.3.tar.gz

---
+++ packages/neurogenome/neurogenome-0.1.3.tar.gz/neurogenome-0.1.3/neurogenome/__init__.py
@@ -0,0 +1,22 @@
+"""
+**************************************
+
+NeuroGenome
+
+Created in 20.11.2021
+by Aiyyskhan Alekseev 
+
+https://github.com/Aiyyskhan
+aiyyskhan@gmail.com
+
+License: MIT
+
+**************************************
+"""
+__version__ = "0.1.3"
+
+import neurogenome.genome as genome
+import neurogenome.builders as builders
+import neurogenome.evolution as evolution
+import neurogenome.macromutations as macromutations
+import neurogenome.file_handler as file_handler
