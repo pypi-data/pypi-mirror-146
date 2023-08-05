1a4d6c2c23ca90a717f665148c1e4fa308c6216d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:44 +0000

    Add package discord-advert-framework/1.7.9.post4/Discord-Advert-Framework-1.7.9.post4.tar.gz

---
+++ packages/discord-advert-framework/Discord-Advert-Framework-1.7.9.post4.tar.gz/Discord-Advert-Framework-1.7.9.post4/src/framework/__init__.py
@@ -0,0 +1,27 @@
+"""
+    DISCORD ADVERTISEMENT FRAMEWORK (DAF)
+    Author      :: David Hozic
+    Copyright   :: Copyright (c) 2022 David Hozic
+    Version     :: V1.7.9
+"""
+
+
+"""
+TODO:
+- Documentation for DirectMESSAGE, USER classes,
+- Doc strings for each sub module,
+- Doc strings for classes and functions where it's missing
+- Examples for DirectMESSAGE and USER classes
+- Linting
+- Test everything
+"""
+
+
+import _discord as discord
+from .client import *
+from .const import *
+from .core import *
+from .dtypes import *
+from .guild import *
+from .message import *
+from .tracing import *
