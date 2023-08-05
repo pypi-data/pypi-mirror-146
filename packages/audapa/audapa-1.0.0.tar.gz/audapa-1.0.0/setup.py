aa36fb8795d20b0782eeb9bcb987c91fee54bd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:18 +0000

    Add package audapa/1.0.0/audapa-1.0.0.tar.gz

---
+++ packages/audapa/audapa-1.0.0.tar.gz/audapa-1.0.0/setup.py
@@ -0,0 +1,27 @@
+pkname='audapa'
+
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "info.md").read_text()
+
+from setuptools import setup
+setup(name=pkname,
+	version='1.0.0',
+	packages=[pkname],
+	#optionals
+	python_requires='>=3.8',
+	install_requires=[
+		"pycairo>=1.20.0","PyGObject>=3.40",
+		"appdirs>=1.4.3",
+		"PyAudio>=0.2.11"],
+	description='Audio wave file manipulator',
+	long_description=README,
+	long_description_content_type="text/markdown",
+	url='https://github.com/colin-i/audapa',
+	author='colin-i',
+	author_email='costin.botescu@gmail.com',
+	license='MIT',
+	entry_points = {
+		'console_scripts': [pkname+'='+pkname+'.main:main']
+	}
+)
