eddfed4cfcf2d8be54362a97b61cf96859c1bd34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:04 +0000

    Add package macos-say/0.1.0/macos_say-0.1.0.tar.gz

---
+++ packages/macos-say/macos_say-0.1.0.tar.gz/macos_say-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='macos_say',
+    version='0.1.0',    
+    description='The easiest way to make your mac speak within python scripts.',
+    url='https://github.com/chrischma/mac_say',
+    author='Christoph Schmalfuß',
+    author_email='post@christoph-schmalfuss.de',
+    license='MIT',
+
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+)
