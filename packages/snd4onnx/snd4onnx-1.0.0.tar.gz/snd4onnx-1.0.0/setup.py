0682860d20fba145e8a471c6a7c008614f1d6122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:27 +0000

    Add package snd4onnx/1.0.0/snd4onnx-1.0.0.tar.gz

---
+++ packages/snd4onnx/snd4onnx-1.0.0.tar.gz/snd4onnx-1.0.0/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="snd4onnx",
+    version="1.0.0",
+    description="Simple node deletion tool for onnx.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Katsuya Hyodo",
+    author_email="rmsdh122@yahoo.co.jp",
+    url="https://github.com/PINTO0309/snd4onnx",
+    license="MIT License",
+    packages=find_packages(),
+    platforms=["linux", "unix"],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "snd4onnx = snd4onnx.onnx_remove_node:main"
+        ]
+    }
+)
