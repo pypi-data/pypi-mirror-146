5ef567cda7fb3e813cfa0c8eb0f04cf0f19ded96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package invenio-records-resources/0.19.7/invenio-records-resources-0.19.7.tar.gz

---
+++ packages/invenio-records-resources/invenio-records-resources-0.19.7.tar.gz/invenio-records-resources-0.19.7/invenio_records_resources/ext.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2021 CERN.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Records Resources module to create REST APIs."""
+
+from . import config
+from .registry import ServiceRegistry
+
+
+class InvenioRecordsResources(object):
+    """Invenio-Records-Resources extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        self.init_config(app)
+        self.registry = ServiceRegistry()
+        app.extensions["invenio-records-resources"] = self
+
+    def init_config(self, app):
+        """Initialize configuration."""
+        for k in dir(config):
+            if k.startswith("RECORDS_RESOURCES_") or k.startswith('SITE_'):
+                app.config.setdefault(k, getattr(config, k))
