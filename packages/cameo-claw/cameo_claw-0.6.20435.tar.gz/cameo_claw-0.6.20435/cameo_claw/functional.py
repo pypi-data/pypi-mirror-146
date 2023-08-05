94560eb10a5f08db5534718e55d079f7296a3e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:32 +0000

    Add package cameo-claw/0.6.20435/cameo_claw-0.6.20435.tar.gz

---
+++ packages/cameo-claw/cameo_claw-0.6.20435.tar.gz/cameo_claw-0.6.20435/cameo_claw/functional.py
@@ -0,0 +1,18 @@
+from multiprocessing.dummy import Pool
+from tqdm import tqdm
+import cameo_claw.istarmap
+
+cameo_claw.istarmap.ignore_no_use_warning()
+
+
+def it_mp_f(f, tup_param):
+    # 2022-04-10 bowen
+    # fastapi + multiprocessing can crash, please use spawn to prevent it
+    # https://miketarpey.medium.com/troubleshooting-usage-of-pythons-multiprocessing-module-in-a-fastapi-app-f1c368673686
+    # also fail
+    # finally we use: from multiprocessing.dummy import Pool
+    int_progress = 0
+    with Pool(25) as p:
+        for result in tqdm(p.istarmap(f, tup_param), total=len(tup_param)):
+            int_progress += 1
+            yield int_progress, result
