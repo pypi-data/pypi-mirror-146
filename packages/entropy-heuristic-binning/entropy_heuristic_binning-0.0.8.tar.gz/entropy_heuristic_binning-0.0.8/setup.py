d253427b31d0e9ef29f5ce75bc6850aaf888a37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:50 +0000

    Add package entropy-heuristic-binning/0.0.8/entropy_heuristic_binning-0.0.8.tar.gz

---
+++ packages/entropy-heuristic-binning/entropy_heuristic_binning-0.0.8.tar.gz/entropy_heuristic_binning-0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import setuptools
+
+
+setup(
+    name="entropy_heuristic_binning",
+    version="0.0.8",
+    author="shazri_shahrir",
+    author_email="mdshazrishahrir@gmail.com",
+    description="entropy heuristic binning",
+    long_description="entropy heuristic binning",
+    long_description_content_type="text/markdown",
+    url="https://github.com/shazri/entropyheuristicbinning",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_save=False
+)
