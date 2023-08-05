3b054ca80a1f4815465a33332745eef57a0fef89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:39 +0000

    Add package donutLol/0.0.8/donutLol-0.0.8.tar.gz

---
+++ packages/donutLol/donutLol-0.0.8.tar.gz/donutLol-0.0.8/README.md
@@ -0,0 +1,18 @@
+# donut3d
+to insatll:
+```
+pip install donutLol
+```
+
+to run in code 
+
+```
+from donutLol import donut 
+donut()
+```
+
+to run in windows cmd:
+
+```
+donut
+```
---
+++ packages/donutLol/donutLol-0.0.8.tar.gz/donutLol-0.0.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.8'
+DESCRIPTION = 'Prints a 3d donut'
+
+# Setting up
+setup(
+    name="donutLol",
+    version=VERSION,
+    scripts=['donut.bat'] ,
+    author="lazylion2",
+    author_email="<lazylion2@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    url="https://github.com/lazylion22/donutLol",
+    keywords=['python', 'donut', 'donut3d'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
