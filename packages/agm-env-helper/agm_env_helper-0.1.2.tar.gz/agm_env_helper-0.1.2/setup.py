c21978cf0fad69c2f2b6d34daed5cd2be814a99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:50 +0000

    Add package agm-env-helper/0.1.2/agm_env_helper-0.1.2.tar.gz

---
+++ packages/agm-env-helper/agm_env_helper-0.1.2.tar.gz/agm_env_helper-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='agm_env_helper',
+      version='0.1.2',
+      description='Custom env helper',
+      packages=['agm_env_helper'],
+      author_email='alexgeniusman@gmail.com',
+      zip_safe=False)
