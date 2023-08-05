5a5c5a5e0a7b2becfc9df2f8f54bda61b0697d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package printnanny-api-client/0.72.2/printnanny-api-client-0.72.2.tar.gz

---
+++ packages/printnanny-api-client/printnanny-api-client-0.72.2.tar.gz/printnanny-api-client-0.72.2/docs/PrintNannyApiConfig.md
@@ -0,0 +1,14 @@
+# PrintNannyApiConfig
+
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**bearer_access_token** | **str** |  | [readonly] 
+**base_path** | **str** |  | [readonly] 
+**static** | **str** |  | [readonly] 
+**ws** | **str** |  | [readonly] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
---
+++ packages/printnanny-api-client/printnanny-api-client-0.72.2.tar.gz/printnanny-api-client-0.72.2/printnanny_api_client/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from printnanny_api_client.api.alerts_api import AlertsApi
+from printnanny_api_client.api.auth_api import AuthApi
+from printnanny_api_client.api.client_config_api import ClientConfigApi
+from printnanny_api_client.api.devices_api import DevicesApi
+from printnanny_api_client.api.events_api import EventsApi
+from printnanny_api_client.api.janus_api import JanusApi
+from printnanny_api_client.api.octoprint_api import OctoprintApi
+from printnanny_api_client.api.partners_geeks3d_api import PartnersGeeks3dApi
+from printnanny_api_client.api.schema_api import SchemaApi
+from printnanny_api_client.api.users_api import UsersApi
