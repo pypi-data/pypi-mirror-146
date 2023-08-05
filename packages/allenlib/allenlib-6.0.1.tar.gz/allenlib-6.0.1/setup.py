925a6f81e027e618133deb6b423d9e4afe6ffd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:06 +0000

    Add package allenlib/6.0.1/allenlib-6.0.1.tar.gz

---
+++ packages/allenlib/allenlib-6.0.1.tar.gz/allenlib-6.0.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+from time import localtime,strftime
+vi='6.0.1'
+day=1
+local=strftime(f'%Y%m%d',localtime())
+setup(
+    name='allenlib',
+    version=f'{vi}',
+    description=(
+        'allenlib'
+    ),
+    long_description=open('README.md',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='刘雨桐',
+    author_email='UNKNOWN',
+    maintainer='刘雨桐',
+    maintainer_email='UNKNOWN',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=['all'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'requests','pygame'
+    ]
+)
