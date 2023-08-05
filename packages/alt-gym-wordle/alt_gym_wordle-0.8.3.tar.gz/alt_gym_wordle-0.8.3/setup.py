d3bd57e88ce53c360448e57f7d72c3ff10ea6a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:02 +0000

    Add package alt-gym-wordle/0.8.3/alt_gym_wordle-0.8.3.tar.gz

---
+++ packages/alt-gym-wordle/alt_gym_wordle-0.8.3.tar.gz/alt_gym_wordle-0.8.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+__version__ = '0.8.3'
+setup(name='alt_gym_wordle',
+    version=__version__,
+    author='Ryan Snyder',
+    description='OpenAI gym environment for training agents on Wordle',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ryan-snyder/gym-wordle',
+    packages=find_packages(
+        include=[
+            'gym_wordle',
+            'gym_wordle.*'
+        ]
+    ),
+    package_data={
+        'gym_wordle': ['envs/*.txt']
+    },
+    install_requires=['gym', 'pandas', 'pygame']  # And any other dependencies foo needs
+)
