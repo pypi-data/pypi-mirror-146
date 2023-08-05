40facc9d293c2e3f4e6c8c0ec0503d6590b8b697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:16 +0000

    Add package dudocode/0.6.0/dudocode-0.6.0.tar.gz

---
+++ packages/dudocode/dudocode-0.6.0.tar.gz/dudocode-0.6.0/setup.py
@@ -0,0 +1,67 @@
+"""
+“Commons Clause” License Condition v1.0
+
+The Software is provided to you by the Licensor under the License, as defined below, subject to the following condition.
+
+Without limiting other conditions in the License, the grant of rights under the License will not include, and the License does not grant to you, the right to Sell the Software.
+
+For purposes of the foregoing, “Sell” means practicing any or all of the rights granted to you under the License to provide to third parties, for a fee or other consideration (including without limitation fees for hosting or consulting/ support services related to the Software), a product or service whose value derives, entirely or substantially, from the functionality of the Software. Any license notice or attribution required by the License must also include this Commons Clause License Condition notice.
+
+Software: Dudocode
+
+License: GNU General Public License Version 3
+
+Licensor: SONG YIDING
+
+Dudocode is a pseudocode-to-Python transpiler based on the format specified in CIE IGCSE (Syllabus 0478).
+Copyright (C) 2022  SONG YIDING
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+import setuptools
+VERSION = "0.6.0"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dudocode",
+    version=VERSION,
+    author="PerceptronV",
+    author_email="neutrinovs@gmail.com",
+    description="A pseudocode-to-Python transpiler based on the format specified in CIE IGCSE (Syllabus 0478)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PerceptronV/dudocode",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'trilobyte',
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dudo = dudocode.dudo:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Other",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Interpreters",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Natural Language :: English"
+    ],
+    python_requires='>=3.3',
+)
