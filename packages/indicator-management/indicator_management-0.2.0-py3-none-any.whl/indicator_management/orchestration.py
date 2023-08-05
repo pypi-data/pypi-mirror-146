91a9f27671d035499d3c48fb1b6ff5dfe1ab6236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:36 +0000

    Add package indicator-management/0.2.0/indicator_management-0.2.0-py3-none-any.whl

---
+++ packages/indicator-management/indicator_management-0.2.0-py3-none-any.whl/indicator_management/orchestration.py
@@ -0,0 +1,45 @@
+from typing import Any, Generator
+
+from .indicators import AbstractIndicator
+
+
+def all_bases(*indicators: AbstractIndicator) -> set[AbstractIndicator]:
+    """
+    Return set of all bases required by given indicators.
+    """
+    visited_indicators: set[AbstractIndicator] = set()
+    result: set[AbstractIndicator] = set()
+
+    def dfs(indicator: AbstractIndicator):
+        if indicator in visited_indicators:
+            return
+        visited_indicators.add(indicator)
+        for pre_requisite in indicator.pre_requisites:
+            dfs(pre_requisite)
+        if indicator.is_basis():
+            result.add(indicator)
+
+    for indicator in indicators:
+        dfs(indicator)
+    return result
+
+
+def generate(**indicators: AbstractIndicator) -> Generator[dict[str, Any], None, None]:
+    """
+    Generate all indicators by flow.
+    """
+
+    bases: set[AbstractIndicator] = all_bases(*indicators.values())
+    if not bases:
+        raise ValueError("There is no basis indicator")
+
+    try:
+        while True:
+            for basis in bases:
+                basis.update()
+            yield {
+                indicator_name: indicator.value
+                for indicator_name, indicator in indicators.items()
+            }
+    except StopIteration:
+        pass
