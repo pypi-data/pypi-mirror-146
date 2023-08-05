a5c19feaf264b130e025661f51c1da9723af2b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:52 +0000

    Add package piraye/0.1.0/piraye-0.1.0.tar.gz

---
+++ packages/piraye/piraye-0.1.0.tar.gz/piraye-0.1.0/src/__init__.py
@@ -0,0 +1,5 @@
+"""This module includes Normalizer and NormalizerBuilder"""
+from .normalizer import Normalizer
+from .normalizer_builder import NormalizerBuilder
+
+__all__ = ["Normalizer", "NormalizerBuilder"]
