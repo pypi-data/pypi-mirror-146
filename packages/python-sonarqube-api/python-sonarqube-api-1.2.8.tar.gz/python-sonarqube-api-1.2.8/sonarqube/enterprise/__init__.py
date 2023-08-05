15f6d9e3a995ff7b23fe1653955249f52977459f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:27 +0000

    Add package python-sonarqube-api/1.2.8/python-sonarqube-api-1.2.8.tar.gz

---
+++ packages/python-sonarqube-api/python-sonarqube-api-1.2.8.tar.gz/python-sonarqube-api-1.2.8/sonarqube/enterprise/__init__.py
@@ -0,0 +1,48 @@
+from sonarqube.community import SonarQubeClient
+from sonarqube.enterprise.applications import SonarQubeApplications
+from sonarqube.enterprise.audit_logs import SonarQubeAuditLogs
+# from sonarqube.enterprise.alm_settings import SonarQubeAlmSettings
+from sonarqube.enterprise.editions import SonarQubeEditions
+from sonarqube.enterprise.views import SonarQubeViews
+
+
+class SonarEnterpriseClient(SonarQubeClient):
+    """
+    A Python Client for SonarQube Enterprise Server APIs.
+    """
+    @property
+    def applications(self):
+        """
+        SonarQube applications Operations
+
+        :return:
+        """
+        return SonarQubeApplications(api=self)
+
+    # @property
+    # def alm_settings(self):
+    #     """
+    #     ALM settings
+    #
+    #     """
+    #     return SonarQubeAlmSettings(api=self)
+
+    @property
+    def audit_logs(self):
+        return SonarQubeAuditLogs(api=self)
+
+    @property
+    def editions(self):
+        """
+        Manage SonarSource commercial editions
+
+        """
+        return SonarQubeEditions(api=self)
+
+    @property
+    def views(self):
+        """
+        Manage Portfolios
+
+        """
+        return SonarQubeViews(api=self)
