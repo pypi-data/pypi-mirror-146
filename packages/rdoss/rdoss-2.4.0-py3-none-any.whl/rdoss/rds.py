caed70592c6bffd906aa4555d075b0799afe52bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:54 +0000

    Add package rdoss/2.4.0/rdoss-2.4.0-py3-none-any.whl

---
+++ packages/rdoss/rdoss-2.4.0-py3-none-any.whl/rdoss/rds.py
@@ -0,0 +1,20 @@
+from .main import RdFile
+from .main import RdBucket
+from ._config_ import secret_id
+from ._config_ import secret_key
+class Bucket(RdBucket):
+    def __init__(self,regionName = 'ap-shanghai',bucketName='zhengjia-1251945645'):
+        self.secretId = secret_id
+        self.secretKey = secret_key
+        self.bucketName =bucketName
+        self.regionName =regionName
+        RdBucket.__init__(self,secretId=self.secretId,secretKey=self.secretKey,regionName=self.regionName,bucketName=self.bucketName)
+class File(RdFile):
+    def __init__(self,secretId,secretKey,key,bucketName='zhengjia-1251945645',regionName = 'ap-shanghai'):
+        self.secretId = secretId
+        self.secretKey = secretKey
+        self.regionName = regionName
+        self.bucketName = bucketName
+        self.key = key
+        RdFile.__init__(self,secretId=self.secretId,secretKey=self.secretKey,key=self.key,bucketName=self.bucketName,regionName=self.regionName)
+
