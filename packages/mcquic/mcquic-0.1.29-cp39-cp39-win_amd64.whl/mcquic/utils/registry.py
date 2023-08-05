018eeb497145b3241cbb5affff78731c03915441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:07 +0000

    Add package mcquic/0.1.29/mcquic-0.1.29-cp39-cp39-win_amd64.whl

---
+++ packages/mcquic/mcquic-0.1.29-cp39-cp39-win_amd64.whl/mcquic/utils/registry.py
@@ -0,0 +1,31 @@
+from typing import Type
+
+from vlutils.base import Registry
+
+from mcquic.baseClass import ValueTuner
+
+__all__ = [
+    "ModuleRegistry",
+    "ValueTunerRegistry",
+    "LrSchedulerRegistry",
+    "OptimizerRegistry",
+    "LossRegistry"
+]
+
+class ModuleRegistry(Registry[Type["torch.nn.Module"]]):
+    pass
+
+class ValueTunerRegistry(Registry[Type[ValueTuner]]):
+    pass
+
+class LrSchedulerRegistry(Registry[Type["torch.optim.lr_scheduler._LRScheduler"]]):
+    pass
+
+class OptimizerRegistry(Registry[Type["torch.optim.optimizer.Optimizer"]]):
+    pass
+
+class LossRegistry(Registry[Type["torch.nn.Module"]]):
+    pass
+
+class HookRegistry(Registry[Type["mcquic.train.hooks.Hook"]]):
+    pass
