f2a1b1c008e6a7b25c636987093b0d1ae01bc736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:24 +0000

    Add package scrapewiki/0.1.1b0/scrapewiki-0.1.1b0.tar.gz

---
+++ packages/scrapewiki/scrapewiki-0.1.1b0.tar.gz/scrapewiki-0.1.1b0/scrapewiki/errors.py
@@ -0,0 +1,35 @@
+__all__ = ["PageNotFound", "QueryNotMatched"]
+
+
+class PageNotFound(Exception):
+    """
+    Raised when no page for the given title was found.
+
+    Attributes:
+    -----------
+    title: str
+        The title of the page that was not found.
+    """
+    def __init__(self, title):
+        super().__init__()
+        self.title = title
+
+    def __str__(self):
+        return f"Page was not found: {self.title}"
+
+
+class QueryNotMatched(Exception):
+    """
+    Raised when the query did not match any pages.
+
+    Attributes:
+    -----------
+    query: str
+        The query that was not matched.
+    """
+    def __init__(self, query):
+        super().__init__()
+        self.query = query
+
+    def __str__(self):
+        return f"Query was not matched: {self.query}"
