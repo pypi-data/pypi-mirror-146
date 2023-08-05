80eb0577976e920e8456d8c440ae941ebd9180e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:15 +0000

    Add package playlist2podcast/0.4.3/playlist2podcast-0.4.3.tar.gz

---
+++ packages/playlist2podcast/playlist2podcast-0.4.3.tar.gz/playlist2podcast-0.4.3/src/playlist2podcast/__init__.py
@@ -0,0 +1,6 @@
+"""Package 'playlist2podcast' level definitions."""
+CODE_VERSION_MAJOR = 0  # Current major version of this code
+CODE_VERSION_MINOR = 4  # Current minor version of this code
+CODE_VERSION_PATCH = 3  # Current patch version of this code
+
+__version__ = f"{CODE_VERSION_MAJOR}.{CODE_VERSION_MINOR}.{CODE_VERSION_PATCH}"
