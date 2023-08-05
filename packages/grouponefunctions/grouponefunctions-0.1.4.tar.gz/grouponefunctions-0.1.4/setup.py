9925251310099eb39ee65423607341facc774d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:16 +0000

    Add package grouponefunctions/0.1.4/grouponefunctions-0.1.4.tar.gz

---
+++ packages/grouponefunctions/grouponefunctions-0.1.4.tar.gz/grouponefunctions-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['grouponefunctions']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.5.1', 'numpy==1.21.5', 'pandas>=1.4.2']
+
+setup_kwargs = {
+    'name': 'grouponefunctions',
+    'version': '0.1.4',
+    'description': 'A package containing the necessary functions to smoothly run the analysis in DSCI-310-Group-1',
+    'long_description': '# grouponefunctions\n\nA package containing the necessary functions to smoothly run the analysis in DSCI-310-Group-1\n\n## Installation\n\n```bash\n$ pip install grouponefunctions\n```\n\n## Usage\n\n- The package has three functions: splitxy, listfun, and plotsquaredata. \n\n- TODO\n\n## Contributing\n\nInterested in contributing? Check out the contributing guidelines. Please note that this project is released with a Code of Conduct. By contributing to this project, you agree to abide by its terms.\n\n## License\n\n`grouponefunctions` was created by Andres Zepeda Perez, Daniel Hou, Zizhen Guo, Timothy Zhou. It is licensed under the terms of the MIT license.\n\n## Credits\n\n`grouponefunctions` was created with [`cookiecutter`](https://cookiecutter.readthedocs.io/en/latest/) and the `py-pkgs-cookiecutter` [template](https://github.com/py-pkgs/py-pkgs-cookiecutter).\n',
+    'author': 'Andres Perez, Daniel Hou, Zizhen Guo, Timothy Zhou',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
