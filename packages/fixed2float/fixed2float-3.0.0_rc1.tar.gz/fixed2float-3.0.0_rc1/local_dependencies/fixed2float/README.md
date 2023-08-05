4cb57b5b4a7ae7e3bfa2410f0ab8915c081dbeba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:59 +0000

    Add package fixed2float/3.0.0rc1/fixed2float-3.0.0_rc1.tar.gz

---
+++ packages/fixed2float/fixed2float-3.0.0_rc1.tar.gz/fixed2float-3.0.0_rc1/local_dependencies/fixed2float/README.md
@@ -0,0 +1,38 @@
+[![Crates.io](https://img.shields.io/crates/v/fixed2float)](https://crates.io/crates/fixed2float)
+[![PyPI](https://img.shields.io/pypi/v/fixed2float)](https://pypi.org/project/fixed2float/)
+
+# fixed2float
+
+Simple utility for fixed point to real number conversions, using the VisSim (Fx*m.b*) and Q (Q*m.n*) [notations](https://en.wikipedia.org/wiki/Fixed-point_arithmetic#Notations).
+
+### Usage
+
+- as a dependency of your Rust library
+
+```sh
+cargo add fixed2float
+```
+
+which will automatically fetch the most recent version from the registry and update one line on Cargo.toml.
+
+- as a dependency of your Python library
+
+```sh
+pip install fixed2float
+```
+
+
+### Examples
+
+See [example.py](https://github.com/urbanij/fixed2float/blob/master/py-fixed2float/examples/example.py) (Python) or the [examples folder](https://github.com/urbanij/fixed2float/tree/master/examples) (Rust) or this online notebook:
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/urbanij/fixed2float/HEAD?labpath=py-fixed2float%2Fexamples%2Fnotebook.ipynb)
+
+<!-- ### Screencast -->
+
+<!-- [![asciicast](https://asciinema.org/a/463661.svg)](https://asciinema.org/a/463661) -->
+
+
+### Similar projects
+
+- [javascript's fixed2float](https://www.npmjs.com/package/fixed2float)
---
+++ packages/fixed2float/fixed2float-3.0.0_rc1.tar.gz/fixed2float-3.0.0_rc1/local_dependencies/fixed2float/src/fixed_point/fx_fx.rs
@@ -0,0 +1,130 @@
+use super::FixedPoint;
+use crate::{fixed_point::debug_print, mask, to_float, UInt};
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub struct Fx {
+    pub val: UInt,
+    pub m: i32,
+    pub b: i32,
+    pub is_exact: bool,
+}
+
+impl Fx {
+    pub fn new(val: UInt, m: i32, b: i32, is_exact: bool) -> Self {
+        if b < m {
+            panic!("Total num of bits must be larger than num of integer bits.")
+        }
+        Self {
+            val,
+            m,
+            b,
+            is_exact,
+        }
+    }
+
+    pub fn get_frac_bits(&self) -> i32 {
+        self.b - self.m
+    }
+}
+
+impl FixedPoint for Fx {
+    fn eval(&self) -> f64 {
+        to_float(self.val, self.b, self.m, self.b - self.m).unwrap()
+    }
+}
+
+impl std::ops::Shl<u32> for Fx {
+    type Output = Self;
+    fn shl(self, rhs: u32) -> Self::Output {
+        Self {
+            val: (self.val << rhs) & mask((self.b) as u32) as UInt,
+            m: self.m,
+            b: self.b,
+            is_exact: self.is_exact,
+        }
+    }
+}
+
+impl std::ops::Shr<u32> for Fx {
+    type Output = Self;
+    fn shr(self, rhs: u32) -> Self::Output {
+        // let val = (self.val >> rhs) & mask((self.b) as u32) as u64;
+        let val = match self.val.checked_shr(rhs) {
+            Some(v) => v & mask((self.b) as u32) as UInt,
+            None => 0,
+        };
+
+        Self {
+            val,
+            m: self.m,
+            b: self.b,
+            is_exact: self.is_exact,
+        }
+    }
+}
+
+impl std::ops::Add for Fx {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.m != rhs.m || self.b != rhs.b {
+            panic!("`m` and `n` field of each fx obj has to match.")
+        }
+        Self {
+            val: self.val + rhs.val,
+            m: self.m + 1,
+            b: self.b + 1,
+            is_exact: true,
+        }
+    }
+}
+
+impl std::ops::Sub for Fx {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        if rhs.eval() > self.eval() {
+            unimplemented!()
+        }
+        Self {
+            val: self.val - rhs.val,
+            m: self.m,
+            b: self.b,
+            is_exact: true,
+        }
+    }
+}
+
+impl std::ops::Mul for Fx {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            val: self.val * rhs.val,
+            m: self.m + rhs.m,
+            b: self.b + rhs.b,
+            is_exact: true,
+        }
+    }
+}
+
+impl std::fmt::Debug for Fx {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let ans = debug_print(self.val, self.m, self.b, self.is_exact);
+        write!(f, "{}", ans)
+    }
+}
+
+impl std::fmt::Display for Fx {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let ans;
+        ans = format!("Fx<{},{}>({})", self.m, self.b, self.val);
+        write!(f, "{}", ans)
+    }
+}
+
+#[allow(non_snake_case)]
+pub fn to_Fx(x: f64, m: i32, b: i32, round: bool) -> Result<Fx, String> {
+    let fx_q = crate::to_fixed(x, m, b - m, round);
+    match fx_q {
+        Ok(fx) => Ok(Fx::new(fx.val, fx.m, fx.m + fx.n, fx.is_exact)),
+        Err(e) => Err(e.to_string()),
+    }
+}
