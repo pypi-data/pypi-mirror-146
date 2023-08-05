edb2b408d60bd03707835b79e4f5720ef2efe9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package zohavi/0.1.18/zohavi-0.1.18.tar.gz

---
+++ packages/zohavi/zohavi-0.1.18.tar.gz/zohavi-0.1.18/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zohavi',
+ 'zohavi.zbase',
+ 'zohavi.zcelery',
+ 'zohavi.zcommon',
+ 'zohavi.zemailer',
+ 'zohavi.zmembers',
+ 'zohavi.zwebui']
+
+package_data = \
+{'': ['*'],
+ 'zohavi.zbase': ['templates/zbase/*'],
+ 'zohavi.zcelery': ['templates/_def/*'],
+ 'zohavi.zemailer': ['templates/zemail/*'],
+ 'zohavi.zmembers': ['templates/zmembers/*'],
+ 'zohavi.zwebui': ['static/zcss/*', 'static/zjs/*', 'static/zwc/*']}
+
+setup_kwargs = {
+    'name': 'zohavi',
+    'version': '0.1.18',
+    'description': 'Web widgets',
+    'long_description': '# Hello World 123456789',
+    'author': 'pub12',
+    'author_email': 'pubudu79@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
