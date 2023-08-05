8fbfad63d53ccf697dcf8a760f3831ef5a5c2fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:53 +0000

    Add package datafiles/1.3/datafiles-1.3.tar.gz

---
+++ packages/datafiles/datafiles-1.3.tar.gz/datafiles-1.3/datafiles/types.py
@@ -0,0 +1,21 @@
+import dataclasses
+from typing import Optional
+
+Trilean = Optional[bool]
+Missing = dataclasses._MISSING_TYPE
+
+
+class List(list):
+    """Patchable `list` type."""
+
+    @classmethod
+    def to_yaml(cls, representer, node):
+        return representer.represent_list(node)
+
+
+class Dict(dict):
+    """Patchable `dict` type."""
+
+    @classmethod
+    def to_yaml(cls, representer, node):
+        return representer.represent_dict(node)
