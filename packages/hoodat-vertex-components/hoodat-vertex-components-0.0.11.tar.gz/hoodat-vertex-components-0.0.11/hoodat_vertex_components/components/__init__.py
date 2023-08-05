f715b4f92f12332d1224391dede666d51b66107d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:05 +0000

    Add package hoodat-vertex-components/0.0.11/hoodat-vertex-components-0.0.11.tar.gz

---
+++ packages/hoodat-vertex-components/hoodat-vertex-components-0.0.11.tar.gz/hoodat-vertex-components-0.0.11/hoodat_vertex_components/components/__init__.py
@@ -0,0 +1,17 @@
+"""
+Hoodat components
+"""
+import os
+
+try:
+    from kfp.v2.components import load_component_from_file
+except ImportError:
+    from kfp.components import load_component_from_file
+
+__all__ = [
+    "AddPyOp",
+]
+
+AddPyOp = load_component_from_file(
+    os.path.join(os.path.dirname(__file__), "add_py/component.yaml")
+)
---
+++ packages/hoodat-vertex-components/hoodat-vertex-components-0.0.11.tar.gz/hoodat-vertex-components-0.0.11/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hoodat_vertex_components', 'hoodat_vertex_components.components']
+
+package_data = \
+{'': ['*'], 'hoodat_vertex_components.components': ['add_py/*']}
+
+install_requires = \
+['kfp>=1.8.12,<2.0.0']
+
+setup_kwargs = {
+    'name': 'hoodat-vertex-components',
+    'version': '0.0.11',
+    'description': 'Re-usable kfp components for hoodat',
+    'long_description': '# Hoodat Pipeline Components\n\nThis repository provides an SDK and a set of components that perform\ntasks in hoodat.\n\nIt is modelled after this repository of shared components for GCP:\nhttps://github.com/kubeflow/pipelines/tree/google-cloud-pipeline-components-1.0.1/components/google-cloud\n\n## To publish package to pypi:\n\n1. Update the version in `pyproject.toml`\n\n2. Run `poetry publish`:\n```\npoetry publish --build\n```\n',
+    'author': 'Eugene Brown',
+    'author_email': 'efbbrown@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
