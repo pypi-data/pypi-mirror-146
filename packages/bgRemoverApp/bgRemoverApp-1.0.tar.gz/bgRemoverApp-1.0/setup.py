53f652cfec486b8d48ba0c2e2bb647e562da4132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:47 +0000

    Add package bgRemoverApp/1.0/bgRemoverApp-1.0.tar.gz

---
+++ packages/bgRemoverApp/bgRemoverApp-1.0.tar.gz/bgRemoverApp-1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='bgRemoverApp',
+	version='1.0',
+	author='kos94ok',
+	author_email='mirmen999@mail.ru',
+	description='Background Remover App',
+	url='https://github.com/kos94ok/bgRemoverApp',
+	packages=['bgRemoverApp'],
+	classifiers=[
+		"Programming Language :: Python :: 3.9",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.9',
+)
