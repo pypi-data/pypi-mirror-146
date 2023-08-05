5a97f9d3fe2ab22df5270ecc4f3e84aee853b70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:59 +0000

    Add package PyPDF2/1.27.3/PyPDF2-1.27.3.tar.gz

---
+++ packages/PyPDF2/PyPDF2-1.27.3.tar.gz/PyPDF2-1.27.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import re
+
+
+VERSIONFILE = "PyPDF2/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
+
+setup(version=verstr)
