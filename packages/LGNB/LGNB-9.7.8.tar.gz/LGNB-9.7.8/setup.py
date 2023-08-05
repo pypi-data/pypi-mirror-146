f017deb6cbcc09e2118cc21462d540f6ba57b196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:31 +0000

    Add package LGNB/9.7.8/LGNB-9.7.8.tar.gz

---
+++ packages/LGNB/LGNB-9.7.8.tar.gz/LGNB-9.7.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='LGNB',
+    version='9.7.8',
+    author='wangcc',
+    author_email='705190651@qq.com',
+    url='https://oa.csc.com.cn',
+    description=r'data api',
+    packages=['BBData'],
+)
