ee6f7a5227277f6fb724859264a09d9e02548879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:22 +0000

    Add package donutLol/0.0.2/donutLol-0.0.2.tar.gz

---
+++ packages/donutLol/donutLol-0.0.2.tar.gz/donutLol-0.0.2/README.md
@@ -0,0 +1,14 @@
+# donut3d
+
+to run in code 
+
+```
+from donutLol import donut 
+donut()
+```
+
+to run in cmd / CLI
+
+```
+donut
+```
---
+++ packages/donutLol/donutLol-0.0.2.tar.gz/donutLol-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Prints a 3d donut'
+LONG_DESCRIPTION = 'A package that allows to print a 3d donut'
+
+# Setting up
+setup(
+    name="donutLol",
+    version=VERSION,
+    author="lazylion2",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'donut', 'donut3d'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
