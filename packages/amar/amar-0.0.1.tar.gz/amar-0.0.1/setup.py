291dcaffe1efcef4168b08188bb4b0c4990e00ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:55 +0000

    Add package amar/0.0.1/amar-0.0.1.tar.gz

---
+++ packages/amar/amar-0.0.1.tar.gz/amar-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='amar',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/dariubs/amar',
+    author='Dariush Abbasi',
+    author_email='poshtehani@gmail.com',
+    packages=[],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
