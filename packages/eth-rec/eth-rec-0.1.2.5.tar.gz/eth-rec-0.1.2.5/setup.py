08f860c12266d7939482dc3293fd346213399561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:07 +0000

    Add package eth-rec/0.1.2.5/eth-rec-0.1.2.5.tar.gz

---
+++ packages/eth-rec/eth-rec-0.1.2.5.tar.gz/eth-rec-0.1.2.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eth_rec']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['eth-rec = eth_rec.eth_rec:main']}
+
+setup_kwargs = {
+    'name': 'eth-rec',
+    'version': '0.1.2.5',
+    'description': 'Download ETH-Livestream',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
