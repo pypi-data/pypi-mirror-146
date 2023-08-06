1455919bd6a41444c7d64f6ac4df5fc01bcbcf33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:32 +0000

    Add package riptide-cpp/1.8.7/riptide_cpp-1.8.7.tar.gz

---
+++ packages/riptide-cpp/riptide_cpp-1.8.7.tar.gz/riptide_cpp-1.8.7/test/riptide_python_test/tests.cpp
@@ -0,0 +1,25 @@
+#include "riptide_python_test.h"
+
+#define BOOST_UT_DISABLE_MODULE
+#include "../ut/include/boost/ut.hpp"
+
+#include <array>
+
+using namespace boost::ut;
+using boost::ut::suite;
+
+namespace
+{
+    suite riptide_ops = []
+    {
+        "test_numpy_float"_test = [&]
+        {
+            PyObject * function_object = riptide_python_test::internal::get_named_function(riptide_module_p, "TestNumpy");
+            PyObject * retval = PyObject_CallFunction(function_object, "d", 3.142);
+
+            expect(function_object != nullptr);
+            expect(retval != nullptr);
+            expect(retval == Py_None);
+        };
+    };
+}
