50432e3300133e13462f5a24da1fdfbd68b1c1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:38 +0000

    Add package shared/0.0.13/shared-0.0.13-py3-none-any.whl

---
+++ packages/shared/shared-0.0.13-py3-none-any.whl/shared/__init__.py
@@ -0,0 +1,7 @@
+"""The Shared Data Interface"""
+from shared.document import Document
+from shared.dossier import Dossier
+from shared.database import Database
+
+
+__all__ = ["Document", "Dossier", "Database"]
