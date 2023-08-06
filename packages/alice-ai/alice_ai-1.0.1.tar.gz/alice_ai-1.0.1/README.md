0d0c16cf2519bea135d74260bc321044d90a4bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:20 +0000

    Add package alice-ai/1.0.1/alice_ai-1.0.1.tar.gz

---
+++ packages/alice-ai/alice_ai-1.0.1.tar.gz/alice_ai-1.0.1/README.md
@@ -0,0 +1,53 @@
+<div align="center">
+  <h2>Alice the Virtual Assistant</h2>
+  <img src="https://github.com/maestroal/alice/blob/main/alice.jpg" width="300"/>
+  <img src="https://img.shields.io/badge/-PYTHON-0000ff?style=flat-square&logo=Python&logoColor=white"/>
+
+</div><br>
+
+Created to train algorithms and logic in developing AI, and helping someone in practicing their pronunciation in English is the main goal in making this all.
+
+## Installing
+Need >=python3.5 and pip 20.0.2(iam use it)
+```markdown
+pip install alice-ai
+```
+<p>if something goes wrong: try to install cmake first with sudo apt install cmake</p>
+
+## Start the ALice AI with python file
+```python
+import alice_ai
+
+# this is default settings
+ENGINE = alice_ai.engine()
+ENGINE.start()
+
+# use this for advanced settings
+AI_NAME = "Alice" # name robot default, change this with your robot names
+MASTER_NAME = "Maestro Alvardo" # change with your name
+ENGINE = alice_ai.engine(
+  AI_NAME,
+  MASTER_NAME
+)
+ENGINE.start()
+```
+## Run Alice
+Remember on linux run without SuperUser just:
+```bash
+python3 alice.py
+```
+
+- try to speak with good vocabulary and try to order something
+- hello
+- how are you
+- search (something) # will search on google
+- open youtube # opening youtube
+- ask the time now
+- do you like (something) # ask alice
+- other will updates
+
+<p>Develop for the better because Alice is an open source project, make the version better because in this early version still have very little knowledge.</p>
+
+
+## @Developed by Maestro Alvardo
+<a href="https://www.buymeacoffee.com/maestroal" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a>
---
+++ packages/alice-ai/alice_ai-1.0.1.tar.gz/alice_ai-1.0.1/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# +++{ coding:utf-8 }++++
+
+from setuptools import setup, find_packages;
+import pathlib;
+
+here = pathlib.Path(__file__).parent.resolve();
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8');
+
+# Setup all required for Silico to Python Library
+setup(
+    name='alice_ai',
+    version='1.0.1',
+    description='Artificial Intelligence with python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://maestroal.github.io/alice/',
+    license='MIT License',
+    author=[
+        'A. Maestro Alvardo'
+    ],
+    author_email='maestroalvardo@gmail.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_packages(),
+    keywords='Alice Python, Alice AI',
+    python_requires='>=3, <4',
+    install_requires=[
+        'requests',
+        'playsound',
+        'gtts',
+        'SpeechRecognition',
+        'pyttsx3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'aliceai=alice_ai.console:startConsole',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/maestroal/alice/issues',
+        'Funding': 'https://donate.pypi.org',
+        'Source': 'https://github.com/maestroal/alice/',
+    },
+);
+
+# end line
