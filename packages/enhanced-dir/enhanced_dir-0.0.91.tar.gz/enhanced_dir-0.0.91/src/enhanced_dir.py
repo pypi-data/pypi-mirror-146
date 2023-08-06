9aca5f00429d009c25e842e39f6af3b6966e401a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:08 +0000

    Add package enhanced-dir/0.0.91/enhanced_dir-0.0.91.tar.gz

---
+++ packages/enhanced-dir/enhanced_dir-0.0.91.tar.gz/enhanced_dir-0.0.91/src/enhanced_dir.py
@@ -0,0 +1,57 @@
+def enhanced_dir(arg, categorize=True, show_types=False, checks=False):
+    from collections import defaultdict
+    if not categorize:
+        return dir(arg)
+    passed = defaultdict(lambda: defaultdict(set))
+    failed = defaultdict(set)
+    passed_ = defaultdict(lambda: defaultdict(set))
+    failed_ = defaultdict(lambda: defaultdict(set))
+    x = arg
+    for method in dir(arg):
+        type_ = type(eval(f'x.{method}'))
+        try:
+            qualname = eval(f'x.{method}.__qualname__')
+            qualname = qualname.split('.')
+            passed[f'{arg}'][qualname[0]].add(qualname[1])
+            passed_[f'{arg}'][type_].add(qualname[1])
+
+        except:
+            failed[f'{arg}'].add(method)
+            failed_[f'{arg}'][type_].add(method)
+
+    if checks:
+        checks_ = {}
+        try:
+            class A(x):
+                pass
+
+            checks_['inheritable'] = True
+        except:
+            checks_['inheritable'] = False
+
+        try:
+            a = defaultdict(arg)
+            checks_['defaultdict_arg'] = True
+        except:
+            checks_['defaultdict_arg'] = False
+
+        try:
+            d = {arg: 1}
+            checks_['dict_key'] = True
+        except:
+            checks_['dict_key'] = False
+
+        try:
+            for i in arg:
+                pass
+            checks_['iterable'] = True
+        except:
+            checks_['iterable'] = False
+    if show_types and checks:
+        return [[passed], [passed_], [checks_]]
+    elif show_types == False and checks == True:
+        return [[passed], [checks_]]
+    elif show_types == True and checks == False:
+        return [[passed], [passed_]]
+
+    return passed
