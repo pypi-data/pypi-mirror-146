925c02512c57a8a5449c3b450a2d269f657c6937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:46 +0000

    Add package lyypdf/1.2/lyypdf-1.2.tar.gz

---
+++ packages/lyypdf/lyypdf-1.2.tar.gz/lyypdf-1.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="lyypdf",  # unique，不能与pypi存储库中的包重名
+    version=1.2,
+    long_description=Path("README.md").read_text(),
+    # 告诉 what packages are going to be 分发distributed
+    # 因为在这个项目中，目前有一个package：lyypdf
+    # lyypdf 包中两个modules：pdf2image, pdf2text
+
+    # 查找项目并自动发现我们定义了的packages，但是需要告诉他派出两个目录test和data【setuptools.find_packages([排除字符串])】
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
