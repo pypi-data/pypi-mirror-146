3fc608c5b62058ef25d70ee97b686d93cc61e030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:26 +0000

    Add package slackblocks/0.2.5/slackblocks-0.2.5-py3-none-any.whl

---
+++ packages/slackblocks/slackblocks-0.2.5-py3-none-any.whl/slackblocks/__init__.py
@@ -0,0 +1,8 @@
+from .attachments import Attachment, Color, Field
+from .blocks import ActionsBlock, ContextBlock, DividerBlock, FileBlock, ImageBlock, SectionBlock,\
+    HeaderBlock
+from .elements import Button, Confirm, Element, Image, Text, TextType
+from .messages import MessageResponse, Message
+from .modals import Modal
+
+name = "slackblocks"
