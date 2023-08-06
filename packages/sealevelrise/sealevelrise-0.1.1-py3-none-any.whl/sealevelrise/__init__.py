25195c7c5c886ad62f9f02244908a7623dc37ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package sealevelrise/0.1.1/sealevelrise-0.1.1-py3-none-any.whl

---
+++ packages/sealevelrise/sealevelrise-0.1.1-py3-none-any.whl/sealevelrise/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.1"
+from sealevelrise.scenario import Scenario
+from sealevelrise.slrprojections import SLRProjections
+from sealevelrise.data import Data
+from sealevelrise.historical import HistoricalSLR
