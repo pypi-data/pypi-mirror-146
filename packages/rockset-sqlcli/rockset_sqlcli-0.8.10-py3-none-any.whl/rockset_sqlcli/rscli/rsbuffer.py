45a9d1681904225f152b759f55a52554832c2289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:36 +0000

    Add package rockset-sqlcli/0.8.10/rockset_sqlcli-0.8.10-py3-none-any.whl

---
+++ packages/rockset-sqlcli/rockset_sqlcli-0.8.10-py3-none-any.whl/rockset_sqlcli/rscli/rsbuffer.py
@@ -0,0 +1,45 @@
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.filters import Condition
+from .packages.parseutils.utils import is_open_quote
+
+
+class RSBuffer(Buffer):
+    def __init__(self, always_multiline, multiline_mode, *args, **kwargs):
+        self.always_multiline = always_multiline
+        self.multiline_mode = multiline_mode
+
+        @Condition
+        def is_multiline():
+            doc = self.document
+            if not self.always_multiline:
+                return False
+            if self.multiline_mode == "safe":
+                return True
+            else:
+                return not _multiline_exception(doc.text)
+
+        super(self.__class__, self).__init__(
+            *args, is_multiline=is_multiline, tempfile_suffix=".sql", **kwargs
+        )
+
+
+def _is_complete(sql):
+    # A complete command is an sql statement that ends with a semicolon, unless
+    # there's an open quote surrounding it, as is common when writing a
+    # CREATE FUNCTION command
+    return sql.endswith(";") and not is_open_quote(sql)
+
+
+def _multiline_exception(text):
+    text = text.strip()
+    return (
+        text.startswith("\\")
+        or text.endswith(r"\e")  # Special Command
+        or _is_complete(text)  # Ended with \e which should launch the editor
+        or (text == "exit")  # A complete SQL command
+        or (text == "quit")  # Exit doesn't need semi-colon
+        or (text == ":q")  # Quit doesn't need semi-colon
+        or (  # To all the vim fans out there
+            text == ""
+        )  # Just a plain enter without any text
+    )
