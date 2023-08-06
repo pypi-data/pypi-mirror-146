14161640c20a778e3e1c0972a01d7a78bbd74a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:46 +0000

    Add package wr-cloner/0.0.1a0/wr-cloner-0.0.1a0.tar.gz

---
+++ packages/wr-cloner/wr-cloner-0.0.1a0.tar.gz/wr-cloner-0.0.1a0/cloner/cloner_process.py
@@ -0,0 +1,23 @@
+import os
+from multiprocessing import Process
+
+from cloner.repository import Repository
+
+
+# todo - needs tests
+class ClonerProcess(Process):
+    """Class to clone a list of given repositories when started."""
+
+    def __init__(self, repos_to_clone: list[Repository], process_id: int):
+        Process.__init__(self)
+        self.repos_list = repos_to_clone
+        self.process_id = process_id
+
+    def run(self):
+        """Clones each repo given in the constructor."""
+        for repo in self.repos_list:
+            # git options? --depth 1  ???
+            # todo -> path as an option?
+            # https://pypi.org/project/multiprocessing-logging/ not in windows
+            # logging.debug(f"Process {self.process_id} cloning {repo}")
+            os.system(f"git clone {repo.clone_url} --quiet ./repos/{repo.name}")
