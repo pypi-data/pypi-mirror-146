aa269f2bbeeb53a2ce27b7b949b547dc92da24d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:42 +0000

    Add package citelang/0.0.23/citelang-0.0.23.tar.gz

---
+++ packages/citelang/citelang-0.0.23.tar.gz/citelang-0.0.23/citelang/main/packages/ruby_gem.py
@@ -0,0 +1,71 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2022, Vanessa Sochat"
+__license__ = "MPL 2.0"
+
+from .base import PackagesFromFile
+
+
+class GemfileManager(PackagesFromFile):
+    """
+    Packages from a ruby Gemfile
+    """
+
+    name = "Gemfile"
+    underlying_manager = "rubygems"
+    default_language = "ruby"
+    project_count = None
+    homepage = "https://rubygems.org/"
+    color = "#e9573f"
+    default_versions = None
+
+    def parse(self, content):
+        """
+        Parse the self.content (the Gemfile)
+        """
+        repo = self.get_repo()
+        libs = []
+
+        for line in content.split("\n"):
+            if "gem" in line and "source" not in line:
+                for sub in ["gem", "'", '"', ","]:
+                    line = line.replace(sub, "").strip()
+                # Do we have a version?
+                if "~>" in line:
+                    line, version = line.split("~>", 1)
+                    line = "%s@%s" % (line.strip(), version.strip())
+                libs.append(line)
+
+        deps = []
+        for package_name in libs:
+            version = None
+            if "@" in package_name:
+                package_name, version = package_name.split("@", 1)
+
+            pkg = self.get_package(package_name, version)
+
+            # Ensure we have version, fallback to latest
+            if not version:
+                version = pkg.data["latest_release_number"]
+
+            # Require saving to cache here - many expensive calls
+            cache_name = f"package/rubygems/{package_name}/{version}"
+            self.cache.set(cache_name, pkg)
+
+            # use latest release version. This will be wrong for an old
+            # dependency, but it's not worth it to make a ton of extra API calls
+            dep = {
+                "name": package_name,
+                "project_name": package_name,
+                "number": version,
+                "published_at": pkg.data["latest_stable_release_published_at"],
+                "researched_at": None,
+                "spdx_expression": "NOASSERTION",
+                "original_license": pkg.data["licenses"],
+                "repository_sources": ["Rubygems"],
+            }
+            deps.append(dep)
+
+        repo["dependencies"] = deps
+        self.data["package"] = repo
+        self.data["dependencies"] = deps
+        return repo
