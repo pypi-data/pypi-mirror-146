767b0e96ef380814980c725bd85814fb72ff9b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:44 +0000

    Add package function-cooldowns/0.0.0/function_cooldowns-0.0.0-py3-none-any.whl

---
+++ packages/function-cooldowns/function_cooldowns-0.0.0-py3-none-any.whl/cooldowns/__init__.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+
+from .buckets import CooldownBucket
+from .protocols import CooldownBucketProtocol
+from .cooldown import Cooldown, cooldown, CooldownTimesPer
+
+__all__ = (
+    "CooldownBucket",
+    "Cooldown",
+    "cooldown",
+    "CooldownTimesPer",
+    "CooldownBucketProtocol",
+)
+__version__ = "0.1.0"
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="final", serial=0)
