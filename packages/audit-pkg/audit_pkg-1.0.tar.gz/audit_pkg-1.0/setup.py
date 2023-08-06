7622e7b7687b03091fca9a6db1c3d323c686b9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:26 +0000

    Add package audit-pkg/1.0/audit_pkg-1.0.tar.gz

---
+++ packages/audit-pkg/audit_pkg-1.0.tar.gz/audit_pkg-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='audit_pkg',
+      version='1.0',
+      description='Package auditors distributions',
+      packages=['audit_pkg'],
+      author_email='fgantua32@gmail.com',
+      zip_safe=False)
