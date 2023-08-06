8fe2f076851f205a7a421d6d900d73891bc88039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:09 +0000

    Add package q5/0.0.4/q5-0.0.4.tar.gz

---
+++ packages/q5/q5-0.0.4.tar.gz/q5-0.0.4/q5/__init__.py
@@ -0,0 +1,7 @@
+from .engine import *  #noqa F403
+from .app import BaseApp, Vector
+
+
+__version__ = '0.0.4'
+__author__ = 'eqs'
+__all__ = [BaseApp, Vector]
---
+++ packages/q5/q5-0.0.4.tar.gz/q5-0.0.4/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from setuptools_rust import Binding, RustExtension
+
+
+setup(
+    name='q5',
+    version='0.0.4',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    rust_extensions=[
+        RustExtension(
+            'q5.engine',
+            path='Cargo.toml',
+            binding=Binding.PyO3,
+            debug=False,
+        )
+    ],
+)
