291b29f79406c0cdd92f4d8ef5ccb2ca130f9525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:58 +0000

    Add package minidata/2022.4.11.16.56.24/minidata-2022.4.11.16.56.24.tar.gz

---
+++ packages/minidata/minidata-2022.4.11.16.56.24.tar.gz/minidata-2022.4.11.16.56.24/minidata/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for minidata."""
+import time
+
+__author__ = """minidata"""
+__email__ = 'yuanjie@xiaomi.com'
+__version__ = time.strftime("%Y.%m.%d.%H.%M.%S", time.localtime())
