807a1a3b66f727f655dfda91456edbd71be1a02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:44 +0000

    Add package dimfred/0.13.0/dimfred-0.13.0-py3-none-any.whl

---
+++ packages/dimfred/dimfred-0.13.0-py3-none-any.whl/dimfred/psutil_ext.py
@@ -0,0 +1,12 @@
+import psutil
+
+def find(process_name):
+    for p in psutil.process_iter():
+        try:
+            if process_name == p.name():
+                return p
+        except:
+            pass
+
+setattr(psutil, "find", find)
+
