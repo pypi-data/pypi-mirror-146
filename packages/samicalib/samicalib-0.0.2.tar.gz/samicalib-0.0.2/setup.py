38456372878ad844489a77f13e605135950a9106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:46 +0000

    Add package samicalib/0.0.2/samicalib-0.0.2.tar.gz

---
+++ packages/samicalib/samicalib-0.0.2.tar.gz/samicalib-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: POSIX :: Linux',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='samicalib',
+  version='0.0.2',
+  description='gets you a samica',
+  long_description=open('README.txt').read(),
+  url='',  
+  author='Antek Meco',
+  author_email='nik000@protonmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='samica', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
