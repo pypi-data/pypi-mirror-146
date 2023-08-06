e4fe19e767e6c1d75b79b0910fd6c060ef2cecec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:30 +0000

    Add package npdf/1.0/npdf-1.0.tar.gz

---
+++ packages/npdf/npdf-1.0.tar.gz/npdf-1.0/setup.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="npdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["data", "tests"])
+)
