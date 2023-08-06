f67c83a23860bec87fd62c446805661514d9d462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:06 +0000

    Add package kabbes-parent-class/0.2.0/kabbes_parent_class-0.2.0.tar.gz

---
+++ packages/kabbes-parent-class/kabbes_parent_class-0.2.0.tar.gz/kabbes_parent_class-0.2.0/README.md
@@ -0,0 +1,4 @@
+[Documentation](https://jameskabbes.github.io/parent_class)
+
+# parent_class
+Parent Class for generic Python use
---
+++ packages/kabbes-parent-class/kabbes_parent_class-0.2.0.tar.gz/kabbes_parent_class-0.2.0/src/parent_class/__main__.py
@@ -0,0 +1,8 @@
+import sys
+sys_args = sys.argv[1:]
+
+def run( *sys_args ):
+    print ('Running main for parent_class')
+
+run( *sys_args )
+
