e4df712c9e63a0d2f71fdb16924dccf12bea5ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:05 +0000

    Add package internxt-data-test/0.2.14/internxt-data-test-0.2.14.tar.gz

---
+++ packages/internxt-data-test/internxt-data-test-0.2.14.tar.gz/internxt-data-test-0.2.14/internxt_data_test/__init__.py
@@ -0,0 +1,4 @@
+from .StripeAdapter import StripeAdapter
+from .BigQuery import BigQuery
+
+__version__ = '0.2.0'
---
+++ packages/internxt-data-test/internxt-data-test-0.2.14.tar.gz/internxt-data-test-0.2.14/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['internxt_data_test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-cloud-bigquery>=2.34.2,<3.0.0',
+ 'pandas>=1.4.2,<2.0.0',
+ 'pyarrow>=7.0.0,<8.0.0',
+ 'stripe>=2.68.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'internxt-data-test',
+    'version': '0.2.14',
+    'description': '',
+    'long_description': None,
+    'author': 'Joan Mora',
+    'author_email': 'elcorreudejoanmora@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.0,<3.11',
+}
+
+
+setup(**setup_kwargs)
