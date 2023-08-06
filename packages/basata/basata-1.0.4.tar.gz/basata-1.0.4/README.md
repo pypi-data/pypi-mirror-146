23815765c89d037ad6c61a3c805e193624d059c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:43 +0000

    Add package basata/1.0.4/basata-1.0.4.tar.gz

---
+++ packages/basata/basata-1.0.4.tar.gz/basata-1.0.4/README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <img width="491" alt="basata" src="https://github.com/sg-tarek/BASATA/blob/main/logo.png">
+</p>
+
+
+Installing
+============
+
+    pip install basata
+
+Usage
+=====
+
+    from basata import basata
+
+Upgrade
+=====
+
+    pip install --upgrade basata
---
+++ packages/basata/basata-1.0.4.tar.gz/basata-1.0.4/setup.py
@@ -0,0 +1,38 @@
+from xml.etree.ElementInclude import include
+from setuptools import setup, find_packages
+
+def readme():
+    with open("README.md", encoding="utf8") as f:
+        README = f.read()
+    return README
+
+setup(
+    name='basata',
+    version='1.0.4',
+    description='BASATA - Code less while gaining more',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author="Tarek K. Ghanoum",
+    author_email='ta.ghanoum@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9'
+    ],
+    packages=find_packages(include=['basata']),
+    url='https://github.com/sg-tarek/BASATA',
+    keywords='ML AI Supervised TimeSeries Unsupservised',
+    install_requires=[
+          'scikit-learn',
+          'catboost',
+          'xgboost',
+          'scikit-learn',
+          'seaborn',
+          'pandas',
+          'matplotlib',
+          'numpy',
+          'tqdm'
+      ],
+)
