a13363c552ebfa73376afc45dc89fc60f25a45a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:04 +0000

    Add package dio-img-processing/0.0.2/dio_img_processing-0.0.2.tar.gz

---
+++ packages/dio-img-processing/dio_img_processing-0.0.2.tar.gz/dio_img_processing-0.0.2/README.md
@@ -0,0 +1,29 @@
+# dio_img_processing
+
+Description. 
+The package dio_img_processing is used to:
+	Processing:	
+		- Histogram matching
+		- Structural similarity
+		- Resize image
+	Utils:
+		- Read image
+		- Save image
+		- Plot image
+		- Plot result
+		- Plot histogram
+
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install dio_img_processing
+
+```bash
+pip install dio_img_processing
+```
+
+## Author
+Josimar Gabriel R. de Araújo
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/dio-img-processing/dio_img_processing-0.0.2.tar.gz/dio_img_processing-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="dio_img_processing",
+    version="0.0.2",
+    author="Josimar Gabriel R. de Araújo",
+    author_email="jgr-araujo@protonmail.com",
+    description="Image processing tool.",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jgr-araujo/dio-img-processing",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
