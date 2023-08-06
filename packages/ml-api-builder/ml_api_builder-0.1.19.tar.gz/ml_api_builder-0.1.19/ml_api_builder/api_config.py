3048e34b27098e7e527225fd180b9d1ba85bb514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:29 +0000

    Add package ml-api-builder/0.1.19/ml_api_builder-0.1.19.tar.gz

---
+++ packages/ml-api-builder/ml_api_builder-0.1.19.tar.gz/ml_api_builder-0.1.19/ml_api_builder/api_config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass
+class ml_config:
+    make_request_url: str
+
+@dataclass
+class jenkins_config:
+    jenkins_job_name: str        
+    Jenkins_url: str
+    jenkins_user: str
+    jenkins_pwd: str
+    buildWithParameters: bool
+    jenkins_params: dict
+
