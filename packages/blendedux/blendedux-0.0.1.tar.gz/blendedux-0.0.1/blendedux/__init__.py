e48d27fc2ff6c68580e503fef617b957b2d42963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:13 +0000

    Add package blendedux/0.0.1/blendedux-0.0.1.tar.gz

---
+++ packages/blendedux/blendedux-0.0.1.tar.gz/blendedux-0.0.1/blendedux/__init__.py
@@ -0,0 +1,4 @@
+
+class HelloWorld (object):
+  def hello(self, name="World"):
+    return "Hello %s!" % name
---
+++ packages/blendedux/blendedux-0.0.1.tar.gz/blendedux-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'blendedux',
+  packages = ['blendedux'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'A cross-platform design theme framework.',
+  author = 'legutierr',
+  author_email = 'legutierr@cognam.com',
+  url = 'https://cognam.com',
+  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['theme', 'web design'],
+  #install_requires=[
+  #        'validators',
+  #        'beautifulsoup4',
+  #    ],
+  classifiers=[
+    # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    #'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
+
