fd9ad7f744da8dbc2995cbc702ac03f7a737ee73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:01 +0000

    Add package open-aea/1.7.0/open_aea-1.7.0-py3-none-manylinux2014_aarch64.whl

---
+++ packages/open-aea/open_aea-1.7.0-py3-none-manylinux2014_aarch64.whl/aea/cli/registry/settings.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021 Valory AG
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+"""Settings for operating Registry with CLI."""
+
+
+from typing import Dict, Tuple
+
+
+REGISTRY_CONFIG_KEY: str = "registry_config"
+
+REGISTRY_HTTP = "http"
+REGISTRY_IPFS = "ipfs"
+REGISTRY_TYPES: Tuple[str, str] = (REGISTRY_HTTP, REGISTRY_IPFS)
+
+REGISTRY_API_URL_KEY = "registry_api_url"
+# we ignore issue B105 because this is not an hard-coded authentication token,
+# but the name of the field in the configuration file.
+AUTH_TOKEN_KEY = "auth_token"  # nosec
+
+DEFAULT_REGISTRY_CONFIG: Dict = {
+    "default": REGISTRY_HTTP,
+    "settings": {
+        REGISTRY_HTTP: {REGISTRY_API_URL_KEY: None, AUTH_TOKEN_KEY: None},
+        REGISTRY_IPFS: {},
+    },
+}
