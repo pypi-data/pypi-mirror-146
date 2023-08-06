338e6345ef1da26ee77b78b296998548162dbf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package bda-service-template/0.0.13/bda-service-template-0.0.13.tar.gz

---
+++ packages/bda-service-template/bda-service-template-0.0.13.tar.gz/bda-service-template-0.0.13/bdaservicetemplate/spark_service.py
@@ -0,0 +1,27 @@
+from bdaserviceutils import get_args
+from pysparkutilities.spark_initializer import spark_initializer
+from pysparkutilities import ds_initializer
+from .generic_service import GenericService
+
+class SparkService(GenericService):
+    def __init__(self, additional_config=[]):
+        super().__init__()
+
+        self._args = get_args()
+        print(self._args)
+
+        #additional_config = additional_config.append(('spark.jars.packages', 'io.prestosql:presto-jdbc:350'))
+        self.spark = spark_initializer("Test-model", self._args, additional_config=additional_config)
+
+    def get_dataset(self):
+        data = ds_initializer.load_dataset(sc=self.spark, read_all=False)
+        return data
+
+    def save_dataset(self, dataset):
+        ds_initializer.save_dataset(sc=self.spark, df=dataset, output_dest=self._args['output-dataset'])
+
+    def download_model(self, path):
+        return get_args()['hdfsUrl'] + get_args()['input-model']
+
+    def upload_model(self, path):
+        pass
