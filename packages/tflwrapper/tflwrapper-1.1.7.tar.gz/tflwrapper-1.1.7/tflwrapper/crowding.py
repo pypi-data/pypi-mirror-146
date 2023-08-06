3b30a859452d3ece0d82b6eaa9d83bd759cb880e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:01 +0000

    Add package tflwrapper/1.1.7/tflwrapper-1.1.7.tar.gz

---
+++ packages/tflwrapper/tflwrapper-1.1.7.tar.gz/tflwrapper-1.1.7/tflwrapper/crowding.py
@@ -0,0 +1,33 @@
+from .tfl import tflAPI
+
+
+class crowding(tflAPI):
+    """Crowding from Unified API"""
+
+    def getAllByNaptan(self, id):
+        """
+        Get crowding information for Naptan
+        :param id: naptanID ID of the stop (eg. 940GZZLUASL)
+        """
+        return super(crowding, self).sendRequestUnified(
+            f"/Crowding/{id}", {}
+        )
+
+    def getByNaptanDay(self, id, day):
+        """
+        Get crowding information for Naptan for a specified day of week
+        :param id: naptanID ID of the stop (eg. 940GZZLUASL)
+        :param day: The day of which you would like data to return (eg. MON, TUE)
+        """
+        return super(crowding, self).sendRequestUnified(
+            f"/Crowding/{id}/{day}", {}
+        )
+
+    def getLiveByNaptan(self, id):
+        """
+        Get crowding information for Naptan for a specified day of week
+        :param id: naptanID ID of the stop (eg. 940GZZLUASL)
+        """
+        return super(crowding, self).sendRequestUnified(
+            f"/Crowding/{id}/Live", {}
+        )
