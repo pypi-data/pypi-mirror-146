d812942b9fdf043a7d874a69be6345931379d50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:30 +0000

    Add package hermessplitter/0.3.7/hermessplitter-0.3.7-py3-none-any.whl

---
+++ packages/hermessplitter/hermessplitter-0.3.7-py3-none-any.whl/hermessplitter/fast_api/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Query
+from hermessplitter.db import db_funcs
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get('/get_hermes_records')
+def get_hermes_records():
+    return db_funcs.get_records()
+
+
+@app.get('/get_hermes_record')
+def get_hermes_record(record_id: int = Query(...,
+                                             description='ID записи из wdb')):
+    return db_funcs.get_record(record_id=record_id)
+
+def run_uvicorn():
+    uvicorn.run("hermessplitter.fast_api.main:app", host="0.0.0.0", port=8001)
+
+
+if __name__ == '__main__':
+    run_uvicorn()
