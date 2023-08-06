dde8e6eb1a3c4821b7f521ad5786027563621841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:46 +0000

    Add package dbnd-airflow-monitor/0.65.8/dbnd-airflow-monitor-0.65.8.tar.gz

---
+++ packages/dbnd-airflow-monitor/dbnd-airflow-monitor-0.65.8.tar.gz/dbnd-airflow-monitor-0.65.8/src/airflow_monitor/shared/runners/__init__.py
@@ -0,0 +1,6 @@
+from airflow_monitor.shared.runners.base_runner import BaseRunner
+from airflow_monitor.shared.runners.multi_process_runner import MultiProcessRunner
+from airflow_monitor.shared.runners.sequential_runner import SequentialRunner
+
+
+RUNNER_FACTORY = {"seq": SequentialRunner, "mp": MultiProcessRunner}
