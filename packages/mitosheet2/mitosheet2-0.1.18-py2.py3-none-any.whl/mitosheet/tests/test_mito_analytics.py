09a6d0a0e84ba68f6ec156e2fa78a285b2a683c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:12 +0000

    Add package mitosheet2/0.1.18/mitosheet2-0.1.18-py2.py3-none-any.whl

---
+++ packages/mitosheet2/mitosheet2-0.1.18-py2.py3-none-any.whl/mitosheet/tests/test_mito_analytics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Saga Inc.
+# Distributed under the terms of the GPL License.
+"""
+Contains tests to make sure that the mito analytics test is
+performing correctly
+"""
+
+# TODO: add tests for anonymization
