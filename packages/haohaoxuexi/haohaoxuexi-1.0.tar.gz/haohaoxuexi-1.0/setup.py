1de1c34623c1c5b95e6954ba798740d23488dde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:28 +0000

    Add package haohaoxuexi/1.0/haohaoxuexi-1.0.tar.gz

---
+++ packages/haohaoxuexi/haohaoxuexi-1.0.tar.gz/haohaoxuexi-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='haohaoxuexi',    #模块名称
+    version='1.0',    #版本号
+    description='这是第一个版本，仅用于测试',     #描述
+    author='zqd',
+    author_email='',
+    py_modules=['haohaoxuexi.demo1','haohaoxuexi.demo2']    #要发布的模块
+    
+)
