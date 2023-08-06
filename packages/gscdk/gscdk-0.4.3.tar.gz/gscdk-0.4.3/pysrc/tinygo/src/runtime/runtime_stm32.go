df275e3ebffdd6d9c561f59fb33b25907d4afaf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:56 +0000

    Add package gscdk/0.4.3/gscdk-0.4.3.tar.gz

---
+++ packages/gscdk/gscdk-0.4.3.tar.gz/gscdk-0.4.3/pysrc/tinygo/src/runtime/runtime_stm32.go
@@ -0,0 +1,18 @@
+// +build stm32
+
+package runtime
+
+import "device/arm"
+
+type timeUnit int64
+
+//export Reset_Handler
+func main() {
+	preinit()
+	run()
+	exit(0)
+}
+
+func waitForEvents() {
+	arm.Asm("wfe")
+}
