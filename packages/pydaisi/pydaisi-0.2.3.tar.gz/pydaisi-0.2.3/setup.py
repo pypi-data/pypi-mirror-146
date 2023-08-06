8c7b69807d861b85d100de42e1dd583dc7ed42f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:06 +0000

    Add package pydaisi/0.2.3/pydaisi-0.2.3.tar.gz

---
+++ packages/pydaisi/pydaisi-0.2.3.tar.gz/pydaisi-0.2.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    version='0.2.3',
+    license='Apache License 2.0',
+    url='https://github.com/BelmontTechnology/PyDaisi',
+    keywords='Daisi SDK',
+    install_requires=[
+     'requests',
+     'python-dotenv',
+     'dill',
+     'rich'
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.8'
+)
