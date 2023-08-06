7118fb100cab50dd6186bbbba1dd638468f1583d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:00 +0000

    Add package ComplexExtension/1.0.1/ComplexExtension-1.0.1.tar.gz

---
+++ packages/ComplexExtension/ComplexExtension-1.0.1.tar.gz/ComplexExtension-1.0.1/README.md
@@ -0,0 +1,7 @@
+---ComplexExtension
+ |- ComplexExtension
+ |  |- __init__.py
+ |  |- complex_extension.py
+ |- setup.py
+ |- README.rst
+ |- LICENSE.txt
---
+++ packages/ComplexExtension/ComplexExtension-1.0.1.tar.gz/ComplexExtension-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ComplexExtension',
+    version='1.0.1',
+    packages=['ComplexExtension'],
+    url='https://github.com/vyacheslavparkhomenko/Complex-Extension.git',
+    license="MIT Lisence",
+    long_description = 'Library for working with complex numbers in polar form',
+    author='VVP',
+    author_email='vyacheslav.parkhomenko@gmail.com',
+    description='Extension for working with complex numeric'
+)
