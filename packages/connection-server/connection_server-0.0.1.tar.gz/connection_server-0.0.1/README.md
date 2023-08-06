008df1f613bdb83a794885daab117f5bb583ecd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:24 +0000

    Add package connection-server/0.0.1/connection_server-0.0.1.tar.gz

---
+++ packages/connection-server/connection_server-0.0.1.tar.gz/connection_server-0.0.1/README.md
@@ -0,0 +1,33 @@
+# connction_server
+
+Description. 
+The package connction_server is used to:
+	- 
+	-
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install connction_server
+
+```bash
+pip install connction_server
+```
+
+## Usage
+
+```python
+from connction_server import connect_tcp
+connect_tcp.createConnectionTCP()
+```
+
+
+```python
+from connction_server import connect_udp
+connect_udp.createConnectionUDP()
+```
+
+## Author
+Milena Limoeiro
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/connection-server/connection_server-0.0.1.tar.gz/connection_server-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+
+
+setup(
+    name="connection_server",
+    version="0.0.1",
+    author="milena_limoeiro",
+    author_email="elenprojects7@gmail.com",
+    description="My short description",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Konstructa/my-package-python",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8'
+)
