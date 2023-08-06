87a7ce367b5c9ef52c53fc2ced05f7d43dd5e755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:37 +0000

    Add package whizbang-deployer/1.3.0a94/whizbang_deployer-1.3.0a94-py3-none-any.whl

---
+++ packages/whizbang-deployer/whizbang_deployer-1.3.0a94-py3-none-any.whl/whizbang/domain/manager/pyodbc/sql_script_manager.py
@@ -0,0 +1,28 @@
+import abc
+import logging
+
+from whizbang.data.pyodbc.pyodbc_context_args import SqlServerContextArgs
+from whizbang.domain.manager.pyodbc.pyodbc_manager_base import PyodbcManager, IPyodbcManager
+from whizbang.domain.repository.sql_server.sql_script_repository import ISqlScriptRepository
+
+_log = logging.getLogger(__name__)
+
+class ISqlScriptManager(IPyodbcManager, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def execute_script(self, context_args: SqlServerContextArgs, sql_script: str):
+        """the execute_script interface"""
+
+
+class SqlScriptManager(PyodbcManager, ISqlScriptManager):
+    def __init__(self, repository: ISqlScriptRepository):
+        PyodbcManager.__init__(self, repository)
+        self._repository: ISqlScriptRepository
+
+    def execute_script(self, context_args: SqlServerContextArgs, sql_script: str):
+        return self._repository.execute_script(context_args, sql_script)
+
+    def execute_script_path(self, context_args, file_path):
+        file = open(file_path)
+        script = file.read()
+        file.close()
+        return self.execute_script(context_args, script)
