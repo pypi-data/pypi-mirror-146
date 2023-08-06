c7b02e4bd618cadad1371dd9ce4ddf5b0fdfd2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:47 +0000

    Add package gscdk/0.4.3/gscdk-0.4.3-py3-none-manylinux1_x86_64.whl

---
+++ packages/gscdk/gscdk-0.4.3-py3-none-manylinux1_x86_64.whl/gscdk/tinygo/src/os/file_other.go
@@ -0,0 +1,75 @@
+// +build baremetal wasm,!wasi,!eosio
+
+package os
+
+import (
+	_ "unsafe"
+)
+
+// Stdin, Stdout, and Stderr are open Files pointing to the standard input,
+// standard output, and standard error file descriptors.
+var (
+	Stdin  = &File{stdioFileHandle(0), "/dev/stdin"}
+	Stdout = &File{stdioFileHandle(1), "/dev/stdout"}
+	Stderr = &File{stdioFileHandle(2), "/dev/stderr"}
+)
+
+// isOS indicates whether we're running on a real operating system with
+// filesystem support.
+const isOS = false
+
+// stdioFileHandle represents one of stdin, stdout, or stderr depending on the
+// number. It implements the FileHandle interface.
+type stdioFileHandle uint8
+
+// Read is unsupported on this system.
+func (f stdioFileHandle) Read(b []byte) (n int, err error) {
+	return 0, ErrUnsupported
+}
+
+func (f stdioFileHandle) ReadAt(b []byte, off int64) (n int, err error) {
+	return 0, ErrNotImplemented
+}
+
+// Write writes len(b) bytes to the output. It returns the number of bytes
+// written or an error if this file is not stdout or stderr.
+func (f stdioFileHandle) Write(b []byte) (n int, err error) {
+	switch f {
+	case 1, 2: // stdout, stderr
+		for _, c := range b {
+			putchar(c)
+		}
+		return len(b), nil
+	default:
+		return 0, ErrUnsupported
+	}
+}
+
+// Close is unsupported on this system.
+func (f stdioFileHandle) Close() error {
+	return ErrUnsupported
+}
+
+// Seek wraps syscall.Seek.
+func (f stdioFileHandle) Seek(offset int64, whence int) (int64, error) {
+	return -1, ErrUnsupported
+}
+
+//go:linkname putchar runtime.putchar
+func putchar(c byte)
+
+func Pipe() (r *File, w *File, err error) {
+	return nil, nil, ErrNotImplemented
+}
+
+func (f *File) Seek(offset int64, whence int) (ret int64, err error) {
+	return 0, ErrNotImplemented
+}
+
+func Readlink(name string) (string, error) {
+	return "", ErrNotImplemented
+}
+
+func tempDir() string {
+	return "/tmp"
+}
---
+++ packages/gscdk/gscdk-0.4.3-py3-none-manylinux1_x86_64.whl/gscdk/tinygo/src/syscall/proc_emulated.go
@@ -0,0 +1,13 @@
+// +build baremetal wasi wasm eosio
+
+// This file emulates some process-related functions that are only available
+// under a real operating system.
+
+package syscall
+
+func Getuid() int  { return -1 }
+func Geteuid() int { return -1 }
+func Getgid() int  { return -1 }
+func Getegid() int { return -1 }
+func Getpid() int  { return -1 }
+func Getppid() int { return -1 }
