b14998fcd026560cee9f302cedcdaaf3f65fd167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:04 +0000

    Add package pre-workbench/0.4.9/pre_workbench-0.4.9.tar.gz

---
+++ packages/pre-workbench/pre_workbench-0.4.9.tar.gz/pre_workbench-0.4.9/tests/parse_helper.py
@@ -0,0 +1,15 @@
+
+from binascii import unhexlify
+
+from pre_workbench.structinfo.parsecontext import FormatInfoContainer, ParseContext
+
+log = False
+
+def parse_me(definition, hexstring, expected):
+	fic = FormatInfoContainer(load_from_string=definition)
+	pc = ParseContext(fic, unhexlify(hexstring.replace(" ","")), logging_enabled=log)
+	result = pc.parse()
+	if pc.failed:
+		raise pc.failed
+	assert result == expected
+
