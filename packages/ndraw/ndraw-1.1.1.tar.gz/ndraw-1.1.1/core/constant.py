135a4c4f291d0c7fce731ae43ea1545faa4fbb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:22 +0000

    Add package ndraw/1.1.1/ndraw-1.1.1.tar.gz

---
+++ packages/ndraw/ndraw-1.1.1.tar.gz/ndraw-1.1.1/core/constant.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Flow(Enum):
+    VERTICAL = "vertical"
+    HORIZONTAL = "horizontal"
+
+
+class Theme(Enum):
+    DEFAULT = "default"
+    BLACK_WHITE = "black_white"
+    GREEN_WHITE = "green_white"
+    LIGHTBLACK_WHITE = "lightblack_white"
+    DEEPGRAY_WHITE = "deepgray_white"
+    BLUE_WHITE = "blue_white"
+    PURPLE_WHITE = "purle_white"
