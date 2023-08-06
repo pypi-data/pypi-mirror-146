abb1dc64b8df4b1da9b8311fdb20b0339ee84628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:42 +0000

    Add package NeoMetaTracker/0.0.7/NeoMetaTracker-0.0.7.tar.gz

---
+++ packages/NeoMetaTracker/NeoMetaTracker-0.0.7.tar.gz/NeoMetaTracker-0.0.7/NeoMetaTracker/capture_point.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from typing import Dict, TYPE_CHECKING
+from NeoMetaTracker.graph_scheme import GraphSchema
+import time
+
+if TYPE_CHECKING:
+    from NeoMetaTracker.neo_meta_logger import NeoMetaTracker
+
+
+class CapturePoint:
+    """The state of a Neo4j database in terms of relationship and node count at a certain time"""
+
+    def __init__(
+        self,
+        parent_logger: NeoMetaTracker,
+        name: str,
+        labels_count: Dict[str, int],
+        relations_count: Dict[str, int],
+        neo4j_schema_vis_data: Dict,
+    ):
+        self.parent_logger = parent_logger
+        self.name = name
+        self.timestamp: float = time.time()
+        self.labels = labels_count
+        self.relations = relations_count
+        self.schema: GraphSchema = GraphSchema.from_neo4j_schema_vis_data(
+            neo4j_schema_vis_data,
+            parent_capture_point=self,
+            extra_props={"capture_name": name} if name else {},
+        )
+
+    def __hash__(self):
+        return hash(self.timestamp)
+
+    def __eq__(self, other: "CapturePoint") -> bool:
+        return type(other) is type(self) and self.timestamp == other.timestamp
---
+++ packages/NeoMetaTracker/NeoMetaTracker-0.0.7.tar.gz/NeoMetaTracker-0.0.7/NeoMetaTracker/neo_meta_logger.py
@@ -0,0 +1,211 @@
+from typing import Union, Dict, List, Generic
+import time
+import py2neo
+from pathlib import Path
+from NeoMetaTracker.capture_point import CapturePoint
+from NeoMetaTracker.graph_scheme import GraphSchema
+from NeoMetaTracker.visualizer._base_visualizer import BaseVisualizer
+
+
+class NeoMetaTracker:
+    def __init__(self, connection: Union[py2neo.Graph, Dict]):
+        self.capture_points: List[CapturePoint] = []
+        self.all_schema_nodes: Dict[tuple, py2neo.Node] = None
+        self.all_schema_rels: Dict[str, py2neo.Relationship] = None
+        if isinstance(connection, dict):
+            self.graph: py2neo.Graph = py2neo.Graph(**connection)
+        elif isinstance(connection, py2neo.Graph):
+            self.graph = connection
+        else:
+            raise TypeError(
+                f"Expected 'py2neo.Graph' or 'dict'. Got '{type(connection)}'"
+            )
+
+    def capture(self, name: str = None):
+        labels_count = self._get_labels_count()
+        relations_count = self._get_relations_count()
+
+        self.capture_points.append(
+            CapturePoint(
+                name=name,
+                parent_logger=self,
+                labels_count=labels_count,
+                relations_count=relations_count,
+                neo4j_schema_vis_data=self._get_neo4j_schema(),
+            )
+        )
+
+    def node_filter_func(self, node: py2neo.Node):
+        """This function can be overiden to filter specifics nodes out of the schema tracker
+
+        example; Remove all schema-nodes with label starting with "_":
+            mlog = NeoMetaTracker(test_graph)
+
+            def node_filter(node: py2neo.Node):
+                if list(node.labels)[0].startswith("_"):
+                    return None
+                return node
+
+            mlog.node_filter_func = node_filter
+
+        """
+        return node
+
+    def visualize(
+        self, visualizer_class: BaseVisualizer, to_file: Union[str, Path] = None
+    ):
+        schemas_changes = []
+        for index, to_cp in enumerate(self.capture_points):
+            if index < 1:
+                continue
+            from_cp = self.capture_points[index - 1]
+            changes = self.get_schemagraph_changes(
+                from_capture=from_cp, to_capture=to_cp
+            )
+            changes.parent_capture_point = to_cp
+            schemas_changes.append(changes)
+        visualizer: BaseVisualizer = visualizer_class(schemas_changes)
+        if to_file:
+            visualizer.generate_file(to_file)
+        else:
+            return visualizer.generate_object()
+
+    def _get_neo4j_schema(self):
+        # call db.schema.visualization
+        return self.graph.run(
+            "call db.schema.visualization yield nodes, relationships"
+        ).data()[0]
+
+    def _get_labels_count(self) -> Dict[str, int]:
+        all_labels: List[str] = self.graph.run(
+            "CALL db.labels() yield label return collect(label) as res"
+        ).data()[0]["res"]
+        labels_count: Dict[str, int] = {}
+        for label in all_labels:
+            query_label_count = f"""
+                    MATCH (n:{label})
+                    RETURN count(n) AS res
+                    """
+            labels_count[label] = self.graph.run(query_label_count).data()[0]["res"]
+        return labels_count
+
+    def _get_relations_count(self) -> Dict[str, int]:
+        all_rels: List[str] = self.graph.run(
+            "CALL db.relationshipTypes() yield relationshipType return collect(relationshipType) as res"
+        ).data()[0]["res"]
+        rels_count: Dict[str, int] = {}
+        for rel in all_rels:
+
+            query_rel_count = f"""
+                    MATCH ()-[:{rel}]->() 
+                    return count(*) AS res
+                    """
+            rels_count[rel] = self.graph.run(query_rel_count).data()[0]["res"]
+        return rels_count
+
+    def get_capture_point_by(
+        self,
+        capture_point_index: int = None,
+        capture_point_time: float = None,
+        capture_point_name: str = None,
+    ):
+        if capture_point_time:
+            return next(
+                cp for cp in self.capture_points if capture_point_time == cp.timestamp
+            )
+        elif capture_point_index:
+            return self.capture_points[capture_point_index]
+        elif capture_point_name:
+            return next(
+                cp for cp in self.capture_points if capture_point_name == cp.name
+            )
+
+    def get_schemagraph_last_changes(self) -> GraphSchema:
+        return self.get_schemagraph_changes_since(
+            self.get_capture_point_by(capture_point_index=-2)
+        )
+
+    def get_schemagraph_changes_since(
+        self,
+        capture_point: CapturePoint = None,
+    ) -> GraphSchema:
+        current_capture_point: CapturePoint = (
+            self.capture_points[-1] if self.capture_points else None
+        )
+        return self.get_schemagraph_changes(
+            from_capture=capture_point, to_capture=current_capture_point
+        )
+
+    def get_schemagraph_changes(
+        self, from_capture: CapturePoint, to_capture: CapturePoint
+    ) -> GraphSchema:
+        # hardcoded class name in subgraph. we need to workaround this. can be removed with https://github.com/py2neo-org/py2neo/issues/940 merged
+        # original line: return from_capture.schema - to_capture.schema
+        # workaround:
+        sb: py2neo.Subgraph = to_capture.schema - from_capture.schema
+        return GraphSchema(nodes=sb.nodes, relationships=sb.relationships)
+
+    def get_numeric_last_changes(self) -> Dict[str, Dict[str, int]]:
+        """Get changes, in terms of quantity, of labels and relations since the last capture compared to the current capture
+
+        Returns:
+            Dict[str,Dict[str,int]]: A base dictonary "{'labels':{...},'relation':{...}}" containing two dictoniries listing changes (in terms of quantity) for labels and relations.
+        """
+        return self.get_numeric_changes_since(
+            self.get_capture_point_by(capture_point_index=-2)
+        )
+
+    def get_numeric_changes_since(
+        self,
+        capture_point: CapturePoint = None,
+    ) -> Dict[str, Dict[str, int]]:
+        """Get changes, in terms of quantity, of labels and relations since the last capture compared to the current capture
+
+        Returns:
+            Dict[str,Dict[str,int]]: A base dictonary "{'labels':{...},'relation':{...}}" containing two dictoniries listing changes (in terms of quantity) for labels and relations.
+        """
+        current_capture_point: CapturePoint = (
+            self.capture_points[-1] if self.capture_points else None
+        )
+        return self.get_numeric_changes(
+            from_capture=capture_point, to_capture=current_capture_point
+        )
+
+    def get_numeric_changes(self, from_capture: CapturePoint, to_capture: CapturePoint):
+        if len(self.capture_points) <= 1:
+            raise ValueError(
+                f"You need at least 2 capture points to compare any changes. Got only {len(self.capture_points)} points"
+            )
+        # Compare labels
+        result: Dict = {"labels": {}, "relations": {}}
+        if to_capture.labels or from_capture.labels:
+            diff_labels = set(to_capture.labels.items()) ^ set(
+                from_capture.labels.items()
+            )
+            for diff_label in set([lbl[0] for lbl in diff_labels]):
+                result["labels"][diff_label] = (
+                    to_capture.labels[diff_label]
+                    if diff_label in to_capture.labels
+                    else 0
+                ) - (
+                    from_capture.labels[diff_label]
+                    if diff_label in from_capture.labels
+                    else 0
+                )
+        # Compare relations
+        if not to_capture.relations and not from_capture.relations:
+            return result
+        diff_relations = set(to_capture.relations.items()) ^ set(
+            from_capture.relations.items()
+        )
+        for diff_rel in set([lbl[0] for lbl in diff_relations]):
+            result["relations"][diff_rel] = (
+                to_capture.relations[diff_rel]
+                if diff_rel in to_capture.relations
+                else 0
+            ) - (
+                from_capture.relations[diff_rel]
+                if diff_rel in from_capture.relations
+                else 0
+            )
+        return result
