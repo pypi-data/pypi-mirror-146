13d48de89e3793a5b50ecdf59d4fcf830b9af3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:32 +0000

    Add package edgeP/0.0.1/edgeP-0.0.1.tar.gz

---
+++ packages/edgeP/edgeP-0.0.1.tar.gz/edgeP-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'edgeP',
+    version = '0.0.1',
+    keywords='GSEA',
+    description = '',
+    license = 'MIT License',
+    url = 'https://github.com/Gutier14/CAAFinder',
+    author = 'YISHEN WANG',
+    author_email = 'wangysh23@mail2.sysu.edu.cn',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
