ef3eee78058e76ca7db1ed89685dbc7b2c0840b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:31 +0000

    Add package pypots/0.0.2/pypots-0.0.2-py3-none-any.whl

---
+++ packages/pypots/pypots-0.0.2-py3-none-any.whl/pypots/tests/test_saits.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: GPL-v3
+
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
