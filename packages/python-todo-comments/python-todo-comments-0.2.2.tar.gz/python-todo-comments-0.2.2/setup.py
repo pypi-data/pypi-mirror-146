257e6590e1b1a94617de970afd724aa3b2c8203e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:53 +0000

    Add package python-todo-comments/0.2.2/python-todo-comments-0.2.2.tar.gz

---
+++ packages/python-todo-comments/python-todo-comments-0.2.2.tar.gz/python-todo-comments-0.2.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['python_todo_comments']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['py-todos = python_todo_comments.main:main']}
+
+setup_kwargs = {
+    'name': 'python-todo-comments',
+    'version': '0.2.2',
+    'description': "A cli that will search for and parse TODO comments in a directory's python modules.",
+    'long_description': None,
+    'author': 'Tylor Dodge',
+    'author_email': 'tdodge@nexamp.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
