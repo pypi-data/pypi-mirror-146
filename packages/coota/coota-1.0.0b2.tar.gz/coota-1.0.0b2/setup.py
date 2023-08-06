b6041b3fdfd57ffb7de9282f9db8e9bec4d42b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:34 +0000

    Add package coota/1.0.0b2/coota-1.0.0b2.tar.gz

---
+++ packages/coota/coota-1.0.0b2.tar.gz/coota-1.0.0b2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coota",
+    version="1.0.0-beta2",
+    author="ATATC",
+    author_email="futerry@outlook.com",
+    description="A powerful data-generating library.",
+    license='Apache License 2.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atatc/coota",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy", "matplotlib"],
+)
