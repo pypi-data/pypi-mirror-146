bede8b7ae5687841f5452e1e8206f2811da9284d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:17 +0000

    Add package tornado-jsonschema/0.1.0/tornado-jsonschema-0.1.0.tar.gz

---
+++ packages/tornado-jsonschema/tornado-jsonschema-0.1.0.tar.gz/tornado-jsonschema-0.1.0/README.md
@@ -0,0 +1,23 @@
+# tornado-jsonschema
+
+JSON Schema support for Tornado.
+
+## Getting started
+
+### Requirements
+
+- Python >= 3.8
+
+### Installation
+
+```bash
+pip install tornado-jsonschema
+```
+
+## Usage
+
+### TO-DO
+
+## License
+
+Distributed under the MIT License. See `LICENSE` file for more information.
---
+++ packages/tornado-jsonschema/tornado-jsonschema-0.1.0.tar.gz/tornado-jsonschema-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+version = '0.1.0'
+
+with open('requirements.txt') as f:
+    requirements = [
+        line.split('#', 1)[0].strip() for line in f.read().splitlines()
+        if not line.strip().startswith('#')
+    ]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tornado-jsonschema",
+    version=version,
+    author="webfucktory",
+    author_email="root@webfucktory.com",
+    description="JSON Schema support for Tornado",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/webfucktory/tornado-jsonschema",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires='>=3.8',
+    install_requires=requirements,
+)
