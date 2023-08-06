09d354085bd81a91abb46412ff10d161a514bbea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package jwt-email-auth/0.6.0/jwt_email_auth-0.6.0-py3-none-any.whl

---
+++ packages/jwt-email-auth/jwt_email_auth-0.6.0-py3-none-any.whl/jwt_email_auth/settings.py
@@ -0,0 +1,176 @@
+import logging
+from datetime import timedelta
+from inspect import cleandoc
+from pathlib import Path
+from typing import Any, Dict, List, Literal, NamedTuple, Optional, Set, Union
+
+from django.conf import settings
+from django.test.signals import setting_changed
+from settings_holder import SettingsHolder, reload_settings
+
+
+__all__ = [
+    "auth_settings",
+]
+
+
+logger = logging.getLogger(__name__)
+
+
+class JWTEmailAuthSettings(NamedTuple):
+    # Whether emails should be sent or not. When off,
+    # login code is logged instead (for development).
+    SENDING_ON: bool = False
+    #
+    # When True, any code will work in login
+    SKIP_CODE_CHECKS: bool = False
+    #
+    # "Dot import notation" to a function to run to load JWT signing key.
+    # Takes no arguments and returns the Ed25519PrivateKey object used to check the JWT signature.
+    # Default function loads an example key, DO NOT USE IT IN PRODUCTION!
+    SIGNING_KEY: str = "jwt_email_auth.utils.load_example_signing_key"
+    #
+    # How long an access token is valid for
+    ACCESS_TOKEN_LIFETIME: timedelta = timedelta(minutes=5)
+    #
+    # How long a refresh token is valid for
+    REFRESH_TOKEN_LIFETIME: timedelta = timedelta(days=14)
+    #
+    # How long a login code is stored in cache
+    LOGIN_CODE_LIFETIME: timedelta = timedelta(minutes=5)
+    #
+    # "Dot import notation" to a function to use for validating use from email.
+    # Takes a single argument "email" of type str and returns None.
+    # Default is no validation.
+    VALIDATION_CALLBACK: str = "jwt_email_auth.utils.login_validation"
+    #
+    # "Dot import notation" to a function to run to gather login data.
+    # Takes a single argument "email" and returns a Dict[str, Any], where Any can be cached in your cache backend.
+    # Default is no data.
+    LOGIN_DATA: str = "jwt_email_auth.utils.default_login_data"
+    #
+    # "Dot import notation" to a function to generate a login code.
+    # Takes no arguments and returns a string.
+    # Default is a function that returns a 6-digit string.
+    CODE_GENERATOR: str = "jwt_email_auth.utils.random_code"
+    #
+    # Email sender. Default is settings.DEFAULT_FROM_EMAIL
+    LOGIN_SENDING_EMAIL: Optional[str] = None
+    #
+    # Email subject line
+    LOGIN_SUBJECT_LINE: str = "Login to Django"
+    #
+    # Message to send in email. Must have {code} and {valid}!
+    LOGIN_EMAIL_MESSAGE: str = cleandoc(
+        """
+            Your login code:
+
+            {code}
+
+            This code is valid for the next {valid} minutes.
+        """
+    )
+    #
+    # Path to html_message template. Context must have {{ code }} and {{ valid }}!
+    LOGIN_EMAIL_HTML_TEMPLATE: Optional[Path] = None
+    #
+    # Encoding and decoding options:
+    #
+    # Issuer of the JWT
+    ISSUER: Optional[str] = None
+    #
+    # Intended recipient of the JWT
+    AUDIENCE: Optional[str] = None
+    #
+    # A time margin in seconds for the expiration check
+    LEEWAY: int = 0
+    #
+    # Algorithm to sign and decrypt the token with
+    ALGORITHM: str = "EdDSA"
+    #
+    # Authorization scheme used in Authorization header, as in `HEADER_PREFIX token`
+    HEADER_PREFIX: str = "Bearer"
+    #
+    # Additional JWT header fields
+    EXTRA_HEADERS: Optional[Dict[str, str]] = None
+    #
+    # List of expected JWT content
+    EXPECTED_CLAIMS: List[str] = []
+    #
+    # IP address spoofing prevention settings:
+    # https://github.com/un33k/django-ipware/blob/master/README.md#advanced-users
+    #
+    # Indicate whether the originating client is on the right or left in the X-Forwarded-For header
+    PROXY_ORDER: Literal["left-most", "right-most"] = "left-most"
+    #
+    # Number of proxies between the server and internet
+    PROXY_COUNT: Optional[int] = None
+    #
+    # Only these proxy IPs are allowed connections
+    PROXY_TRUSTED_IPS: Optional[List[str]] = None
+    #
+    # Meta precedence order
+    REQUEST_HEADER_ORDER: Optional[List[str]] = None
+    #
+    # Cache prefix for login codes and banned IPs
+    CACHE_PREFIX: str = "Django"
+    #
+    # Number of login attempts until banned
+    LOGIN_ATTEMPTS: int = 10
+    #
+    # How long until login ban lifted
+    LOGIN_COOLDOWN: timedelta = timedelta(minutes=5)
+    #
+    # "Dot import notation" to a function that does additional handling for blocked IPs.
+    # Takes a single argument "ip" of type str, and return None.
+    # Default is no additional handling
+    BLOCKING_HANDLER: str = "jwt_email_auth.utils.blocking_handler"
+    #
+    # "Dot import notation" to a function that sends the login email.
+    # Takes three arguments: request (Request), email (str), and login data (Dict[str, Any]).
+    # Default handler uses django's send_mail function.
+    LOGIN_CALLBACK: str = "jwt_email_auth.utils.send_login_email"
+    #
+    # When True (default), OPTIONS requests can be made to the endpoint without token for schema access
+    OPTIONS_SCHEMA_ACCESS: bool = True
+    #
+    # If True, Refresh view sould return both the access token, and the refresh token
+    REFRESH_VIEW_BOTH_TOKENS: bool = False
+    #
+    # "Dot import notation" to a function that is used to check if request is allowed to check login code.
+    # Takes a single argument: request (Request) and should return a boolean. Default handler blocks
+    # user based on their IP address after a number of attempts defined by LOGIN_ATTEMPTS.
+    LOGIN_BLOCKER_CALLBACK: str = "jwt_email_auth.utils.user_login_blocked"
+
+
+SETTING_NAME: str = "JWT_EMAIL_AUTH"
+
+USER_SETTINGS: Optional[Dict[str, Any]] = getattr(settings, SETTING_NAME, None)
+
+DEFAULTS: Dict[str, Any] = JWTEmailAuthSettings()._asdict()
+
+IMPORT_STRINGS: Set[Union[bytes, str]] = {
+    b"SIGNING_KEY",
+    "VALIDATION_CALLBACK",
+    "LOGIN_DATA",
+    "CODE_GENERATOR",
+    "BLOCKING_HANDLER",
+    "LOGIN_CALLBACK",
+    "LOGIN_BLOCKER_CALLBACK",
+}
+
+REMOVED_SETTINGS: Set[str] = {
+    "LOGIN_EMAIL_CALLBACK",
+    "SEND_EMAILS",
+}
+
+
+auth_settings = SettingsHolder(
+    user_settings=USER_SETTINGS,
+    defaults=DEFAULTS,
+    import_strings=IMPORT_STRINGS,
+    removed_settings=REMOVED_SETTINGS,
+)
+
+
+setting_changed.connect(reload_settings(SETTING_NAME, auth_settings))
---
+++ packages/jwt-email-auth/jwt_email_auth-0.6.0-py3-none-any.whl/jwt_email_auth/views.py
@@ -0,0 +1,171 @@
+import logging
+from typing import Any, List, Type
+
+from django.core.cache import cache
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed, NotFound, PermissionDenied
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.schemas.openapi import AutoSchema
+from rest_framework.serializers import BaseSerializer
+from rest_framework.views import APIView
+
+from .exceptions import CorruptedDataException, ServerException
+from .schema import LoginSchemaMixin, RefreshTokenSchemaMixin, SendLoginCodeSchemaMixin
+from .serializers import LoginSerializer, RefreshTokenSerializer, SendLoginCodeSerializer
+from .settings import auth_settings
+from .tokens import RefreshToken
+from .utils import generate_cache_key
+
+
+__all__ = [
+    "SendLoginCodeView",
+    "LoginView",
+    "RefreshTokenView",
+]
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseAuthView(APIView):
+    """Base class for JWT authentication"""
+
+    serializer_class: Type[BaseSerializer]
+
+    def get_serializer(self, *args: Any, **kwargs: Any) -> BaseSerializer:
+        kwargs["serializer_class"] = self.get_serializer_class()
+        return self.initialize_serializer(*args, **kwargs)
+
+    def initialize_serializer(self, *args: Any, **kwargs: Any) -> BaseSerializer:
+        serializer_class: Type[BaseSerializer] = kwargs.pop("serializer_class")
+        kwargs.setdefault("context", self.get_serializer_context())
+        kwargs.setdefault("many", getattr(serializer_class, "many", False))
+        return serializer_class(*args, **kwargs)
+
+    def get_serializer_class(self) -> Type[BaseSerializer]:
+        assert self.serializer_class, "Serializer class not defined"
+        return self.serializer_class
+
+    def get_serializer_context(self):
+        return {"request": self.request, "format": self.format_kwarg, "view": self}
+
+    @classmethod
+    def get_extra_actions(cls) -> List[str]:
+        return []
+
+
+class SendLoginCodeView(BaseAuthView):
+    """Send a new login code to the given email."""
+
+    authentication_classes: List[Type[BaseAuthentication]] = []
+    permission_classes: List[Type[BasePermission]] = []
+    serializer_class: Type[BaseSerializer] = SendLoginCodeSerializer
+    schema = type("Schema", (SendLoginCodeSchemaMixin, AutoSchema), {})()
+
+    def post(self, request: Request, *args, **kwargs) -> Response:  # pylint: disable=W0613
+        login_info = self.serializer_class(data=request.data)
+        login_info.is_valid(raise_exception=True)
+        data = login_info.data
+        key = list(login_info.fields.keys())[0]
+        value = data[key]
+
+        cache_key = generate_cache_key(value)
+        login_data = cache.get(cache_key, None)
+        if login_data is None:
+            auth_settings.VALIDATION_CALLBACK(value)
+            login_data = auth_settings.LOGIN_DATA(value)
+
+        login_data["code"] = auth_settings.CODE_GENERATOR()
+        logger.debug(login_data)
+        cache.set(cache_key, login_data, auth_settings.LOGIN_CODE_LIFETIME.total_seconds())
+
+        if not auth_settings.SENDING_ON:
+            logger.info(f"Login code: '{login_data['code']}'")
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        try:
+            auth_settings.LOGIN_CALLBACK(value, login_data=login_data, request=self.request)
+        except Exception as error:
+            cache.delete(cache_key)
+            logger.critical(f"Login code sending failed: {type(error).__name__}('{error}')")
+            raise ServerException(_("Failed to send login codes. Try again later.")) from error
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class LoginView(BaseAuthView):
+    """Get new refresh and access token pair from a login code and email."""
+
+    authentication_classes: List[Type[BaseAuthentication]] = []
+    permission_classes: List[Type[BasePermission]] = []
+    serializer_class: Type[BaseSerializer] = LoginSerializer
+    schema = type("Schema", (LoginSchemaMixin, AutoSchema), {})()
+
+    def post(self, request: Request, *args, **kwargs) -> Response:  # pylint: disable=W0613
+        if auth_settings.LOGIN_BLOCKER_CALLBACK(request):
+            raise PermissionDenied(
+                _("Maximum number of attempts reached. Try again in %(x)s minutes.")
+                % {"x": int(auth_settings.LOGIN_COOLDOWN.total_seconds() // 60)}
+            )
+
+        login = self.serializer_class(data=request.data)
+        login.is_valid(raise_exception=True)
+        data = login.data
+        key = [key for key in login.fields.keys() if key != "code"][0]
+        value = data[key]
+
+        cache_key = generate_cache_key(value)
+        login_data = cache.get(cache_key, None)
+        if login_data is None:
+            raise NotFound(_("No login code found for '%(value)s'.") % {"value": value})
+
+        login_code = login_data.pop("code", None)
+        if not auth_settings.SKIP_CODE_CHECKS:
+            if login_code != data["code"]:
+                raise AuthenticationFailed(_("Incorrect login code."))
+
+        refresh = RefreshToken()
+        try:
+            refresh.update({key: login_data[key] for key in auth_settings.EXPECTED_CLAIMS})
+        except KeyError as error:
+            logger.warning(
+                "Some data was missing from saved login info. If you set EXPECTED_CLAIMS, "
+                "you should provide a custom LOGIN_DATA function that returns them."
+            )
+            cache.delete(cache_key)
+            raise CorruptedDataException(_("Data was corrupted. Try to send another login code.")) from error
+
+        cache.delete(cache_key)
+
+        access = refresh.new_access_token(sync=True)
+
+        data = {"access": str(access), "refresh": str(refresh)}
+
+        return Response(data=data, status=status.HTTP_200_OK)
+
+
+class RefreshTokenView(BaseAuthView):
+    """Get new access token from a refresh token."""
+
+    authentication_classes: List[Type[BaseAuthentication]] = []
+    permission_classes: List[Type[BasePermission]] = []
+    serializer_class: Type[BaseSerializer] = RefreshTokenSerializer
+    schema = type("Schema", (RefreshTokenSchemaMixin, AutoSchema), {})()
+
+    def post(self, request: Request, *args, **kwargs) -> Response:  # pylint: disable=W0613
+        token = self.serializer_class(data=request.data)
+        token.is_valid(raise_exception=True)
+        data = token.data
+
+        refresh = RefreshToken(data["token"])
+        access = refresh.new_access_token()
+        data = {"access": str(access)}
+
+        if auth_settings.REFRESH_VIEW_BOTH_TOKENS:
+            data["refresh"] = str(refresh)
+
+        return Response(data=data, status=status.HTTP_200_OK)
