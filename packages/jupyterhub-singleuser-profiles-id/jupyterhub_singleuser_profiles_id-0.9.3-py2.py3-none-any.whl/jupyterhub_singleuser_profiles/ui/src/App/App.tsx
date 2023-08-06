2c298f7e5e2c3db17a0947dede093d3c01946ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:13 +0000

    Add package jupyterhub-singleuser-profiles-id/0.9.3/jupyterhub_singleuser_profiles_id-0.9.3-py2.py3-none-any.whl

---
+++ packages/jupyterhub-singleuser-profiles-id/jupyterhub_singleuser_profiles_id-0.9.3-py2.py3-none-any.whl/jupyterhub_singleuser_profiles/ui/src/App/App.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import '@patternfly/patternfly/patternfly.min.css';
+import {
+  Title,
+  EmptyState,
+  EmptyStateVariant,
+  EmptyStateIcon,
+  EmptyStateBody,
+  Spinner,
+} from '@patternfly/react-core';
+import { WarningTriangleIcon } from '@patternfly/react-icons';
+import ImageForm from '../ImageForm/ImageForm';
+import SizesForm from '../SizesForm/SizesForm';
+import EnvVarForm from '../EnvVarForm/EnvVarForm';
+import { APIGet } from '../utils/APICalls';
+import { UI_CONFIG_PATH } from '../utils/const';
+import { UiConfigType } from '../utils/types';
+
+import './App.scss';
+
+const App: React.FC = () => {
+  const [uiConfig, setUiConfig] = React.useState<UiConfigType>();
+  const [configError, setConfigError] = React.useState<string>();
+  React.useEffect(() => {
+    let cancelled = false;
+
+    APIGet(UI_CONFIG_PATH)
+      .then((data: UiConfigType) => {
+        if (!cancelled) {
+          setUiConfig(data);
+        }
+      })
+      .catch((e) => {
+        console.dir(e);
+        setConfigError(e);
+      });
+
+    return () => {
+      cancelled = true;
+    };
+  }, []);
+
+  const renderContent = () => {
+    if (configError) {
+      return (
+        <EmptyState variant={EmptyStateVariant.full}>
+          <EmptyStateIcon icon={WarningTriangleIcon} />
+          <Title headingLevel="h5" size="lg">
+            Unable to load notebook server configuration options
+          </Title>
+          <EmptyStateBody>Please contact your system administrator</EmptyStateBody>
+        </EmptyState>
+      );
+    }
+
+    if (!uiConfig) {
+      return (
+        <EmptyState variant={EmptyStateVariant.full}>
+          <Spinner isSVG size="xl" />
+        </EmptyState>
+      );
+    }
+
+    return (
+      <>
+        <ImageForm uiConfig={uiConfig} />
+        <SizesForm uiConfig={uiConfig} />
+        {uiConfig.envVarConfig?.enabled !== false && <EnvVarForm uiConfig={uiConfig} />}
+        <div className="jsp-spawner__buttons-bar">
+          <input
+            type="submit"
+            value="Start server"
+            className="jsp-spawner__submit-button pf-c-button pf-m-primary"
+          />
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="jsp-spawner">
+      <div className="jsp-spawner__header">
+        <div className="jsp-spawner__header__title" align="center"><strong> InnerData </strong></div>
+        <div className="jsp-spawner__header__sub-title">
+          Select options for your notebook server.
+        </div>
+      </div>
+      {renderContent()}
+    </div>
+  );
+};
+
+export default App;
