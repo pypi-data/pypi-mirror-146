f2a8ed964c9ec5c8e567fa5c8b3881b947e72dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:48 +0000

    Add package tlidb/1.0.2/tlidb-1.0.2.tar.gz

---
+++ packages/tlidb/tlidb-1.0.2.tar.gz/tlidb-1.0.2/TLiDB/__init__.py
@@ -0,0 +1,8 @@
+from .version import __version__
+
+supported_datasets = [
+    "clinc150",
+    "DailyDialog",
+    "Friends"
+]
+
---
+++ packages/tlidb/tlidb-1.0.2.tar.gz/tlidb-1.0.2/TLiDB/datasets/get_dataset.py
@@ -0,0 +1,28 @@
+import TLiDB
+# dataset url can be found in the google drive, where the original link is:
+#   https://drive.google.com/file/d/1sqaiYTm9b9SPEzehdjp_DXEovVId6Fvq/view?usp=sharing
+# and needs to be reformatted as:
+#   https://drive.google.com/uc?export=download&id=1sqaiYTm9b9SPEzehdjp_DXEovVId6Fvq
+
+def get_dataset(dataset, task, dataset_folder, **dataset_kwargs):
+    """
+    Returns the appropriate TLiDB dataset class
+    Args:
+        dataset: name of the dataset
+        dataset_kwargs: keyword arguments to pass to the dataset class
+    Returns:
+        dataset: TLiDB dataset class
+    """
+    if dataset not in TLiDB.supported_datasets:
+        raise ValueError(f"{dataset} is not a supported dataset, must be one of: {TLiDB.supported_datasets}")
+
+    if dataset == "DailyDialog":
+        from TLiDB.datasets.DailyDialog_dataset import DailyDialog_dataset as dataset_class
+    elif dataset == "Friends":
+        from TLiDB.datasets.Friends_dataset import Friends_dataset as dataset_class
+    elif dataset == "clinc150":
+        from TLiDB.datasets.clinc150_dataset import clinc150_dataset as dataset_class
+    else:
+        raise ValueError(f"{dataset} is not a supported dataset, must be one of: {TLiDB.supported_datasets}")
+        
+    return dataset_class(task, dataset_folder, **dataset_kwargs)
