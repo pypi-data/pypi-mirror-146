04c00808957e7edc93ddd862b43c9eb210761c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:41 +0000

    Add package seldom/2.8.0/seldom-2.8.0.tar.gz

---
+++ packages/seldom/seldom-2.8.0.tar.gz/seldom-2.8.0/seldom/logging/log.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import time
+import inspect
+from loguru import logger
+from seldom.running.config import BrowserConfig
+from seldom.running.config import Seldom
+
+
+stack_t = inspect.stack()
+ins = inspect.getframeinfo(stack_t[1][0])
+file_dir = os.path.dirname(os.path.abspath(ins.filename))
+report_dir = os.path.join(file_dir, "reports")
+if os.path.exists(report_dir) is False:
+    os.mkdir(report_dir)
+
+
+now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+if BrowserConfig.LOG_PATH is None:
+    BrowserConfig.LOG_PATH = os.path.join(os.getcwd(), "reports", now_time + "_log.log")
+if BrowserConfig.REPORT_PATH is None:
+    BrowserConfig.REPORT_PATH = os.path.join(os.getcwd(), "reports", now_time + "_result.html")
+
+logger.add(BrowserConfig.LOG_PATH, encoding="utf-8")
+
+colorLog = True
+
+
+def debug(msg):
+    now = time.strftime("%Y-%m-%d %H:%M:%S")
+    if Seldom.debug is False:
+        print(now + " [DEBUG] " + str(msg))
+    if colorLog is True:
+        return logger.debug(msg)
+    else:
+        msg = msg.encode('gbk', 'ignore').decode('gbk', "ignore")
+        return logger.debug(msg)
+
+
+def info(msg):
+    now = time.strftime("%Y-%m-%d %H:%M:%S")
+    if Seldom.debug is False:
+        print(now + " [INFO] " + str(msg))
+    if colorLog is True:
+        return logger.info(msg)
+    else:
+        msg = msg.encode('gbk', 'ignore').decode('gbk', "ignore")
+        return logger.info(msg)
+
+
+def error(msg):
+    now = time.strftime("%Y-%m-%d %H:%M:%S")
+    if Seldom.debug is False:
+        print(now + " [ERROR] " + str(msg))
+    if colorLog is True:
+        return logger.error(msg)
+    else:
+        msg = msg.encode('gbk', 'ignore').decode('gbk', "ignore")
+        return logger.info(msg)
+
+
+def warn(msg):
+    now = time.strftime("%Y-%m-%d %H:%M:%S")
+    if Seldom.debug is False:
+        print(now + " [WARNING] " + str(msg))
+    if colorLog is True:
+        return logger.warning(msg)
+    else:
+        msg = msg.encode('gbk', 'ignore').decode('gbk', "ignore")
+        return logger.warning(msg)
+
+
+def printf(msg):
+    if colorLog is True:
+        return logger.success(msg)
+    else:
+        msg = msg.encode('gbk', 'ignore').decode('gbk', "ignore")
+        return logger.success(msg)
