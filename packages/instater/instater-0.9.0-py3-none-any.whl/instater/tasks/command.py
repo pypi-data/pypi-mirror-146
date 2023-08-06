1c4b3619f8d49f90f403077b174b5e53791b6fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:54 +0000

    Add package instater/0.9.0/instater-0.9.0-py3-none-any.whl

---
+++ packages/instater/instater-0.9.0-py3-none-any.whl/instater/tasks/command.py
@@ -0,0 +1,40 @@
+from typing import List, Union
+
+from .. import util
+from ..context import Context
+from . import Task
+
+
+class Command(Task):
+    def __init__(
+        self,
+        command: Union[str, List[str]],
+        condition: str = None,
+        condition_code: int = 0,
+        become: str = None,
+        directory: str = None,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        if isinstance(command, str):
+            command = [command]
+
+        self.commands = command
+        self.condition = condition
+        self.condition_code = int(condition_code)
+        self.become = become
+        self.directory = directory
+
+    def run_action(self, context: Context) -> bool:
+        if self.condition:
+            # TODO: only consider specific return codes as valid?
+            result = util.shell(self.condition, self.directory, valid_return_codes=None)
+            if result.return_code != self.condition_code:
+                return False
+
+        if not context.dry_run:
+            for command in self.commands:
+                util.shell(command, self.directory, become=self.become)
+
+        return True
