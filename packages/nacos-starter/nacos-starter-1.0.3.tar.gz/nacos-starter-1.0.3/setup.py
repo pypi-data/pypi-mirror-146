2efab161e8b98ba27285764f3f7758836072f7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:54 +0000

    Add package nacos-starter/1.0.3/nacos-starter-1.0.3.tar.gz

---
+++ packages/nacos-starter/nacos-starter-1.0.3.tar.gz/nacos-starter-1.0.3/setup.py
@@ -0,0 +1,36 @@
+"""
+build for publish script.
+"""
+import setuptools
+import re
+
+module_dir = "nacos_starter"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open(f"src/{module_dir}/__init__.py", encoding="utf-8") as fh:
+    lines = fh.readlines()
+    vl = [x for x in lines if "__version__" in x][0]
+    version = re.sub(r"[^\d.]", "", str.strip(vl.split("=")[1]))
+    namel = [x for x in lines if "name" in x][0]
+    name = re.sub(r"[^\w\-_]", "", str.strip(namel.split("=")[1]))
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="chizongyang",
+    author_email="chizongyang@aliyun.com",
+    description="Nacos starter for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    include_package_data=True,
+    packages=["src"], # 自动查找模块
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
