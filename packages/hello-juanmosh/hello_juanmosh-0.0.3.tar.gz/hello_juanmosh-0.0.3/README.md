c91b9c65ec2aabd5b2c5ddfb020f46368ff582a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:39 +0000

    Add package hello-juanmosh/0.0.3/hello_juanmosh-0.0.3.tar.gz

---
+++ packages/hello-juanmosh/hello_juanmosh-0.0.3.tar.gz/hello_juanmosh-0.0.3/README.md
@@ -0,0 +1,27 @@
+# hello
+
+Description. 
+The package package_name is used to:
+	- Hello world!
+	-
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install hello_juanmosh
+```
+
+## Usage
+
+```python
+from hello_juanmosh.hello_juanmosh import hello_juanmosh
+hello_juanmosh("your name")
+```
+
+## Author
+Juan
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/hello-juanmosh/hello_juanmosh-0.0.3.tar.gz/hello_juanmosh-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="hello_juanmosh",
+    version="0.0.3",
+    author="juan",
+    author_email="apolinario.psi@gmail.com",
+    description="Simple Hello World",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/juanapolinario/calc_package.git",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
