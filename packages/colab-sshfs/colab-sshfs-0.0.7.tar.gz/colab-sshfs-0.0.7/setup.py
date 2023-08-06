17e874729cfa69a066de1bd8164771805125e4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:09 +0000

    Add package colab-sshfs/0.0.7/colab-sshfs-0.0.7.tar.gz

---
+++ packages/colab-sshfs/colab-sshfs-0.0.7.tar.gz/colab-sshfs-0.0.7/setup.py
@@ -0,0 +1,17 @@
+import pathlib, setuptools
+
+VERSION = '0.0.7'
+
+pwd = pathlib.Path(__file__).parent
+long_description = (pwd / "README.md").read_text()
+
+setuptools.setup(
+        name = 'colab-sshfs',
+        version = VERSION,
+        description = 'Mount sshfs remotes on Google Colaboratory',
+        author = 'Paul Nuyujukian',
+        url = 'https://github.com/bil/colab-sshfs',
+        long_description = long_description,
+        long_description_content_type='text/markdown',
+        license = 'GPL2',
+        packages = setuptools.find_packages() )
