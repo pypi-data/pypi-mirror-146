9bb4ec1891d277624c6227d313bf2bb9f25cd8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:28 +0000

    Add package yangxian/1.0/yangxian-1.0-py3-none-any.whl

---
+++ packages/yangxian/yangxian-1.0-py3-none-any.whl/configs/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time:    2022-04-12 17:40
+# @Author:  geng
+# @Email:   yonglonggeng@163.com
+# @WeChat:  superior_god
+# @File:    __init__.py.py
+# @Project: yangxian
