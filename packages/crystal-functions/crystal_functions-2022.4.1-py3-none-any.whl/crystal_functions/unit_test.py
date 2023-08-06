6379a54daed4a6fc32098bff6e24d0a1213eae07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:51 +0000

    Add package crystal-functions/2022.4.1/crystal_functions-2022.4.1-py3-none-any.whl

---
+++ packages/crystal-functions/crystal_functions-2022.4.1-py3-none-any.whl/crystal_functions/unit_test.py
@@ -0,0 +1,11 @@
+def crystal_input_test():
+    
+    from crystal_functions.file_readwrite import Crystal_input
+
+    mgo_input = Crystal_input('data_test/mgo.d12')
+
+
+
+crystal_input_test()
+
+        
