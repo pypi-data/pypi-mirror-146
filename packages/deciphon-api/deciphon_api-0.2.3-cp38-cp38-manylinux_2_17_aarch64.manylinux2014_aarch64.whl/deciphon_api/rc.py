1632f5212e322ad2ff03bde155c6a588a399581d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:51 +0000

    Add package deciphon-api/0.2.3/deciphon_api-0.2.3-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/deciphon-api/deciphon_api-0.2.3-cp38-cp38-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/deciphon_api/rc.py
@@ -0,0 +1,25 @@
+from enum import Enum, IntEnum
+
+__all__ = ["RC"]
+
+
+class RC(IntEnum):
+    OK = 0
+    END = 1
+    NOTFOUND = 2
+    EFAIL = 3
+    EIO = 4
+    EINVAL = 5
+    ENOMEM = 6
+    EPARSE = 7
+
+
+class StrRC(str, Enum):
+    OK = "ok"
+    END = "end"
+    NOTFOUND = "notfound"
+    EFAIL = "efail"
+    EIO = "eio"
+    EINVAL = "einval"
+    ENOMEM = "enomem"
+    EPARSE = "eparse"
