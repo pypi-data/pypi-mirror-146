626913247b8a5037dcacf32187985c32f09cd6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:56 +0000

    Add package VividHues/5.6.18/VividHues-5.6.18.tar.gz

---
+++ packages/VividHues/VividHues-5.6.18.tar.gz/VividHues-5.6.18/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+"""
+Setup file for VividHues
+"""
+
+import codecs
+import os.path
+
+
+class Info:
+    VERSION = "5.6.18"
+
+
+NAME = "VividHues"
+print("\033[93m" + f"Installing {NAME}..." + "\033[0m")
+setup(
+    name="VividHues",
+    url=f"https://github.com/KennyOliver/{NAME}",
+    author="Kenneth Oliver",
+    author_email="kenny_oliver@icloud.com",
+    packages=[NAME],
+    version=Info.VERSION,
+    license="AGPL-3.0",
+    description=f"{NAME}: Super light Python string formatter!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Terminals",
+    ]
+)
+print("\033[32m" + "\033[1m" + f"{NAME} was installed successfully! Have colorful fun! :D" + "\033[0m")
