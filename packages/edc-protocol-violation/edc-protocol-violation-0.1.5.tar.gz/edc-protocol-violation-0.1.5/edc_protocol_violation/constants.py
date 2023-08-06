62c4a2fb24dd601c36870f2a99b0f8e7249bf5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:58 +0000

    Add package edc-protocol-violation/0.1.5/edc-protocol-violation-0.1.5.tar.gz

---
+++ packages/edc-protocol-violation/edc-protocol-violation-0.1.5.tar.gz/edc-protocol-violation-0.1.5/edc_protocol_violation/constants.py
@@ -0,0 +1,4 @@
+DEVIATION = "protocol_deviation"
+PROTOCOL_DEVIATION_VIOLATION_ACTION = "submit-protocol-deviation-violation"
+VIOLATION = "protocol_violation"
+MEDICATION_NONCOMPLIANCE = "medication_noncompliance"
