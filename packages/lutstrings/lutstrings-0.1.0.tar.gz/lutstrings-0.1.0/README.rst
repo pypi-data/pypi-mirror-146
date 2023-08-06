76ca4021d0ab10c94558912948c55a1896d7e305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:58 +0000

    Add package lutstrings/0.1.0/lutstrings-0.1.0.tar.gz

---
+++ packages/lutstrings/lutstrings-0.1.0.tar.gz/lutstrings-0.1.0/README.rst
@@ -0,0 +1,2 @@
+A package to use to manipulate init strings for FPGA LUTs
+  based on changed input pins. Use with rapidwright.
---
+++ packages/lutstrings/lutstrings-0.1.0.tar.gz/lutstrings-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='lutstrings',
+    version='0.1.0',    
+    description='Python package to reorder init strings',
+    url='https://github.com/byuccl/encrypted_ip/tree/main/lutstrings',
+    author='Daniel Hutchings',
+    author_email='dhutch@dazoo.org',
+    license='MIT',
+    packages=['lutstrings'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+)
