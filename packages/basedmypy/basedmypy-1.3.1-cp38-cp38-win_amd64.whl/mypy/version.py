403188a49799126d319348ff4744c5d32a4ad84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:56 +0000

    Add package basedmypy/1.3.1/basedmypy-1.3.1-cp38-cp38-win_amd64.whl

---
+++ packages/basedmypy/basedmypy-1.3.1-cp38-cp38-win_amd64.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "0.942"
+based_version_info = VersionInfo(major=1, minor=3, patch=1, release_level='final', iteration=0, mypy_version='0.942', mypy_release_level='final')
+__based_version__ = "1.3.1"
