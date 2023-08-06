5e9668fd87ac3806ba1238dd8b609a34b058662a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:27 +0000

    Add package metathing/0.1.8/metathing-0.1.8.tar.gz

---
+++ packages/metathing/metathing-0.1.8.tar.gz/metathing-0.1.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='metathing',
+    version='0.1.8',
+    description='MT-Service Python',
+    license='N/A',
+    packages=find_packages(exclude=['test', 'workdir', 'metathing/__pycache__']),
+)
