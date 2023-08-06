fec4a1f039dd9f1196447c65f886ca7dcb1a8e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:32 +0000

    Add package nwon-django-toolbox/0.2.0/nwon_django_toolbox-0.2.0.tar.gz

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.2.0.tar.gz/nwon_django_toolbox-0.2.0/nwon_django_toolbox/url_helper/__init__.py
@@ -0,0 +1,17 @@
+from nwon_django_toolbox.url_helper.url_helper import (
+    detail_url_for_model,
+    detail_url_for_model_class,
+    django_admin_link_for_model,
+    formatted_django_admin_link,
+    list_url_for_model,
+    list_url_for_model_class,
+)
+
+__all__ = [
+    "detail_url_for_model",
+    "detail_url_for_model_class",
+    "list_url_for_model_class",
+    "list_url_for_model",
+    "formatted_django_admin_link",
+    "django_admin_link_for_model",
+]
