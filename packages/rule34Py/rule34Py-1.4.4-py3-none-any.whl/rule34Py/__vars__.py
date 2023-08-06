f89e71064e1a57a0522ed8285fc6e29ef899ff61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:55 +0000

    Add package rule34Py/1.4.4/rule34Py-1.4.4-py3-none-any.whl

---
+++ packages/rule34Py/rule34Py-1.4.4-py3-none-any.whl/rule34Py/__vars__.py
@@ -0,0 +1,14 @@
+__version__tuple__ = ("1", "4", "4")
+__author__ = ("b3yc0d3")
+__email__ = ("b3yc0d3@gmail.com")
+
+__version__ = ".".join(__version__tuple__) # xx.xx.xx
+
+
+# Variables
+__base_url__ = "https://rule34.xxx/"
+__useragent__ = f"Mozilla/5.0 (compatible; rule34Py/{__version__})"
+
+__headers__ = {
+    "User-Agent": __useragent__
+}
