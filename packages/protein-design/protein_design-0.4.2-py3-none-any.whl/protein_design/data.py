2a1e500074f27684c52d5f859b20d596255c49ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:25 +0000

    Add package protein-design/0.4.2/protein_design-0.4.2-py3-none-any.whl

---
+++ packages/protein-design/protein_design-0.4.2-py3-none-any.whl/protein_design/data.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from protein_design.constants import device
+
+
+def to_tensor(X) -> torch.tensor:
+    return X.to(device) if torch.is_tensor(X) else torch.from_numpy(X).to(device)
+
+
+def to_numpy(X) -> np.ndarray:
+    return X.detach().cpu().numpy() if torch.is_tensor(X) else np.array(X)
+
+
+class ProteinData(Dataset):
+    def __init__(self, X, y):
+        self.X = to_tensor(X)
+        self.y = to_tensor(y)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+def cycle(dataloader):
+    while True:
+        for data in dataloader:
+            yield data
