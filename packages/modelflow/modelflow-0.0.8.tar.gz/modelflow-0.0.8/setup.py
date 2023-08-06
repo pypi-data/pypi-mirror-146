a2297a4d8d6787f326f5ce1c78cd3ff1bffe6e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:29 +0000

    Add package modelflow/0.0.8/modelflow-0.0.8.tar.gz

---
+++ packages/modelflow/modelflow-0.0.8.tar.gz/modelflow-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="modelflow",
+    version="0.0.8",
+    license="MIT",
+    author="Felix Zhu",
+    author_email="zhu.felix@outlook.com",
+    description="ML flows from start to finish",
+    long_description=long_description,
+    packages=find_packages(),
+    url="https://github.com/felixzhu17/ModelFlow",
+    install_requires=[
+        "interpret",
+        "pandas",
+        "plotly",
+        "scikit_learn",
+        "scipy",
+        "plotly_express",
+        "xgboost",
+        "statsmodels",
+        "shap",
+        "numpy==1.21",
+        "pytest",
+    ],
+)
