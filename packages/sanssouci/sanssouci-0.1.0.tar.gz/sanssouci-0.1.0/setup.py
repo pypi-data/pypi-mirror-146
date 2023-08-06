36c29b3003d8db0704584fa639a40911dc6852f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:05 +0000

    Add package sanssouci/0.1.0/sanssouci-0.1.0.tar.gz

---
+++ packages/sanssouci/sanssouci-0.1.0.tar.gz/sanssouci-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["numpy>=1.15.0", "scipy>=1.0.0",
+                "joblib>=1.0.1", "scikit-learn>=0.22"]
+
+release_url = "https://github.com/pneuvial/\
+               sanssouci.python/archive/refs/tags/0.1.0.tar.gz"
+
+setup(
+    name="sanssouci",
+    version="0.1.0",
+    author="Laurent Risser and Pierre Neuvial",
+    author_email="pierre.neuvial@math.univ-toulouse.fr",
+    description="Post hoc inference via multiple testing",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sanssouci.python/",
+    download_url=release_url,
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+)
