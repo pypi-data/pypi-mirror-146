2ad6f06c17b74964a1de39a21623e7653b528534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package ignition-api/8.0.17.post6/ignition_api-8.0.17.post6.tar.gz

---
+++ packages/ignition-api/ignition_api-8.0.17.post6.tar.gz/ignition_api-8.0.17.post6/src/com/inductiveautomation/ignition/common/model/values/__init__.py
@@ -0,0 +1,87 @@
+__all__ = ["BasicQualifiedValue", "QualifiedValue", "QualityCode"]
+
+
+from java.lang import Object
+
+
+class QualityCode(Object):
+    """QualityCode contains a 32-bit integer code and optionally a
+    diagnostic string.
+    """
+
+    def __init__(self, *args):
+        pass
+
+    def derive(self, diagnosticMessage):
+        pass
+
+    def getCode(self):
+        pass
+
+    def getDiagnosticMessage(self):
+        pass
+
+    def getLevel(self):
+        pass
+
+    def getName(self):
+        pass
+
+    def isBad(self):
+        pass
+
+    def isBadOrError(self):
+        pass
+
+    def isError(self):
+        pass
+
+    def isGood(self):
+        pass
+
+    def isNotGood(self):
+        pass
+
+    def isUncertain(self):
+        pass
+
+    class Level(Object):
+        def __init__(self):
+            pass
+
+        def code(self, userCode):
+            pass
+
+        @staticmethod
+        def valueOf(name):
+            pass
+
+        @staticmethod
+        def values():
+            pass
+
+
+class QualifiedValue(object):
+    """Represents a value with a DataQuality & timestamp attached to
+    it.
+    """
+
+    def getQuality(self):
+        raise NotImplementedError
+
+    def getTimestamp(self):
+        raise NotImplementedError
+
+    def getValue(self):
+        raise NotImplementedError
+
+
+class BasicQualifiedValue(QualifiedValue):
+    def getQuality(self):
+        pass
+
+    def getTimestamp(self):
+        pass
+
+    def getValue(self):
+        pass
---
+++ packages/ignition-api/ignition_api-8.0.17.post6.tar.gz/ignition_api-8.0.17.post6/src/system/__version__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+"""Package information."""
+
+__version__ = "8.0.17"
+__build__ = "2020111210"
+__cycle__ = ".post6"
+__package_version__ = "{0}{1}".format(__version__, __cycle__)
