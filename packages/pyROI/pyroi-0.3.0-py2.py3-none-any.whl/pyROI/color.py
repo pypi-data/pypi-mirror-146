51f4e76c6ff625269976f81c591e5ff0625c0c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:38 +0000

    Add package pyROI/0.3.0/pyroi-0.3.0-py2.py3-none-any.whl

---
+++ packages/pyROI/pyroi-0.3.0-py2.py3-none-any.whl/pyROI/color.py
@@ -0,0 +1,7 @@
+# Basic RGB Color (B, G, R)
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (255, 0, 0)
+YELLOW = (0, 255, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
---
+++ packages/pyROI/pyroi-0.3.0-py2.py3-none-any.whl/pyROI/screen/__runner__.py
@@ -0,0 +1,36 @@
+import struct
+import tkinter
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Tuple
+
+
+@contextmanager
+def tk_instance():
+    root = tkinter.Tk()
+    root.wm_withdraw()
+    yield root
+    root.destroy()
+
+
+def retrieve_screen_resolution() -> Tuple[int, int]:
+    with tk_instance() as root:
+        width = root.winfo_screenwidth()
+        height = root.winfo_screenheight()
+    return width, height
+
+
+def runner():
+    folder = Path(__file__).resolve().parent / "__pycache__"
+    cache_file = folder / "__cache__"
+    if not folder.is_dir():
+        folder.mkdir()
+    if cache_file.exists():
+        return
+    width, height = retrieve_screen_resolution()
+    with open(cache_file, "wb") as f:
+        f.write(struct.pack("<ll", width, height))
+
+
+if __name__ == "__main__":
+    runner()
