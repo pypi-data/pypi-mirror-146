eaa4418c8b5e55d3ad5962ad8c8bcda060fa5232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:16 +0000

    Add package evalml/0.50.0/evalml-0.50.0.tar.gz

---
+++ packages/evalml/evalml-0.50.0.tar.gz/evalml-0.50.0/evalml/tests/data_checks_tests/test_datetime_format_data_check.py
@@ -0,0 +1,342 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from evalml.data_checks import (
+    DataCheckError,
+    DataCheckMessageCode,
+    DateTimeFormatDataCheck,
+)
+
+datetime_format_check_name = DateTimeFormatDataCheck.name
+
+
+@pytest.mark.parametrize("input_type", ["pd", "ww"])
+@pytest.mark.parametrize(
+    "issue", ["redundant", "missing", "uneven", "type_errors", None]
+)
+@pytest.mark.parametrize("datetime_loc", [1, "X_index", "y_index"])
+def test_datetime_format_data_check_typeerror_uneven_intervals(
+    issue, input_type, datetime_loc
+):
+    X, y = pd.DataFrame({"features": range(30)}), pd.Series(range(30))
+
+    if issue == "type_errors":
+        dates = range(30)
+    else:
+        dates = pd.date_range("2021-01-01", periods=30)
+
+    if issue == "missing":
+        # Skips 2021-01-25 and starts again at 2021-01-27, skipping a date and triggering the error
+        dates = pd.date_range("2021-01-01", periods=25).append(
+            pd.date_range("2021-01-27", periods=5)
+        )
+    if issue == "uneven":
+        dates = pd.DatetimeIndex(
+            pd.date_range("2021-01-01", periods=6).append(
+                pd.date_range("2021-01-07", periods=24, freq="H")
+            )
+        )
+    if issue == "redundant":
+        dates = pd.date_range("2021-01-01", periods=29).append(
+            pd.date_range("2021-01-29", periods=1)
+        )
+
+    datetime_column = "index"
+    if datetime_loc == 1:
+        X[datetime_loc] = dates
+        datetime_column = datetime_loc
+    elif datetime_loc == "X_index":
+        X.index = dates
+    else:
+        y.index = dates
+
+    if input_type == "ww":
+        X.ww.init()
+        y.ww.init()
+
+    datetime_format_check = DateTimeFormatDataCheck(datetime_column=datetime_column)
+
+    if issue == "type_errors":
+        assert datetime_format_check.validate(X, y) == [
+            DataCheckError(
+                message=f"Datetime information could not be found in the data, or was not in a supported datetime format.",
+                data_check_name=datetime_format_check_name,
+                message_code=DataCheckMessageCode.DATETIME_INFORMATION_NOT_FOUND,
+            ).to_dict()
+        ]
+    else:
+        col_name = datetime_loc if datetime_loc == 1 else "either index"
+        if issue is None:
+            assert datetime_format_check.validate(X, y) == []
+        elif issue == "missing":
+            assert datetime_format_check.validate(X, y) == [
+                DataCheckError(
+                    message=f"Column '{col_name}' has datetime values missing between start and end date.",
+                    data_check_name=datetime_format_check_name,
+                    message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+                ).to_dict()
+            ]
+        elif issue == "redundant":
+            assert datetime_format_check.validate(X, y) == [
+                DataCheckError(
+                    message=f"Column '{col_name}' has more than one row with the same datetime value.",
+                    data_check_name=datetime_format_check_name,
+                    message_code=DataCheckMessageCode.DATETIME_HAS_REDUNDANT_ROW,
+                ).to_dict()
+            ]
+        else:
+            assert datetime_format_check.validate(X, y) == [
+                DataCheckError(
+                    message=f"No frequency could be detected in column '{col_name}', possibly due to uneven intervals.",
+                    data_check_name=datetime_format_check_name,
+                    message_code=DataCheckMessageCode.DATETIME_HAS_UNEVEN_INTERVALS,
+                ).to_dict(),
+            ]
+
+
+@pytest.mark.parametrize("sort_order", ["increasing", "decreasing", "mixed"])
+@pytest.mark.parametrize("datetime_loc", ["datetime_feature", "X_index", "y_index"])
+def test_datetime_format_data_check_monotonic(datetime_loc, sort_order):
+    X, y = pd.DataFrame({"features": range(300)}), pd.Series(range(300))
+    dates = pd.date_range("2021-01-01", periods=300)
+
+    if sort_order == "decreasing":
+        dates = dates[::-1]
+    elif sort_order == "mixed":
+        dates = dates[:50].append(dates[200:]).append(dates[50:200])
+
+    datetime_column = "index"
+    if datetime_loc == "datetime_feature":
+        X[datetime_loc] = dates
+        datetime_column = "datetime_feature"
+    elif datetime_loc == "X_index":
+        X.index = dates
+    else:
+        y.index = dates
+
+    datetime_format_check = DateTimeFormatDataCheck(datetime_column=datetime_column)
+
+    if sort_order == "increasing":
+        assert datetime_format_check.validate(X, y) == []
+    else:
+        col_name = (
+            datetime_loc if datetime_loc == "datetime_feature" else "either index"
+        )
+        freq_error = DataCheckError(
+            message=f"No frequency could be detected in column '{col_name}', possibly due to uneven intervals.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_HAS_UNEVEN_INTERVALS,
+        ).to_dict()
+        mono_error = DataCheckError(
+            message="Datetime values must be sorted in ascending order.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_NOT_MONOTONIC,
+        ).to_dict()
+        if sort_order == "decreasing":
+            assert datetime_format_check.validate(X, y) == [mono_error]
+        else:
+            assert datetime_format_check.validate(X, y) == [mono_error, freq_error]
+
+
+@pytest.mark.parametrize("n_missing", [2, 5, 7])
+def test_datetime_format_data_check_multiple_missing(n_missing):
+    X, y = pd.DataFrame({"features": range(100)}), pd.Series(range(100))
+
+    dates = pd.date_range("2021-01-01", periods=15)
+    if n_missing == 2:
+        # Two missing dates in separate spots
+        dates = dates.append(pd.date_range("2021-01-17", periods=86)).drop("2021-01-22")
+    elif n_missing == 5:
+        # A chunk of 5 missing days in a row
+        dates = dates.append(pd.date_range("2021-01-21", periods=85))
+    else:
+        # Some chunks missing and some alone missing
+        dates = dates.append(pd.date_range("2021-01-20", periods=88)).drop("2021-01-27")
+        dates = dates.drop("2021-02-22")
+        dates = dates.drop("2021-01-11")
+
+    X["dates"] = dates
+    datetime_format_check = DateTimeFormatDataCheck(datetime_column="dates")
+
+    assert datetime_format_check.validate(X, y) == [
+        DataCheckError(
+            message=f"Column 'dates' has datetime values missing between start and end date.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+        ).to_dict()
+    ]
+
+
+def test_datetime_format_data_check_multiple_errors():
+    dates = (
+        pd.date_range("2021-01-01", periods=9).tolist()
+        + ["2021-01-31", "2021-02-02", "2021-02-04"]
+        + pd.date_range("2021-02-05", periods=90).tolist()
+    )
+    X = pd.DataFrame({"dates": dates})
+    y = pd.Series(range(21))
+    datetime_format_check = DateTimeFormatDataCheck(datetime_column="dates")
+
+    assert datetime_format_check.validate(X, y) == [
+        DataCheckError(
+            message=f"Column 'dates' has datetime values missing between start and end date.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+        ).to_dict(),
+    ]
+
+    dates = (
+        pd.date_range("2021-01-01", periods=9).tolist()
+        + ["2021-01-09", "2021-01-31", "2021-02-02", "2021-02-04"]
+        + pd.date_range("2021-02-05", periods=90).tolist()
+    )
+    X = pd.DataFrame({"dates": dates})
+
+    assert datetime_format_check.validate(X, y) == [
+        DataCheckError(
+            message=f"Column 'dates' has more than one row with the same datetime value.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_HAS_REDUNDANT_ROW,
+        ).to_dict(),
+        DataCheckError(
+            message=f"Column 'dates' has datetime values missing between start and end date.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+        ).to_dict(),
+    ]
+
+    dates = (
+        pd.date_range("2021-01-01", periods=15)
+        .drop("2021-01-10")
+        .append(pd.date_range("2021-01-15", periods=86))
+    )
+    X = pd.DataFrame({"dates": dates})
+
+    assert datetime_format_check.validate(X, y) == [
+        DataCheckError(
+            message=f"Column 'dates' has more than one row with the same datetime value.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_HAS_REDUNDANT_ROW,
+        ).to_dict(),
+        DataCheckError(
+            message=f"Column 'dates' has datetime values missing between start and end date.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+        ).to_dict(),
+    ]
+
+    dates = (
+        pd.date_range("2021-01-01", periods=15, freq="2D")
+        .drop("2021-01-13")
+        .append(pd.date_range("2021-01-30", periods=1))
+        .append(pd.date_range("2021-01-31", periods=86, freq="2D"))
+    )
+    X = pd.DataFrame({"dates": dates})
+
+    assert datetime_format_check.validate(X, y) == [
+        DataCheckError(
+            message=f"Column 'dates' has datetime values missing between start and end date.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+        ).to_dict(),
+        DataCheckError(
+            message=f"Column 'dates' has datetime values that do not align with the inferred frequency.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_HAS_MISALIGNED_VALUES,
+        ).to_dict(),
+    ]
+
+
+def test_datetime_format_unusual_interval():
+    dates = pd.date_range(start="2021-01-01", periods=100, freq="4D")
+    X = pd.DataFrame({"dates": dates})
+    y = pd.Series(range(100))
+
+    datetime_format_check = DateTimeFormatDataCheck(datetime_column="dates")
+    assert datetime_format_check.validate(X, y) == []
+
+    expected = [
+        DataCheckError(
+            message=f"Column 'dates' has datetime values missing between start and end date.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+        ).to_dict()
+    ]
+    dates = dates.drop("2021-01-21")
+    X = pd.DataFrame({"dates": dates})
+    assert datetime_format_check.validate(X, y) == expected
+
+    expected = [
+        DataCheckError(
+            message=f"Column 'dates' has more than one row with the same datetime value.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_HAS_REDUNDANT_ROW,
+        ).to_dict(),
+        DataCheckError(
+            message=f"Column 'dates' has datetime values missing between start and end date.",
+            data_check_name=datetime_format_check_name,
+            message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+        ).to_dict(),
+    ]
+    dates = dates.append(pd.date_range(dates[-1], periods=2, freq="4D"))
+    X = pd.DataFrame({"dates": dates})
+    assert datetime_format_check.validate(X, y) == expected
+
+
+def test_datetime_format_nan_data_check_error():
+    dates = pd.Series(pd.date_range(start="2021-01-01", periods=20))
+    dates[0] = np.NaN
+    X = pd.DataFrame(dates, columns=["date"])
+
+    expected = [
+        DataCheckError(
+            message="Input datetime column 'date' contains NaN values. Please impute NaN values or drop these rows.",
+            data_check_name=DateTimeFormatDataCheck.name,
+            message_code=DataCheckMessageCode.DATETIME_HAS_NAN,
+        ).to_dict()
+    ]
+
+    dt_nan_check = DateTimeFormatDataCheck(datetime_column="date")
+    assert dt_nan_check.validate(X, pd.Series()) == expected
+
+    dates = pd.Series(pd.date_range(start="2021-01-01", periods=100))
+    dates[0] = np.NaN
+    dates[20] = pd.to_datetime("2021-01-20")
+    X = pd.DataFrame(dates, columns=["date"])
+
+    expected.extend(
+        [
+            DataCheckError(
+                message=f"Column 'date' has more than one row with the same datetime value.",
+                data_check_name=datetime_format_check_name,
+                message_code=DataCheckMessageCode.DATETIME_HAS_REDUNDANT_ROW,
+            ).to_dict(),
+            DataCheckError(
+                message=f"Column 'date' has datetime values missing between start and end date.",
+                data_check_name=datetime_format_check_name,
+                message_code=DataCheckMessageCode.DATETIME_IS_MISSING_VALUES,
+            ).to_dict(),
+        ]
+    )
+    assert dt_nan_check.validate(X, pd.Series()) == expected
+
+
+def test_datetime_nan_check_ww():
+    dt_nan_check = DateTimeFormatDataCheck(datetime_column="dates")
+    y = pd.Series()
+
+    expected = [
+        DataCheckError(
+            message="Input datetime column 'dates' contains NaN values. Please impute NaN values or drop these rows.",
+            data_check_name=DateTimeFormatDataCheck.name,
+            message_code=DataCheckMessageCode.DATETIME_HAS_NAN,
+        ).to_dict()
+    ]
+
+    dates = np.arange(np.datetime64("2017-01-01"), np.datetime64("2017-01-08"))
+    dates[0] = np.datetime64("NaT")
+
+    ww_input = pd.DataFrame(dates, columns=["dates"])
+    ww_input.ww.init()
+    assert dt_nan_check.validate(ww_input, y) == expected
---
+++ packages/evalml/evalml-0.50.0.tar.gz/evalml-0.50.0/evalml/tests/integration_tests/test_data_checks_and_actions_integration.py
@@ -0,0 +1,166 @@
+import numpy as np
+import pandas as pd
+import pytest
+import woodwork as ww
+from pandas.testing import assert_frame_equal, assert_series_equal
+
+from evalml.automl import get_default_primary_search_objective
+from evalml.data_checks import DefaultDataChecks, OutliersDataCheck
+from evalml.data_checks.invalid_target_data_check import InvalidTargetDataCheck
+from evalml.data_checks.null_data_check import NullDataCheck
+from evalml.pipelines import BinaryClassificationPipeline
+from evalml.pipelines.components import (
+    DropColumns,
+    DropRowsTransformer,
+    TargetImputer,
+)
+from evalml.pipelines.components.transformers.imputers.per_column_imputer import (
+    PerColumnImputer,
+)
+from evalml.pipelines.multiclass_classification_pipeline import (
+    MulticlassClassificationPipeline,
+)
+from evalml.pipelines.regression_pipeline import RegressionPipeline
+from evalml.pipelines.utils import make_pipeline_from_data_check_output
+
+
+def test_data_checks_with_healthy_data(X_y_binary):
+    # Checks do not return any error.
+    X, y = X_y_binary
+    data_check = DefaultDataChecks(
+        "binary", get_default_primary_search_objective("binary")
+    )
+    data_checks_output = data_check.validate(X, y)
+
+    assert make_pipeline_from_data_check_output(
+        "binary", data_checks_output
+    ) == BinaryClassificationPipeline(component_graph={}, parameters={}, random_seed=0)
+
+
+def test_data_checks_suggests_drop_and_impute_cols():
+    X = pd.DataFrame(
+        {
+            "null_with_categorical": ["a", None, "b", "c", "c"],
+            "lots_of_null": [None, 7, None, 3, 5],
+            "all_null": [None, None, None, None, None],
+            "no_null": [1, 2, 3, 4, 5],
+        }
+    )
+    X.ww.init(logical_types={"null_with_categorical": "categorical"})
+    y = pd.Series([1, 0, 0, 1, 1])
+    data_check = NullDataCheck()
+    data_checks_output = data_check.validate(X, y)
+
+    action_pipeline = make_pipeline_from_data_check_output("binary", data_checks_output)
+    assert action_pipeline == BinaryClassificationPipeline(
+        component_graph={
+            "Per Column Imputer": [PerColumnImputer, "X", "y"],
+            "Drop Columns Transformer": [
+                DropColumns,
+                "Per Column Imputer.x",
+                "y",
+            ],
+        },
+        parameters={
+            "Per Column Imputer": {
+                "impute_strategies": {
+                    "null_with_categorical": {"impute_strategy": "most_frequent"},
+                    "lots_of_null": {"impute_strategy": "mean"},
+                },
+                "default_impute_strategy": "most_frequent",
+            },
+            "Drop Columns Transformer": {"columns": ["all_null"]},
+        },
+        random_seed=0,
+    )
+    X_expected = pd.DataFrame(
+        {
+            "null_with_categorical": ["a", "c", "b", "c", "c"],
+            "lots_of_null": [5, 7, 5, 3, 5],
+            "no_null": [1, 2, 3, 4, 5],
+        }
+    )
+    X_expected.ww.init(
+        logical_types={"lots_of_null": "double", "null_with_categorical": "categorical"}
+    )
+    action_pipeline.fit(X, y)
+    X_t = action_pipeline.transform(X, y)
+    assert_frame_equal(X_expected, X_t)
+
+
+@pytest.mark.parametrize("problem_type", ["binary", "multiclass", "regression"])
+def test_data_checks_impute_cols(problem_type):
+    X = pd.DataFrame()
+    if problem_type == "binary":
+        y = ww.init_series(pd.Series([0, 1, 1, None, None]))
+        objective = "Log Loss Binary"
+        expected_pipeline_class = BinaryClassificationPipeline
+        y_expected = ww.init_series(pd.Series([0, 1, 1, 1, 1]), logical_type="double")
+
+    elif problem_type == "multiclass":
+        y = ww.init_series(pd.Series([0, 1, 2, 2, None]))
+        objective = "Log Loss Multiclass"
+        expected_pipeline_class = MulticlassClassificationPipeline
+        y_expected = ww.init_series(pd.Series([0, 1, 2, 2, 2]), logical_type="double")
+
+    else:
+        y = ww.init_series(pd.Series([0, 0.1, 0.2, None, None]))
+        objective = "R2"
+        expected_pipeline_class = RegressionPipeline
+        y_expected = ww.init_series(
+            pd.Series([0, 0.1, 0.2, 0.1, 0.1]), logical_type="double"
+        )
+    data_check = InvalidTargetDataCheck(problem_type, objective)
+    data_checks_output = data_check.validate(None, y)
+
+    action_pipeline = make_pipeline_from_data_check_output(
+        problem_type, data_checks_output
+    )
+    expected_parameters = (
+        {"Target Imputer": {"impute_strategy": "mean", "fill_value": None}}
+        if problem_type == "regression"
+        else {
+            "Target Imputer": {"impute_strategy": "most_frequent", "fill_value": None}
+        }
+    )
+    assert action_pipeline == expected_pipeline_class(
+        component_graph={"Target Imputer": [TargetImputer, "X", "y"]},
+        parameters=expected_parameters,
+        random_seed=0,
+    )
+
+    action_pipeline.fit(X, y)
+    _, y_t = action_pipeline.transform(X, y)
+    assert_series_equal(y_expected, y_t)
+
+
+def test_data_checks_suggests_drop_rows():
+    a = np.arange(10) * 0.01
+    data = np.tile(a, (100, 10))
+
+    X = pd.DataFrame(data=data)
+    X.iloc[0, 3] = 1000
+    X.iloc[3, 25] = 1000
+    X.iloc[5, 55] = 10000
+    X.iloc[10, 72] = -1000
+    X.iloc[:, 90] = "string_values"
+    y = pd.Series(np.tile([0, 1], 50))
+
+    outliers_check = OutliersDataCheck()
+    data_checks_output = outliers_check.validate(X)
+
+    action_pipeline = make_pipeline_from_data_check_output("binary", data_checks_output)
+    assert action_pipeline == BinaryClassificationPipeline(
+        component_graph={"Drop Rows Transformer": [DropRowsTransformer, "X", "y"]},
+        parameters={"Drop Rows Transformer": {"indices_to_drop": [0, 3, 5, 10]}},
+        random_seed=0,
+    )
+
+    X_expected = X.drop([0, 3, 5, 10])
+    X_expected.ww.init()
+    y_expected = y.drop([0, 3, 5, 10])
+
+    action_pipeline.fit(X, y)
+    X_t, y_t = action_pipeline.transform(X, y)
+    assert_frame_equal(X_expected, X_t)
+    assert_series_equal(y_expected, y_t)
