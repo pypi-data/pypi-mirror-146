99739404b96edb8606259e8b6839366e4e8b4ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:33 +0000

    Add package scippnexus/0.0.6/scippnexus-0.0.6.tar.gz

---
+++ packages/scippnexus/scippnexus-0.0.6.tar.gz/scippnexus-0.0.6/src/scippnexus/typing.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+from typing import Protocol, Union, Tuple, Dict, List, Callable
+
+
+# TODO Define more required methods
+class H5Dataset(Protocol):
+    """h5py.Dataset-like"""
+    @property
+    def shape(self) -> List[int]:
+        """Shape of a dataset"""
+
+
+class H5Group(Protocol):
+    """h5py.Group-like"""
+    def visititems(self, func: Callable) -> None:
+        """"""
+
+
+# Note that scipp does not support dicts yet, but this HDF5 code does, to
+# allow for loading blocks of 2d (or higher) data efficiently.
+ScippIndex = Union[type(Ellipsis), int, slice, Tuple[str, Union[int, slice]],
+                   Dict[str, Union[int, slice]]]
