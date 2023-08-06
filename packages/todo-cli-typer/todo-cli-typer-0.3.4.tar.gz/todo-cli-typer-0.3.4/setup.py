442b6093cfa96e4cbe809a0dd6214bdfd51efb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:40 +0000

    Add package todo-cli-typer/0.3.4/todo-cli-typer-0.3.4.tar.gz

---
+++ packages/todo-cli-typer/todo-cli-typer-0.3.4.tar.gz/todo-cli-typer-0.3.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['todo_cli_typer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama==0.4.4', 'shellingham==1.4.0', 'typer==0.3.2']
+
+entry_points = \
+{'console_scripts': ['todo = todo_cli_typer.__main__:main']}
+
+setup_kwargs = {
+    'name': 'todo-cli-typer',
+    'version': '0.3.4',
+    'description': 'CLI Todo app with typer, FastAPI and sqlite',
+    'long_description': None,
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
