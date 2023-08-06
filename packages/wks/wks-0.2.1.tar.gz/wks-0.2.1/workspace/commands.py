a9d7743ad72e1ee80ad049ba4675bedc52dca7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:22 +0000

    Add package wks/0.2.1/wks-0.2.1.tar.gz

---
+++ packages/wks/wks-0.2.1.tar.gz/wks-0.2.1/workspace/commands.py
@@ -0,0 +1,5 @@
+import sys
+
+from workspace import command_build, command_cmake, command_help, command_install, command_pull, command_status, command_graph
+
+commands = [command_build, command_cmake, command_help, command_install, command_pull, command_status, command_graph]
