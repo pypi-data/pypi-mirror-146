1d8be9807b3992acd0495ca6e570e49a8b7bfe80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:04 +0000

    Add package ipympl/0.9.0/ipympl-0.9.0-py2.py3-none-any.whl

---
+++ packages/ipympl/ipympl-0.9.0-py2.py3-none-any.whl/ipympl/_version.py
@@ -0,0 +1,9 @@
+version_info = (0, 9, 0)
+__version__ = '.'.join(map(str, version_info))
+
+
+# The versions of protocol of communication with the frontend that this python verison knows
+# how to speak. See counterpart in the src/version.ts file.
+# These should not be changed unless we introduce changes to communication between
+# frontend and backend.
+__MODEL_VERSION__ = "1.0.0"
