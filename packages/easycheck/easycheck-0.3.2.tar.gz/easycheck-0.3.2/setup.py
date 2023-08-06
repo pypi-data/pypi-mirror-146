24b86bb84065fa4f87d4f05c3647769e5bf155a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:09 +0000

    Add package easycheck/0.3.2/easycheck-0.3.2.tar.gz

---
+++ packages/easycheck/easycheck-0.3.2.tar.gz/easycheck-0.3.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+extras_requirements = {
+    "dev": ["pytest==7.0.1", "wheel==0.37.1", ],
+}
+
+setuptools.setup(
+    name="easycheck",
+    version="0.3.2",
+    author="Nyggus & Ke Boan",
+    author_email="nyggus@gmail.com",
+    license="MIT",
+    description="A tool for simple functionalized assertions in Python",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/nyggus/easycheck",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+    extras_require=extras_requirements,
+)
