34354b2b3605420ac688192ce49b1bf25bce9e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:39 +0000

    Add package FireDM/2022.4.14/FireDM-2022.4.14.tar.gz

---
+++ packages/FireDM/FireDM-2022.4.14.tar.gz/FireDM-2022.4.14/firedm/about.py
@@ -0,0 +1,27 @@
+about_notes = """FireDM is an open source multi-connections download manager.
+
+Developed in Python, based on "LibCurl"
+  
+This application is free to use, in a hope to be useful for someone,
+
+Conditions and usage restrictions:
+------------------------------------------------------------------------
+  - The following contents are not allowed to be downloaded using this
+    application: 
+       - DRM "Digital rights management", protected videos / streams 
+         or Copyright materials. 
+       - Porn videos/streams or any pornography materials
+       - Illegal contents or any material that encourage/promote violence
+         or criminal/illegal behaviours.
+  
+  - This application is provided "AS IS" without any warranty, it is 
+    under no circumstances the FireDM author could be held liable for any 
+    claim, or damages or responsible for any misuse of this application.
+------------------------------------------------------------------------
+
+website:
+https://github.com/firedm/FireDM
+
+Author,
+Mahmoud Elshahat
+2019-2022"""
