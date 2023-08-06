c3a44e1594bbdb924c7e6e9ffcc8d86cdc944dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:13 +0000

    Add package whacc/0.1.16/whacc-0.1.16.tar.gz

---
+++ packages/whacc/whacc-0.1.16.tar.gz/whacc-0.1.16/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='whacc',
+      version='0.1.16',
+      author="Phillip Maire",
+      license='MIT',
+      description='Automatic and customizable pipeline for creating a CNN + GBM to predict whiskers contacting objects',
+      packages=find_packages(),
+      author_email='phillip.maire@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/hireslab/whacc",
+      zip_safe=False,
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent"],
+      python_requires='>=3.6',
+      install_requires=["natsort==7.1.1"],
+      package_data={'whacc': ['/whacc_data/*']},
+              # 'datasets/*.h5',
+              #       'model_checkpoints/*',
+              #       'model_checkpoints/model_201008/*',
+              #       'model_checkpoints/model_201008/variables/*',
+              #       'model_checkpoints/model_201008/assets/*']},
+      include_package_data=True)
+
+# package_data={
+#     'whacc': ['datasets/*.h5',
+#               'model_checkpoints/*',
+#               'model_checkpoints/model_201008/*',
+#               'model_checkpoints/model_201008/variables/*',
+#               'model_checkpoints/model_201008/assets/*'],
+# },
+# include_package_data=True,
