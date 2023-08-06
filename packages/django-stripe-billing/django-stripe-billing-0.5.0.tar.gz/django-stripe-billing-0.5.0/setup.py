24fdbe9afee9c9fc429ffe43000c10ee361ddd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:38 +0000

    Add package django-stripe-billing/0.5.0/django-stripe-billing-0.5.0.tar.gz

---
+++ packages/django-stripe-billing/django-stripe-billing-0.5.0.tar.gz/django-stripe-billing-0.5.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="django-stripe-billing",
+    author="Harry Khanna",
+    author_email="harry@khanna.cc",
+    description="Django billing plans and payments with Stripe",
+    license="MIT",
+    url="https://github.com/hkhanna/django-stripe-billing",
+    packages=setuptools.find_packages(),
+    install_requires=["Django", "stripe==2.*"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+    ],
+)
