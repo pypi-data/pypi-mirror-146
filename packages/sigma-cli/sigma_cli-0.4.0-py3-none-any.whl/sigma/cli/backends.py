924a641c02c016cd645e0362ce89f08f229868b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:23 +0000

    Add package sigma-cli/0.4.0/sigma_cli-0.4.0-py3-none-any.whl

---
+++ packages/sigma-cli/sigma_cli-0.4.0-py3-none-any.whl/sigma/cli/backends.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+from sigma.backends.splunk import SplunkBackend
+from sigma.backends.insight_idr import InsightIDRBackend
+
+Backend = namedtuple("Backend", ("cls", "text", "formats"))
+
+backends = {
+    "splunk": Backend(SplunkBackend, "Splunk", {
+        "default": "Plain Splunk queries",
+        "savedsearches": "Splunk savedsearches.conf"
+    }),
+    "insightidr": Backend(InsightIDRBackend, "Rapid7 InsightIDR", {
+        "default": "Simple queries",
+        "leql_advanced_search": "Advanced queries",
+        "leql_detection_definition": "LEQL detection rule logic format",
+    }),
+}
