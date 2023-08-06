af99aa3d53a1c9fe9f966d16f91caeda4ff43ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:08 +0000

    Add package CameraSimulator/0.0.13/CameraSimulator-0.0.13.tar.gz

---
+++ packages/CameraSimulator/CameraSimulator-0.0.13.tar.gz/CameraSimulator-0.0.13/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Basic Camera Emulator'
+LONG_DESCRIPTION = 'Camera emulator'
+
+# Setting up
+setup(
+    name="CameraSimulator",
+    version=VERSION,
+    author="David Navarro ",
+    author_email="<dcnavarros@unal.edu.co>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy',],
+    keywords=['python', 'camera', 'gain'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
