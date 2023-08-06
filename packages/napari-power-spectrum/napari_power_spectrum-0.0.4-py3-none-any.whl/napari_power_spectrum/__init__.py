0b6dd3e25d82738f61b636b2dbd3abbc9c714ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:58 +0000

    Add package napari-power-spectrum/0.0.4/napari_power_spectrum-0.0.4-py3-none-any.whl

---
+++ packages/napari-power-spectrum/napari_power_spectrum-0.0.4-py3-none-any.whl/napari_power_spectrum/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.0.4"
+
+
+
+
+
+from ._widget import calculate_spectrum
---
+++ packages/napari-power-spectrum/napari_power_spectrum-0.0.4-py3-none-any.whl/napari_power_spectrum/_widget.py
@@ -0,0 +1,22 @@
+from magicgui import magic_factory
+import numpy as np
+from numpy.fft import ifft2, fft2, fftshift
+import napari
+from napari import Viewer
+from napari.layers import Image
+
+@magic_factory(call_button = "Calculate Power Spectrum")
+def calculate_spectrum(viewer: Viewer, image: Image,
+                       entire_stack: bool = False)->Image:
+    stack = image.data
+    current_step = viewer.dims.current_step
+    epsilon = 1e-6
+    if entire_stack:
+        ps = np.log((np.abs(fftshift(fft2(stack))))**2+epsilon)
+        im_name = 'Spectrum ' + image.name
+    else:
+        im = stack[current_step[0:-2]]
+        ps = np.log((np.abs(fftshift(fft2(im))))**2+epsilon)
+        im_name = f'Spectrum {image.name} frame_{current_step[0]}'
+        
+    return Image(ps, name = im_name)
