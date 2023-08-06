89c63cb71eb5b28f673b122231434e26143748cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:27 +0000

    Add package toolkit-py/0.17.1/toolkit_py-0.17.1-py3-none-any.whl

---
+++ packages/toolkit-py/toolkit_py-0.17.1-py3-none-any.whl/project_scaffold/templates/golang/internal/service/websocket.go
@@ -0,0 +1,57 @@
+{{GOLANG_HEADER}}
+
+package {{GOLANG_PACKAGE}}
+
+import (
+	"encoding/json"
+	"time"
+)
+
+var (
+	HubServer                    *Hub
+	HubClient                    *Hub
+	HubRealTimeMessageSubscriber *Hub
+	MapClients                   sync.Map
+)
+
+const (
+	Activation     int = iota + 1
+	Authentication
+)
+
+const (
+	KeepAlive     = "KeepAlive"
+	GetPrivateKey = "GetPrivateKey"
+	PostPublicKey = "PostPublicKey"
+	GetPosition   = "GetPosition"
+)
+
+type WebsocketMessage struct {
+	Cmd string `json:"cmd" example:"GetPrivateKey"`
+	query.Response
+}
+
+type WebsocketRealTimeMessage struct {
+	Type    int    `json:"type" example:"1"`
+	Message string `json:"message" example:"以 | 分割字段"`
+}
+
+// RealTimeMessageBroadcast 实时推送
+var RealTimeMessageBroadcast chan []byte
+
+func init() {
+	go func() {
+		message, _ := json.Marshal(WebsocketRealTimeMessage{
+			Type:    1,
+			Message: "golang|python|cpp",
+		})
+
+		for {
+			if HubRealTimeMessageSubscriber != nil {
+				HubRealTimeMessageSubscriber.Broadcast <- message
+			}
+
+			time.Sleep(3 * time.Second)
+		}
+	}()
+}
