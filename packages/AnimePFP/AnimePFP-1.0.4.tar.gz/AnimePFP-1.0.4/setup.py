df964c45053baf0ae5afe32ba0d7460363c9c139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:59 +0000

    Add package AnimePFP/1.0.4/AnimePFP-1.0.4.tar.gz

---
+++ packages/AnimePFP/AnimePFP-1.0.4.tar.gz/AnimePFP-1.0.4/setup.py
@@ -0,0 +1,22 @@
+import re
+from setuptools import setup
+
+with open('AnimePFP/AnimePFP.py', 'r', encoding='utf-8') as file:
+    code = file.read()
+
+file.close()
+
+version_code = re.search(r'__version__ = .*', code).group()
+exec(version_code, globals(), locals())
+
+setup(
+    name='AnimePFP',
+    version=__version__, # type: ignore
+    description='Anime profile picture downloader using https://avatars.alphacoders.com/by_category/3',
+    author='LUA9',
+    maintainer='LUA9',
+    url='https://github.com/LUA9/AnimePFPGenerator',
+    packages=['AnimePFP'],
+    requires=['aiohttp', 'pyppeteer', 'bs4', 'tqdm'],
+    include_package_data=True,
+)
