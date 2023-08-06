676ce0750e8e448aa7f558a05143c218dae4fccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:41 +0000

    Add package octosuite/1.5.1/octosuite-1.5.1-py2.py3-none-any.whl

---
+++ packages/octosuite/octosuite-1.5.1-py2.py3-none-any.whl/octosuite/colors.py
@@ -0,0 +1,16 @@
+import sys
+
+# Colors will be unavailable on non-linux machines
+colors = True
+machine = sys.platform
+if machine.lower().startswith(('os', 'win', 'darwin' 'ios')): 
+    colors = False
+
+if not colors:
+	reset = red = white = green  = ''
+
+else:                                                 
+    white = '\033[97m'
+    red = '\033[91m'    
+    reset = '\033[0m'
+    green = '\033[92m'
