8889aeacf5c6d5d6089c07cfa917a4eb722f3628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:07 +0000

    Add package LibHanger/1.0.33/LibHanger-1.0.33.tar.gz

---
+++ packages/LibHanger/LibHanger-1.0.33.tar.gz/LibHanger-1.0.33/LibHanger/Library/uwDeclare.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class uwDeclare():
+    
+    """
+    各種定義
+    """
+    
+    class result(Enum):
+        
+        """
+        処理結果
+        """
+        
+        success = 0
+        """ 成功 """
+        
+        warning = 1
+        """ 警告 """
+        
+        critical = 2
+        """ 致命的エラー """
