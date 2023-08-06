62bc9b469d7f4458f58cb0472547bad1bf585a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:39 +0000

    Add package krishify-testing-module/0.1.3/krishify-testing-module-0.1.3.tar.gz

---
+++ packages/krishify-testing-module/krishify-testing-module-0.1.3.tar.gz/krishify-testing-module-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='krishify-testing-module',
+    version='0.1.3',
+    license='MIT',
+    author="Ashish Kumar",
+    author_email='ashish@krishify.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://www.krishify.com/',
+    keywords='fbm module',
+    install_requires=[
+          'scikit-learn',
+      ],
+
+)
