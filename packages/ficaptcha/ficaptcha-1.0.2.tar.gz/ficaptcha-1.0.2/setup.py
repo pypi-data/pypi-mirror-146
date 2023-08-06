63fe34f606bc7464f3a54ea5a161223959c65818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:27 +0000

    Add package ficaptcha/1.0.2/ficaptcha-1.0.2.tar.gz

---
+++ packages/ficaptcha/ficaptcha-1.0.2.tar.gz/ficaptcha-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+	name="ficaptcha",
+	version="1.0.2",
+	author="Danil Kononyuk",
+	author_email="me@0x7o.link",
+	description="Simple captcha",
+	long_description="""# fiCaptcha
+Module for image-captcha generation
+
+## Build from source
+```
+git clone https://github.com/0x7o/fiCaptcha
+cd fiCaptcha
+pip install .
+```""",
+	long_description_content_type="text/markdown",
+	url="https://github.com/0x7o/fiCaptcha",
+	packages=setuptools.find_packages(),
+	classifiers=[],
+	python_requires='>=3.6',
+)
