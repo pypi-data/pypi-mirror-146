930e08b44fce78ed850d9d751810eac0b5df66f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:45 +0000

    Add package bibtexautocomplete/1.0.5/bibtexautocomplete-1.0.5-py3-none-any.whl

---
+++ packages/bibtexautocomplete/bibtexautocomplete-1.0.5-py3-none-any.whl/bibtexautocomplete/APIs/__init__.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from ..lookups.abstract_base import LookupType
+from .crossref import CrossrefLookup
+from .dblp import DBLPLookup
+from .researchr import ResearchrLookup
+from .unpaywall import UnpaywallLookup
+
+# List of lookup to use, in the order they will be used
+LOOKUPS: List[LookupType] = [
+    CrossrefLookup,
+    DBLPLookup,
+    ResearchrLookup,
+    UnpaywallLookup,
+]
+LOOKUP_NAMES = [cls.name for cls in LOOKUPS]
+
+__all__ = (
+    "LookupType",
+    "LOOKUPS",
+    "LOOKUP_NAMES",
+    "CrossrefLookup",
+    "DBLPLookup",
+    "ResearchrLookup",
+    "UnpaywallLookup",
+)
