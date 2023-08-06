83d698d1492a89da169df691d3af89d156dbbd11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:39 +0000

    Add package extinfo/0.0.5/extinfo-0.0.5-py3-none-any.whl

---
+++ packages/extinfo/extinfo-0.0.5-py3-none-any.whl/extinfo/cli.py
@@ -0,0 +1,60 @@
+import sys
+
+import click
+import deal
+
+from extinfo.extractors import fileinfo_com, filesuffix_com
+
+from .exceptions import ExtensionNotFoundError
+from .utils import Extractor
+
+deal.activate()
+
+__EXTRACTORS__ = [
+    Extractor(filesuffix_com),
+    Extractor(fileinfo_com),
+]
+
+
+@click.command(
+    no_args_is_help=True,
+)
+@click.version_option()
+@click.option(
+    "--short/--long",
+    help="Print only short descriptions",
+    default=False,
+)
+@click.option(
+    "-1",
+    "--one/--all",
+    help="Print print only one report, from the first extractor",
+    default=False,
+)
+@click.argument("extension", type=str)
+def cli(extension: str, short: bool, one: bool) -> None:
+    for extractor in __EXTRACTORS__:
+        try:
+            results = extractor.extract(extension)
+        except ExtensionNotFoundError as e:
+            print(str(e))
+        else:
+            for report in results:
+                if short:
+                    print(f"{report.description_short}")
+                else:
+                    print(f"# From {extractor.site}\n")
+                    print(f"## {report.description_short}")
+                    print("")
+                    if report.description_long:
+                        print(f"{report.description_long}")
+                        print("")
+                    if report.how_to_open:
+                        print("### How to open")
+                        print("")
+                        print(report.how_to_open)
+                        print("")
+            if one:
+                return
+
+    sys.exit(1)
