08921414275eb57bd985f9d6b74fe0e1750cfde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:57 +0000

    Add package kutilities/0.1.0/kutilities-0.1.0.tar.gz

---
+++ packages/kutilities/kutilities-0.1.0.tar.gz/kutilities-0.1.0/kutilities/__init__.py
@@ -0,0 +1,13 @@
+"""
+kutilities
+
+Provides template utility functions as well as 
+generate base template files for use with webdevelopment frameworks
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Chase Kaleikilo'
+__credits__ = 'Kaleiko'
+
+def health_check():
+    print("hello world!")
---
+++ packages/kutilities/kutilities-0.1.0.tar.gz/kutilities-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='kutilities',
+    version='0.1.0',
+    description='Provides template utility functions as well as generate'
+                'base teßßmplate files used within web development frameworks',
+    url='https://github.com/Kaleiko/kutilities',
+    author='Chase Kaleikilo',
+    author_email='code@kaleiko.net',
+    license='MIT License',
+    packages=['kutilities'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: Freeware',  
+        'Operating System :: POSIX :: Linux',    
+        'Programming Language :: Python :: 3.10',
+    ],
+)
