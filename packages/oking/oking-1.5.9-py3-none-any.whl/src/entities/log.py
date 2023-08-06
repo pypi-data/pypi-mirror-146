9582da8331719a94da8ca6c83de0dd10390bd737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:23 +0000

    Add package oking/1.5.9/oking-1.5.9-py3-none-any.whl

---
+++ packages/oking/oking-1.5.9-py3-none-any.whl/src/entities/log.py
@@ -0,0 +1,7 @@
+
+class Log:
+    def __init__(self, error: str, date_time: str, identifier: str, method: str):
+        self.erro: str = error
+        self.data_hora: str = date_time
+        self.identificador: str = identifier
+        self.metodo: str = method
