6298ec070dda1e3be2df0a780798cc102cb6cd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:32 +0000

    Add package hamming-check/1.0.0/hamming_check-1.0.0-py3-none-any.whl

---
+++ packages/hamming-check/hamming_check-1.0.0-py3-none-any.whl/hamming_check/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the version of the package.
+"""
+
+__title__ = 'hamming-check'
+__description__ = 'hamming-check: File integrity checker'
+__version__ = '1.0.0'
+__author__ = 'Pablo Alessandro Santos Hugen'
+__author_email__ = 'PabloASHugen@protonmail.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/tomcat-42/hamming-check'
