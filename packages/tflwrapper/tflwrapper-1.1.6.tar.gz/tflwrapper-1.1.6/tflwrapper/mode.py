8784a6300d285efa6cfae1e631a8d028d4b9bc15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:00 +0000

    Add package tflwrapper/1.1.6/tflwrapper-1.1.6.tar.gz

---
+++ packages/tflwrapper/tflwrapper-1.1.6.tar.gz/tflwrapper-1.1.6/tflwrapper/mode.py
@@ -0,0 +1,24 @@
+from .tfl import tflAPI
+
+
+class mode(tflAPI):
+    """Mode API, /mode"""
+
+    def getActiveServiceTypes(self):
+        """Returns the service type active for a mode, currently only supports tube"""
+
+        return super(mode, self).sendRequestUnified(
+            "/Mode/ActiveServiceTypes", {}
+        )
+
+    def getAllArrivalPredictions(self, modes: str, count: str):
+        """
+        Returns arrival predictions for all stops
+
+        :param modes: A mode name e.g. tube, dlr
+        :param count: A number of arrivals to return for each stop, -1 to return all available
+        """
+
+        return super(mode, self).sendRequestUnified(
+            f"/Mode/{modes}/Arrivals", {'count': count}
+        )
