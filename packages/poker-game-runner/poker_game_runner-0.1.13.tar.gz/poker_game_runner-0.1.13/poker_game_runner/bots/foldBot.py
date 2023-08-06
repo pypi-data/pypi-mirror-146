db01997bb2214caeeb699630ca896e320f22cee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:39 +0000

    Add package poker-game-runner/0.1.13/poker_game_runner-0.1.13.tar.gz

---
+++ packages/poker-game-runner/poker_game_runner-0.1.13.tar.gz/poker_game_runner-0.1.13/poker_game_runner/bots/foldBot.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from poker_game_runner.state import Observation
+
+class Bot:
+    def get_name(self):
+        return "foldBot"
+
+    def act(self, observation: Observation):
+        return 0
