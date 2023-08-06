b476f7c9e9597640f6aee4553140653a76e58611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:34 +0000

    Add package calculatorall/1.0.2/calculatorall-1.0.2.tar.gz

---
+++ packages/calculatorall/calculatorall-1.0.2.tar.gz/calculatorall-1.0.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['calculatorall']
+setup_kwargs = {
+    'name': 'calculatorall',
+    'version': '1.0.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+}
+
+
+setup(**setup_kwargs)
