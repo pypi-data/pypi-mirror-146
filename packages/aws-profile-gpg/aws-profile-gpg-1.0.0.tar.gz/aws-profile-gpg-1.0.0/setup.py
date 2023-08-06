36239098f51aefe5b5b75143ffca06fb76c98704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:35 +0000

    Add package aws-profile-gpg/1.0.0/aws-profile-gpg-1.0.0.tar.gz

---
+++ packages/aws-profile-gpg/aws-profile-gpg-1.0.0.tar.gz/aws-profile-gpg-1.0.0/setup.py
@@ -0,0 +1,35 @@
+
+from setuptools import setup
+
+setup(
+    name='aws-profile-gpg',
+    author="jeff oconnell",
+    author_email="jeffo@rulez.com",
+    license="MIT",
+
+    url='https://github.com/jefforulez/aws-profile-gpg',
+
+    version='1.0.0',
+
+    description='call aws-cli using access keys from an encrypted credentials file',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    scripts=['bin/aws-profile-gpg'],
+
+    python_requires='>=3.7, <4',
+
+    install_requires=[
+       'botocore>=1.12,<2',
+       'PyGPGME>=0.3,<1',
+    ],
+
+    classifiers=(
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Security :: Cryptography",
+        "Topic :: System :: Systems Administration"
+    )
+)
