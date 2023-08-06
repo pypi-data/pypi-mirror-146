e6167697bd9b5ae4852e5495e07b32619e337536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:21 +0000

    Add package openboxes-client/0.1a2/openboxes_client-0.1a2-py2.py3-none-any.whl

---
+++ packages/openboxes-client/openboxes_client-0.1a2-py2.py3-none-any.whl/openboxes/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openboxes.api.authentication_api import AuthenticationApi
+from openboxes.api.configuration_api import ConfigurationApi
+from openboxes.api.location_api import LocationApi
+from openboxes.api.product_api import ProductApi
+from openboxes.api.user_api import UserApi
