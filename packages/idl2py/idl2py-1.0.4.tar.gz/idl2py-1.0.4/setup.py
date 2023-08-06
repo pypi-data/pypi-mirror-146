e281ca3b8e3ea1d8ed0bdd69f3e56f586b94d375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:39 +0000

    Add package idl2py/1.0.4/idl2py-1.0.4.tar.gz

---
+++ packages/idl2py/idl2py-1.0.4.tar.gz/idl2py-1.0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='idl2py',
+      version='1.0.4',
+      description='Simple IDL to Python converter',
+      author='David Nidever',
+      author_email='dnidever@montana.edu',
+      url='https://github.com/dnidever/idl2py',
+      packages=find_packages(exclude=["tests"]),
+      #package_dir={'idl2py':'python/idl2py'},
+      scripts=['bin/idl2py'],
+      requires=['numpy'],
+      include_package_data=True,
+)
