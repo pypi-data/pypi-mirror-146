dea6efdbb53aa8f2218d1a015093c7ea0ac7ffaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:33 +0000

    Add package feathr/0.3.0/feathr-0.3.0.tar.gz

---
+++ packages/feathr/feathr-0.3.0.tar.gz/feathr-0.3.0/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Use the README.md in root project
+root_path = Path(__file__).resolve().parent.parent
+long_description = (root_path / "README.md").read_text()
+
+setup(
+    name='feathr',
+    version='0.3.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email="frame_dev@linkedin.com",
+    description="An Enterprise-Grade, High Performance Feature Store",
+    url="https://github.com/linkedin/feathr",
+    project_urls={
+        "Bug Tracker": "https://github.com/linkedin/feathr/issues",
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    # consider
+    install_requires=[
+        'Click',
+        "azure-storage-file-datalake>=12.5.0",
+        "azure-synapse-spark>=0.7.0",
+        "azure-identity",
+        "py4j",
+        "loguru",
+        "pandas",
+        "redis",
+        "requests",
+        "pyapacheatlas",
+        "pyhocon",
+        "pandavro",
+        "pyyaml",
+        "Jinja2",
+        "tqdm",
+        "pyarrow",
+        "pyspark>=3.1.2",
+        "python-snappy",
+        "deltalake",
+        "google>=3.0.0",
+        "graphlib_backport",
+        "google-api-python-client>=2.41.0",
+        "azure-keyvault-secrets",
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    entry_points={
+        'console_scripts': ['feathr=feathrcli.cli:cli']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7"
+)
