1fe358acbb33c412cf8da4042a1782bddd7eb056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:30 +0000

    Add package python-boardgamegeek/1.1.0/python_boardgamegeek-1.1.0-py3.8.egg

---
+++ packages/python-boardgamegeek/python_boardgamegeek-1.1.0-py3.8.egg/boardgamegeek/cache.py
@@ -0,0 +1,35 @@
+import requests
+from requests_cache import CachedSession
+
+from .exceptions import BGGValueError
+
+
+class CacheBackend(object):
+    pass
+
+
+class CacheBackendNone(CacheBackend):
+    def __init__(self):
+        self.cache = requests.Session()
+
+
+class CacheBackendMemory(CacheBackend):
+    """ Cache HTTP requests in memory """
+
+    def __init__(self, ttl):
+        try:
+            int(ttl)
+        except ValueError:
+            raise BGGValueError
+        self.cache = CachedSession(backend="memory", expire_after=ttl, allowable_codes=(200,))
+
+
+class CacheBackendSqlite(CacheBackend):
+    def __init__(self, path, ttl, fast_save=True):
+        try:
+            int(ttl)
+        except ValueError:
+            raise BGGValueError
+
+        self.cache = CachedSession(cache_name=path, backend="sqlite", expire_after=ttl, extension="",
+                                   fast_save=fast_save, allowable_codes=(200,))
