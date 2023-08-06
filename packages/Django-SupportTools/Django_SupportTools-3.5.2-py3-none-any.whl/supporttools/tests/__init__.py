27950ff2e80dcf072c74f84e13233b15edaa10e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:52 +0000

    Add package Django-SupportTools/3.5.2/Django_SupportTools-3.5.2-py3-none-any.whl

---
+++ packages/Django-SupportTools/Django_SupportTools-3.5.2-py3-none-any.whl/supporttools/tests/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+from django.urls import reverse
+from django.test.client import RequestFactory
+from userservice.user import UserServiceMiddleware
+
+
+def get_request():
+    """
+    mock request with UserServiceMiddleware initialization
+    """
+    now_request = RequestFactory().get(reverse('supporttools_home'))
+    now_request.session = {}
+    UserServiceMiddleware().process_request(now_request)
+    return now_request
