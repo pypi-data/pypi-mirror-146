2fdf4f8e834de8e3369d13e633cebc54cfb13d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:52:51 +0000

    Add package rollet/0.1.5/rollet-0.1.5-py3-none-any.whl

---
+++ packages/rollet/rollet-0.1.5-py3-none-any.whl/rollet/__init__.py
@@ -0,0 +1,11 @@
+"""
+Rollet
+"""
+
+__version__ = "0.1.5"
+
+
+import rollet.settings as settings
+import rollet.extractor as extractor
+from rollet.pdfclient import grobid_service
+from rollet.rollet import *
