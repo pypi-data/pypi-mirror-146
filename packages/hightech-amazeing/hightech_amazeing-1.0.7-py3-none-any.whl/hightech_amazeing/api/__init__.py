d687b34ba3c73853e6e5d6b2fe78561ffa0fc492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:53:47 +0000

    Add package hightech-amazeing/1.0.7/hightech_amazeing-1.0.7-py3-none-any.whl

---
+++ packages/hightech-amazeing/hightech_amazeing-1.0.7-py3-none-any.whl/hightech_amazeing/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from hightech_amazeing.api.maze_api import MazeApi
+from hightech_amazeing.api.mazes_api import MazesApi
+from hightech_amazeing.api.player_api import PlayerApi
