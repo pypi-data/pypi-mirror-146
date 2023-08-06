2e428a849fe87f66dce25c98e903ff7a4ecabde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:07 +0000

    Add package jupyterhub-singleuser-profiles-id/0.9.6/jupyterhub_singleuser_profiles_id-0.9.6-py2.py3-none-any.whl

---
+++ packages/jupyterhub-singleuser-profiles-id/jupyterhub_singleuser_profiles_id-0.9.6-py2.py3-none-any.whl/jupyterhub_singleuser_profiles/ui/src/utils/types.ts
@@ -0,0 +1,80 @@
+export type EnvVarCategoryType = {
+  name: string;
+  variables: [
+    {
+      name: string;
+      type: string;
+    },
+  ];
+};
+
+export type UiConfigType = {
+  envVarConfig: {
+    enabled: boolean;
+    categories: EnvVarCategoryType[];
+  };
+  gpuConfig: {
+    enabled: boolean;
+    type: string;
+    gpuDropdown?: {
+      start: number;
+      end: number;
+    };
+  };
+  sizeConfig: {
+    enabled: boolean;
+  };
+};
+
+export type EnvVarType = {
+  name: string;
+  type: string;
+  value: string | number;
+};
+
+export type UserConfigMapType = {
+  env?: EnvVarType[];
+  gpu: number;
+  last_selected_image: string;
+  last_selected_size: string;
+};
+
+export type ImageSoftwareType = {
+  name: string;
+  version?: string;
+};
+
+export type ImageTagType = {
+  content?: {
+    software: ImageSoftwareType[];
+    dependencies: ImageSoftwareType[];
+  };
+  name: string;
+  recommended: boolean;
+  default: boolean | undefined;
+  build_status: string | null;
+};
+
+export type ImageType = {
+  description: string | null;
+  url: string | null;
+  display_name: string;
+  name: string;
+  order: number;
+  tags?: ImageTagType[];
+};
+
+export type SizeDescription = {
+  name: string;
+  resources: {
+    limits: {
+      cpu: number;
+      memory: string;
+    };
+    requests: {
+      cpu: number;
+      memory: string;
+    };
+  };
+  schedulable?: boolean;
+};
