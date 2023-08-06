5cdf1b470fac62fe8904c789d806e03723c01f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:25 +0000

    Add package sh3ll/1.1.3/sh3ll-1.1.3.tar.gz

---
+++ packages/sh3ll/sh3ll-1.1.3.tar.gz/sh3ll-1.1.3/sh3ll/sample.py
@@ -0,0 +1,23 @@
+from sh3ll import IS
+
+
+app = IS(name="Depth-Finder", prefix="dp>")
+
+
+@app.command(name="test", aliases=["test", "tst"], help="test command")
+def test(ctx):
+    print(ctx.parameters)
+
+
+@app.command(name="add", aliases=["add", "a"], help="Add command", category="operator")
+def add(ctx):
+    print(int(ctx.parameters[0]) + int(ctx.parameters[1]))
+
+
+@app.command(name="multiply", aliases=["m", "multi"], help="Multiply command", category="operator")
+def multiply(ctx):
+    print(ctx.parameters)
+    print("M:", int(ctx.parameters[0]) * int(ctx.parameters[1]))
+
+
+app.run()
