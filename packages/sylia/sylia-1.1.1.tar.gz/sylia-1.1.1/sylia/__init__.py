5c67a123437f59dcec088fe133cb21ca14be21d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:51 +0000

    Add package sylia/1.1.1/sylia-1.1.1.tar.gz

---
+++ packages/sylia/sylia-1.1.1.tar.gz/sylia-1.1.1/sylia/__init__.py
@@ -0,0 +1,7 @@
+from .sylia import Clock as clock, Screen as screen, init
+from .sylia import Sound as sound, Key as key, Mouse as mouse, running
+
+from .image import Image as image
+from .shape import Shape as shape
+from .text import Text as text
+from .duck import Duck as duck
