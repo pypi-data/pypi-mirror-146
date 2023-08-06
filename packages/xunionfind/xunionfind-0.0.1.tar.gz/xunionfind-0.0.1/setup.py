dde68f1a93e3bca587f84a16bd2608ff90eec2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:43 +0000

    Add package xunionfind/0.0.1/xunionfind-0.0.1.tar.gz

---
+++ packages/xunionfind/xunionfind-0.0.1.tar.gz/xunionfind-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#############################################
+# File Name: setup.py
+# Author: W-Mai
+# Mail: 1341398182@qq.com
+# Created Time:  2022-04-11
+#############################################
+from setuptools import setup, find_packages
+
+
+setup(
+    name="xunionfind",
+    version="0.0.1",
+    description=("easy UnionFind model."),
+    long_description="It's a UnionFind model.",
+    author="W-Mai",
+    author_email="1341398182@qq.com",
+    url="https://github.com/XCLZ-STUDIO/xUnionFind",
+    license="MIT Licence",
+    packages=find_packages(),
+    install_requires=['schemdraw[svgmath]','pillow'],
+)
+
