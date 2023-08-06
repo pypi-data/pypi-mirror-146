3061f3eeef1ddd7f9f9431b1f97d21a206f0e66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:47 +0000

    Add package boneIO/0.3.0.dev7/boneio-0.3.0.dev7.tar.gz

---
+++ packages/boneIO/boneio-0.3.0.dev7.tar.gz/boneio-0.3.0.dev7/boneio/helper/exceptions.py
@@ -0,0 +1,25 @@
+"""BoneIO Errors"""
+
+
+class BoneIOException(Exception):
+    """BoneIO standard exception."""
+
+
+class GPIOInputException(BoneIOException):
+    """GPIOInput Exception."""
+
+
+class I2CError(BoneIOException):
+    """I2C Exception."""
+
+
+class OneWireError(BoneIOException):
+    """One Wire Exception."""
+
+
+class ConfigurationException(BoneIOException):
+    """Configuration yaml exception."""
+
+
+class CoverRelayException(BoneIOException):
+    """Cover configuration exception."""
