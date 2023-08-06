eefb8b057890a8d2cff0564091a33eafca163c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:10 +0000

    Add package PythonStarterPackage/0.4.4/PythonStarterPackage-0.4.4-py3-none-any.whl

---
+++ packages/PythonStarterPackage/PythonStarterPackage-0.4.4-py3-none-any.whl/pythonstarterpackage/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3 -B
+from .starterpkg import StarterPkg
+from .utils.configloader import ConfigLoader
+
+def main():
+	configparser = ConfigLoader()
+	starterpkg = StarterPkg(configparser)
+	starterpkg.run()
+
+if __name__ == '__main__':
+	raise SystemExit(main())
