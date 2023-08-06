f0d5851d5cb06ead0a1f4a5dbeab43134c4c6d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:26 +0000

    Add package webwatcher/1.2.12/webwatcher-1.2.12.tar.gz

---
+++ packages/webwatcher/webwatcher-1.2.12.tar.gz/webwatcher-1.2.12/setup.py
@@ -0,0 +1,41 @@
+import os
+from distutils.core import setup
+
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+
+setup(
+    name='webwatcher',
+    packages=['webwatcher'],
+    version='1.2.12',
+    license='MIT',
+    description='Convert media files to a modern smaller equivalent.',
+    author='cclloyd',
+    author_email='cclloyd@cclloyd.com',
+    url='https://gitlab.com/cclloyd1',
+    keywords=['media', 'webp', 'webm', 'convert', 'compress'],
+    install_requires=[
+        'environs',
+        'watchdog',
+        'ffmpeg-python',
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        # "3 - Alpha"    "4 - Beta"    "5 - Production/Stable"
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
