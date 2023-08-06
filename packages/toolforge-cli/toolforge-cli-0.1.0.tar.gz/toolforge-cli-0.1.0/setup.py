4ed68223a186c13518b6d8cdc0ee07d6fc038742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:47 +0000

    Add package toolforge-cli/0.1.0/toolforge-cli-0.1.0.tar.gz

---
+++ packages/toolforge-cli/toolforge-cli-0.1.0.tar.gz/toolforge-cli-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['toolforge_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'click>=8.0.3,<9.0.0',
+ 'cryptography>=36.0.1,<37.0.0',
+ 'requests>=2.27.1,<3.0.0',
+ 'urllib3>=1.26.8,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['toolforge = toolforge_cli.cli:main']}
+
+setup_kwargs = {
+    'name': 'toolforge-cli',
+    'version': '0.1.0',
+    'description': 'Toolforge client',
+    'long_description': None,
+    'author': 'David Caro',
+    'author_email': 'dcaro@wikimedia.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
