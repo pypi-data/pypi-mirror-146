d6fdd78f6324cf77b2ef3d358249296d14aea099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:26 +0000

    Add package masonite-orm/2.7.0/masonite-orm-2.7.0.tar.gz

---
+++ packages/masonite-orm/masonite-orm-2.7.0.tar.gz/masonite-orm-2.7.0/src/masoniteorm/relationships/__init__.py
@@ -0,0 +1,7 @@
+from .BelongsTo import BelongsTo as belongs_to
+from .BelongsToMany import BelongsToMany as belongs_to_many
+from .HasOne import HasOne as has_one
+from .HasMany import HasMany as has_many
+from .HasOneThrough import HasOneThrough as has_one_through
+from .HasManyThrough import HasManyThrough as has_many_through
+from .MorphTo import MorphTo as morph_to
