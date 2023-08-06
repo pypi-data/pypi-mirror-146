3baf606d4c6899729e2c5e62837bf70841fd41a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:54 +0000

    Add package audit-tools/0.1.2/audit_tools-0.1.2-py3-none-any.whl

---
+++ packages/audit-tools/audit_tools-0.1.2-py3-none-any.whl/audit_tools/core/functions/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+from audit_tools.core.functions.logger import Logger
+
+
+def clear():
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
