cc65cbb79470c149b52369134d024ba46ad1411a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:30 +0000

    Add package functorch/0.1.1/functorch-0.1.1-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/functorch/functorch-0.1.1-cp39-cp39-macosx_10_15_x86_64.whl/functorch/compile/__init__.py
@@ -0,0 +1,28 @@
+from .._src.operator_authoring import pointwise_operator
+from .._src.python_key import pythonkey_decompose, pythonkey_meta
+from .._src.decompositions import register_decomposition, decomposition_table
+from .._src.fx_minifier import minifier, check_nvfuser_subprocess
+from .._src.aot_autograd import (
+    aot_function,
+    aot_module,
+    compiled_function,
+    compiled_module,
+    num_of_recompilations,
+    clear_compile_cache,
+)
+from .._src.compilers import (
+    ts_compile,
+    tvm_compile,
+    draw_graph_compile,
+    nop,
+    nnc_jit,
+    memory_efficient_fusion,
+    debug_compile,
+    print_compile
+)
+from .._src.partitioners import (
+    min_cut_rematerialization_partition,
+    default_partition,
+    draw_graph,
+    draw_joint_graph,
+)
