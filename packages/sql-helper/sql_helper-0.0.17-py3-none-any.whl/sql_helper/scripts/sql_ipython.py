0accd74d2089c39260fa2d3eae4347c59149670d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:59 +0000

    Add package sql-helper/0.0.17/sql_helper-0.0.17-py3-none-any.whl

---
+++ packages/sql-helper/sql_helper-0.0.17-py3-none-any.whl/sql_helper/scripts/sql_ipython.py
@@ -0,0 +1,31 @@
+import click
+import input_helper as ih
+import sql_helper as sqh
+
+
+@click.command()
+@click.option(
+    '--no-colors', '-c', 'no_colors', is_flag=True, default=False,
+    help='Do not use shell colors in ipython'
+)
+@click.option(
+    '--no-vi', '-v', 'no_vi', is_flag=True, default=False,
+    help='Do not use vi editing mode in ipython'
+)
+def main(no_colors, no_vi):
+    """Start an ipython session with an instance of an SQL object"""
+    selected = sqh.select_url_from_settings()
+    if selected:
+        sql = sqh.SQL(selected, attempt_docker=True, wait=True)
+        ih.start_ipython(
+            warn=True,
+            colors=not no_colors,
+            vi=not no_vi,
+            sql=sql
+        )
+    else:
+        print('No connection string selected')
+
+
+if __name__ == '__main__':
+    main()
