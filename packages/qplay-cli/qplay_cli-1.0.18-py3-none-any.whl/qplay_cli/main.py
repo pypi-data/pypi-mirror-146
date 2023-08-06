305a4fb7fb2d10ee441ef6268aca914aba9487e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:58 +0000

    Add package qplay-cli/1.0.18/qplay_cli-1.0.18-py3-none-any.whl

---
+++ packages/qplay-cli/qplay_cli-1.0.18-py3-none-any.whl/qplay_cli/main.py
@@ -0,0 +1,19 @@
+import click
+from qplay_cli.dataset.commands import dataset
+from qplay_cli.backtest.commands import backtest
+from qplay_cli.user.commands import user
+from qplay_cli.machine.commands import machine
+from qplay_cli.broker.commands import broker
+
+@click.group()
+def quantplay():
+    pass
+
+quantplay.add_command(dataset)
+quantplay.add_command(backtest)
+quantplay.add_command(user)
+quantplay.add_command(machine)
+quantplay.add_command(broker)
+    
+if __name__ == '__main__':
+    quantplay()
