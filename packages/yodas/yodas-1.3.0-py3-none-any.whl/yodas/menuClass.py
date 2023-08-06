709ef285441747032044efe1eb92cff83d2753cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:01 +0000

    Add package yodas/1.3.0/yodas-1.3.0-py3-none-any.whl

---
+++ packages/yodas/yodas-1.3.0-py3-none-any.whl/yodas/menuClass.py
@@ -0,0 +1,78 @@
+from .utilities.strings import caseSplit
+
+
+class Menu:
+    """
+    This class takes a list of functions or strings to put in a menu
+
+    items
+    in the format [{"Quit python", exit}]
+
+    execute
+    if true, the menu automatically executes the selected function
+    """
+    def __init__(self, items, title=None, subtitle=None, execute=True):
+        assert type(items) is list
+        self.items = []
+        self.title = title
+        self.subtitle = subtitle
+        self.execute = bool(execute)
+        for item in items:
+            self.add(item)
+
+    def select(self):
+        print("=" * 100)
+        if self.title:
+            print("\n" + self.title)
+        if self.subtitle:
+            print("\n" + self.subtitle)
+
+        for i in range(len(self.items)):
+            functionName = list(self.items[i].keys())[0]
+            print("{0} : {1}".format(i, functionName))
+
+        # Select option
+        option = -1
+        try:
+            option = int(input("\nChoose an option: "))
+        except ValueError:
+            print("Not a number :(")
+            self.select()
+        except NameError:
+            print("Not a number :(")
+            return self.select()
+
+        # Validate
+        if 0 <= option <= len(self.items) - 1:
+            chosenFunctionName = list(self.items[option].keys())[0]
+            chosenFunction = self.items[option][chosenFunctionName]
+        else:
+            print("Number out of bounds")
+            return self.select()
+
+        if self.execute and callable(chosenFunction):
+            chosenFunction()
+        return chosenFunction
+
+    def add(self, item):
+        """
+        This adds items into the menu
+        """
+        if callable(item):  # Is the menu item a function/callable variable?
+            try:
+                name = caseSplit(item.__name__)
+                self.items.append({name: item})
+            except AttributeError:
+                # I will assume it is quit
+                self.items.append({"Quit": item})
+        elif type(item) is dict:
+            # Dictionary item must be in format
+            # {"String", functionVariable}
+            self.items.append(item)
+        else:
+            # is string, float, int... etc
+            self.items.append({item: item})
+
+    def setExecute(self, execute):
+        assert execute is bool
+        self.execute = execute
