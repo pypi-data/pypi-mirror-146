cc2ee3ed0ff2dafe462f0d7d92329455e2238f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package minos-microservice-aggregate/0.7.0.dev1/minos_microservice_aggregate-0.7.0.dev1-py3-none-any.whl

---
+++ packages/minos-microservice-aggregate/minos_microservice_aggregate-0.7.0.dev1-py3-none-any.whl/minos/aggregate/__init__.py
@@ -0,0 +1,82 @@
+"""The Aggregate pattern of the Minos Framework."""
+
+__author__ = "Minos Framework Devs"
+__email__ = "hey@minos.run"
+__version__ = "0.7.0.dev1"
+
+from .actions import (
+    Action,
+)
+from .aggregate import (
+    Aggregate,
+)
+from .collections import (
+    IncrementalSet,
+    IncrementalSetDiff,
+    IncrementalSetDiffEntry,
+)
+from .contextvars import (
+    IS_REPOSITORY_SERIALIZATION_CONTEXT_VAR,
+)
+from .entities import (
+    Entity,
+    EntitySet,
+    ExternalEntity,
+    Ref,
+    RefExtractor,
+    RefInjector,
+    RefResolver,
+    RootEntity,
+)
+from .events import (
+    Event,
+    EventEntry,
+    EventRepository,
+    FieldDiff,
+    FieldDiffContainer,
+    IncrementalFieldDiff,
+    InMemoryEventRepository,
+    PostgreSqlEventRepository,
+)
+from .exceptions import (
+    AggregateException,
+    AlreadyDeletedException,
+    EventRepositoryConflictException,
+    EventRepositoryException,
+    NotFoundException,
+    RefException,
+    SnapshotRepositoryConflictException,
+    SnapshotRepositoryException,
+    TransactionNotFoundException,
+    TransactionRepositoryConflictException,
+    TransactionRepositoryException,
+    ValueObjectException,
+)
+from .queries import (
+    Condition,
+    Ordering,
+)
+from .snapshots import (
+    InMemorySnapshotRepository,
+    PostgreSqlSnapshotQueryBuilder,
+    PostgreSqlSnapshotReader,
+    PostgreSqlSnapshotRepository,
+    PostgreSqlSnapshotSetup,
+    PostgreSqlSnapshotWriter,
+    SnapshotEntry,
+    SnapshotRepository,
+    SnapshotService,
+)
+from .transactions import (
+    TRANSACTION_CONTEXT_VAR,
+    InMemoryTransactionRepository,
+    PostgreSqlTransactionRepository,
+    TransactionEntry,
+    TransactionRepository,
+    TransactionService,
+    TransactionStatus,
+)
+from .value_objects import (
+    ValueObject,
+    ValueObjectSet,
+)
