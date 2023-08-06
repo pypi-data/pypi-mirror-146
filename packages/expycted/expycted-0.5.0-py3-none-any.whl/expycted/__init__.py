6bf1273d28b41db85e57396e5318e316b4b72b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:11 +0000

    Add package expycted/0.5.0/expycted-0.5.0-py3-none-any.whl

---
+++ packages/expycted/expycted-0.5.0-py3-none-any.whl/expycted/__init__.py
@@ -0,0 +1,45 @@
+from typing import Callable, Any
+
+from expycted.internals.filesystem import Directory
+from expycted.internals.function import Function
+from expycted.internals.value import To, ToNot
+
+
+class expect:
+    def __init__(self, value: Any):
+        """Expect a value to be something
+
+        Args:
+            value (Any): Value to check for some sort of condition
+        """
+        self.to = To(value)
+        self.to_not = ToNot(value)
+
+    @staticmethod
+    def function(function: Callable):
+        """Expect a function to do something
+
+        Args:
+            function (callable): Function to check for some sort of condition
+        """
+        return Function(function)
+
+    @staticmethod
+    def value(value: Any):
+        """Expect a value to be something
+
+        Args:
+            value (Any): Value to check for some sort of condition
+        """
+        return expect(value)
+
+    @staticmethod
+    def folder(path: str):
+        """Expect a folder to be something
+
+        Args:
+            path (str): Path to folder to check for some sort of condition
+        """
+        return Directory(path)
+
+    directory = folder
