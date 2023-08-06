0d29082805a92a66b9546b0081e512fb0306b3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:45 +0000

    Add package ai-tensor/0.1/ai_tensor-0.1.tar.gz

---
+++ packages/ai-tensor/ai_tensor-0.1.tar.gz/ai_tensor-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ai_tensor',
+    version='0.1',
+    license='MIT',
+    author="Yuetian Sun",
+    author_email='yuetians@irvineia.org',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='example project',
+    install_requires=[
+          'math',
+          'numpy'
+      ],
+
+)
