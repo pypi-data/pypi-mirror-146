28ea4e925f64a1fbd8be0c77eedeac12d7149a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:15 +0000

    Add package geocachingapi/0.1.1/geocachingapi-0.1.1-py3-none-any.whl

---
+++ packages/geocachingapi/geocachingapi-0.1.1-py3-none-any.whl/geocachingapi/const.py
@@ -0,0 +1,13 @@
+"""Geocaching Api Constants."""
+GEOCACHING_API_SCHEME = "https"
+GEOCACHING_API_HOST = "staging.api.groundspeak.com"
+GEOCACHING_API_PORT = 443
+GEOCACHING_API_BASE_PATH = "/"
+GEOCACHING_API_VERSION = "v1"
+
+MEMBERSHIP_LEVELS = {
+    0: "Unknown",
+    1: "Basic",
+    2: "Charter",
+    3: "Premium"
+}
