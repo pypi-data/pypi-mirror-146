ecd8d56b35ad4d6d6082d1ce73470b80bbbd10f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:29 +0000

    Add package iChingX/0.0.78/iChingX-0.0.78-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/iChingX/iChingX-0.0.78-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl/iChingX/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+iChingX是一套通用的发开工具套件，类似Django，但iChingX不限于Web开发，也可用于桌面开发。
+项目仓库：https://github.com/oneincloud/iChingX
+'''
+__version__ = '0.0.78'
+__author__ = 'Alex<alex@one-in-cloud.com>'
