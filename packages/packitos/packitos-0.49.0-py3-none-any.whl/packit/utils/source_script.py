78d47ab80da5e5b6d0fbb4bbc6c35f75b0578913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:25 +0000

    Add package packitos/0.49.0/packitos-0.49.0-py3-none-any.whl

---
+++ packages/packitos/packitos-0.49.0-py3-none-any.whl/packit/utils/source_script.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from packit.constants import COPR_SOURCE_SCRIPT
+
+
+def create_source_script(
+    url: str,
+    ref: Optional[str] = None,
+    pr_id: Optional[str] = None,
+    merge_pr: Optional[bool] = True,
+    target_branch: Optional[str] = None,
+    job_config_index: Optional[int] = None,
+):
+    options = []
+    if ref:
+        options += ["--ref", ref]
+    if pr_id:
+        options += ["--pr-id", pr_id, f"--{'no-' if not merge_pr else ''}merge-pr"]
+        if merge_pr and target_branch:
+            options += ["--target-branch", target_branch]
+    if job_config_index is not None:
+        options += ["--job-config-index", str(job_config_index)]
+
+    options += [url]
+    return COPR_SOURCE_SCRIPT.format(options=" ".join(options))
