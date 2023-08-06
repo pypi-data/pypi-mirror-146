cadde41ca44d4825994ae6d99a7d130d43c00ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:51 +0000

    Add package tox/4.0.0b2/tox-4.0.0b2-py3-none-any.whl

---
+++ packages/tox/tox-4.0.0b2-py3-none-any.whl/tox/session/cmd/version_flag.py
@@ -0,0 +1,21 @@
+"""
+Display the version information about tox.
+"""
+from __future__ import annotations
+
+from pathlib import Path
+
+from tox.config.cli.parser import ToxParser
+from tox.plugin import impl
+
+
+@impl
+def tox_add_option(parser: ToxParser) -> None:
+    import tox
+    from tox.version import version
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"{version} from {Path(tox.__file__).absolute()}",
+    )
