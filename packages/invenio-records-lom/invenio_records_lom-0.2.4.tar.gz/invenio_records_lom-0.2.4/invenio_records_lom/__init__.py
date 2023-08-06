6f667c1d9791701ed3b5308428bad59c56462932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:14 +0000

    Add package invenio-records-lom/0.2.4/invenio_records_lom-0.2.4.tar.gz

---
+++ packages/invenio-records-lom/invenio_records_lom-0.2.4.tar.gz/invenio_records_lom-0.2.4/invenio_records_lom/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 Graz University of Technology.
+#
+# invenio-records-lom is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+
+"""invenio data model for Learning object metadata."""
+
+from .ext import InvenioRecordsLOM
+from .proxies import current_records_lom
+from .version import __version__
+
+__all__ = (
+    "__version__",
+    "current_records_lom",
+    "InvenioRecordsLOM",
+)
