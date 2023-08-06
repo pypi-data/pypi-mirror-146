9a7a86a31aae804f35841f66735acd4333bd88f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:51 +0000

    Add package edc-form-validators/0.3.9/edc-form-validators-0.3.9.tar.gz

---
+++ packages/edc-form-validators/edc-form-validators-0.3.9.tar.gz/edc-form-validators-0.3.9/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import find_packages, setup
+
+with open(join(dirname(__file__), "README.rst")) as readme:
+    README = readme.read()
+
+with open(join(dirname(__file__), "VERSION")) as f:
+    VERSION = f.read()
+
+# allow setup.py to be run from any path
+os.chdir(normpath(join(abspath(__file__), os.pardir)))
+
+
+setup(
+    name="edc-form-validators",
+    version=VERSION,
+    author="Erik van Widenfelt",
+    author_email="ew2789@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/clinicedc/edc-form-validators",
+    license="GPL license, see LICENSE",
+    description="Form validator classes for django ModelForms",
+    long_description_content_type="text/x-rst",
+    long_description=README,
+    zip_safe=False,
+    keywords="django modelform form validation edc",
+    install_requires=[],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    python_requires=">=3.7",
+)
