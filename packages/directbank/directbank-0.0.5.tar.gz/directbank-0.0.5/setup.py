c33bf767d9bbef1d471e36bfa3f81f52bc646a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:47 +0000

    Add package directbank/0.0.5/directbank-0.0.5.tar.gz

---
+++ packages/directbank/directbank-0.0.5.tar.gz/directbank-0.0.5/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['directbank']
+install_requires = \
+['xsdata==22.4']
+
+setup_kwargs = {
+    'name': 'directbank',
+    'version': '0.0.5',
+    'description': '',
+    'long_description': '# directbank\n### Test',
+    'author': 'Toony',
+    'author_email': 'kizyanov@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/kizyanov/directbank',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
