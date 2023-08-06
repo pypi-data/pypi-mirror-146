f6375f7e8ed0a1fb27bfa3e1aa279ccc7e387fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:14 +0000

    Add package mlogs/0.3.0/mlogs-0.3.0.tar.gz

---
+++ packages/mlogs/mlogs-0.3.0.tar.gz/mlogs-0.3.0/tests/tmp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/9 12:14 PM
+# @Author  : chenDing
+
+from loguru import logger
+
+logger.info("This is log info!")
+logger.warning("This is log warn!")
+logger.error("This is log error!")
+logger.debug("This is log debug!")
