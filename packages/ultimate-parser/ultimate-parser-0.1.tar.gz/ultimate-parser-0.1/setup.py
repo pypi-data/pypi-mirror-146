901f58554e82bdda059594eda6d1a287311061f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:02 +0000

    Add package ultimate-parser/0.1/ultimate-parser-0.1.tar.gz

---
+++ packages/ultimate-parser/ultimate-parser-0.1.tar.gz/ultimate-parser-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1"
+DESCRIPTION = "A simple parser."
+
+setup(
+    name="ultimate-parser",
+    version=VERSION,
+    author="Swanchick (Kyryl Lebedenko)",
+    author_email="Kiryll.Swan@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests']
+)
