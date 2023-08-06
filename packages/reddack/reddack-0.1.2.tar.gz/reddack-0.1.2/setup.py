be57d2dd030e5628346548a30091e9185e03014c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:24 +0000

    Add package reddack/0.1.2/reddack-0.1.2.tar.gz

---
+++ packages/reddack/reddack-0.1.2.tar.gz/reddack-0.1.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Stub setup.py for use with legacy build tooling."""
+
+# Future imports
+from __future__ import (
+    annotations,
+)
+
+# Third party imports
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
