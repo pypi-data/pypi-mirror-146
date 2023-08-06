17b55018742f912ce2392334c72e1377287452e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:29 +0000

    Add package deeptime/0.4.2/deeptime-0.4.2.tar.gz

---
+++ packages/deeptime/deeptime-0.4.2.tar.gz/deeptime-0.4.2/deeptime/plots/__init__.py
@@ -0,0 +1,13 @@
+r"""
+.. currentmodule: deeptime.plots
+
+.. autosummary::
+    :toctree: generated/
+    :template: class_nomodule.rst
+
+    plot_implied_timescales
+    plot_ck_test
+"""
+
+from .implied_timescales import plot_implied_timescales
+from .chapman_kolmogorov import plot_ck_test
