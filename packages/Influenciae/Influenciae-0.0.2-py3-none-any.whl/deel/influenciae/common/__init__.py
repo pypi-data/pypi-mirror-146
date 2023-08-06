c6e277b35003d37950dc10c10d32968bbfb19804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:26 +0000

    Add package Influenciae/0.0.2/Influenciae-0.0.2-py3-none-any.whl

---
+++ packages/Influenciae/Influenciae-0.0.2-py3-none-any.whl/deel/influenciae/common/__init__.py
@@ -0,0 +1,12 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+"""
+Utility classes and functions
+"""
+
+from .tf_operations import find_layer, is_dataset_batched, assert_batched_dataset, dataset_size
+from .model_wrappers import InfluenceModel
+from .conjugate_gradients import conjugate_gradients_solve
+from .compute_hessian_block import compute_hessian_block
---
+++ packages/Influenciae/Influenciae-0.0.2-py3-none-any.whl/deel/influenciae/common/tf_operations.py
@@ -0,0 +1,87 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+"""
+Custom operations related to tensorflow objects
+"""
+
+import tensorflow as tf
+
+from ..types import Union
+
+
+def find_layer(model: tf.keras.Model, layer: Union[str, int]) -> tf.keras.layers.Layer:
+    """
+    Finds a layer in a model either by its name or by its index.
+
+    Parameters
+    ----------
+    model
+        Model on which to search.
+    layer
+        Layer name or layer index
+
+    Returns
+    -------
+    layer
+        Layer found
+    """
+    if isinstance(layer, str):
+        return model.get_layer(layer)
+    if isinstance(layer, int):
+        return model.layers[layer]
+    raise ValueError(f"Could not find any layer {layer}.")
+
+
+def is_dataset_batched(dataset: tf.data.Dataset) -> Union[int, bool]:
+    """
+    Ensure the dataset is batched, if true return the batch_size else false.
+
+    Parameters
+    ----------
+    dataset
+        Tensorflow dataset to check.
+
+    Returns
+    -------
+    batch_size
+        False if the dataset if not batched else the batch_size of the dataset.
+    """
+    if hasattr(dataset, '_batch_size'):
+        return dataset._batch_size # pylint: disable=W0212
+
+    return False
+
+
+def assert_batched_dataset(dataset: tf.data.Dataset):
+    """
+    Throw an error if the dataset is not batched.
+
+    Parameters
+    ----------
+    dataset
+        Tensorflow dataset to check.
+    """
+    if not is_dataset_batched(dataset):
+        raise ValueError("The dataset must be batched before performing this operation.")
+
+
+def dataset_size(dataset: tf.data.Dataset):
+    """
+    Compute the size of a batched tensorflow dataset: batch_size * number of batchs
+
+    Parameters
+    ----------
+    dataset
+        Tensorflow dataset to check.
+
+    Returns
+    -------
+    size
+        Number of points in the dataset
+    """
+    assert_batched_dataset(dataset)
+
+    size = dataset.cardinality().numpy() * dataset._batch_size # pylint: disable=W0212
+    return size
