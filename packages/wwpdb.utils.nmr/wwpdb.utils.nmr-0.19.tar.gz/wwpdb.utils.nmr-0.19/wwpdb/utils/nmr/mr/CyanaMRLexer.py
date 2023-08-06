65e57e8c939f21e088bcd225761275404ce5a2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:27 +0000

    Add package wwpdb.utils.nmr/0.19/wwpdb.utils.nmr-0.19.tar.gz

---
+++ packages/wwpdb.utils.nmr/wwpdb.utils.nmr-0.19.tar.gz/wwpdb.utils.nmr-0.19/wwpdb/utils/nmr/mr/CyanaMRLexer.py
@@ -0,0 +1,92 @@
+# Generated from CyanaMRLexer.g4 by ANTLR 4.9
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\b")
+        buf.write("w\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write("\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r\4\16")
+        buf.write("\t\16\4\17\t\17\4\20\t\20\4\21\t\21\3\2\3\2\3\3\5\3\'")
+        buf.write("\n\3\3\3\3\3\5\3+\n\3\3\4\3\4\3\4\5\4\60\n\4\3\4\3\4\5")
+        buf.write("\4\64\n\4\3\5\3\5\3\6\6\69\n\6\r\6\16\6:\3\7\3\7\3\b\3")
+        buf.write("\b\3\t\3\t\5\tC\n\t\3\n\3\n\5\nG\n\n\3\13\3\13\5\13K\n")
+        buf.write("\13\3\f\3\f\5\fO\n\f\3\r\3\r\5\rS\n\r\3\16\3\16\7\16W")
+        buf.write("\n\16\f\16\16\16Z\13\16\3\17\6\17]\n\17\r\17\16\17^\3")
+        buf.write("\17\3\17\3\20\3\20\3\20\7\20f\n\20\f\20\16\20i\13\20\3")
+        buf.write("\20\3\20\3\20\3\20\3\21\3\21\7\21q\n\21\f\21\16\21t\13")
+        buf.write("\21\3\21\3\21\3g\2\22\3\3\5\4\7\2\t\2\13\2\r\5\17\2\21")
+        buf.write("\2\23\2\25\2\27\2\31\2\33\2\35\6\37\7!\b\3\2\t\4\2--/")
+        buf.write("/\3\2\62;\4\2C\\c|\5\2))--/\60\5\2\13\f\17\17\"\"\7\2")
+        buf.write("##%%,,\61\61==\4\2\f\f\17\17\2{\2\3\3\2\2\2\2\5\3\2\2")
+        buf.write("\2\2\r\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2!\3\2\2\2\3")
+        buf.write("#\3\2\2\2\5&\3\2\2\2\7\63\3\2\2\2\t\65\3\2\2\2\138\3\2")
+        buf.write("\2\2\r<\3\2\2\2\17>\3\2\2\2\21B\3\2\2\2\23F\3\2\2\2\25")
+        buf.write("J\3\2\2\2\27N\3\2\2\2\31R\3\2\2\2\33T\3\2\2\2\35\\\3\2")
+        buf.write("\2\2\37b\3\2\2\2!n\3\2\2\2#$\5\13\6\2$\4\3\2\2\2%\'\t")
+        buf.write("\2\2\2&%\3\2\2\2&\'\3\2\2\2\'*\3\2\2\2(+\5\13\6\2)+\5")
+        buf.write("\7\4\2*(\3\2\2\2*)\3\2\2\2+\6\3\2\2\2,-\5\13\6\2-/\7\60")
+        buf.write("\2\2.\60\5\13\6\2/.\3\2\2\2/\60\3\2\2\2\60\64\3\2\2\2")
+        buf.write("\61\62\7\60\2\2\62\64\5\13\6\2\63,\3\2\2\2\63\61\3\2\2")
+        buf.write("\2\64\b\3\2\2\2\65\66\t\3\2\2\66\n\3\2\2\2\679\5\t\5\2")
+        buf.write("8\67\3\2\2\29:\3\2\2\2:8\3\2\2\2:;\3\2\2\2;\f\3\2\2\2")
+        buf.write("<=\5\33\16\2=\16\3\2\2\2>?\t\4\2\2?\20\3\2\2\2@C\5\17")
+        buf.write("\b\2AC\5\t\5\2B@\3\2\2\2BA\3\2\2\2C\22\3\2\2\2DG\5\21")
+        buf.write("\t\2EG\7a\2\2FD\3\2\2\2FE\3\2\2\2G\24\3\2\2\2HK\5\23\n")
+        buf.write("\2IK\t\5\2\2JH\3\2\2\2JI\3\2\2\2K\26\3\2\2\2LO\5\21\t")
+        buf.write("\2MO\7)\2\2NL\3\2\2\2NM\3\2\2\2O\30\3\2\2\2PS\5\21\t\2")
+        buf.write("QS\t\2\2\2RP\3\2\2\2RQ\3\2\2\2S\32\3\2\2\2TX\5\23\n\2")
+        buf.write("UW\5\25\13\2VU\3\2\2\2WZ\3\2\2\2XV\3\2\2\2XY\3\2\2\2Y")
+        buf.write("\34\3\2\2\2ZX\3\2\2\2[]\t\6\2\2\\[\3\2\2\2]^\3\2\2\2^")
+        buf.write("\\\3\2\2\2^_\3\2\2\2_`\3\2\2\2`a\b\17\2\2a\36\3\2\2\2")
+        buf.write("bg\7}\2\2cf\5\37\20\2df\13\2\2\2ec\3\2\2\2ed\3\2\2\2f")
+        buf.write("i\3\2\2\2gh\3\2\2\2ge\3\2\2\2hj\3\2\2\2ig\3\2\2\2jk\7")
+        buf.write("\177\2\2kl\3\2\2\2lm\b\20\3\2m \3\2\2\2nr\t\7\2\2oq\n")
+        buf.write("\b\2\2po\3\2\2\2qt\3\2\2\2rp\3\2\2\2rs\3\2\2\2su\3\2\2")
+        buf.write("\2tr\3\2\2\2uv\b\21\3\2v\"\3\2\2\2\22\2&*/\63:BFJNRX^")
+        buf.write("egr\4\b\2\2\2\3\2")
+        return buf.getvalue()
+
+
+class CyanaMRLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    Integer = 1
+    Float = 2
+    Simple_name = 3
+    SPACE = 4
+    COMMENT = 5
+    LINE_COMMENT = 6
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+ ]
+
+    symbolicNames = [ "<INVALID>",
+            "Integer", "Float", "Simple_name", "SPACE", "COMMENT", "LINE_COMMENT" ]
+
+    ruleNames = [ "Integer", "Float", "DEC_DOT_DEC", "DEC_DIGIT", "DECIMAL", 
+                  "Simple_name", "ALPHA", "ALPHA_NUM", "START_CHAR", "NAME_CHAR", 
+                  "ATM_NAME_CHAR", "ATM_TYPE_CHAR", "SIMPLE_NAME", "SPACE", 
+                  "COMMENT", "LINE_COMMENT" ]
+
+    grammarFileName = "CyanaMRLexer.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.9")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
