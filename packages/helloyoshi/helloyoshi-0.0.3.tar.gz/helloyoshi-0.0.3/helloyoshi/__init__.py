b3f3bb8037f8e03b523f0bc90abef2873dc16b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:53 +0000

    Add package helloyoshi/0.0.3/helloyoshi-0.0.3.tar.gz

---
+++ packages/helloyoshi/helloyoshi-0.0.3.tar.gz/helloyoshi-0.0.3/helloyoshi/__init__.py
@@ -0,0 +1,13 @@
+# from hello import sayhello
+from pyfiglet import figlet_format
+
+print(figlet_format("Hello from Yoshi"))
+
+class hy:
+    def __init__(self):
+        print("Constructer success build")
+
+    def working(self, b):
+        # print(sayhello(5))
+        # print(f'{b} is working')
+        return "Hello"
