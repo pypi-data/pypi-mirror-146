059dc9866c915851a5e7c2f6f5422e537ae42869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:48 +0000

    Add package PyHyperChat/0.0.3/PyHyperChat-0.0.3.tar.gz

---
+++ packages/PyHyperChat/PyHyperChat-0.0.3.tar.gz/PyHyperChat-0.0.3/PyHyperChat/__init__.py
@@ -0,0 +1,33 @@
+from .chat import ChatManager
+from .api import API
+from functools import wraps
+
+
+class HyperChat:
+
+    def __init__(self, token):
+        self.host = "86.127.254.51:8519"
+        self.ssl = False
+        self.token = token
+        self.rutas = {}
+        self.api = API(self.host, self.token, ssl_active=self.ssl)
+        self.chat_manager = None
+
+    def run(self):
+        self.chat_manager = ChatManager(self, self.host, self.token, self.api.lista_canales(), ssl_active=self.ssl)
+
+    def route(self, command):
+        def decorator(f):
+            @wraps(f)
+            def wrapper(*args, **kwargs):
+                f(args, kwargs)
+
+            self.rutas[command] = wrapper
+            return wrapper
+
+        return decorator
+
+    def send(self, request, mensaje):
+        token = request.get("channel")
+        chat = self.chat_manager.chat(token)
+        chat.enviar(mensaje)
