1fdf38a68293657d429bc35e0f8a5a48f8d50f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:38 +0000

    Add package ymir-cmd/0.5.0/ymir_cmd-0.5.0-py3-none-any.whl

---
+++ packages/ymir-cmd/ymir_cmd-0.5.0-py3-none-any.whl/mir/tools/errors.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+
+class MirError(Exception):
+    """
+    error raised by ymir-command
+    """
+    def __init__(self, error_code: int, error_message: str) -> None:
+        super().__init__()
+        self.error_code = error_code
+        self.error_message = error_message
+
+    def __str__(self) -> str:
+        return f"code: {self.error_code}, content: {self.error_message}"
+
+
+class MirRuntimeError(MirError):
+    """
+
+    error raised by ymir-command
+
+    if raised from a function surrounded by decorator `@command_run_in_out`,
+    new commit will made if `needs_new_commit` true
+
+    """
+    def __init__(self, error_code: int, error_message: str, needs_new_commit: bool = True, task: Any = None):
+        super().__init__(error_code=error_code, error_message=error_message)
+        self.needs_new_commit = needs_new_commit
+        self.task = task
+
+    def __copy__(self) -> 'MirRuntimeError':
+        return MirRuntimeError(error_code=self.error_code,
+                               error_message=self.error_message,
+                               needs_new_commit=self.needs_new_commit,
+                               task=self.task)
+
+    def __str__(self) -> str:
+        return f"code: {self.error_code} content: {self.error_message}, needs new commit: {self.needs_new_commit}"
