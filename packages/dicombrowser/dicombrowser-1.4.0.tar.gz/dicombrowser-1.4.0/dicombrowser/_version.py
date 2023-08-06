64fb3c74353c8cc60bb9f06d390ab464881ef275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:24 +0000

    Add package dicombrowser/1.4.0/dicombrowser-1.4.0.tar.gz

---
+++ packages/dicombrowser/dicombrowser-1.4.0.tar.gz/dicombrowser-1.4.0/dicombrowser/_version.py
@@ -0,0 +1,25 @@
+# DicomBrowser
+# Copyright (C) 2016-9 Eric Kerfoot, King's College London, all rights reserved
+#
+# This file is part of DicomBrowser.
+#
+# DicomBrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DicomBrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program (LICENSE.txt).  If not, see <http://www.gnu.org/licenses/>
+
+__appname__ = "DicomBrowser"
+__version_info__ = (1, 4, 0)  # global application version, major/minor/patch
+__version__ = "%i.%i.%i" % __version_info__
+__author__ = "Eric Kerfoot"
+__author_email__="eric.kerfoot@kcl.ac.uk"
+__copyright__ = "Copyright (c) 2016-22 Eric Kerfoot, King's College London, all rights reserved. Licensed under the GPL (see LICENSE.txt)."
+
