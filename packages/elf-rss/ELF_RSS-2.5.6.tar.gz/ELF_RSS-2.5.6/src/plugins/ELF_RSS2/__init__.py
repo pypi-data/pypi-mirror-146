a8e1a01dcfb850546b18035401292399e4a41ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:57:10 +0000

    Add package elf-rss/2.5.6/ELF_RSS-2.5.6.tar.gz

---
+++ packages/elf-rss/ELF_RSS-2.5.6.tar.gz/ELF_RSS-2.5.6/src/plugins/ELF_RSS2/__init__.py
@@ -0,0 +1,14 @@
+from . import (
+    add_cookies,
+    add_dy,
+    bot_info,
+    change_dy,
+    config,
+    del_dy,
+    patch,
+    permission,
+    show_all,
+    show_dy,
+    start,
+    upload_group_file,
+)
