6d4fdb0107c5a3d263e80c4ce251eeaa86accbf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:00 +0000

    Add package kabbes-aws-credentials/0.4.0/kabbes_aws_credentials-0.4.0.tar.gz

---
+++ packages/kabbes-aws-credentials/kabbes_aws_credentials-0.4.0.tar.gz/kabbes_aws_credentials-0.4.0/src/aws_credentials/__main__.py
@@ -0,0 +1,14 @@
+import sys
+import time
+sys_args = sys.argv[1:]
+
+from aws_credentials import update
+
+SUCCESS_SLEEPY_TIME = 1
+ERROR_SLEEPY_TIME = 5
+
+if update():
+    time.sleep( SUCCESS_SLEEPY_TIME )
+else:
+    time.sleep( ERROR_SLEEPY_TIME )
+
