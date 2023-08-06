38c9750dc425195600ab4b983b078352a5d22e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:13 +0000

    Add package django-dynamic-model-admin/0.2.2/django_dynamic_model_admin-0.2.2-py3-none-any.whl

---
+++ packages/django-dynamic-model-admin/django_dynamic_model_admin-0.2.2-py3-none-any.whl/django_dynamic_model_admin/__init__.py
@@ -0,0 +1,10 @@
+default_app_config = "django_dynamic_model_admin.apps.DjangoDynamicModelAdminConfig"
+
+app_requires = [
+    "mptt",
+    "django_dynamic_resource_admin",
+    "django_cascading_dropdown_widget",
+    "django_gazing_select_widget",
+    "django_tabbed_changeform_admin",
+    "django_yearmonth_widget",
+]
