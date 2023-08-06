588e83e39d60ecac6b726e2487c71b947a70a8a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:17 +0000

    Add package hypatie/2.15.1/hypatie-2.15.1.tar.gz

---
+++ packages/hypatie/hypatie-2.15.1.tar.gz/hypatie-2.15.1/hypatie/__init__.py
@@ -0,0 +1,7 @@
+from .horizons import Vector, Observer, download
+from .animation import Body, play, play2d
+from .transform import mag, radec_to_altaz, sph2car, car2sph 
+from .simbad import sql2df
+from .time import datetime_to_jd, jd_to_datetime, utc2tdb, tdb2utc
+
+__version__ = "2.15.1"
