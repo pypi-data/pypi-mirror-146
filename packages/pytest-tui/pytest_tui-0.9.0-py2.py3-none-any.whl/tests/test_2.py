624848f102777797c7f56f18b81ab8ff8db862ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:37 +0000

    Add package pytest-tui/0.9.0/pytest_tui-0.9.0-py2.py3-none-any.whl

---
+++ packages/pytest-tui/pytest_tui-0.9.0-py2.py3-none-any.whl/tests/test_2.py
@@ -0,0 +1,32 @@
+import pytest
+import logging
+import sys
+
+LOG_LEVELS = ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+logger = logging.getLogger()
+logger.setLevel(logging.NOTSET)
+logger.propagate = True
+stdout_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stdout_handler)
+logging.getLogger("faker").setLevel(logging.ERROR)
+
+# These tests have the same name as in testfile test_1.py
+# Used for testing ability to handle duplicate tes names
+# across different files
+
+
+@pytest.fixture
+def error_fixture():
+    assert 0
+
+
+def test_a_ok():
+    print("ok")
+
+
+def test_b_fail():
+    assert 0
+
+
+def test_c_error(error_fixture):
+    pass
