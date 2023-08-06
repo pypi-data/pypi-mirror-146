f1eaaed4385fb42d846adaeecb5331adeb7fd29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:46 +0000

    Add package shopyo/4.4.2/shopyo-4.4.2.tar.gz

---
+++ packages/shopyo/shopyo-4.4.2.tar.gz/shopyo-4.4.2/shopyo/__init__.py
@@ -0,0 +1,25 @@
+version_info = (4, 4, 2)
+__version__ = ".".join([str(v) for v in version_info])
+
+
+"""
+
+4.4.2
+
+- Fix email-validator version
+
+4.4.1
+
+- Fix app.py on new projects
+- Fix validator required in reqs
+- Fix remove settings functions from www.view
+
+4.4.0
+
+- Add global configs via module
+
+4.3.7
+
+- Remove 3.6 support
+
+"""
