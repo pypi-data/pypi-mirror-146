63e45f54712a00db926b0a539c586239f6faebc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:21 +0000

    Add package elegy/0.9.0.dev0/elegy-0.9.0.dev0-py3-none-any.whl

---
+++ packages/elegy/elegy-0.9.0.dev0-py3-none-any.whl/elegy/__init__.py
@@ -0,0 +1,29 @@
+# isort:skip_file
+
+__version__ = "0.8.6"
+
+from treex import *
+
+import elegy.types as types
+import elegy.utils as utils
+
+
+from . import (
+    callbacks,
+    data,
+    model,
+    # nets,
+)
+
+from .model.model import Model
+from .model.model_base import ModelBase, load
+from .model.model_core import (
+    GradStepOutput,
+    PredStepOutput,
+    TestStepOutput,
+    TrainStepOutput,
+    LossStepOutput,
+    ModelCore,
+)
+from .types import KeySeq
+from .utils import inject_dependencies
