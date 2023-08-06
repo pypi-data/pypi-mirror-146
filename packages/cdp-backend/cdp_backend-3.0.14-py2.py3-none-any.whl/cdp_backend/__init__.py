63b95ba8c8b106bb00241d9874f494d7ff992741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:04 +0000

    Add package cdp-backend/3.0.14/cdp_backend-3.0.14-py2.py3-none-any.whl

---
+++ packages/cdp-backend/cdp_backend-3.0.14-py2.py3-none-any.whl/cdp_backend/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cdp-backend."""
+
+__author__ = "Jackson Maxfield Brown"
+__email__ = "jmaxfieldbrown@gmail.com"
+
+from .version import __version__  # noqa: F401
