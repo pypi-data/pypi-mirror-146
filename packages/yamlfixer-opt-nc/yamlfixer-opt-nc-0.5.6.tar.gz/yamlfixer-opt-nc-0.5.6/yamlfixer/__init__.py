ffe7de288ccd5909280f86f014aa01b6db61bd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:45 +0000

    Add package yamlfixer-opt-nc/0.5.6/yamlfixer-opt-nc-0.5.6.tar.gz

---
+++ packages/yamlfixer-opt-nc/yamlfixer-opt-nc-0.5.6.tar.gz/yamlfixer-opt-nc-0.5.6/yamlfixer/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2022 OPT-NC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""yamlfixer automates the fixing of problems reported by yamllint
+by feeding it with files and parsing its output."""
+
+import time
+
+__version__ = "0.5.6"
+__author__ = "OPT-NC"
+__license__ = "GPLv3+"
+__copyright__ = "Copyright (C) 2021-%s %s" % (time.strftime("%Y",
+                                                            time.localtime(time.time())),
+                                              __author__)
