dd9a353b3b7af94d13cf9d818f9ca556a3edc388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:44 +0000

    Add package zzha529-test/1.16/zzha529_test-1.16-py3-none-any.whl

---
+++ packages/zzha529-test/zzha529_test-1.16-py3-none-any.whl/RedisQueue/redis_connector.py
@@ -0,0 +1,47 @@
+import json
+import redis
+import random
+import configparser
+
+filename = 'config.ini'
+
+
+def singleton(cls, *args, **kw):
+    instances = {}
+
+    def _singleton():
+        if cls not in instances:
+            instances[cls] = cls(*args, **kw)
+        return instances[cls]
+
+    return _singleton
+
+
+@singleton
+class RedisPool(object):
+    def __init__(self):
+        con = configparser.ConfigParser()
+
+        con.read(filename, encoding='utf8')
+
+        sections = con.sections()
+
+        self.host = "localhost"
+        self.password = None
+        self.port = 6379
+        self.db = 0
+        if 'redis' in sections:
+            redis_section = con['redis']
+            if 'port' in redis_section:
+                self.port = redis_section['port']
+            if 'host' in redis_section:
+                self.host = redis_section['host']
+            if 'db' in redis_section:
+                self.db = redis_section['db']
+            if 'password' in redis_section:
+                self.password = redis_section['password']
+        self.random = random.random()
+        self.pool = redis.ConnectionPool(host=self.host, port=self.port, db=self.db, password=self.password, decode_responses=True)
+
+    def connector(self):
+        return redis.Redis(connection_pool=self.pool, decode_responses=True)
---
+++ packages/zzha529-test/zzha529_test-1.16-py3-none-any.whl/run/multi_process.py
@@ -0,0 +1,60 @@
+import json
+import multiprocessing
+import time
+import traceback
+
+from run.exception.local_exceptions import NumberProcessException
+from run.redis_connector import RedisPool
+
+
+# rpop from redis given key as list name
+# return rpop result
+def single_start(key, pid, func):
+    """
+    read list-key from redis,
+    pop list element,
+    call function with popped element as param
+    :param key: model name defined in tool and model
+    :param pid: process id
+    :param func: call model function
+    """
+    print('process start: 【' + str(pid) + '】')
+    pool = RedisPool()
+    conn = pool.connector()
+
+    while True:
+        val = conn.brpop(key)
+        print(pid, val[1])
+
+        # do ack
+        # do persistance work
+
+        try:
+            val_json = json.loads(val[1])
+            func(val_json)
+            # do exception work
+        except:
+            traceback.print_exc()
+            # record failed vals
+            err_json = {"val": val[1], "timestamp": int(round(time.time() * 1000))}
+            conn.lpush('failed_' + key, json.dumps(err_json))
+
+
+def start(key: str, func, np: int = 5):
+    """
+    call np number of processes,
+    do func work, func params read from redis
+    :param key: model name defined in tool and model
+    :param func: call model function
+    :param np: number of processes, default 5
+    """
+
+    if np < 1:
+        raise NumberProcessException(np)
+        return
+
+    for i in range(np):
+        p = multiprocessing.Process(target=single_start, kwargs={"key": key, "pid": i, "func": func})
+        p.start()
+
+        time.sleep(0.5)
