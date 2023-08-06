323078f3917f9ca8beb6075add35756f798f5488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:06 +0000

    Add package adhoccomputing/2.0.0/adhoccomputing-2.0.0.tar.gz

---
+++ packages/adhoccomputing/adhoccomputing-2.0.0.tar.gz/adhoccomputing-2.0.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+  # TODO: Fix the configs in this part
+  name='adhoccomputing',
+  version='2.0.0',    
+  description="Ad Hoc Computing Library",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/cengwins/ahc',
+  author='CengWins',
+  author_email='ahc@ceng.metu.edu.tr',
+  license='BSD 2-clause',
+  packages=['adhoccomputing'],
+  install_requires=[
+    'matplotlib',
+    'mnist',
+    'networkx',
+    'numpy',
+    'pandas',
+    'Pillow',
+    'pygraphviz',
+    'scikit_learn',
+    'seaborn',
+    'setuptools',
+    'torch',
+    'yaml'
+  ],
+  project_urls={
+        "Bug Tracker": "https://github.com/cengwins/ahc/issues",
+        "Simulator Website": "https://ahc.ceng.metu.edu.tr"
+  },
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+  python_requires=">=3.10",
+)
