b668e7dae761c40e7847c582d4aaddc2cbe48d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:22 +0000

    Add package basic-robotics/0.2.12/basic_robotics-0.2.12.tar.gz

---
+++ packages/basic-robotics/basic_robotics-0.2.12.tar.gz/basic_robotics-0.2.12/basic_robotics/robot_collisions/__init__.py
@@ -0,0 +1,6 @@
+from .collision_manager import createBox, createCylinder, createSphere, createMesh
+from .collision_manager import ColliderManager
+from .collision_manager import ColliderObject
+from .collision_manager import ColliderArm
+from .collision_manager import ColliderSP
+from .collision_manager import ColliderObstacles
---
+++ packages/basic-robotics/basic_robotics-0.2.12.tar.gz/basic_robotics-0.2.12/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='basic_robotics',
+      version='0.2.12',
+      long_description=README,
+      long_description_content_type='text/markdown',
+      description='Basic Robotics Toolbox Developed in FASER Lab',
+      url='https://github.com/64-B1T/basic_robotics',
+      author='William Chapin',
+      author_email='liam@64b1t.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'numpy <= 1.21.5, >= 1.19',
+          'pyserial',
+          'scipy',
+          'numba',
+          'modern_robotics',
+          'numpy-stl',
+          'descartes',
+          'alphashape',
+          'trimesh',
+          'rtree',
+      ],
+      zip_safe=False)
