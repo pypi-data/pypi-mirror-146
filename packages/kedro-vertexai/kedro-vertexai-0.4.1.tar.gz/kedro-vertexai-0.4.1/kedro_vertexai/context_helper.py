597bf80f339014450a2a448346537e29e26b1bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:48 +0000

    Add package kedro-vertexai/0.4.1/kedro-vertexai-0.4.1.tar.gz

---
+++ packages/kedro-vertexai/kedro-vertexai-0.4.1.tar.gz/kedro-vertexai-0.4.1/kedro_vertexai/context_helper.py
@@ -0,0 +1,79 @@
+import os
+from functools import lru_cache
+from typing import Dict, Iterable
+
+from kedro.config import TemplatedConfigLoader
+
+from kedro_vertexai.client import VertexAIPipelinesClient
+
+from .config import PluginConfig
+from .constants import KEDRO_GLOBALS_PATTERN
+
+
+class EnvTemplatedConfigLoader(TemplatedConfigLoader):
+    """Config loader that can substitute $(commit_id) and $(branch_name)
+    placeholders with information taken from env variables."""
+
+    VAR_PREFIX = "KEDRO_CONFIG_"
+    # defaults provided so default variables ${commit_id|dirty} work for some entries
+    ENV_DEFAULTS = {"commit_id": None, "branch_name": None, "run_id": ""}
+
+    def __init__(self, conf_paths: Iterable[str]):
+        super().__init__(
+            conf_paths,
+            globals_dict=self.read_env(),
+            globals_pattern=os.getenv(KEDRO_GLOBALS_PATTERN, None),
+        )
+
+    def read_env(self) -> Dict:
+        config = EnvTemplatedConfigLoader.ENV_DEFAULTS.copy()
+        overrides = {
+            k.replace(EnvTemplatedConfigLoader.VAR_PREFIX, "").lower(): v
+            for k, v in os.environ.copy().items()
+            if k.startswith(EnvTemplatedConfigLoader.VAR_PREFIX)
+        }
+        config.update(**overrides)
+        return config
+
+
+class ContextHelper(object):
+
+    CONFIG_FILE_PATTERN = "vertexai*"
+
+    def __init__(self, metadata, env):
+        self._metadata = metadata
+        self._env = env
+
+    @property
+    def project_name(self):
+        return self._metadata.project_name
+
+    @property
+    @lru_cache()
+    def session(self):
+        from kedro.framework.session import KedroSession
+
+        return KedroSession.create(self._metadata.package_name, env=self._env)
+
+    @property
+    def context(self):
+        return self.session.load_context()
+
+    @property
+    @lru_cache()
+    def config(self) -> PluginConfig:
+        raw = EnvTemplatedConfigLoader(
+            self.context.config_loader.conf_paths
+        ).get(self.CONFIG_FILE_PATTERN)
+        return PluginConfig.parse_obj(raw)
+
+    @property
+    @lru_cache()
+    def vertexai_client(self) -> VertexAIPipelinesClient:
+        return VertexAIPipelinesClient(
+            self.config, self.project_name, self.context
+        )
+
+    @staticmethod
+    def init(metadata, env):
+        return ContextHelper(metadata, env)
---
+++ packages/kedro-vertexai/kedro-vertexai-0.4.1.tar.gz/kedro-vertexai-0.4.1/setup.py
@@ -0,0 +1,71 @@
+"""kedro_vertexai module."""
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    README = f.read()
+
+# Runtime Requirements.
+INSTALL_REQUIRES = [
+    "kedro>=0.17,<0.18",
+    "click<8.0",
+    "kfp~=1.8.0",
+    "tabulate>=0.8.7",
+    "semver~=2.10",
+    "pydantic~=1.9.0",
+    "google-auth<2",
+    "google-cloud-scheduler>=2.3.2",
+    "google-cloud-iam",
+    "gcsfs>=2021.11.1",
+    # The requirements bellow are pinned, because the build were not repeatable as of 2022-04-04 - by @marrrcin
+    "google-cloud-storage<2.0.0",
+    "grpcio~=1.44.0",
+    "grpcio-status~=1.44.0",
+]
+
+# Dev Requirements
+EXTRA_REQUIRE = {
+    "mlflow": ["kedro-mlflow>=0.4.1,<0.8.0"],
+    "tests": [
+        "pytest>=5.4.0, <7.0.0",
+        "pytest-cov>=2.8.0, <3.0.0",
+        "pytest-subtests>=0.5.0, <1.0.0",
+        "tox==3.23.1",
+        "pre-commit==2.9.3",
+        "responses>=0.13.4",
+    ],
+    "docs": [
+        "sphinx==3.4.2",
+        "recommonmark==0.7.1",
+        "sphinx_rtd_theme==0.6.0",
+    ],
+}
+
+setup(
+    name="kedro-vertexai",
+    version="0.4.1",
+    description="Kedro plugin with Vertex AI support",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="Apache Software License (Apache 2.0)",
+    python_requires=">=3",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="kedro Vertex AI plugin",
+    author="Mateusz Pytel, Mariusz Strzelecki, Marcin Zabłocki",
+    author_email="mateusz@getindata.com",
+    url="https://github.com/getindata/kedro-vertexai/",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests", "docs"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRA_REQUIRE,
+    entry_points={
+        "kedro.project_commands": ["vertexai = kedro_vertexai.cli:commands"],
+        "kedro.hooks": [
+            "vertexai_mlflow_tags_hook = kedro_vertexai.hooks:mlflow_tags_hook",
+            "vertexai_cfg_hook = kedro_vertexai.hooks:env_templated_config_loader_hook",
+        ],
+    },
+)
