d8b787706a7b4cf19fb6c31361cca5c8f0073c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:04 +0000

    Add package DNBC4dev/1.0.5/DNBC4dev-1.0.5-py3-none-any.whl

---
+++ packages/DNBC4dev/DNBC4dev-1.0.5-py3-none-any.whl/DNBC4tools/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+_version = "1.0.5"
+
+_pipelist = [
+    "data",
+    'count',
+    'analysis',
+    'report',
+    'clean',
+    'mkref',
+    'run',
+    'list',
+]
+
+_root_dir = os.path.abspath(os.path.dirname(__file__))
