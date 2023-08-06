2c4fbea8563a735816f00098259b5400344fc408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:55 +0000

    Add package xknx/0.20.2/xknx-0.20.2-py3-none-any.whl

---
+++ packages/xknx/xknx-0.20.2-py3-none-any.whl/xknx/knxip/routing_indication.py
@@ -0,0 +1,60 @@
+"""
+Module for Serialization and Deserialization of KNX Routing Indications.
+
+Routing indications are used to transport CEMI Messages.
+"""
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from xknx.exceptions import CouldNotParseKNXIP, UnsupportedCEMIMessage
+
+from .body import KNXIPBody
+from .cemi_frame import CEMIFrame
+from .knxip_enum import CEMIMessageCode, KNXIPServiceType
+
+if TYPE_CHECKING:
+    from xknx.xknx import XKNX
+logger = logging.getLogger("xknx.log")
+
+
+class RoutingIndication(KNXIPBody):
+    """Representation of a KNX Routing Indication."""
+
+    SERVICE_TYPE = KNXIPServiceType.ROUTING_INDICATION
+
+    def __init__(self, xknx: XKNX, cemi: CEMIFrame | None = None):
+        """Initialize SearchRequest object."""
+        super().__init__(xknx)
+        self.cemi: CEMIFrame | None = (
+            cemi
+            if cemi is not None
+            else CEMIFrame(xknx, code=CEMIMessageCode.L_DATA_IND)
+        )
+
+    def calculated_length(self) -> int:
+        """Get length of KNX/IP body."""
+        assert self.cemi is not None
+        return self.cemi.calculated_length()
+
+    def from_knx(self, raw: bytes) -> int:
+        """Parse/deserialize from KNX/IP raw data."""
+        assert self.cemi is not None
+
+        try:
+            return self.cemi.from_knx(raw)
+        except UnsupportedCEMIMessage as unsupported_cemi_err:
+            logger.warning("CEMI not supported: %s", unsupported_cemi_err)
+            self.cemi = None
+            return len(raw)
+
+    def to_knx(self) -> bytes:
+        """Serialize to KNX/IP raw data."""
+        if self.cemi is None:
+            raise CouldNotParseKNXIP("No CEMIFrame defined.")
+        return self.cemi.to_knx()
+
+    def __str__(self) -> str:
+        """Return object as readable string."""
+        return f'<RoutingIndication cemi="{self.cemi}" />'
---
+++ packages/xknx/xknx-0.20.2-py3-none-any.whl/xknx/knxip/search_request.py
@@ -0,0 +1,46 @@
+"""
+Module for Serialization and Deserialization of KNX Search Requests.
+
+Search Requests are used to search for KNX/IP devices within the network.
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .body import KNXIPBody
+from .hpai import HPAI
+from .knxip_enum import KNXIPServiceType
+
+if TYPE_CHECKING:
+    from xknx.xknx import XKNX
+
+
+class SearchRequest(KNXIPBody):
+    """Representation of a KNX Search Request."""
+
+    SERVICE_TYPE = KNXIPServiceType.SEARCH_REQUEST
+
+    def __init__(self, xknx: XKNX, discovery_endpoint: HPAI | None = None):
+        """Initialize SearchRequest object."""
+        super().__init__(xknx)
+        self.discovery_endpoint = (
+            discovery_endpoint
+            if discovery_endpoint is not None
+            else HPAI(ip_addr=xknx.multicast_group, port=xknx.multicast_port)
+        )
+
+    def calculated_length(self) -> int:
+        """Get length of KNX/IP body."""
+        return HPAI.LENGTH
+
+    def from_knx(self, raw: bytes) -> int:
+        """Parse/deserialize from KNX/IP raw data."""
+        return self.discovery_endpoint.from_knx(raw)
+
+    def to_knx(self) -> bytes:
+        """Serialize to KNX/IP raw data."""
+        return self.discovery_endpoint.to_knx()
+
+    def __str__(self) -> str:
+        """Return object as readable string."""
+        return f'<SearchRequest discovery_endpoint="{self.discovery_endpoint}" />'
