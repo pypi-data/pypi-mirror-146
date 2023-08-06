08508a51e974dfedbf4a399496830d7eaefead93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:59 +0000

    Add package hamming-check/1.0.2/hamming_check-1.0.2-py3-none-any.whl

---
+++ packages/hamming-check/hamming_check-1.0.2-py3-none-any.whl/hamming_check/utils/__init__.py
@@ -0,0 +1,14 @@
+"""
+utils
+~~~~~~
+
+utils - Various utility functions.
+"""
+
+__all__ = ["Utils"]
+
+__author__ = "Pablo Alessandro Santos Hugen"
+__doc__ = "utils - Various utility functions."
+# __import__ = ["Bytes", "File"]
+
+from .utils import Utils
