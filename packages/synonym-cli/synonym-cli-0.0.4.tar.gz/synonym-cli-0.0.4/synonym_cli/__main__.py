d952517b8e6cfa6745419a0fe4e27f1b831dea26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:32 +0000

    Add package synonym-cli/0.0.4/synonym-cli-0.0.4.tar.gz

---
+++ packages/synonym-cli/synonym-cli-0.0.4.tar.gz/synonym-cli-0.0.4/synonym_cli/__main__.py
@@ -0,0 +1,31 @@
+import argparse
+from synonym_cli.cli import Synonym
+
+__version__ = "0.0.4"
+
+# parse arguments
+ap = argparse.ArgumentParser()
+ap.add_argument(
+    "word",
+    type=str,
+    nargs="*",
+    help="<word>",
+)
+ap.add_argument(
+    "-p",
+    "--plain",
+    action="store_true",
+    default=False,
+    help="returns plain text output",
+)
+ap.add_argument("-v", "--version", action="version", version="%(prog)s v" + __version__)
+args = ap.parse_args()
+
+
+def cli():
+    word = "".join(args.word)
+    Synonym(word, args.plain)
+
+
+if __name__ == "__main__":
+    cli()
