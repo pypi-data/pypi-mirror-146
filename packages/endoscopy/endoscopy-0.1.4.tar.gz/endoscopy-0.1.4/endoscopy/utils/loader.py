b63ebee78e9a716d3ecd0da3cbbadd5bec81d456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:17 +0000

    Add package endoscopy/0.1.4/endoscopy-0.1.4.tar.gz

---
+++ packages/endoscopy/endoscopy-0.1.4.tar.gz/endoscopy-0.1.4/endoscopy/utils/loader.py
@@ -0,0 +1,24 @@
+import torch
+from enum import Enum
+
+
+class MODEL(object):
+    r"""Model meta class.
+
+    URLs to models are to include a 'cpu' or 'cuda' tag, respectively.
+    """
+    class HOMOGRAPHY_ESTIMATION_ENUM(Enum):
+        H_48_RESNET_34 = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/h_est_48_resnet_34_{}.pt"
+        H_64_RESNET_34 = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/h_est_64_resnet_34_{}.pt"
+
+    class SEGMENTATION_ENUM(Enum):
+        UNET_RESNET_34 = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/seg_unet_resnet_34_{}.pt"
+        UNET_RESNET_34_TINY = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/seg_unet_resnet_34_{}_tiny.pt"
+
+    HOMOGRAPHY_ESTIMATION = HOMOGRAPHY_ESTIMATION_ENUM
+    SEGMENTATION = SEGMENTATION_ENUM
+
+
+def load_model(model: MODEL, device: str="cuda"):
+    model = torch.hub.load_state_dict_from_url(model.value.format(device))
+    return model.eval()
