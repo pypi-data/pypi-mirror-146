3b121875d4c257e3e3881f805f14c51cc3d2ffc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:16 +0000

    Add package seelabutk-substrate/0.17/seelabutk-substrate-0.17.tar.gz

---
+++ packages/seelabutk-substrate/seelabutk-substrate-0.17.tar.gz/seelabutk-substrate-0.17/src/substrate/tools/dchm.py
@@ -0,0 +1,45 @@
+from docker import from_env
+from docker.types.services import EndpointSpec, ServiceMode
+
+from . import Tool
+
+
+class DCHM(Tool):
+	def __init__(self, config, data_sources):
+		super().__init__(config, data_sources)
+
+		self.name = 'dchm'
+		self.port = 8000
+
+		self.config = config
+		self.data_sources = data_sources
+
+		self.service_command = (
+			'docker service create '
+			'--name dchm '
+			'--publish 80:5000/tcp '
+			f'--replicas {self.config.get("aws", {}).get("replicas", 1)} '
+			'--mount type=bind,src=/mnt/efs/data,dst=/data '
+			'npatel79/water-and-land:latest '
+			'flask run --host=0.0.0.0'
+		)
+
+	def start(self):
+		mounts = super().start()
+
+		docker = from_env()
+
+		self.port = self.config['cluster'].get('port', self.port)
+		docker.services.create(
+			'npatel79/water-and-land:latest',
+			'flask',
+			args=['run', '--host=0.0.0.0'],
+			endpoint_spec=EndpointSpec(ports={self.port: (5000, 'tcp')}),
+			mode=ServiceMode(
+				mode='replicated',
+				replicas=self.config['cluster'].get('replicas', 1)
+			),
+			mounts=mounts,
+			name='dchm',
+			networks=['substrate-dchm-net']
+		)
