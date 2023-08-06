9d3c3b69955b16a301d4591d4713ca15d4142e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:07 +0000

    Add package embutils/0.8.6/embutils-0.8.6.tar.gz

---
+++ packages/embutils/embutils-0.8.6.tar.gz/embutils-0.8.6/embutils/serial/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Embutils serial utilities.
+
+:date:      2021
+:author:    Christian Wiche
+:contact:   cwichel@gmail.com
+:license:   The MIT License (MIT)
+"""
+# -------------------------------------
+from .device import *
+from .stream import *
+from .interface import *
