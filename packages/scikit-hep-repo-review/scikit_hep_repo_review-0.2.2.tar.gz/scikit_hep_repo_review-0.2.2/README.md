8ecb100e40fd94de47cfc1827ed4651c7d94ce95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:29 +0000

    Add package scikit-hep-repo-review/0.2.2/scikit_hep_repo_review-0.2.2.tar.gz

---
+++ packages/scikit-hep-repo-review/scikit_hep_repo_review-0.2.2.tar.gz/scikit_hep_repo_review-0.2.2/README.md
@@ -0,0 +1,20 @@
+# scikit-hep-repo-review
+
+[![Actions Status][actions-badge]][actions-link]
+[![Code style: black][black-badge]][black-link]
+
+[![PyPI version][pypi-version]][pypi-link]
+[![PyPI platforms][pypi-platforms]][pypi-link]
+
+[![Scikit-HEP][sk-badge]](https://scikit-hep.org/)
+
+
+
+[actions-badge]:            https://github.com/Scikit-HEP/scikit-hep-repo-review/workflows/CI/badge.svg
+[actions-link]:             https://github.com/Scikit-HEP/scikit-hep-repo-review/actions
+[black-badge]:              https://img.shields.io/badge/code%20style-black-000000.svg
+[black-link]:               https://github.com/psf/black
+[pypi-link]:                https://pypi.org/project/scikit-hep-repo-review/
+[pypi-platforms]:           https://img.shields.io/pypi/pyversions/scikit-hep-repo-review
+[pypi-version]:             https://badge.fury.io/py/scikit-hep-repo-review.svg
+[sk-badge]:                 https://scikit-hep.org/assets/images/Scikit--HEP-Project-blue.svg
---
+++ packages/scikit-hep-repo-review/scikit_hep_repo_review-0.2.2.tar.gz/scikit_hep_repo_review-0.2.2/src/scikit_hep_repo_review/ratings/general.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+from importlib.abc import Traversable
+
+# PY: Python Project
+## 0xx: File existence
+
+
+class General:
+    family = "general"
+
+
+class PY001(General):
+    "Has a pyproject.toml"
+
+    @staticmethod
+    def check(package: Traversable) -> bool:
+        """
+        All projects should have a `pyproject.toml` file to support a modern
+        build system and support wheel installs properly.
+        """
+        return package.joinpath("pyproject.toml").is_file()
+
+
+class PY002(General):
+    "Has a README.(md|rst) file"
+
+    @staticmethod
+    def check(package: Traversable) -> bool:
+        "Projects must have a readme file"
+        return (
+            package.joinpath("README.md").is_file()
+            or package.joinpath("README.rst").is_file()
+        )
+
+
+class PY003(General):
+    "Has a LICENSE* file"
+
+    @staticmethod
+    def check(package: Traversable) -> bool:
+        "Projects must have a license"
+        return len(list(p for p in package.iterdir() if "LICENSE" in p.name)) > 0
+
+
+class PY004(General):
+    "Has docs folder"
+
+    @staticmethod
+    def check(package: Traversable) -> bool:
+        "Projects must have documentation in a folder called docs (disable if not applicable)"
+        return package.joinpath("docs").is_dir()
+
+
+class PY005(General):
+    "Has tests folder"
+
+    @staticmethod
+    def check(package: Traversable) -> bool:
+        "Projects must have a folder called tests"
+        return package.joinpath("tests").is_dir()
+
+
+class PY006(General):
+    "Has pre-commit config"
+
+    @staticmethod
+    def check(package: Traversable) -> bool:
+        "Projects must have a `.pre-commit-config.yaml` file"
+        return package.joinpath(".pre-commit-config.yaml").is_file()
+
+
+class PY007(General):
+    "Has a noxfile.py"
+
+    @staticmethod
+    def check(package: Traversable) -> bool:
+        "Projects must have a noxfile.py to encourage new contributors"
+        return package.joinpath("noxfile.py").is_file()
+
+
+repo_review_fixtures = set[str]()
+repo_review_checks = {p.__name__ for p in General.__subclasses__()}
