a2c01dfc93f3f291732c1012eb8807a345e7fac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:17 +0000

    Add package azure-kusto-data/3.1.2/azure-kusto-data-3.1.2.tar.gz

---
+++ packages/azure-kusto-data/azure-kusto-data-3.1.2.tar.gz/azure-kusto-data-3.1.2/azure/kusto/data/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from ._version import VERSION as __version__
+from .client import KustoClient, KustoConnectionStringBuilder, ClientRequestProperties
+from .data_format import DataFormat
