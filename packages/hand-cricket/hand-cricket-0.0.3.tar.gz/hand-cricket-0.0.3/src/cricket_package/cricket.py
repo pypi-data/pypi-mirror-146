054ffefb5dd23dfd7bfad6a80275ff3d4c352857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:42 +0000

    Add package hand-cricket/0.0.3/hand-cricket-0.0.3.tar.gz

---
+++ packages/hand-cricket/hand-cricket-0.0.3.tar.gz/hand-cricket-0.0.3/src/cricket_package/cricket.py
@@ -0,0 +1,10 @@
+
+
+
+class Author:
+  def tell(sell):
+
+     print("-----------------------------")
+     print("   Author : Tamil Selvan")
+     print("  Version : 3.0")
+     print("-----------------------------")
