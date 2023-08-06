e89991c1498e8fa64c3fa50b7b3422ad92d4896a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:28 +0000

    Add package chilitools/0.1.3/chilitools-0.1.3.tar.gz

---
+++ packages/chilitools/chilitools-0.1.3.tar.gz/chilitools-0.1.3/chilitools/Settings/config.py
@@ -0,0 +1,4 @@
+LOGIN_FILE = './login.txt'
+APIKEY_FILE = './apiKeys.txt'
+CONNECTOR_LOG_FILE = './logs/ChiliConnector.log'
+STAFF_OAUTH_LOGIN = True
---
+++ packages/chilitools/chilitools-0.1.3.tar.gz/chilitools-0.1.3/chilitools/Utilities/CommonChecks.py
@@ -0,0 +1,7 @@
+#TODO make a file that does common checks for documents
+
+# 1) check for timeline elements
+# 2) at least store a value on whether saved in editor is true
+# 3) calculate a complexity score, based on content-length, amount of frame/snippets/variable elements
+
+
