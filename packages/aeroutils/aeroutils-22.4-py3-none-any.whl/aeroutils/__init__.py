a20be81d5591768aaf62a96b1304a4eec95fc37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:18 +0000

    Add package aeroutils/22.4/aeroutils-22.4-py3-none-any.whl

---
+++ packages/aeroutils/aeroutils-22.4-py3-none-any.whl/aeroutils/__init__.py
@@ -0,0 +1,15 @@
+"""
+Aerospace utilities.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Author: Matthew C. Jones
+Email: matt.c.jones.aoe@gmail.com
+
+:copyright: 2021 Matthew C. Jones
+:license: MIT License, see LICENSE for more details.
+"""
+
+from .version import __version__
+
+# 2022-04-15: aeroutils was rebranded, so importing all from new package
+from flightcondition import *
