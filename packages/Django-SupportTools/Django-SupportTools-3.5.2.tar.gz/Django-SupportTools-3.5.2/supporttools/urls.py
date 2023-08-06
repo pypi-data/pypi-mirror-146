91fff2789247c6114ca9472dba5a860e4d3519c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:52 +0000

    Add package Django-SupportTools/3.5.2/Django-SupportTools-3.5.2.tar.gz

---
+++ packages/Django-SupportTools/Django-SupportTools-3.5.2.tar.gz/Django-SupportTools-3.5.2/supporttools/urls.py
@@ -0,0 +1,13 @@
+# Copyright 2022 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+from django.conf.urls import url
+from supporttools.views import home
+
+urlpatterns = [
+    # for now... make the home view the default landing for
+    # /support
+    url(r'^', home, name='supporttools_home'),
+    # url(r'^', include('status_app.urls')),
+    ]
---
+++ packages/Django-SupportTools/Django-SupportTools-3.5.2.tar.gz/Django-SupportTools-3.5.2/supporttools/views.py
@@ -0,0 +1,9 @@
+# Copyright 2022 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'supporttools/home.html', {})
