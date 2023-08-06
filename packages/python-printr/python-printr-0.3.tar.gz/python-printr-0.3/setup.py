64650af8ab295a98a9e207bcd395770fc059af58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:01 +0000

    Add package python-printr/0.3/python-printr-0.3.tar.gz

---
+++ packages/python-printr/python-printr-0.3.tar.gz/python-printr-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'python-printr',
+    packages = ['printr'],
+    version = '0.3',
+    description = 'printr',
+    url = 'https://github.com/xjxckk/printr/',
+    download_url = 'https://github.com/xjxckk/printr/archive/refs/tags/v0.1.tar.gz',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+    )
