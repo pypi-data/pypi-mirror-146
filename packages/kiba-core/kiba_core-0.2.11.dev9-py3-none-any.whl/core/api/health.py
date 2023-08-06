3bc6f535220e532713debee0a0b4377b39847c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:03 +0000

    Add package kiba-core/0.2.11.dev9/kiba_core-0.2.11.dev9-py3-none-any.whl

---
+++ packages/kiba-core/kiba_core-0.2.11.dev9-py3-none-any.whl/core/api/health.py
@@ -0,0 +1,13 @@
+from typing import Dict
+
+from core.api.kiba_router import KibaRouter
+
+
+def create_api(name: str, version: str) -> KibaRouter:
+    router = KibaRouter()
+
+    @router.get('/')
+    async def root() -> Dict[str, str]:
+        return {'server': name, 'version': version}
+
+    return router
