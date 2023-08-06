765d8fbf61f94e3bbd581fd470265e8c76fe3b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:09 +0000

    Add package djangoldp-hiphopcommunity/0.0.9/djangoldp_hiphopcommunity-0.0.9-py3-none-any.whl

---
+++ packages/djangoldp-hiphopcommunity/djangoldp_hiphopcommunity-0.0.9-py3-none-any.whl/djangoldp_hiphopcommunity/djangoldp_settings.py
@@ -0,0 +1,10 @@
+
+"""This module is loaded by DjangoLDP core during setup."""
+
+# define an extra variable (should be prefix with package name)
+MYPACKAGE_VAR = 'MY_DEFAULT_VAR'
+
+# register an extra middleware
+MIDDLEWARE = []
+
+REGISTRATION_USER_FORM = 'djangoldp_hiphopcommunity.forms.HipHopUserForm'
