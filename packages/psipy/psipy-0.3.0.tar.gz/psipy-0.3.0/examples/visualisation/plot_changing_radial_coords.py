499c8ba8487c2831a75f77f02fe8eff763194d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:56 +0000

    Add package psipy/0.3.0/psipy-0.3.0.tar.gz

---
+++ packages/psipy/psipy-0.3.0.tar.gz/psipy-0.3.0/examples/visualisation/plot_changing_radial_coords.py
@@ -0,0 +1,38 @@
+"""
+Changing the units of a plot
+=============================
+
+This example shows how to change the radial coordiantes. This is helpful if you
+want to change the radial coordinates e.g. from solar radii to AU.
+"""
+###############################################################################
+# First, load the required modules.
+import astropy.constants as const
+import matplotlib.pyplot as plt
+
+from psipy.data import sample_data
+from psipy.model import MASOutput
+
+###############################################################################
+# Next, load a set of MAS output files. You will need to change this line to
+# point to a folder with MAS files in them.
+mas_path = sample_data.mas_sample_data()
+model = MASOutput(mas_path)
+
+###############################################################################
+# Define a conversion factor from solar radii to AU, and apply this to the
+# coordinates of a variable
+rsun_to_au = float(const.R_sun / const.au)
+
+br = model['br']
+print(f'Old coords: {br.r_coords}')
+br.r_coords = br.r_coords * rsun_to_au
+print(f'New coords: {br.r_coords}')
+
+###############################################################################
+# Plot
+cbar_kwargs = {'orientation': 'horizontal'}
+ax = plt.subplot(projection='polar')
+br.plot_equatorial_cut(ax=ax, cbar_kwargs=cbar_kwargs)
+
+plt.show()
---
+++ packages/psipy/psipy-0.3.0.tar.gz/psipy-0.3.0/psipy/data/sample_data.py
@@ -0,0 +1,105 @@
+"""
+Helper functions for downloading sample model output data.
+"""
+from pathlib import Path
+
+import pooch
+
+__all__ = ['mas_sample_data', 'mas_helio_timesteps']
+
+
+file_url = 'cr{cr}-medium/hmi_masp_mas_std_0201/{type}/{var}002.hdf'
+cache_dir = pooch.os_cache("psipy")
+
+
+def _get_url(*, type: str, var: str, cr: int = 2210):
+    return file_url.format(cr=cr, type=type, var=var)
+
+
+registry = {}
+
+# Add consecutive Carrington rotation sample data
+for cr in [2210, 2211]:
+    registry[_get_url(cr=cr, type='helio', var='vr')] = None
+
+
+# Add various variables for helio and corona solutions
+vars = ['rho', 'vr', 'br', 'bt', 'bp']
+for type in ['helio', 'corona']:
+    for var in vars:
+        registry[_get_url(cr=2210, type=type, var=var)] = None
+
+mas_pooch = pooch.create(
+    path=cache_dir,
+    base_url="https://www.predsci.com/data/runs/",
+    registry=registry,
+)
+
+# Add some PLUTO data
+pluto_reg = {}
+PLUTO_FILES = ['grid.out', 'dbl.out', 'rho.0000.dbl']
+for file in PLUTO_FILES:
+    pluto_reg[file] = None
+
+pluto_pooch = pooch.create(
+    path=cache_dir,
+    base_url='doi:10.6084/m9.figshare.19401089.v1/',
+    registry=pluto_reg,
+)
+
+
+def mas_sample_data(type='helio'):
+    """
+    Get some MAS data files. These are taken from CR2210, which
+    is used for PSP data comparisons in the documentation examples.
+
+    Parameters
+    ----------
+    type : {'helio', 'corona'}
+
+    Returns
+    -------
+    pathlib.Path
+        Download directory.
+    """
+    for var in vars:
+        path = mas_pooch.fetch(_get_url(cr=2210, type=type, var=var),
+                               progressbar=True)
+    return Path(path).parent
+
+
+def mas_helio_timesteps():
+    """
+    Get two MAS heliospheric data files for two subsequent Carrington
+    rotations.
+
+    This is used as sample data for animations - animations are intended to be
+    used with output from time dependent simulations, but for ease of
+    downloading sample data here we pretend that two Carrington rotations are
+    time animations.
+
+    Returns
+    -------
+    pathlib.Path
+        Download directory.
+    """
+    for cr in [2210, 2211]:
+        path = mas_pooch.fetch(_get_url(cr=cr, type='helio', var='vr'),
+                               progressbar=True)
+
+    return Path(path).parent
+
+
+def pluto_sample_data():
+    """
+    Get some sample PLUTO data.
+
+    Returns
+    -------
+    pathlib.Path
+        Download directory.
+    """
+    for file in PLUTO_FILES:
+        path = pluto_pooch.fetch(file, progressbar=True)
+
+    return Path(path).parent
