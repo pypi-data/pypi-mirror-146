dc939e4bc454dc729f4d1d765ca217593db5fe84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:21 +0000

    Add package malariagen-data/4.1.0/malariagen_data-4.1.0.tar.gz

---
+++ packages/malariagen-data/malariagen_data-4.1.0.tar.gz/malariagen_data-4.1.0/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['malariagen_data']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['BioPython',
+ 'bokeh',
+ 'dask[array]',
+ 'fsspec',
+ 'gcsfs',
+ 'igv-notebook',
+ 'importlib_metadata',
+ 'ipinfo',
+ 'ipyleaflet',
+ 'ipywidgets',
+ 'numba',
+ 'numpy',
+ 'plotly',
+ 'scikit-allel',
+ 'scipy',
+ 'statsmodels',
+ 'xarray',
+ 'zarr']
+
+extras_require = \
+{':python_version > "3.7" and python_version < "3.10"': ['pandas'],
+ ':python_version >= "3.7" and python_version < "3.8"': ['pandas<1.4.0']}
+
+setup_kwargs = {
+    'name': 'malariagen-data',
+    'version': '4.1.0',
+    'description': 'A package for accessing and analysing MalariaGEN data.',
+    'long_description': None,
+    'author': 'Alistair Miles',
+    'author_email': 'alistair.miles@sanger.ac.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<3.10',
+}
+
+
+setup(**setup_kwargs)
