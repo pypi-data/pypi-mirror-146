7e4362994b0fa1616929525a09903c1748bb483b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:02 +0000

    Add package pygfx/0.1.8/pygfx-0.1.8.tar.gz

---
+++ packages/pygfx/pygfx-0.1.8.tar.gz/pygfx-0.1.8/setup.py
@@ -0,0 +1,45 @@
+import re
+
+from setuptools import find_packages, setup
+
+
+NAME = "pygfx"
+SUMMARY = "A threejs-like render engine based on wgpu"
+
+with open(f"{NAME}/__init__.py") as fh:
+    VERSION = re.search(r"__version__ = \"(.*?)\"", fh.read()).group(1)
+
+
+runtime_deps = [
+    "numpy",
+    "wgpu>=0.7.6,<0.8.0",
+    "Jinja2",
+]
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(
+        exclude=["tests", "tests.*", "examples", "examples.*", "exp", "exp.*"]
+    ),
+    python_requires=">=3.7.0",
+    install_requires=runtime_deps,
+    license="BSD 2-Clause",
+    description=SUMMARY,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Almar Klein",
+    author_email="almar.klein@gmail.com",
+    url="https://github.com/pygfx/pygfx",
+    data_files=[("", ["LICENSE"])],
+    zip_safe=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
