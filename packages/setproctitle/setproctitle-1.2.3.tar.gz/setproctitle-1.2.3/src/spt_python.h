84f1783f595b4724c05535b45a0ed191de6cb0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:05 +0000

    Add package setproctitle/1.2.3/setproctitle-1.2.3.tar.gz

---
+++ packages/setproctitle/setproctitle-1.2.3.tar.gz/setproctitle-1.2.3/src/spt_python.h
@@ -0,0 +1,25 @@
+/*-------------------------------------------------------------------------
+ *
+ * spt_python.h
+ *    Include and customize Python definitions.
+ *
+ * Copyright (c) 2010-2021 Daniele Varrazzo <daniele.varrazzo@gmail.com>
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef SPT_PYTHON_H
+#define SPT_PYTHON_H
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+/* Detect pypy */
+#ifdef PYPY_VERSION
+#define IS_PYPY
+#endif
+
+/* defined in Modules/main.c but not publically declared */
+void Py_GetArgcArgv(int *argc, wchar_t ***argv);
+
+#endif   /* SPT_PYTHON_H */
