6f049a7677280fca8fafa48fe2e0e599157bf065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:25 +0000

    Add package cv2PySide6/2.1.0/cv2PySide6-2.1.0.tar.gz

---
+++ packages/cv2PySide6/cv2PySide6-2.1.0.tar.gz/cv2PySide6-2.1.0/cv2PySide6/typing.py
@@ -0,0 +1,61 @@
+from numpy.typing import NDArray
+from PySide6.QtCore import Signal
+from PySide6.QtMultimedia import QMediaPlayer
+from typing import Protocol, Callable, Any
+
+__all__ = [
+    'ArrayProcessorProtocol',
+    'NDArrayVideoPlayerProtocol',
+    'CameraProtocol',
+    'NDArrayMediaCaptureSessionProtocol',
+]
+
+
+class ArrayProcessorProtocol(Protocol):
+    """
+    Type annotation for array processor protocol.
+
+    New image array is passed down from upstream pipeline to
+    ``setArray`` slot. ``arrayChanged`` emits processed array.
+    """
+    arrayChanged: Signal
+    setArray: Callable[['ArrayProcessorProtocol', NDArray], Any]
+
+
+class NDArrayVideoPlayerProtocol(Protocol):
+    """
+    Type annotation for video player protocol which emits frames from
+    video file as arrays.
+    """
+    arrayChanged: Signal
+    positionChanged: Signal
+    durationChanged: Signal
+    playbackStateChanged: Signal
+    setPosition: Callable[['NDArrayVideoPlayerProtocol', int], Any]
+    setSource: Callable[['NDArrayVideoPlayerProtocol', Any], Any]
+    play: Callable[['NDArrayVideoPlayerProtocol'], Any]
+    pause: Callable[['NDArrayVideoPlayerProtocol'], Any]
+    stop: Callable[['NDArrayVideoPlayerProtocol'], Any]
+    playbackState: Callable[
+        ['NDArrayVideoPlayerProtocol'], QMediaPlayer.PlaybackState
+    ]
+
+
+class CameraProtocol(Protocol):
+    """
+    Type annotation for camera protocol.
+    """
+    start: Callable[['CameraProtocol'], Any]
+    stop: Callable[['CameraProtocol'], Any]
+
+
+class NDArrayMediaCaptureSessionProtocol(Protocol):
+    """
+    Type annotation for media capture session protocol which emits
+    frames from camera as arrays.
+    """
+    arrayChanged: Signal
+    setCamera: Callable[
+        ['NDArrayMediaCaptureSessionProtocol', CameraProtocol], Any
+    ]
+    camera: Callable[['NDArrayMediaCaptureSessionProtocol'], CameraProtocol]
