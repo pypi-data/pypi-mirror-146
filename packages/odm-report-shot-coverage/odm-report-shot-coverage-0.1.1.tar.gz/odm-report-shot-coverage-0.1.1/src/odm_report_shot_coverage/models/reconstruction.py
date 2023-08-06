b623812f74fe72da49cf7e50a112cb0ec7c0876f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:17 +0000

    Add package odm-report-shot-coverage/0.1.1/odm-report-shot-coverage-0.1.1.tar.gz

---
+++ packages/odm-report-shot-coverage/odm-report-shot-coverage-0.1.1.tar.gz/odm-report-shot-coverage-0.1.1/src/odm_report_shot_coverage/models/reconstruction.py
@@ -0,0 +1,96 @@
+import geojson
+from scipy import stats
+
+from odm_report_shot_coverage.models.camera import Camera, json_parse_camera
+from odm_report_shot_coverage.models.shot import Shot, shot_boundaries_from_points
+from odm_report_shot_coverage.models.wavefront_25d import Wavefront25D, parse_wavefront_25d_obj
+
+
+class Reconstruction:
+    cameras: dict[str, Camera] = {}
+    _shots: list[Shot] = []
+    mesh = Wavefront25D
+
+    @property
+    def shots(self) -> list[Shot]:
+        self._shots.sort(key=lambda s: s.image_name)
+        return self._shots
+
+    def add_camera(self, name: str, camera: Camera):
+        self.cameras[name] = camera
+
+    def add_shot(self, shot: Shot):
+        self._shots.append(shot)
+
+    def to_json(self) -> dict:
+        return {
+            'cameras': {n: c.to_json() for n, c in self.cameras.items()},
+            'shots': [s.to_json() for s in self.shots],
+            # 'mesh': self.mesh.to_json(),
+            'boundaries': self.mesh.boundaries.to_json(),
+        }
+
+    def compute_shot_boundaries(self):
+        """
+        From shots and points, fill the shot_boundaries
+        :rtype: None
+        """
+
+        for shot in self.shots:
+            points = []
+            for i, point in enumerate(self.mesh.points):
+                pixel = shot.camera_pixel(point)
+                if shot.camera.in_frame(pixel):
+                    points.append(point)
+
+            shot.boundaries = shot_boundaries_from_points(points)
+
+    def find_camera_by_width_height(self, width: int, height: int) -> Camera:
+        cs = [c for c in self.cameras.values() if c.width == width and c.height == height]
+        if len(cs) != 1:
+            raise Exception('Not exactly one camera found with size %s x %s' % (width, height))
+        return cs[0]
+
+
+class ReconstructionCollection:
+    reconstructions: list[Reconstruction] = []
+
+    def append(self, reconstruction: Reconstruction):
+        self.reconstructions.append(reconstruction)
+
+    def __getitem__(self, i: int):
+        return self.reconstructions[i]
+
+    def __len__(self):
+        return len(self.reconstructions)
+
+
+def lin_reg(pairs: list[(float, float)]) -> (float, float, float, float):
+    x = [p[0] for p in pairs]
+    y = [p[1] for p in pairs]
+    return stats.linregress(x, y)
+
+
+def parse_reconstruction(path: str) -> Reconstruction:
+    reconstruction = Reconstruction()
+    with open('%s/cameras.json' % path, 'r') as fd:
+        cameras_json = geojson.load(fd)
+        for n, j in cameras_json.items():
+            camera = json_parse_camera(n, j)
+            reconstruction.add_camera(n, camera)
+
+    with open('%s/odm_report/shots.geojson' % path, 'r') as fd:
+        shots_geojson = geojson.load(fd)
+        for feat in shots_geojson['features']:
+            shot = Shot()
+            props = feat['properties']
+            shot.camera = reconstruction.find_camera_by_width_height(props['width'], props['height'])
+            shot.image_name = props['filename']
+            shot.translation = props['translation']
+            shot.rotation = props['rotation']
+            reconstruction.add_shot(shot)
+
+    wf = parse_wavefront_25d_obj('%s/odm_texturing_25d/odm_textured_model_geo.obj' % path)
+    reconstruction.mesh = wf
+
+    return reconstruction
---
+++ packages/odm-report-shot-coverage/odm-report-shot-coverage-0.1.1.tar.gz/odm-report-shot-coverage-0.1.1/src/odm_report_shot_coverage/models/test_camera.py
@@ -0,0 +1,69 @@
+import json
+from unittest import TestCase
+
+import numpy as np
+
+from odm_report_shot_coverage.models.camera import json_parse_camera
+from odm_report_shot_coverage.models.test_fixtures import TestFixtures as Fixtures
+
+
+class Test(TestCase):
+    def test_json_load_camera(self):
+        json_str = """
+        {
+                "projection_type": "brown",
+                "width": 3000,
+                "height": 4000,
+                "focal_x": 0.5207834102328533,
+                "focal_y": 0.5207834102328533,
+                "c_x": -0.010331518981731644,
+                "c_y": -0.0036169971231284,
+                "k1": -0.10638507280457302,
+                "k2": 0.06769290794144624,
+                "p1": -0.0009394968204580917,
+                "p2": -0.0007005575451251272,
+                "k3": -0.005369531191619131
+            }
+            """
+        given = json.loads(json_str)
+
+        got = json_parse_camera('gopro8', given)
+        self.assertEqual('gopro8', got.name, 'name')
+        self.assertEqual('brown', got.projection_type, 'project_type')
+        self.assertEqual(3000, got.width, 'width')
+        self.assertEqual(4000, got.height, 'height')
+        self.assertEqual(0.5207834102328533, got.focal, 'focal')
+        self.assertEqual(-0.10638507280457302, got.k1, 'k1')
+        self.assertEqual(0.06769290794144624, got.k2, 'k2')
+        self.assertEqual(-0.005369531191619131, got.k3, 'k3')
+        self.assertEqual(-0.0009394968204580917, got.p1, 'p1')
+        self.assertEqual(-0.0007005575451251272, got.p2, 'p2')
+
+    # GoPro in linear mode has a horizontal FOV of 86.7º
+    # I've adapted it to 89.9 (water diffraction ??), based on trial an error to fit the camera.json specs
+    horiz_fov = 89.9 / 2 / 180 * np.pi
+    tan_camera = np.tan(horiz_fov)
+    given_z = 10
+
+    def test_perspective_center(self):
+        got = Fixtures.a_camera_gopro8_linear().perspective_pixel((0, 0, self.given_z))
+        self.assertEqual(0, got[0])
+        self.assertEqual(0, got[1])
+
+    def test_perspective_max_right(self):
+        given_x = self.given_z * self.tan_camera
+        got = Fixtures.a_camera_gopro8_linear().perspective_pixel((given_x, 0, self.given_z))
+        self.assertAlmostEqual(0.5, got[0], 3)
+        self.assertAlmostEqual(0, got[1], 3)
+
+    def test_perspective_max_right_factor_2(self):
+        given_x = self.given_z * self.tan_camera
+        got = Fixtures.a_camera_gopro8_linear().perspective_pixel((given_x * 2, 0, self.given_z * 2))
+        self.assertAlmostEqual(0.5, got[0], 3)
+        self.assertAlmostEqual(0, got[1], 3)
+
+    def test_perspective_max_top(self):
+        given_x = self.given_z * self.tan_camera
+        got = Fixtures.a_camera_gopro8_linear().perspective_pixel((0, given_x, self.given_z))
+        self.assertAlmostEqual(0, got[0], 3)
+        self.assertAlmostEqual(0.5, got[1], 3)
