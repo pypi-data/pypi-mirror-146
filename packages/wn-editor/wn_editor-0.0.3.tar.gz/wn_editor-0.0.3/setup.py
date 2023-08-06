7fa4274b05516f06d0e59b22ccf7c7e18712c16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:22 +0000

    Add package wn-editor/0.0.3/wn_editor-0.0.3.tar.gz

---
+++ packages/wn-editor/wn_editor-0.0.3.tar.gz/wn_editor-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='wn_editor',
+    version='0.0.3',
+    packages=['wn_editor'],
+    url='https://github.com/Hypercookie/wn-editor',
+    license='MIT',
+    author='Jannes Müller',
+    author_email='jannes@mlrjs.de',
+    python_requires='>3.5',
+    description='Make wordnets editable with wn and this wn_editor extension.',
+    install_requires=[
+        "wn"
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
