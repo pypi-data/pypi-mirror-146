56755c8a982c4e797f9686c362a5e5945b67e092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:03 +0000

    Add package cs-models/0.0.377/cs-models-0.0.377.tar.gz

---
+++ packages/cs-models/cs-models-0.0.377.tar.gz/cs-models-0.0.377/src/cs_models/resources/CompanyLicensingCollabOutbox/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+)
+
+from ...database import Base
+
+
+class CompanyLicensingCollabOutboxModel(Base):
+    __tablename__ = "company_licensing_collab_outbox"
+
+    id = Column(Integer, primary_key=True)
+    licensing_collab_outbox_id = Column(
+        Integer,
+        ForeignKey('licensing_collab_outbox.id'),
+        nullable=False,
+    )
+    company_sec_id = Column(
+        Integer,
+        ForeignKey('companies_sec.id'),
+        nullable=True,
+    )
+    company_ous_id = Column(
+        Integer,
+        ForeignKey('companies_ous.id'),
+        nullable=True,
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
