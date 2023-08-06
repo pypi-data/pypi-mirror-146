5c418a66d4222e0f88e88594b0e930e64c0aaea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:31 +0000

    Add package execpp/0.0.1/execpp-0.0.1.tar.gz

---
+++ packages/execpp/execpp-0.0.1.tar.gz/execpp-0.0.1/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*_
+# Author: ***<***gmail.com>
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='execpp', # 包的名字
+    version='0.0.1',  # 版本号
+    description='Exec cpp code',  # 描述
+    author='xjq', # 作者
+    author_email='xjq701229@outlook.com',  # 你的邮箱**
+    url='',  # 可以写github上的地址，或者其他地址
+    packages=setuptools.find_packages(exclude=[]),  # 包内不需要引用的文件夹
+    
+    # 依赖包
+    install_requires=[
+        "cppimport",
+        "robotpy-cppheaderparser"
+    ],
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        # 'Operating System :: Microsoft'  # 你的操作系统
+        # 'Intended Audience :: Developers',
+        # 'License :: OSI Approved :: BSD License', # BSD认证
+        # 'Programming Language :: Python',   # 支持的语言
+        'Programming Language :: Python :: 3',  # python版本 。。。
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.6',
+        # 'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=True,
+)
