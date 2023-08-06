849060a0e71dcd70b880857c8975cab5abaea15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package kojis/1.4/kojis-1.4.tar.gz

---
+++ packages/kojis/kojis-1.4.tar.gz/kojis-1.4/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+import pathlib
+
+setup(
+    name='kojis',
+    version='1.4',
+    url= 'https://alantimes2020.blogspot.com/',
+    author='Pylan64 & and the Kojis Team',
+    description='Koji helps teach kids programming with Python through emojis and fun tinker tools!'
+)
