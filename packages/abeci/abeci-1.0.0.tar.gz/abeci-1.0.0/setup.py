d5f377fd9c735c9707f493be4959a92be182bbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:37 +0000

    Add package abeci/1.0.0/abeci-1.0.0.tar.gz

---
+++ packages/abeci/abeci-1.0.0.tar.gz/abeci-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Natural Language :: English",
+    "Intended Audience :: Science/Research",
+    "Topic :: Text Processing :: Linguistic",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+setuptools.setup(
+    name="abeci",
+    version="1.0.0",
+    author="TVQuizPhD",
+    long_description=long_description,
+    author_email="tvquizphd@email.com",
+    description="Find perfect pangrams",
+    url="https://github.com/tvquizphd/abeci",
+    long_description_content_type="text/markdown",
+    entry_points=dict(console_scripts=[
+        'abeci-pangrams = abeci.pangrams:pangramsCli'
+    ]),
+    packages=setuptools.find_packages('src'),
+    package_dir={'':'src'},
+    install_requires=['abeci'],
+    classifiers=classifiers
+)
