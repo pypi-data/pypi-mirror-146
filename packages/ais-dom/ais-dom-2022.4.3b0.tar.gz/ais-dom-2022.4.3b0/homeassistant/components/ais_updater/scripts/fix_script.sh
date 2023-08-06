d5a54671ba0e389ac97ab54b583a7f91657b196b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:57 +0000

    Add package ais-dom/2022.4.3b0/ais-dom-2022.4.3b0.tar.gz

---
+++ packages/ais-dom/ais-dom-2022.4.3b0.tar.gz/ais-dom-2022.4.3b0/homeassistant/components/ais_updater/scripts/fix_script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "echo -e ''" > /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "echo -e '\e[38;5;220m Cześć tu \e[30;48;5;208m Jolka \e[0m'" >> /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "echo -e '\e[38;5;220m komenda sudo została zablokowana dla Twojego dobra\e[0m'" >> /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "echo -e ''" >> /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "2022-03-16" > /data/data/pl.sviete.dom/files/home/AIS/.ais_fix
