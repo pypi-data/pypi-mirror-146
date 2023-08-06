1b170bfcde54854b69d2ce609fb0078b15f56276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:41 +0000

    Add package InstagramFakeAPI/0.1.10/InstagramFakeAPI-0.1.10.tar.gz

---
+++ packages/InstagramFakeAPI/InstagramFakeAPI-0.1.10.tar.gz/InstagramFakeAPI-0.1.10/src/InstagramFakeAPI/lib/exception/main.py
@@ -0,0 +1,23 @@
+class ClientError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class PageNotFound(ClientError):
+    pass
+
+
+class NonAuthorizedRequest(ClientError):
+    pass
+
+
+class ProxyRequestError(ClientError):
+    pass
+
+
+class ChallengeRequired(ClientError):
+    pass
+
+
+class Response429(ClientError):
+    pass
