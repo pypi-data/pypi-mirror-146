145db0c2d092c5d4d38ac8d408890173bd4fe995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:29 +0000

    Add package pynumato/0.0.1/pynumato-0.0.1-py3-none-any.whl

---
+++ packages/pynumato/pynumato-0.0.1-py3-none-any.whl/pynumato/__init__.py
@@ -0,0 +1,13 @@
+"""Library to communicate with relays
+
+"""
+
+__version__ = '0.0.1'
+__title__ = 'pynumato'
+__description__ = 'Library to communicate with relay module - numato series'
+#__uri__ = 'https://github.com/Tiempogithub/...'
+#__doc__ = __description__ + ' <' + __uri__ + '>'
+__author__ = 'Tiempo'
+# For all support requests, please open a new issue on GitHub
+__email__ = 'henitsoa.rakotomalala@tiempo-secure.com'
+__copyright__ = 'Tiempo-Secure'
