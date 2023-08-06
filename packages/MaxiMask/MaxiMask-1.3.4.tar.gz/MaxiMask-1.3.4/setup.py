588ccfb88074d8c87cbfd49eec3e56f7e2e130b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:13 +0000

    Add package MaxiMask/1.3.4/MaxiMask-1.3.4.tar.gz

---
+++ packages/MaxiMask/MaxiMask-1.3.4.tar.gz/MaxiMask-1.3.4/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="MaxiMask",
+    version="1.3.4",
+    author="Maxime Paillassa",
+    author_email="maxime.paillassa@nagoya-u.jp",
+    description="Convolutional neural networks to detect contaminants in astronomical images.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mpaillassa/MaxiMask",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "maximask = maximask_and_maxitrack.scripts.maximask:main",
+            "maxitrack = maximask_and_maxitrack.scripts.maxitrack:main",
+        ]
+    },
+    install_requires=["astropy>=4.3.1", "scipy>1.7.1", "numpy>=1.21.5", "tqdm>=4.62.3", "tensorflow>=2.3"],
+    python_requires=">=3.6",
+    license="MIT",
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
