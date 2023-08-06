d63434e905d503790fa9b2b5af29104be5ed2176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:46 +0000

    Add package nwon-baseline/0.1.24/nwon_baseline-0.1.24.tar.gz

---
+++ packages/nwon-baseline/nwon_baseline-0.1.24.tar.gz/nwon_baseline-0.1.24/nwon_baseline/typings/__init__.py
@@ -0,0 +1,19 @@
+from nwon_baseline.typings.bounding_box import BoundingBoxCoordinates, BoundingBoxTuple
+from nwon_baseline.typings.celery_return import CeleryReturn
+from nwon_baseline.typings.django_ready_enum import DjangoReadyEnum
+from nwon_baseline.typings.pydantic_base_django import PydanticBaseDjango
+from nwon_baseline.typings.request_body_format import RequestBodyFormat
+from nwon_baseline.typings.terminal_colors import TerminalColors, TerminalStyling
+from nwon_baseline.typings.version_change import VersionChange
+
+__all__ = [
+    "BoundingBoxCoordinates",
+    "BoundingBoxTuple",
+    "CeleryReturn",
+    "DjangoReadyEnum",
+    "PydanticBaseDjango",
+    "RequestBodyFormat",
+    "TerminalColors",
+    "TerminalStyling",
+    "VersionChange",
+]
