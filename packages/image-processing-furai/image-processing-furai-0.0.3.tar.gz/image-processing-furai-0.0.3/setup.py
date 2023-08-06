f35308fc42f10e1e68ccff3e574da9eb95449160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:53 +0000

    Add package image-processing-furai/0.0.3/image-processing-furai-0.0.3.tar.gz

---
+++ packages/image-processing-furai/image-processing-furai-0.0.3.tar.gz/image-processing-furai-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image-processing-furai",
+    version="0.0.3",
+    author="Furaime",
+    author_email="louan.carvalho@gmail.com",
+    description="Image processing package to resize and analize images and histogram",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Furaime/image_processing",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
