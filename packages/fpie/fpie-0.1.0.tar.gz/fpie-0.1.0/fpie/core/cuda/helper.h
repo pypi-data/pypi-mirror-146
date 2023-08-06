a3292ce1c5917e6320c760f64e71f93d99d8904c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:24 +0000

    Add package fpie/0.1.0/fpie-0.1.0.tar.gz

---
+++ packages/fpie/fpie-0.1.0.tar.gz/fpie-0.1.0/fpie/core/cuda/helper.h
@@ -0,0 +1,46 @@
+#ifndef FPIE_CORE_CUDA_HELPER_H_
+#define FPIE_CORE_CUDA_HELPER_H_
+
+#include <tuple>
+
+#include "solver.h"
+#include "util.h"
+
+class CudaEquSolver : public EquSolver {
+ protected:
+  int* maskbuf;
+  unsigned char* imgbuf;
+  int block_size;
+  // CUDA
+  int* cA;
+  unsigned char* cimgbuf;
+  float *cB, *cX, *cerr, *tmp;
+
+ public:
+  explicit CudaEquSolver(int block_size);
+  ~CudaEquSolver();
+
+  py::array_t<int> partition(py::array_t<int> mask);
+  void post_reset();
+  std::tuple<py::array_t<unsigned char>, py::array_t<float>> step(
+      int iteration);
+};
+
+class CudaGridSolver : public GridSolver {
+ protected:
+  unsigned char* imgbuf;
+  // CUDA
+  int* cmask;
+  unsigned char* cimgbuf;
+  float *ctgt, *cgrad, *cerr, *tmp;
+
+ public:
+  CudaGridSolver(int grid_x, int grid_y);
+  ~CudaGridSolver();
+
+  void post_reset();
+  std::tuple<py::array_t<unsigned char>, py::array_t<float>> step(
+      int iteration);
+};
+
+#endif  // FPIE_CORE_CUDA_HELPER_H_
---
+++ packages/fpie/fpie-0.1.0.tar.gz/fpie-0.1.0/fpie/core/cuda/solver.cc
@@ -0,0 +1,19 @@
+#include "solver.h"
+
+#include <tuple>
+
+#include "helper.h"
+
+PYBIND11_MODULE(core_cuda, m) {
+  py::class_<CudaEquSolver>(m, "EquSolver")
+      .def(py::init<int>())
+      .def("partition", &CudaEquSolver::partition)
+      .def("reset", &CudaEquSolver::reset)
+      .def("sync", &CudaEquSolver::sync)
+      .def("step", &CudaEquSolver::step);
+  py::class_<CudaGridSolver>(m, "GridSolver")
+      .def(py::init<int, int>())
+      .def("reset", &CudaGridSolver::reset)
+      .def("sync", &CudaGridSolver::sync)
+      .def("step", &CudaGridSolver::step);
+}
