e8f3c7c4f2872141f51db891d31dbd9fe11378f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:30 +0000

    Add package requre/0.8.2/requre-0.8.2.tar.gz

---
+++ packages/requre/requre-0.8.2.tar.gz/requre-0.8.2/tests/conftest.py
@@ -0,0 +1,24 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+import pytest
+
+from requre.utils import get_datafile_filename
+
+
+@pytest.fixture
+def remove_storage_file(request):
+    storage_file = get_datafile_filename(request.node)
+    if storage_file.is_file():
+        storage_file.unlink()
+    return storage_file
+
+
+@pytest.fixture
+def remove_storage_file_after(request):
+    storage_file = get_datafile_filename(request.node)
+    yield storage_file
+    # TODO: found why this were necessary before change of singletons
+    # PersistentObjectStorage().cassette.dump()
+    if storage_file.is_file():
+        storage_file.unlink()
