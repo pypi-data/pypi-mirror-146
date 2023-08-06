01ecd52909cb6f92578abfb8444c64beed3e3274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:57:01 +0000

    Add package pyacadcom/0.0.7/pyacadcom-0.0.7-py3-none-any.whl

---
+++ packages/pyacadcom/pyacadcom-0.0.7-py3-none-any.whl/pyacadcom/__init__.py
@@ -0,0 +1,15 @@
+""""
+    pyacadcom
+    ******************
+
+    AutoCAD automation through COM using pywin32
+
+    :copyright: (c) 2022 by Dmitriy Lobyntsev
+    :licence: BSD
+"""
+
+__version__ = "0.0.7"
+
+from pyacadcom.api import *
+from pyacadcom.datatype import *
+from pyacadcom.userinput import *
