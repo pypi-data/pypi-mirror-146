2be49992a75f765c6e6085385c922844cc3c312f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:50 +0000

    Add package CppStyleIO/0.0.4/CppStyleIO-0.0.4.tar.gz

---
+++ packages/CppStyleIO/CppStyleIO-0.0.4.tar.gz/CppStyleIO-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='CppStyleIO',
+    version='0.0.4',
+    packages=[
+        'CppStyleIO'
+    ],
+    author='FCWYzzr',
+    author_email='fcwyzzr@163.com',
+)
