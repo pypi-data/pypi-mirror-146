428f79d10b06eb0a53e35d3b404afa5ff92580de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:06 +0000

    Add package raptor-functions/0.2.4/raptor_functions-0.2.4.tar.gz

---
+++ packages/raptor-functions/raptor_functions-0.2.4.tar.gz/raptor_functions-0.2.4/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['raptor_functions',
+ 'raptor_functions.dev',
+ 'raptor_functions.supervised',
+ 'raptor_functions.unsupervised']
+
+package_data = \
+{'': ['*'], 'raptor_functions': ['mlruns/0/*', 'mlruns/1/*']}
+
+setup_kwargs = {
+    'name': 'raptor-functions',
+    'version': '0.2.4',
+    'description': 'raptor functions',
+    'long_description': None,
+    'author': 'Daniel Fiuza, Ibrahim Animashaun',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Bryant-Dental/raptor_functions.git',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
