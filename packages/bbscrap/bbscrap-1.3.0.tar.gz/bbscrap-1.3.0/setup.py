0244bec8d9af290c1b0d0335a40c19c7a84d60ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package bbscrap/1.3.0/bbscrap-1.3.0.tar.gz

---
+++ packages/bbscrap/bbscrap-1.3.0.tar.gz/bbscrap-1.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bbscrap', 'bbscrap.navegacao', 'bbscrap.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ofxparse>=0.21,<0.22', 'pandas>=1.4.1,<2.0.0', 'selenium>=4.1.2,<5.0.0']
+
+setup_kwargs = {
+    'name': 'bbscrap',
+    'version': '1.3.0',
+    'description': 'Baixa extatos bancários do Banco do Brasil',
+    'long_description': None,
+    'author': 'Vinicius Maciel',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
