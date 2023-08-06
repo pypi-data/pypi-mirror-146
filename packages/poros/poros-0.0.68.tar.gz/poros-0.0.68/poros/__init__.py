c3515a684c374275af1cdf33de41a484b3ff96fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:30 +0000

    Add package poros/0.0.68/poros-0.0.68.tar.gz

---
+++ packages/poros/poros-0.0.68.tar.gz/poros-0.0.68/poros/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+comments
+author: diqiuzhuanzhuan
+email: diqiuzhuanzhuan@gmail.com
+
+"""
+
+name = "poros"
+from poros import poros_chars
+from poros import poros_dataset
+from poros import poros_loss
