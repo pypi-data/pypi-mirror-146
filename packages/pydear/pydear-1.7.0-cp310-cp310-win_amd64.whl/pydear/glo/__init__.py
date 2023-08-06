0c60e42a170a8840790c1b7100c20f9372b39730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:23 +0000

    Add package pydear/1.7.0/pydear-1.7.0-cp310-cp310-win_amd64.whl

---
+++ packages/pydear/pydear-1.7.0-cp310-cp310-win_amd64.whl/pydear/glo/__init__.py
@@ -0,0 +1,40 @@
+'''
+OpenGL Object Library
+'''
+from .vbo import Vbo, Ibo
+from .shader import Shader, UniformLocation, ShaderProp, UniformBlockIndex
+from .texture import Texture
+from .vertex_layout import AttributeLocation, VertexLayout
+from .vao import Vao
+from .fbo import Fbo, FboRenderer
+from .drawable import Drawable
+
+__all__ = [
+    'Vbo',
+    'Ibo',
+    'Shader',
+    'UniformLocation',
+    'ShaderProp',
+    'UniformBlockIndex',
+    'Texture',
+    'AttributeLocation',
+    'VertexLayout',
+    'Vao',
+    'Fbo',
+    'FboRenderer',
+    'Drawable',
+]
+
+
+def get_info():
+    from OpenGL.GL import (glGetString, glGetIntegerv, GL_VENDOR, GL_VERSION,
+                           GL_SHADING_LANGUAGE_VERSION, GL_RENDERER,
+                           GL_MAJOR_VERSION, GL_MINOR_VERSION)
+    return {
+        'major': glGetIntegerv(GL_MAJOR_VERSION),
+        'minor': glGetIntegerv(GL_MINOR_VERSION),
+        'vendor': glGetString(GL_VENDOR),
+        'version': glGetString(GL_VERSION),
+        'shader_language_version': glGetString(GL_SHADING_LANGUAGE_VERSION),
+        'renderer': glGetString(GL_RENDERER),
+    }
