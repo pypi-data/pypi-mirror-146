084f70a78a7d7b226bc1249a44c8adba573c620f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:31 +0000

    Add package Dickbot.py/1/Dickbot.py-1.tar.gz

---
+++ packages/Dickbot.py/Dickbot.py-1.tar.gz/Dickbot.py-1/BotAmino/__init__.py
@@ -0,0 +1,18 @@
+from json import loads
+from requests import get
+
+__title__ = 'Dickbot'
+__author__ = 'ThePhoenix78'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-2022 ThePhoenix78'
+__version__ = '1'
+
+
+from .BotAmino import *
+
+__newest__ = loads(get("https://pypi.python.org/pypi/Dickbot/json").text)["info"]["version"]
+
+if __version__ != __newest__:
+    print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
+else:
+    print(f"version : {__version__}")
