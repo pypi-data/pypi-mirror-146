b2f9d7a43bb006e98c7d2fbffa35e5230c65cb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:44 +0000

    Add package colcon-core/0.8.0/colcon-core-0.8.0.tar.gz

---
+++ packages/colcon-core/colcon-core-0.8.0.tar.gz/colcon-core-0.8.0/test/test_environment_pythonpath.py
@@ -0,0 +1,30 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+from pathlib import Path
+import sysconfig
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
+
+from colcon_core.environment.pythonpath import PythonPathEnvironment
+
+
+def test_pythonpath():
+    extension = PythonPathEnvironment()
+
+    with TemporaryDirectory(prefix='test_colcon_') as prefix_path:
+        prefix_path = Path(prefix_path)
+        with patch(
+            'colcon_core.shell.create_environment_hook',
+            return_value=['/some/hook', '/other/hook']
+        ):
+            # Python path does not exist
+            hooks = extension.create_environment_hooks(prefix_path, 'pkg_name')
+            assert len(hooks) == 0
+
+            # Python path exists
+            python_path = Path(
+                sysconfig.get_path('purelib', vars={'base': prefix_path}))
+            python_path.mkdir(parents=True)
+            hooks = extension.create_environment_hooks(prefix_path, 'pkg_name')
+            assert len(hooks) == 2
