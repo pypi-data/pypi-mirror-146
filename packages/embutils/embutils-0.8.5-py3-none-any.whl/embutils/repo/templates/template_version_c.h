dd4fe0e7725c257548f669689986f4796b780212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:04 +0000

    Add package embutils/0.8.5/embutils-0.8.5-py3-none-any.whl

---
+++ packages/embutils/embutils-0.8.5-py3-none-any.whl/embutils/repo/templates/template_version_c.h
@@ -0,0 +1,18 @@
+/**
+  *****************************************************************************
+  * @file    {file}
+  * @author  {author}
+  * @date    {date}
+  * @brief   Version definition.
+  * @note    Generated automatically. DO NOT EDIT.
+  *****************************************************************************
+  */
+#ifndef {guard}
+#define {guard}
+
+#define VERSION_MAJOR   {major}
+#define VERSION_MINOR   {minor}
+#define VERSION_BUILD   {build}
+#define VERSION_STRING  "{version}"
+
+#endif /* {guard} */
