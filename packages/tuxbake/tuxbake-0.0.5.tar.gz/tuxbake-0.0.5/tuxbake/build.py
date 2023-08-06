b76b778308bd38c218d61b28a7077be9f47e08fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:30 +0000

    Add package tuxbake/0.0.5/tuxbake-0.0.5.tar.gz

---
+++ packages/tuxbake/tuxbake-0.0.5.tar.gz/tuxbake-0.0.5/tuxbake/build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: set ts=4
+#
+# Copyright 2021-present Linaro Limited
+#
+# SPDX-License-Identifier: MIT
+
+from tuxbake.models import OEBuild
+from tuxmake.runtime import Terminated
+import signal
+from tuxbake.exceptions import (
+    TuxbakeParsingError,
+    TuxbakeRunCmdError,
+)
+import sys
+
+
+def build(**kwargs):
+    old_sigterm = signal.signal(signal.SIGTERM, Terminated.handle_signal)
+    oebuild = OEBuild(**kwargs)
+    oebuild.validate()
+    oebuild.prepare()
+    oebuild.do_build()
+    oebuild.do_cleanup()
+    signal.signal(signal.SIGTERM, old_sigterm)
+    return oebuild
