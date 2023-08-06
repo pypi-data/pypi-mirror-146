f7496e4df1d882f1e763198fa8e73e32dde92e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:05 +0000

    Add package blender-stubs/2.91.27/blender-stubs-2.91.27.tar.gz

---
+++ packages/blender-stubs/blender-stubs-2.91.27.tar.gz/blender-stubs-2.91.27/bpy/ops/simulation.pyi
@@ -0,0 +1,21 @@
+"""
+
+
+Simulation Operators
+********************
+
+:func:`new`
+
+"""
+
+import typing
+
+def new() -> None:
+
+  """
+
+  Create a new simulation data block and edit it in the opened simulation editor
+
+  """
+
+  ...
