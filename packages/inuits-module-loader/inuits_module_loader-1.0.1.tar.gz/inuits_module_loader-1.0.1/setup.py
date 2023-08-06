e2d6e9312e6129da4f73814551348fb3079ae80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:00 +0000

    Add package inuits-module-loader/1.0.1/inuits_module_loader-1.0.1.tar.gz

---
+++ packages/inuits-module-loader/inuits_module_loader-1.0.1.tar.gz/inuits_module_loader-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.md").read_text()
+
+setup(
+    name='inuits_module_loader',
+    version='1.0.1',
+    description='A library to load modules/classes based on their dotted string path.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    author='Matthias Dillen',
+    author_email='matthias.dillen@inuits.eu',
+    license='GPLv2',
+    packages=[
+        'inuits_module_loader'
+    ],
+    provides=['inuits_module_loader'],
+    url='https://github.com/inuits/module-loader'
+)
