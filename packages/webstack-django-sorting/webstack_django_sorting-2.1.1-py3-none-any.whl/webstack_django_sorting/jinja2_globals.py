27fa066b4ec54782d775914274eead205d856a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:57:11 +0000

    Add package webstack-django-sorting/2.1.1/webstack_django_sorting-2.1.1-py3-none-any.whl

---
+++ packages/webstack-django-sorting/webstack_django_sorting-2.1.1-py3-none-any.whl/webstack_django_sorting/jinja2_globals.py
@@ -0,0 +1,14 @@
+from markupsafe import Markup
+
+from . import common
+
+
+def sorting_anchor(request, field_name, title):
+    return Markup(common.render_sort_anchor(request, field_name, title))
+
+
+def sort_queryset(request, queryset, **null_ordering):
+    if not null_ordering:
+        null_ordering = {}
+    order_by = common.get_order_by_from_request(request)
+    return common.sort_queryset(queryset, order_by, null_ordering)
