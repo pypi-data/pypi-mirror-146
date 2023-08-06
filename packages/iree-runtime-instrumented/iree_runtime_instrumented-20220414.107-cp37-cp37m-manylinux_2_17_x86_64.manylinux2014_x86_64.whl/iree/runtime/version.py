55a13e6f5613d6e468e9a72eb3ff8c702e081a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:13 +0000

    Add package iree-runtime-instrumented/20220414.107/iree_runtime_instrumented-20220414.107-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/iree-runtime-instrumented/iree_runtime_instrumented-20220414.107-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/iree/runtime/version.py
@@ -0,0 +1,9 @@
+# Copyright 2020 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+PACKAGE_SUFFIX = "-instrumented"
+VERSION = "20220414.107"
+REVISION = "75fa2e7c8be3dfb3c5c1ac08d5ae5c1708a3e42f"
