ca0d6736b13c8b1895365f87d719f080f271ab4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:17 +0000

    Add package matchfarm/0.1.2/matchFarm-0.1.2.tar.gz

---
+++ packages/matchfarm/matchFarm-0.1.2.tar.gz/MatchFarm-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name = 'MatchFarm',
+    version = '0.1.2',
+    author = 'Carson Vache',
+    author_email = 'carsonvache@gmail.com',
+    license = 'GNU GENERAL PUBLIC LICENSE',
+    description = 'Farm LOL matches',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/cvache/LeagueMatchFarm',
+    py_modules = ['matchFarm', 'src'],
+    packages = find_packages(),
+    install_requires = [
+        "requests",
+        "riotwatcher",
+        "boto3"
+        "json"     
+        ],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ]
+)
