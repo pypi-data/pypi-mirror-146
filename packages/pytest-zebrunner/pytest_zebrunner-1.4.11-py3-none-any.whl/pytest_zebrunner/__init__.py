502f0617127a44a3b54713ac0577931bf1771b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package pytest-zebrunner/1.4.11/pytest_zebrunner-1.4.11-py3-none-any.whl

---
+++ packages/pytest-zebrunner/pytest_zebrunner-1.4.11-py3-none-any.whl/pytest_zebrunner/__init__.py
@@ -0,0 +1,20 @@
+from pytest_zebrunner.attachments import (
+    attach_test_artifact,
+    attach_test_artifact_reference,
+    attach_test_label,
+    attach_test_run_artifact,
+    attach_test_run_artifact_reference,
+    attach_test_run_label,
+    attach_test_screenshot,
+)
+
+__version__ = "1.4.11"
+__all__ = [
+    "attach_test_artifact_reference",
+    "attach_test_run_artifact_reference",
+    "attach_test_label",
+    "attach_test_run_label",
+    "attach_test_artifact",
+    "attach_test_run_artifact",
+    "attach_test_screenshot",
+]
