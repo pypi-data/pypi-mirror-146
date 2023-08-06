34bd4801944b11ea9750966ef66d122a8f756eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:29 +0000

    Add package metashape-dwieka-test/0.0.1/metashape_dwieka_test-0.0.1-py3-none-any.whl

---
+++ packages/metashape-dwieka-test/metashape_dwieka_test-0.0.1-py3-none-any.whl/get_location/get_location.py
@@ -0,0 +1,11 @@
+import Metashape
+
+class get_location_data_from_metashape:
+    """
+    This is how you process the data
+    """    
+    def __init__(self) -> None:
+        pass
+    def get_export_data():
+        path_ekspor = Metashape.app.getExistingDirectory("Hasil Ekspor")
+        print(path_ekspor)
