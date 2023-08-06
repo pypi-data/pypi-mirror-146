b9f397625b8a78955110b245a30ba1253f23a5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:50 +0000

    Add package makerpy/0.0.1/makerpy-0.0.1.tar.gz

---
+++ packages/makerpy/makerpy-0.0.1.tar.gz/makerpy-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='makerpy',
+  version='0.0.1',
+  description='A very work in progress tool that helps you create a python project by helping you with for example pygame',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Rocko Visser',
+  author_email='rockodagamer@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='app development',
+  packages=find_packages(),
+  install_requires=['pygame', 'tensorflow']
+)
