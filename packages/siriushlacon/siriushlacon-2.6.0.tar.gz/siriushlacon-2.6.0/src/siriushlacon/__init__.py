1e8e9b9496e0f029fc87b5bd091cff86d51e5468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:19 +0000

    Add package siriushlacon/2.6.0/siriushlacon-2.6.0.tar.gz

---
+++ packages/siriushlacon/siriushlacon-2.6.0.tar.gz/siriushlacon-2.6.0/src/siriushlacon/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "2.6.0"
+__author__ = "Claudio F. Carneiro"
+__date__ = "Mon, 11 Apr 2022 12:39:43 +0000"
+
+DEFAULT_WINDOW_TITLE: str = "Sirius Interfaces"
+GENERIC_LAUNCHER_FILE_NAME: str = "sirius-hla-as-ap-generic-launcher"
+VBC_LAUNCHER_FILE_NAME: str = "sirius-hla-as-va-vbc"
+MAIN_LAUNCHER_FILE_NAME: str = "sirius-hla-as-ap-conlauncher"
