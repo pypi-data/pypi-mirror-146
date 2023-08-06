9421cb75e697fb7cff68414fc630db2bfae42c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:06 +0000

    Add package modelflow/0.0.12/modelflow-0.0.12-py2.py3-none-any.whl

---
+++ packages/modelflow/modelflow-0.0.12-py2.py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,81 @@
+import pytest
+import pandas as pd
+from modelflow import model_data
+from .params import *
+
+
+@pytest.fixture(scope="session")
+def test_csv():
+    return pd.read_csv(CLASS_DF_PATH)
+
+
+@pytest.fixture(scope="session")
+def rf_class_object(test_csv):
+    return model_data(
+        test_csv,
+        cat_names=CAT_COLS,
+        cont_names=CONT_COLS,
+        dep_var=CLASS_DEP_VAR,
+        model="rf",
+        perc_train=TRAIN_SPLIT,
+    )
+
+
+@pytest.fixture(scope="session")
+def xgb_class_object(test_csv):
+    return model_data(
+        test_csv,
+        cat_names=CAT_COLS,
+        cont_names=CONT_COLS,
+        dep_var=CLASS_DEP_VAR,
+        model="xgb",
+        perc_train=TRAIN_SPLIT,
+    )
+
+
+@pytest.fixture(scope="session")
+def ebm_class_object(test_csv):
+    return model_data(
+        test_csv,
+        cat_names=CAT_COLS,
+        cont_names=CONT_COLS,
+        dep_var=CLASS_DEP_VAR,
+        model="ebm",
+        perc_train=TRAIN_SPLIT,
+    )
+
+
+@pytest.fixture(scope="session")
+def rf_reg_object(test_csv):
+    return model_data(
+        test_csv,
+        cat_names=CAT_COLS,
+        cont_names=CONT_COLS,
+        dep_var=REG_DEP_VAR,
+        model="rf",
+        perc_train=TRAIN_SPLIT,
+    )
+
+
+@pytest.fixture(scope="session")
+def xgb_reg_object(test_csv):
+    return model_data(
+        test_csv,
+        cat_names=CAT_COLS,
+        cont_names=CONT_COLS,
+        dep_var=REG_DEP_VAR,
+        model="xgb",
+        perc_train=TRAIN_SPLIT,
+    )
+
+
+@pytest.fixture(scope="session")
+def ebm_reg_object(test_csv):
+    return model_data(
+        test_csv,
+        cat_names=CAT_COLS,
+        cont_names=CONT_COLS,
+        dep_var=REG_DEP_VAR,
+        model="ebm",
+        perc_train=TRAIN_SPLIT,
+    )
