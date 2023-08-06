64d17f743bc8667ed5c1c64505240956f0b2ea6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:44 +0000

    Add package nlhappy/0.1.53/nlhappy-0.1.53.tar.gz

---
+++ packages/nlhappy/nlhappy-0.1.53.tar.gz/nlhappy-0.1.53/nlhappy/__init__.py
@@ -0,0 +1,31 @@
+
+def explain(key: str):
+
+    """
+    Explain the meaning of a key.
+
+    :param key: The key to explain.
+    :return: The explanation.
+    """
+
+    if key == "ICD-STS":
+        return "医疗术语语义相似度任务,在ICD10上根据章节,亚目,类目的不同筛选出的疾病名称句子对,相似度分别为0, 0.1, 0.3, 0.5"
+    elif key == "CHIP-CDN":
+        return "CBLUE排行榜中临床术语标准化任务的数据集, 诊断词与标准词含有一对多的情况"
+    elif key == "CMeIE":
+        return "CBLUE排行榜中中文医学文本实体关系抽取任务数据集,文本中含有@,并且语义模糊."
+    elif key == "CMeEE":
+        return "CBLUE排行榜中中文医学命名实体识别数据集, 含有嵌套的情况."
+    elif key == "LCQMC":
+        return "中文问答匹配数据集, 238766训练集、8802验证集和12500测试集"
+    elif key == "CHIP-CTC":
+        return "临床试验筛选标准短文本分类, https://tianchi.aliyun.com/dataset/dataDetail?dataId=95414#1"
+    elif key == "CHIP-STS":
+        return "平安医疗科技疾病问答迁移学习, https://tianchi.aliyun.com/dataset/dataDetail?dataId=95414#1"
+    elif key == "TNEWS":
+        return "今日头条中文新闻（短文本）分类, 该数据集来自今日头条的新闻版块，共提取了15个类别的新闻，包括旅游，教育，金融，军事等"
+    elif key == "JDNER":
+        return "京东商城商品标题命名实体识别数据集. 标签全部为脱敏数字, 标签非常不均衡."
+    
+    
+    else: return "No such key"
---
+++ packages/nlhappy/nlhappy-0.1.53.tar.gz/nlhappy-0.1.53/setup.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nlhappy',
+ 'nlhappy.callbacks',
+ 'nlhappy.configs',
+ 'nlhappy.datamodules',
+ 'nlhappy.layers',
+ 'nlhappy.layers.attention',
+ 'nlhappy.layers.classifier',
+ 'nlhappy.layers.embedding',
+ 'nlhappy.metrics',
+ 'nlhappy.models',
+ 'nlhappy.models.event_extraction',
+ 'nlhappy.models.language_modeling',
+ 'nlhappy.models.span_classification',
+ 'nlhappy.models.text_classification',
+ 'nlhappy.models.text_multi_classification',
+ 'nlhappy.models.text_pair_classification',
+ 'nlhappy.models.text_pair_regression',
+ 'nlhappy.models.token_classification',
+ 'nlhappy.models.triple_extraction',
+ 'nlhappy.spacy_components',
+ 'nlhappy.tricks',
+ 'nlhappy.utils']
+
+package_data = \
+{'': ['*'],
+ 'nlhappy.configs': ['callbacks/*',
+                     'datamodule/*',
+                     'experiment/*',
+                     'hparams_search/*',
+                     'logger/*',
+                     'mode/*',
+                     'model/*',
+                     'trainer/*']}
+
+install_requires = \
+['catalogue>=2.0.7,<3.0.0',
+ 'datasets>=2.0.0,<3.0.0',
+ 'hydra-colorlog>=1.1.0,<2.0.0',
+ 'hydra-core>=1.1.1,<2.0.0',
+ 'onnx>=1.11.0,<2.0.0',
+ 'onnxruntime>=1.10.0,<2.0.0',
+ 'oss2>=2.15.0,<3.0.0',
+ 'pytorch-lightning>=1.5.10,<2.0.0',
+ 'rich>=12.0.0,<13.0.0',
+ 'spacy>=3.2.3,<4.0.0',
+ 'transformers>=4.17.0,<5.0.0',
+ 'wandb>=0.12.11,<0.13.0']
+
+entry_points = \
+{'console_scripts': ['nlhappy = nlhappy.run:train'],
+ 'spacy_factories': ['span_classifier = nlhappy.spacy_components:make_spancat']}
+
+setup_kwargs = {
+    'name': 'nlhappy',
+    'version': '0.1.53',
+    'description': '致力于SOTA的中文自然语言处理库',
+    'long_description': None,
+    'author': 'wangmengdi',
+    'author_email': '790990241@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
