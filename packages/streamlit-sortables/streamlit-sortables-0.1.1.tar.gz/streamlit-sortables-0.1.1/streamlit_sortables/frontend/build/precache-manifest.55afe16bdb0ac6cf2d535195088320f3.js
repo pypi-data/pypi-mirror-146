077f308ca6bd9464c17f4cc2c7b2c021e165b251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:43 +0000

    Add package streamlit-sortables/0.1.1/streamlit-sortables-0.1.1.tar.gz

---
+++ packages/streamlit-sortables/streamlit-sortables-0.1.1.tar.gz/streamlit-sortables-0.1.1/streamlit_sortables/frontend/build/precache-manifest.55afe16bdb0ac6cf2d535195088320f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8029bc5b3a9c671de85a8f811b9121",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67376360b646fcd99ddb",
+    "url": "./static/css/main.abab62d5.chunk.css"
+  },
+  {
+    "revision": "c652ec5191651b5bb12e",
+    "url": "./static/js/2.a4777b71.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.a4777b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67376360b646fcd99ddb",
+    "url": "./static/js/main.383337f1.chunk.js"
+  },
+  {
+    "revision": "af938b039e0e624810f1",
+    "url": "./static/js/runtime-main.09df5465.js"
+  }
+]);
