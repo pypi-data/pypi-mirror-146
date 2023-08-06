7bb9d6e21c742c39cfb38492233d0296b70e141a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:52 +0000

    Add package mopidy-yamusic/0.9.5/mopidy-yamusic-0.9.5.tar.gz

---
+++ packages/mopidy-yamusic/mopidy-yamusic-0.9.5.tar.gz/mopidy-yamusic-0.9.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='mopidy-yamusic',
+    version='0.9.5',
+    description='Mopidy extension for playing music from YandexMusic',
+    description_file='README.md',
+    url='https://github.com/stffart/mopidy-yamusic',
+    author='stffart',
+    author_email='stffart@gmail.com',
+    license='GPLv3',
+    packages=['mopidy_yamusic'],
+    package_dir={'mopidy_yamusic':'mopidy_yamusic'},
+    package_data={'mopidy_yamusic':['ext.conf']},
+    install_requires=['yandex-music<2.0',
+                      ],
+    entry_points={
+        'mopidy.ext': [
+            'yamusic = mopidy_yamusic:Extension',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
+
