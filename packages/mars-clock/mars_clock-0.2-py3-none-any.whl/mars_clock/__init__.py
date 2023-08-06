a449fa1b4d192e5daa7d40cb1773488e76ae5d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:45 +0000

    Add package mars-clock/0.2/mars_clock-0.2-py3-none-any.whl

---
+++ packages/mars-clock/mars_clock-0.2-py3-none-any.whl/mars_clock/__init__.py
@@ -0,0 +1,8 @@
+"""A package for determining Ls-values from mission sols"""
+
+__version__ = '0.2'
+__author__ = 'Brian Jackson <bjackson@boisestate.edu>'
+__all__ = ['Ls_utils', 'mission_sols']
+
+from .Ls_utils import *
+from .mission_sols import *
