9de4a8970fbd61226d7ecff71aaffdac8e334e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:19 +0000

    Add package metaapi-cloud-sdk/19.2.0/metaapi_cloud_sdk-19.2.0.tar.gz

---
+++ packages/metaapi-cloud-sdk/metaapi_cloud_sdk-19.2.0.tar.gz/metaapi_cloud_sdk-19.2.0/lib/__init__.py
@@ -0,0 +1,7 @@
+from .metaApi.metaApi import MetaApi
+from .metaApi.historyStorage import HistoryStorage
+from .metaApi.memoryHistoryStorage import MemoryHistoryStorage
+from .clients.metaApi.synchronizationListener import SynchronizationListener
+from .metaApi.models import format_error, format_date, date
+from metaapi_cloud_copyfactory_sdk import CopyFactory, StopoutListener
+from metaapi_cloud_metastats_sdk import MetaStats
