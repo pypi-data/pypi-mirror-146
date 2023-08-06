12df83bf8ffddb63a3d6d72d985b3e629060f383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:49 +0000

    Add package nnext/0.0.3/nnext-0.0.3-py2.py3-none-any.whl

---
+++ packages/nnext/nnext-0.0.3-py2.py3-none-any.whl/nnext/client.py
@@ -0,0 +1,23 @@
+# #!/usr/bin/env python
+
+__authors__ = ["Peter W. Njenga"]
+__copyright__ = "Copyright 2021, NNext, Co."
+
+import logging
+import string
+import random
+
+import grpc
+
+from main_pb2 import VectorAddRequest
+from main_pb2_grpc import NNextStub
+
+class Client(object):
+    def __init__(self, config_dict):
+
+        with grpc.insecure_channel(target='localhost:6040',
+                                   options=[('grpc.lb_policy_name', 'pick_first'),
+                                            ('grpc.enable_retries', 0),
+                                            ('grpc.keepalive_timeout_ms', 10000)
+                                            ]) as channel:
+            stub = NNextStub(channel)
