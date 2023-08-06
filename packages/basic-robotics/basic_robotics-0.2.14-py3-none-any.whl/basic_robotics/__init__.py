b71df18c80e0704506c64204767050643b00102d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:30 +0000

    Add package basic-robotics/0.2.14/basic_robotics-0.2.14-py3-none-any.whl

---
+++ packages/basic-robotics/basic_robotics-0.2.14-py3-none-any.whl/basic_robotics/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+
+# Version
+__version__ = "0.1.26"
+from . import interfaces
+from . import modern_robotics_numba
+from . import general
+from . import plotting
+from . import kinematics
+from . import utilities
+from . import robot_collisions
