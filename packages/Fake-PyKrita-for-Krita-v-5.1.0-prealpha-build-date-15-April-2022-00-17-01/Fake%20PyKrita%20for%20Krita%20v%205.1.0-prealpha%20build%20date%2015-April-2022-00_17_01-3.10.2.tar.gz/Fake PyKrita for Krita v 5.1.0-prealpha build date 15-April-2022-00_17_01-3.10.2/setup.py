0c538f95c4b8e52bb38c02c4db8378c6f6880f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:15 +0000

    Add package Fake-PyKrita-for-Krita-v-5.1.0-prealpha-build-date-15-April-2022-00-17-01/3.10.2/Fake%20PyKrita%20for%20Krita%20v%205.1.0-prealpha%20build%20date%2015-April-2022-00_17_01-3.10.2.tar.gz

---
+++ packages/Fake-PyKrita-for-Krita-v-5.1.0-prealpha-build-date-15-April-2022-00-17-01/Fake%20PyKrita%20for%20Krita%20v%205.1.0-prealpha%20build%20date%2015-April-2022-00_17_01-3.10.2.tar.gz/Fake PyKrita for Krita v 5.1.0-prealpha build date 15-April-2022-00_17_01-3.10.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Fake PyKrita for Krita v 5.1.0-prealpha build date 15-April-2022-00_17_01',
+    # version='0.1',
+    version = '3.10.2',
+    license='Unknown',
+    author="Source code generator by scottpetrovic, modified by Olliver Aira aka officernickwilde.",
+    author_email='olliver.aira@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    # url='https://github.com',
+    keywords='Krita intellisense autocomplete autocompletion Python PyKrita code highlight fake module',
+    install_requires=[],
+
+)
