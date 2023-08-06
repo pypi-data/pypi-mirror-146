d14807c3358d14d484353ff0cd2c6ea35f9801d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:48 +0000

    Add package pdc-bot-updater/1.1.0/pdc_bot_updater-1.1.0.tar.gz

---
+++ packages/pdc-bot-updater/pdc_bot_updater-1.1.0.tar.gz/pdc_bot_updater-1.1.0/README.md
@@ -0,0 +1,6 @@
+# pdc_updater
+implement autoupdater aplication shipment
+
+
+## set environtment
+set GOOGLE_APPLICATION_CREDENTIALS=credentials.json
---
+++ packages/pdc-bot-updater/pdc_bot_updater-1.1.0.tar.gz/pdc_bot_updater-1.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pdc_bot_updater',
+ 'pdc_bot_updater.client',
+ 'pdc_bot_updater.core',
+ 'pdc_bot_updater.publisher']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-cloud-storage>=2.2.1,<3.0.0',
+ 'pydantic>=1.9.0,<2.0.0',
+ 'requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pdc-bot-updater',
+    'version': '1.1.0',
+    'description': 'pdc shipment aplication bot',
+    'long_description': '# pdc_updater\nimplement autoupdater aplication shipment\n\n\n## set environtment\nset GOOGLE_APPLICATION_CREDENTIALS=credentials.json\n',
+    'author': 'kampretcode',
+    'author_email': 'manorder123@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
