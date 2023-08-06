a5f12b38075d64c7ce5dc71f96a2dbc48bbdd35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:16 +0000

    Add package get-unipa/0.8.0/get-unipa-0.8.0.tar.gz

---
+++ packages/get-unipa/get-unipa-0.8.0.tar.gz/get-unipa-0.8.0/setup.py
@@ -0,0 +1,26 @@
+"""
+setup.py
+"""
+import os
+
+import setuptools
+
+setuptools.setup(
+    name='get-unipa',
+    version=open("get-unipa.version").read().strip() if os.path.exists("get-unipa.version") else "0.0.0",
+    packages=setuptools.find_packages(exclude=['examples']),
+    install_requires=["beautifulsoup4", "requests", "html5lib"],
+    url='https://github.com/book000/get-unipa',
+    license='MIT',
+    author='Tomachi',
+    author_email='tomachi@tomacheese.com',
+    maintainer='Tomachi',
+    maintainer_email='tomachi@tomacheese.com',
+    description='Library for get various information about UNIVERSAL PASSPORT.',
+    long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
