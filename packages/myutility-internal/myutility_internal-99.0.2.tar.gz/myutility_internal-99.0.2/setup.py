db69313bb78f54342974ceba85caf8d178dc53e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:21 +0000

    Add package myutility-internal/99.0.2/myutility_internal-99.0.2.tar.gz

---
+++ packages/myutility-internal/myutility_internal-99.0.2.tar.gz/myutility_internal-99.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='myutility_internal',
+    version='99.0.2',
+    description='This is an internal utility that will process numbers.',
+    py_modules=["myutility_internal"],
+    package_dir={'': 'src'},
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+            "check-manifest",
+            "twine"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+	"Programming Language :: Python :: 3.9",
+	'Programming Language :: Python :: 3.10',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Super Secure",
+    author_email="sectester15@gmail.com",
+    url="https://github.com/ndoell/myutility_internal"
+)
