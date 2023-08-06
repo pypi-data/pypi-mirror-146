ecb8a6e87780dba858f03ad8bf5ccd38dc68d5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:39 +0000

    Add package openmetadata-ingestion/0.9.2.dev6/openmetadata-ingestion-0.9.2.dev6.tar.gz

---
+++ packages/openmetadata-ingestion/openmetadata-ingestion-0.9.2.dev6.tar.gz/openmetadata-ingestion-0.9.2.dev6/src/metadata/ingestion/bulksink/metadata_usage.py
@@ -0,0 +1,183 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import json
+import logging
+from datetime import datetime
+
+from metadata.config.common import ConfigModel
+from metadata.generated.schema.entity.data.database import Database
+from metadata.generated.schema.entity.data.table import ColumnJoins, Table, TableJoins
+from metadata.ingestion.api.bulk_sink import BulkSink, BulkSinkStatus
+from metadata.ingestion.api.common import WorkflowContext
+from metadata.ingestion.models.table_queries import (
+    ColumnJoinedWith,
+    TableColumn,
+    TableUsageCount,
+    TableUsageRequest,
+)
+from metadata.ingestion.ometa.client import APIError
+from metadata.ingestion.ometa.ometa_api import OpenMetadata
+from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
+
+logger = logging.getLogger(__name__)
+
+
+class MetadataUsageSinkConfig(ConfigModel):
+    filename: str
+
+
+class MetadataUsageBulkSink(BulkSink):
+    config: MetadataUsageSinkConfig
+
+    def __init__(
+        self,
+        ctx: WorkflowContext,
+        config: MetadataUsageSinkConfig,
+        metadata_config: MetadataServerConfig,
+    ):
+        super().__init__(ctx)
+        self.config = config
+        self.metadata_config = metadata_config
+        self.service_name = None
+        self.wrote_something = False
+        self.file_handler = open(self.config.filename, "r")
+        self.metadata = OpenMetadata(self.metadata_config)
+        self.status = BulkSinkStatus()
+        self.table_join_dict = {}
+        self.today = datetime.today().strftime("%Y-%m-%d")
+
+    @classmethod
+    def create(
+        cls, config_dict: dict, metadata_config_dict: dict, ctx: WorkflowContext
+    ):
+        config = MetadataUsageSinkConfig.parse_obj(config_dict)
+        metadata_config = MetadataServerConfig.parse_obj(metadata_config_dict)
+        return cls(ctx, config, metadata_config)
+
+    def handle_work_unit_start(self, wu):
+        pass
+
+    def handle_work_unit_end(self, wu):
+        pass
+
+    def write_records(self) -> None:
+        usage_records = [json.loads(l) for l in self.file_handler.readlines()]
+        for record in usage_records:
+            table_usage = TableUsageCount(**json.loads(record))
+            if "." in table_usage.table:
+                table_usage.database, table_usage.table = table_usage.table.split(".")[
+                    -2:
+                ]
+            self.service_name = table_usage.service_name
+            table_entity = self.__get_table_entity(
+                table_usage.database, table_usage.table
+            )
+            if table_entity is not None:
+                table_usage_request = TableUsageRequest(
+                    date=table_usage.date, count=table_usage.count
+                )
+                try:
+                    self.metadata.publish_table_usage(table_entity, table_usage_request)
+                except Exception as err:
+                    self.status.failures.append(table_usage_request)
+                    logger.error(
+                        "Failed to update usage for {} {}".format(
+                            table_usage.table, err
+                        )
+                    )
+                table_join_request = self.__get_table_joins(table_usage)
+                logger.debug("table join request {}".format(table_join_request))
+                try:
+                    if (
+                        table_join_request is not None
+                        and len(table_join_request.columnJoins) > 0
+                    ):
+                        self.metadata.publish_frequently_joined_with(
+                            table_entity, table_join_request
+                        )
+                except APIError as err:
+                    self.status.failures.append(table_join_request)
+                    logger.error(
+                        "Failed to update query join for {}, {}".format(
+                            table_usage.table, err
+                        )
+                    )
+
+            else:
+                logger.warning(
+                    "Table does not exist, skipping usage publish {}, {}".format(
+                        table_usage.table, table_usage.database
+                    )
+                )
+        try:
+            self.metadata.compute_percentile(Table, self.today)
+            self.metadata.compute_percentile(Database, self.today)
+        except APIError:
+            logger.error("Failed to publish compute.percentile")
+
+    def __get_table_joins(self, table_usage):
+        table_joins: TableJoins = TableJoins(columnJoins=[], startDate=table_usage.date)
+        column_joins_dict = {}
+        for column_join in table_usage.joins:
+            joined_with = {}
+            if column_join.table_column is None or len(column_join.joined_with) == 0:
+                continue
+            if column_join.table_column.column in column_joins_dict.keys():
+                joined_with = column_joins_dict[column_join.table_column.column]
+            else:
+                column_joins_dict[column_join.table_column.column] = {}
+            main_column_fqdn = self.__get_column_fqdn(
+                table_usage.database, column_join.table_column
+            )
+            for column in column_join.joined_with:
+                joined_column_fqdn = self.__get_column_fqdn(
+                    table_usage.database, column
+                )
+                if joined_column_fqdn in joined_with.keys():
+                    column_joined_with = joined_with[joined_column_fqdn]
+                    column_joined_with.joinCount += 1
+                    joined_with[joined_column_fqdn] = column_joined_with
+                elif joined_column_fqdn is not None:
+                    joined_with[joined_column_fqdn] = ColumnJoinedWith(
+                        fullyQualifiedName=joined_column_fqdn, joinCount=1
+                    )
+                else:
+                    logger.info(
+                        f"Skipping join columns for {column} {joined_column_fqdn}"
+                    )
+            column_joins_dict[column_join.table_column.column] = joined_with
+
+        for key, value in column_joins_dict.items():
+            table_joins.columnJoins.append(
+                ColumnJoins(columnName=key, joinedWith=list(value.values()))
+            )
+        return table_joins
+
+    def __get_column_fqdn(self, database: str, table_column: TableColumn):
+        table_entity = self.__get_table_entity(database, table_column.table)
+        if table_entity is None:
+            return None
+        for tbl_column in table_entity.columns:
+            if table_column.column.lower() == tbl_column.name.__root__.lower():
+                return tbl_column.fullyQualifiedName.__root__
+
+    def __get_table_entity(self, database_name: str, table_name: str) -> Table:
+        table_fqn = f"{self.service_name}.{database_name}.{table_name}"
+        table_entity = self.metadata.get_by_name(Table, fqdn=table_fqn)
+        return table_entity
+
+    def get_status(self):
+        return self.status
+
+    def close(self):
+        self.file_handler.close()
+        self.metadata.close()
---
+++ packages/openmetadata-ingestion/openmetadata-ingestion-0.9.2.dev6.tar.gz/openmetadata-ingestion-0.9.2.dev6/src/metadata/utils/helpers.py
@@ -0,0 +1,245 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import logging
+import traceback
+from datetime import datetime, timedelta
+from typing import Any, Dict, Iterable, List
+
+from metadata.generated.schema.api.lineage.addLineage import AddLineageRequest
+from metadata.generated.schema.api.services.createDashboardService import (
+    CreateDashboardServiceRequest,
+)
+from metadata.generated.schema.api.services.createDatabaseService import (
+    CreateDatabaseServiceRequest,
+)
+from metadata.generated.schema.api.services.createMessagingService import (
+    CreateMessagingServiceRequest,
+)
+from metadata.generated.schema.api.services.createPipelineService import (
+    CreatePipelineServiceRequest,
+)
+from metadata.generated.schema.api.services.createStorageService import (
+    CreateStorageServiceRequest,
+)
+from metadata.generated.schema.entity.data.table import Table
+from metadata.generated.schema.entity.services.dashboardService import DashboardService
+from metadata.generated.schema.entity.services.databaseService import DatabaseService
+from metadata.generated.schema.entity.services.messagingService import MessagingService
+from metadata.generated.schema.entity.services.pipelineService import PipelineService
+from metadata.generated.schema.entity.services.storageService import StorageService
+from metadata.generated.schema.type.entityLineage import EntitiesEdge
+from metadata.generated.schema.type.entityReference import EntityReference
+from metadata.ingestion.ometa.ometa_api import OpenMetadata
+
+logger = logging.getLogger(__name__)
+
+
+def get_start_and_end(duration):
+    today = datetime.utcnow()
+    start = (today + timedelta(0 - duration)).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+    end = (today + timedelta(3)).replace(hour=0, minute=0, second=0, microsecond=0)
+    return start, end
+
+
+def snake_to_camel(s):
+    a = s.split("_")
+    a[0] = a[0].capitalize()
+    if len(a) > 1:
+        a[1:] = [u.title() for u in a[1:]]
+    return "".join(a)
+
+
+def get_database_service_or_create(
+    config, metadata_config, service_name=None
+) -> DatabaseService:
+    metadata = OpenMetadata(metadata_config)
+    config.service_name = service_name if service_name else config.service_name
+    service = metadata.get_by_name(entity=DatabaseService, fqdn=config.service_name)
+    if service:
+        return service
+    else:
+        password = (
+            config.password.get_secret_value()
+            if hasattr(config, "password") and config.password
+            else None
+        )
+        service = {
+            "databaseConnection": {
+                "hostPort": config.host_port if hasattr(config, "host_port") else None,
+                "username": config.username if hasattr(config, "username") else None,
+                "password": password,
+                "database": config.database if hasattr(config, "database") else None,
+                "connectionOptions": config.options
+                if hasattr(config, "options")
+                else None,
+                "connectionArguments": config.connect_args
+                if hasattr(config, "connect_args")
+                else None,
+            },
+            "name": config.service_name,
+            "description": "",
+            "serviceType": config.get_service_type(),
+        }
+        logger.info(f"Creating DatabaseService instance for {config.service_name}")
+        created_service = metadata.create_or_update(
+            CreateDatabaseServiceRequest(**service)
+        )
+        return created_service
+
+
+def get_messaging_service_or_create(
+    service_name: str,
+    message_service_type: str,
+    schema_registry_url: str,
+    brokers: List[str],
+    metadata_config,
+) -> MessagingService:
+    metadata = OpenMetadata(metadata_config)
+    service = metadata.get_by_name(entity=MessagingService, fqdn=service_name)
+    if service is not None:
+        return service
+    else:
+        created_service = metadata.create_or_update(
+            CreateMessagingServiceRequest(
+                name=service_name,
+                serviceType=message_service_type,
+                brokers=brokers,
+                schemaRegistry=schema_registry_url,
+            )
+        )
+        return created_service
+
+
+def get_dashboard_service_or_create(
+    service_name: str,
+    dashboard_service_type: str,
+    username: str,
+    password: str,
+    dashboard_url: str,
+    metadata_config,
+) -> DashboardService:
+    metadata = OpenMetadata(metadata_config)
+    service = metadata.get_by_name(entity=DashboardService, fqdn=service_name)
+    if service is not None:
+        return service
+    else:
+        created_service = metadata.create_or_update(
+            CreateDashboardServiceRequest(
+                name=service_name,
+                serviceType=dashboard_service_type,
+                username=username,
+                password=password,
+                dashboardUrl=dashboard_url,
+            )
+        )
+        return created_service
+
+
+def get_pipeline_service_or_create(service_json, metadata_config) -> PipelineService:
+    metadata = OpenMetadata(metadata_config)
+    service = metadata.get_by_name(entity=PipelineService, fqdn=service_json["name"])
+    if service is not None:
+        return service
+    else:
+        created_service = metadata.create_or_update(
+            CreatePipelineServiceRequest(**service_json)
+        )
+        return created_service
+
+
+def get_storage_service_or_create(service_json, metadata_config) -> StorageService:
+    metadata = OpenMetadata(metadata_config)
+    service = metadata.get_by_name(entity=StorageService, fqdn=service_json["name"])
+    if service is not None:
+        return service
+    else:
+        created_service = metadata.create_or_update(
+            CreateStorageServiceRequest(**service_json)
+        )
+        return created_service
+
+
+def get_database_service_or_create_v2(service_json, metadata_config) -> DatabaseService:
+    metadata = OpenMetadata(metadata_config)
+    service = metadata.get_by_name(entity=DatabaseService, fqdn=service_json["name"])
+    if service is not None:
+        return service
+    else:
+        created_service = metadata.create_or_update(
+            CreateDatabaseServiceRequest(**service_json)
+        )
+    return created_service
+
+
+def datetime_to_ts(date: datetime) -> int:
+    """
+    Convert a given date to a timestamp as an Int
+    """
+    return int(date.timestamp())
+
+
+def create_lineage(from_table, to_table, query_info, metadata):
+    try:
+        from_fqdn = f"{query_info.get('service_name')}.{from_table}"
+        from_entity: Table = metadata.get_by_name(entity=Table, fqdn=from_fqdn)
+        to_fqdn = f"{query_info.get('service_name')}.{to_table}"
+        to_entity: Table = metadata.get_by_name(entity=Table, fqdn=to_fqdn)
+        if not from_entity or not to_entity:
+            return None
+
+        lineage = AddLineageRequest(
+            edge=EntitiesEdge(
+                fromEntity=EntityReference(
+                    id=from_entity.id.__root__,
+                    type=query_info["from_type"],
+                ),
+                toEntity=EntityReference(
+                    id=to_entity.id.__root__,
+                    type=query_info["to_type"],
+                ),
+            )
+        )
+
+        created_lineage = metadata.add_lineage(lineage)
+        logger.info(f"Successfully added Lineage {created_lineage}")
+
+    except Exception as err:
+        logger.debug(traceback.print_exc())
+        logger.error(err)
+
+
+def ingest_lineage(query_info, metadata_config):
+    from sqllineage.runner import LineageRunner
+
+    result = LineageRunner(query_info["sql"])
+    metadata = OpenMetadata(metadata_config)
+    for intermediate_table in result.intermediate_tables:
+        for source_table in result.source_tables:
+            create_lineage(source_table, intermediate_table, query_info, metadata)
+        for target_table in result.target_tables:
+            create_lineage(intermediate_table, target_table, query_info, metadata)
+    if not result.intermediate_tables:
+        for target_table in result.target_tables:
+            for source_table in result.source_tables:
+                create_lineage(source_table, target_table, query_info, metadata)
+
+
+def get_raw_extract_iter(alchemy_helper) -> Iterable[Dict[str, Any]]:
+    """
+    Provides iterator of result row from SQLAlchemy helper
+    :return:
+    """
+    rows = alchemy_helper.execute_query()
+    for row in rows:
+        yield row
