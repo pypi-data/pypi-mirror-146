c8e4356e0a57fd306fe74fdec2257dd470771886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:43 +0000

    Add package repo-daniel/0.0.5/repo_daniel-0.0.5.tar.gz

---
+++ packages/repo-daniel/repo_daniel-0.0.5.tar.gz/repo_daniel-0.0.5/setup.py
@@ -0,0 +1,28 @@
+# python setup.py bdist_wheel 
+# to build the wheel
+# to pip install this from the repo_daniel folder run
+# pip install -e .
+# to use run from hello_world import say_hello
+# you can then use the function say_hello()
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description=fh.read()
+
+setup(
+    name='repo_daniel',
+    version='0.0.5',
+    description = 'junk',
+    py_modules="[hello_world]",
+    package_dir ={'': 'src'},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['textfsm>=1.1.2',],
+    extras_require={
+        "dev": ['pytest>=3.7']
+    },
+    url='https://github.com/GoreNetwork/test_package',
+    author="Daniel Himes",
+    author_email="dhimes@gmail.com",
+)
