74518df0dc735c7c8dfc7546076f7aa57db6f75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:52 +0000

    Add package mars-clock/0.2.2/mars_clock-0.2.2.tar.gz

---
+++ packages/mars-clock/mars_clock-0.2.2.tar.gz/mars_clock-0.2.2/README.md
@@ -0,0 +1,5 @@
+# mars_clock
+A package for converting mission sols to local and terrestrial times
+
+The calculations here are based on [Piqueux et al. (2015) Icarus 251,
+332](https://ui.adsabs.harvard.edu/abs/2015Icar..251..332P/abstract).
---
+++ packages/mars-clock/mars_clock-0.2.2.tar.gz/mars_clock-0.2.2/mars_clock/__init__.py
@@ -0,0 +1,12 @@
+"""A package for determining Ls-values from mission sols"""
+import os
+
+__version__ = '0.2.2'
+__author__ = 'Brian Jackson <bjackson@boisestate.edu>'
+__all__ = ['Ls_utils', 'mission_sols']
+
+mars_clock_dir = os.path.dirname(__file__)
+DATADIR = os.path.join(mars_clock_dir, 'data/')
+
+from .Ls_utils import *
+from .mission_sols import *
