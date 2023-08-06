e830e803caf639e19c136964faa74c3ecb48e99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:53 +0000

    Add package kerykeion/2.3.10/kerykeion-2.3.10-py3-none-any.whl

---
+++ packages/kerykeion/kerykeion-2.3.10-py3-none-any.whl/kerykeion/__init__.py
@@ -0,0 +1,10 @@
+import sys
+from os.path import dirname
+from kerykeion.main import KrInstance
+from kerykeion.output_exemple import output
+from kerykeion.utilities.charts import MakeSvgInstance as MakeSvgInstance
+from kerykeion.utilities.discepolo import DiscepoloNumber
+from kerykeion.types import KerykeionException
+
+
+sys.path.append(dirname(__file__))
