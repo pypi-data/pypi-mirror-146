71e9415d146ea7e033e33e82b0da35b9b5071e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:16 +0000

    Add package dingTalk-notify-source/1.0.1/dingTalk_notify_source-1.0.1-py3-none-any.whl

---
+++ packages/dingTalk-notify-source/dingTalk_notify_source-1.0.1-py3-none-any.whl/dingTalk_notify_source/__init__.py
@@ -0,0 +1,9 @@
+"""
+  @Project     : dingTalk_notify_source
+  @Time        : 2022/02/24 13:35:48
+  @File        : __init__.py
+  @Author      : Source
+  @Software    : VSCode
+  @Desc        : 
+"""
+name = "dingTalk_notify_source"
