8e8ecd2326c301b743edca103887aa52062538ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:32 +0000

    Add package hotedit/0.9.1.post1/hotedit-0.9.1.post1-py3-none-any.whl

---
+++ packages/hotedit/hotedit-0.9.1.post1-py3-none-any.whl/hotedit/__init__.py
@@ -0,0 +1,13 @@
+"""
+Hotedit, the library for opening an editor
+"""
+
+__version__ = "0.9.0"
+
+from hotedit.editor import (
+    HoteditException,
+    EditingException,
+    Unchanged,
+    determine_editor,
+    hotedit,
+)
