537d49f4d6f93f1927f8f0513fd2087c1e4c0179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:07 +0000

    Add package poker-game-runner/0.1.11/poker_game_runner-0.1.11.tar.gz

---
+++ packages/poker-game-runner/poker_game_runner-0.1.11.tar.gz/poker_game_runner-0.1.11/poker_game_runner/bots/callBot.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from poker_game_runner.state import Observation
+
+class Bot:
+    def get_name(self):
+        return "callBot"
+
+    def act(self, observation: Observation):
+        return 1
