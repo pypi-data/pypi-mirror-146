d72e7bb927794c8b45310c5cfeca94ffa15d326b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:01 +0000

    Add package common-test/4.8.1/common-test-4.8.1.tar.gz

---
+++ packages/common-test/common-test-4.8.1.tar.gz/common-test-4.8.1/common/common/constant.py
@@ -0,0 +1,18 @@
+class Constant(object):
+    #重要的
+    P0 = 'critical'
+    #正常用例
+    P1 = 'normal'
+    #不太重要
+    P2 = 'minor'
+    #不重要
+    P3 = 'trivial'
+
+    #未查找到内容
+    DATA_NO_CONTENT = '在DataBus中未提取到内容!!!'
+
+    RESPONSE_CODE = 'responseCode'
+
+
+
+
