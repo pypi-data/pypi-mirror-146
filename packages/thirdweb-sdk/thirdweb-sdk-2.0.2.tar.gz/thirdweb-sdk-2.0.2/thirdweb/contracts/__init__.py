db1e197b43e456adfc9407663db19ef9563222f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:25 +0000

    Add package thirdweb-sdk/2.0.2/thirdweb-sdk-2.0.2.tar.gz

---
+++ packages/thirdweb-sdk/thirdweb-sdk-2.0.2.tar.gz/thirdweb-sdk-2.0.2/thirdweb/contracts/__init__.py
@@ -0,0 +1,6 @@
+from .nft_collection import NFTCollection
+from .edition import Edition
+from .token import Token
+from .marketplace import Marketplace
+from .nft_drop import NFTDrop
+from .maps import *
---
+++ packages/thirdweb-sdk/thirdweb-sdk-2.0.2.tar.gz/thirdweb-sdk-2.0.2/thirdweb/contracts/maps.py
@@ -0,0 +1,40 @@
+from typing import Any, Dict, Union
+
+# from thirdweb.abi.token_erc1155.bytecode import bytecode as TOKEN_ERC1155_BYTECODE
+# from thirdweb.abi.token_erc20.bytecode import bytecode as TOKEN_ERC20_BYTECODE
+# from thirdweb.abi.token_erc721.bytecode import bytecode as TOKEN_ERC721_BYTECODE
+from thirdweb.types.contract import ContractType
+from thirdweb.contracts import NFTCollection, Edition, Token, Marketplace, NFTDrop
+
+
+CONTRACTS_MAP: Dict[ContractType, Union[NFTCollection, Edition, Token]] = {
+    NFTCollection.contract_type: NFTCollection,  # type: ignore
+    Edition.contract_type: Edition,  # type: ignore
+    Token.contract_type: Token,  # type: ignore
+    Marketplace.contract_type: Marketplace,  # type: ignore
+    NFTDrop.contract_type: NFTDrop,  # type: ignore
+}
+
+CONTRACT_BYTECODE: Dict[ContractType, str] = {
+    NFTCollection.contract_type: "",
+    Edition.contract_type: "",
+    Token.contract_type: "",
+    Marketplace.contract_type: "",
+    NFTDrop.contract_type: "",
+}
+
+REMOTE_CONTRACT_NAME: Dict[ContractType, str] = {
+    NFTCollection.contract_type: "TokenERC721",
+    Edition.contract_type: "TokenERC1155",
+    Token.contract_type: "TokenERC20",
+    Marketplace.contract_type: "Marketplace",
+    NFTDrop.contract_type: "DropERC721",
+}
+
+REMOTE_CONTRACT_NAME_TO_CONTRACT_TYPE: Dict[str, ContractType] = {
+    "TokenERC721": NFTCollection.contract_type,
+    "TokenERC1155": Edition.contract_type,
+    "TokenERC20": Token.contract_type,
+    "Marketplace": Marketplace.contract_type,
+    "DropERC721": NFTDrop.contract_type,
+}
