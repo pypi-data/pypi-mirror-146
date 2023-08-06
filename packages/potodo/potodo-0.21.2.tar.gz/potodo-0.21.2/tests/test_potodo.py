a64584771ca655b96c880e705139964ad4cddf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:07 +0000

    Add package potodo/0.21.2/potodo-0.21.2.tar.gz

---
+++ packages/potodo/potodo-0.21.2.tar.gz/potodo-0.21.2/tests/test_potodo.py
@@ -0,0 +1,69 @@
+import json
+
+from potodo.potodo import exec_potodo
+
+
+def test_txt_output(capsys, base_config):
+    exec_potodo(**base_config)
+    captured = capsys.readouterr()
+
+    assert "file1.po" in captured.out
+    assert "file2.po" in captured.out
+    assert "# folder" in captured.out
+    assert "file3.po" in captured.out
+    assert "1 fuzzy" in captured.out
+    assert "2 fuzzy" not in captured.out
+    assert "excluded" not in captured.out
+
+
+def test_output(capsys, base_config, repo_dir):
+    base_config["json_format"] = True
+    exec_potodo(**base_config)
+    output = json.loads(capsys.readouterr().out)
+
+    expected = [
+        {
+            "name": "folder/",
+            "percent_translated": 0.0,
+            "files": [
+                {
+                    "name": "folder/file3",
+                    "path": f"{repo_dir}/folder/file3.po",
+                    "entries": 1,
+                    "fuzzies": 0,
+                    "translated": 0,
+                    "percent_translated": 0,
+                    "reserved_by": None,
+                    "reservation_date": None,
+                },
+            ],
+        },
+        {
+            "name": "repository/",
+            "percent_translated": 25.0,
+            "files": [
+                {
+                    "name": "repository/file1",
+                    "path": f"{repo_dir}/file1.po",
+                    "entries": 3,
+                    "fuzzies": 1,
+                    "translated": 1,
+                    "percent_translated": 33,
+                    "reserved_by": None,
+                    "reservation_date": None,
+                },
+                {
+                    "name": "repository/file2",
+                    "path": f"{repo_dir}/file2.po",
+                    "entries": 1,
+                    "fuzzies": 0,
+                    "translated": 0,
+                    "percent_translated": 0,
+                    "reserved_by": None,
+                    "reservation_date": None,
+                },
+            ],
+        },
+    ]
+
+    assert output == expected
---
+++ packages/potodo/potodo-0.21.2.tar.gz/potodo-0.21.2/tests/test_potodo_args_errors.py
@@ -0,0 +1,78 @@
+import sys
+from subprocess import check_output, CalledProcessError
+
+
+class TestPotodoArgsErrors:
+    def test_potodo_help(self):
+        output = check_output([sys.executable, "-m", "potodo", "--help"]).decode(
+            "utf-8"
+        )
+        output_short = check_output([sys.executable, "-m", "potodo", "-h"]).decode(
+            "utf-8"
+        )
+        assert output == output_short
+        assert "-h, --help            show this help message and exit" in output
+        # TODO: Find a better way of testing help output
+
+    def test_potodo_above_below_conflict(self):
+        try:
+            check_output(
+                [sys.executable, "-m", "potodo", "--above", "50", "--below", "40"]
+            ).decode("utf-8")
+        except CalledProcessError as e:
+            output = e.output
+        try:
+            check_output(
+                [sys.executable, "-m", "potodo", "-a", "50", "-b", "40"]
+            ).decode("utf-8")
+        except CalledProcessError as e:
+            output_short = e.output
+        assert output == output_short
+        assert output == b"Potodo: 'below' value must be greater than 'above' value.\n"
+
+    def test_potodo_json_interactive_conflict(self):
+        try:
+            check_output(
+                [sys.executable, "-m", "potodo", "--json", "--interactive"]
+            ).decode("utf-8")
+        except CalledProcessError as e:
+            output = e.output
+        try:
+            check_output([sys.executable, "-m", "potodo", "-j", "-i"]).decode("utf-8")
+        except CalledProcessError as e:
+            output_short = e.output
+        assert output == output_short
+        assert (
+            output
+            == b"Potodo: Json format and interactive modes cannot be activated at the same time.\n"
+        )
+
+    def test_potodo_exclude_and_only_fuzzy_conflict(self):
+        try:
+            check_output(
+                [sys.executable, "-m", "potodo", "--exclude-fuzzy", "--only-fuzzy"]
+            ).decode("utf-8")
+        except CalledProcessError as e:
+            output = e.output
+        assert (
+            output
+            == b"Potodo: Cannot pass --exclude-fuzzy and --only-fuzzy at the same time.\n"
+        )
+
+    def test_potodo_exclude_and_only_reserved_conflict(self):
+        try:
+            check_output(
+                [
+                    sys.executable,
+                    "-m",
+                    "potodo",
+                    "--exclude-reserved",
+                    "--only-reserved",
+                ]
+            ).decode("utf-8")
+        except CalledProcessError as e:
+            output = e.output
+        assert (
+            output
+            == b"Potodo: Cannot pass --exclude-reserved and --only-reserved at the same time.\n"
+        )
