a2ed232b24520b173d3867b47a9e9914c6b309c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:01 +0000

    Add package near-client/0.1.0/near-client-0.1.0.tar.gz

---
+++ packages/near-client/near-client-0.1.0.tar.gz/near-client-0.1.0/near_api/providers.py
@@ -0,0 +1,124 @@
+import requests
+import base64
+import json
+
+
+class JsonProviderError(Exception):
+    pass
+
+
+class JsonProvider(object):
+    def __init__(self, rpc_addr):
+        if isinstance(rpc_addr, tuple):
+            self._rpc_addr = "http://%s:%s" % rpc_addr
+        else:
+            self._rpc_addr = rpc_addr
+
+    def rpc_addr(self):
+        return self._rpc_addr
+
+    def json_rpc(self, method, params, timeout=2):
+        j = {
+            'method': method,
+            'params': params,
+            'id': 'dontcare',
+            'jsonrpc': '2.0'
+        }
+        r = requests.post(self.rpc_addr(), json=j, timeout=timeout)
+        r.raise_for_status()
+        content = json.loads(r.content)
+        if "error" in content:
+            raise JsonProviderError(content["error"])
+        return content["result"]
+
+    def send_tx(self, signed_tx):
+        return self.json_rpc('broadcast_tx_async',
+                             [base64.b64encode(signed_tx).decode('utf8')])
+
+    def send_tx_and_wait(self, signed_tx, timeout):
+        return self.json_rpc('broadcast_tx_commit',
+                             [base64.b64encode(signed_tx).decode('utf8')],
+                             timeout=timeout)
+
+    def get_status(self):
+        r = requests.get("%s/status" % self.rpc_addr(), timeout=2)
+        r.raise_for_status()
+        return json.loads(r.content)
+
+    def get_validators(self):
+        return self.json_rpc('validators', [None])
+
+    def query(self, query_object):
+        return self.json_rpc('query', query_object)
+
+    def get_account(self, account_id, finality='optimistic'):
+        return self.json_rpc(
+            'query', {
+                "request_type": "view_account",
+                "account_id": account_id,
+                "finality": finality
+            })
+
+    def get_access_key_list(self, account_id, finality='optimistic'):
+        return self.json_rpc(
+            'query', {
+                "request_type": "view_access_key_list",
+                "account_id": account_id,
+                "finality": finality
+            })
+
+    def get_access_key(self, account_id, public_key, finality='optimistic'):
+        return self.json_rpc(
+            'query', {
+                "request_type": "view_access_key",
+                "account_id": account_id,
+                "public_key": public_key,
+                "finality": finality
+            })
+
+    def view_call(self, account_id, method_name, args, finality='optimistic'):
+        return self.json_rpc(
+            'query', {
+                "request_type": "call_function",
+                "account_id": account_id,
+                "method_name": method_name,
+                "args_base64": base64.b64encode(args).decode('utf8'),
+                "finality": finality
+            })
+
+    def get_block(self, block_id):
+        return self.json_rpc('block', [block_id])
+
+    def get_chunk(self, chunk_id):
+        return self.json_rpc('chunk', [chunk_id])
+
+    def get_tx(self, tx_hash, tx_recipient_id):
+        return self.json_rpc('tx', [tx_hash, tx_recipient_id])
+
+    def get_changes_in_block(self, changes_in_block_request):
+        return self.json_rpc('EXPERIMENTAL_changes_in_block',
+                             changes_in_block_request)
+
+    def get_validators_ordered(self, block_hash):
+        return self.json_rpc('EXPERIMENTAL_validators_ordered', [block_hash])
+
+    def get_light_client_proof(self, outcome_type, tx_or_receipt_id,
+                               sender_or_receiver_id, light_client_head):
+        if outcome_type == "receipt":
+            params = {
+                "type": "receipt",
+                "receipt_id": tx_or_receipt_id,
+                "receiver_id": sender_or_receiver_id,
+                "light_client_head": light_client_head
+            }
+        else:
+            params = {
+                "type": "transaction",
+                "transaction_hash": tx_or_receipt_id,
+                "sender_id": sender_or_receiver_id,
+                "light_client_head": light_client_head
+            }
+        return self.json_rpc('light_client_proof', params)
+
+    def get_next_light_client_block(self, last_block_hash):
+        return self.json_rpc('next_light_client_block', [last_block_hash])
