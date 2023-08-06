8d0254857a1f975564d17391ec08cc5afc7590be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:13 +0000

    Add package freqtrade-custom-indicators/0.1.0/freqtrade_custom_indicators-0.1.0.tar.gz

---
+++ packages/freqtrade-custom-indicators/freqtrade_custom_indicators-0.1.0.tar.gz/freqtrade_custom_indicators-0.1.0/freqtrade_custom_indicators/tsv.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import talib.abstract as ta
+
+TSVAction = {
+    "PAL": "PA Long",
+    "PAL_FAIL": "PA Long Fail",
+    "PAS": "PA Short",
+    "PAS_FAIL": "PA Short Fail",
+    "PA_N": "PA Neutral"
+}
+
+
+def tsv(
+        candles: pd.DataFrame,
+        tsv_length: int = 13,
+        ma_length: int = 7,
+        source_type: str = "close") -> pd.DataFrame:
+    """
+    Enhanced Time Segmented Volume
+    """
+    src = candles[source_type]
+    volume = candles["volume"]
+    t = src \
+        .transform(lambda c: np.convolve(c, [1, -1], 'same') * volume) \
+        .rolling(window=tsv_length) \
+        .sum() \
+        .fillna(0)
+    m = pd.Series(ta.SMA(t, timeperiod=ma_length)).fillna(0)
+    action = t.combine(m, _compute_tsv_action)
+
+    return pd.DataFrame({'tsv_t': t, 'tsv_m': m, 'tsv_action': action})
+
+
+def _compute_tsv_action(t: float, m: float) -> str:
+    if t > m and t > 0:
+        return TSVAction["PAL"]
+    elif t < m and t > 0:
+        return TSVAction["PAL_FAIL"]
+    elif t < m and t < 0:
+        return TSVAction["PAS"]
+    elif t > m and t < 0:
+        return TSVAction["PAS_FAIL"]
+    else:
+        return TSVAction["PA_N"]
