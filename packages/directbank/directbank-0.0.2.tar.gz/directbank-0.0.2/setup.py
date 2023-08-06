ce3b81a2d6311474e43449d95a98f2c771804764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:42 +0000

    Add package directbank/0.0.2/directbank-0.0.2.tar.gz

---
+++ packages/directbank/directbank-0.0.2.tar.gz/directbank-0.0.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['directbank']
+install_requires = \
+['xsdata==22.4']
+
+setup_kwargs = {
+    'name': 'directbank',
+    'version': '0.0.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Toony',
+    'author_email': 'kizyanov@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
