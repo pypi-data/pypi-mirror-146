e3faf72a033ce25e8d2e8b2b772adfd96ee02450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:36 +0000

    Add package superwise/6.2.7/superwise-6.2.7.tar.gz

---
+++ packages/superwise/superwise-6.2.7.tar.gz/superwise-6.2.7/superwise/controller/model.py
@@ -0,0 +1,20 @@
+""" This module implement tasks functionality  """
+from superwise.controller.base import BaseController
+
+
+class ModelController(BaseController):
+    """Class ModelController - responsible for task functionality"""
+
+    def __init__(self, client, sw):
+        """
+
+        ### Args:
+
+        `client`: superwise client object
+
+        `sw`: superwise  object
+
+        """
+        super().__init__(client, sw)
+        self.path = "admin/v1/models"
+        self.model_name = "Model"
