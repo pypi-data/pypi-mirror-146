e00491436603ce41ccda3f08c5c2248ed2b1881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:55:41 +0000

    Add package kcaptcha/1.0.2/kcaptcha-1.0.2.tar.gz

---
+++ packages/kcaptcha/kcaptcha-1.0.2.tar.gz/kcaptcha-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="kcaptcha",
+    version="1.0.2",
+    author="xpc",
+    description="kill captcha",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(where='.', exclude=(), include=('*',)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'onnxruntime', 'Pillow', 'opencv-python', 'loguru'],
+    python_requires='<3.10',
+    include_package_data=True,
+    install_package_data=True,
+)
